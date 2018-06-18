63646ebaed341955322565bf20a774dffe25dfd3
nixxholas <nicholas@slyck.net>
2018-06-18 17:18:06 +0800

    Nozomi.Data: CurrencyModels: Source: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Data/CurrencyModels/Source.cs
@@ -0,0 +1,11 @@
+﻿using CounterCore.Data.Models.CommonCurrencyModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nozomi.Data.CurrencyModels
+{
+    public class Source : CurrencySource
+    {
+    }
+}
