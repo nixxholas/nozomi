4865766e131aef39232386caeecc691de4d5bdd9
nixxholas <nicholas@slyck.net>
2018-06-19 15:20:27 +0800

    Nozomi.Repo: Data: Mappings: CurrencyMap: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/CurrencyMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.CurrencyModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nozomi.Repo.Data.Mappings
+{
+    public class CurrencyMap
+    {
+        public CurrencyMap(EntityTypeBuilder<Currency> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(c => c.Id);
+            entityTypeBuilder.Property(c => c.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(c => c.Abbrv).IsRequired();
+            entityTypeBuilder.Property(c => c.Name).IsRequired();
+
+            entityTypeBuilder.HasOne(c => c.CurrencyType).WithMany(ct => ct.Currencies)
+                .HasForeignKey(c => c.CurrencyTypeId).OnDelete(DeleteBehavior.Cascade);
+            entityTypeBuilder.HasOne(c => c.CurrencySource).WithMany(cs => cs.Currencies)
+                .HasForeignKey(c => c.CurrencySourceId).OnDelete(DeleteBehavior.Cascade);
+            entityTypeBuilder.HasMany(c => c.PartialCurrencyPairs).WithOne(pcp => pcp.Currency)
+                .HasForeignKey(pcp => pcp.CurrencyId);
+        }
+    }
+}
