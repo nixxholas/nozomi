a89b75b57e556bbec116299fadf0f65cba776f13
nixxholas <nicholas@slyck.net>
2018-06-19 15:20:10 +0800

    Nozomi.Repo: Data: NozomiDbContext: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/NozomiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nozomi.Repo.Data
+{
+    public class NozomiDbContext : DbContext
+    {
+        public NozomiDbContext(DbContextOptions<NozomiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
