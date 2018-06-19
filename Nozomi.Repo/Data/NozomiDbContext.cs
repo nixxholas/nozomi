d4a3edac40ab0df2a000f04af44449cc4bb91fdf
nixxholas <nicholas@slyck.net>
2018-06-19 15:33:54 +0800

    Nozomi.Repo: Data: NozomiDbContext: Currency Scaffolding
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Repo/Data/NozomiDbContext.cs
+++ Nozomi.Repo/Data/NozomiDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Repo.Data.Mappings;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +9,15 @@ namespace Nozomi.Repo.Data
 {
     public class NozomiDbContext : DbContext
     {
+        public DbSet<Currency> Currencies { get; set; }
+
         public NozomiDbContext(DbContextOptions<NozomiDbContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            var currencyMap = new CurrencyMap(modelBuilder.Entity<Currency>());
 
             base.OnModelCreating(modelBuilder);
         }
