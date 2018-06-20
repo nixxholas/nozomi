4a54e43d06b2dbee2c7b6d5ff557ca56d2108394
nixxholas <nicholas@slyck.net>
2018-06-20 11:54:15 +0700

    Nozomi.Repo: Data: Mappings: WebModels: RequestComponentMap: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/WebModels/RequestComponentMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.WebModels;
+
+namespace Nozomi.Repo.Data.Mappings.WebModels
+{
+    public class RequestComponentMap
+    {
+        public RequestComponentMap(EntityTypeBuilder<RequestComponent> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(rc => rc.Id).HasName("RequestComponent_PK_Id");
+            entityTypeBuilder.Property(rc => rc.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(rc => rc.QueryComponent).IsRequired(false);
+            entityTypeBuilder.Property(rc => rc.Value).IsRequired(false);
+
+            entityTypeBuilder.HasOne(rc => rc.Request).WithMany(r => r.RequestComponents)
+                .HasForeignKey(rc => rc.RequestId);
+        }
+    }
+}
