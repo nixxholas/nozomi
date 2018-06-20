21aab287c4d75e565f677dfde37f1a19a8272098
nixxholas <nicholas@slyck.net>
2018-06-20 10:33:19 +0700

    Nozomi.Repo: Data: Mappings: CurrencyPairComponentMap: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/CurrencyPairComponentMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Repo.Data.Mappings
+{
+    public class CurrencyPairComponentMap
+    {
+        public CurrencyPairComponentMap(EntityTypeBuilder<CurrencyPairComponent> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(cpc => cpc.Id);
+            entityTypeBuilder.Property(cpc => cpc.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(cpc => cpc.QueryComponent).IsRequired();
+            entityTypeBuilder.Property(cpc => cpc.Value).IsRequired(false);
+
+            entityTypeBuilder.HasOne(cpc => cpc.CurrencyPair).WithMany(cp => cp.CurrencyPairComponents)
+                .HasForeignKey(cpc => cpc.CurrencyPairId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
