0e85af3e429518cb755c0359ca70c8733a1aa14f
nixxholas <nicholas@slyck.net>
2018-06-20 10:54:23 +0700

    Nozomi.Repo: Data: Mappings: CurrencyTypeMap: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/CurrencyTypeMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Repo.Data.Mappings
+{
+    public class CurrencyTypeMap
+    {
+        public CurrencyTypeMap(EntityTypeBuilder<CurrencyType> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(ct => ct.Id);
+            entityTypeBuilder.Property(ct => ct.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(ct => ct.Name).IsRequired();
+            entityTypeBuilder.Property(ct => ct.TypeShortForm).IsRequired();
+
+            entityTypeBuilder.HasMany(ct => ct.Currencies).WithOne(c => c.CurrencyType)
+                .HasForeignKey(c => c.CurrencyTypeId);
+        }
+    }
+}
