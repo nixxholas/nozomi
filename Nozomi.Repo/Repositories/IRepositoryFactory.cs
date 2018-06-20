eb82fff23375e9654f22a826ce86c8b28976710a
nixxholas <nicholas@slyck.net>
2018-06-20 23:38:20 +0700

    Nozomi.Repo: Repositories: Unit Of Work, Repository Pattern Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Repositories/IRepositoryFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Nozomi.Repo.Repositories
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+    }
+}
---
+++ Nozomi.Repo/Repositories/Repository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Nozomi.Repo.Data;
+
+namespace Nozomi.Repo.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly IDbContext _dbContext;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(IDbContext context)
+        {
+            _dbContext = context;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            var entry = _dbSet.Add(entity);
+
+        }
+
+        public void Delete(T entity)
+        {
+            T existing = _dbSet.Find(entity);
+            if (existing != null) _dbSet.Remove(existing);
+        }
+
+        public void Delete(Expression<Func<T, bool>> predicate)
+        {
+            T existing = _dbSet.SingleOrDefault<T>(predicate);
+            if (existing != null) _dbSet.Remove(existing);
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _dbSet.AsEnumerable<T>();
+        }
+
+        public IQueryable<T> GetQueryable() 
+        {
+            return _dbSet.AsQueryable<T>();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).AsEnumerable<T>();
+        }
+
+        public IQueryable<T> GetQueryable(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).AsQueryable<T>();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Update(params T[] entities) => _dbSet.UpdateRange(entities);
+
+
+        public void Update(IEnumerable<T> entities) => _dbSet.UpdateRange(entities);
+
+    }
+}
