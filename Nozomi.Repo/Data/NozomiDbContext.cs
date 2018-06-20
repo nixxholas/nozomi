39a4d6a461f6860520c8b03a944fae2d2c2e302e
nixxholas <nicholas@slyck.net>
2018-06-20 11:51:27 +0700

    Nozomi.Repo: Data: NozomiDbContext: RequestMap Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Repo/Data/NozomiDbContext.cs
+++ Nozomi.Repo/Data/NozomiDbContext.cs
@@ -3,7 +3,9 @@ using Nozomi.Data.CurrencyModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Nozomi.Data.WebModels;
 using Nozomi.Repo.Data.Mappings.CurrencyModels;
+using Nozomi.Repo.Data.Mappings.WebModels;
 
 namespace Nozomi.Repo.Data
 {
@@ -14,6 +16,7 @@ namespace Nozomi.Repo.Data
         public DbSet<CurrencyPairComponent> CurrencyPairComponents { get; set; }
         public DbSet<CurrencyType> CurrencyTypes { get; set; }
         public DbSet<PartialCurrencyPair> PartialCurrencyPairs { get; set; }
+        public DbSet<Request> Requests { get; set; }
         public DbSet<Source> Sources { get; set; }
 
         public NozomiDbContext(DbContextOptions options) : base(options)
@@ -27,6 +30,7 @@ namespace Nozomi.Repo.Data
             var currencyPairComponentMap = new CurrencyPairComponentMap(modelBuilder.Entity<CurrencyPairComponent>());
             var currencyTypeMap = new CurrencyTypeMap(modelBuilder.Entity<CurrencyType>());
             var partialCurrencyPairMap = new PartialCurrencyPairMap(modelBuilder.Entity<PartialCurrencyPair>());
+            var requestMap = new RequestMap(modelBuilder.Entity<Request>());
             var sourceMap = new SourceMap(modelBuilder.Entity<Source>());
             
             base.OnModelCreating(modelBuilder);
