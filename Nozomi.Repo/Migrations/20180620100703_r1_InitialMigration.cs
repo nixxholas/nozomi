786a02b69af8636c21592a68eba02e7da4f63b71
nixxholas <nicholas@slyck.net>
2018-06-20 17:07:39 +0700

    Nozomi.Repo: Migrations: r1 - Initial Migration
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Migrations/20180620100703_r1_InitialMigration.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r1_InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrencyTypes",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    TypeShortForm = table.Column<string>(maxLength: 12, nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Guid = table.Column<Guid>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    RequestType = table.Column<int>(nullable: false),
+                    DataPath = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Request_PK_Id", x => x.Id);
+                    table.UniqueConstraint("Request_AK_Guid", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sources",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Abbreviation = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    APIDocsURL = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequestComponents",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    QueryComponent = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    RequestId = table.Column<long>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    ComponentType = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("RequestComponent_PK_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RequestComponents_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequestProperties",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    RequestPropertyType = table.Column<int>(nullable: false),
+                    Key = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    RequestId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("RequestProperty_PK_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RequestProperties_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CurrencyTypeId = table.Column<long>(nullable: false),
+                    Abbrv = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    CurrencySourceId = table.Column<long>(nullable: false),
+                    WalletTypeId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Currencies_Sources_CurrencySourceId",
+                        column: x => x.CurrencySourceId,
+                        principalTable: "Sources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Currencies_CurrencyTypes_CurrencyTypeId",
+                        column: x => x.CurrencyTypeId,
+                        principalTable: "CurrencyTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyPairs",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CurrencyPairType = table.Column<int>(nullable: false),
+                    APIUrl = table.Column<string>(nullable: false),
+                    DefaultComponent = table.Column<string>(nullable: false),
+                    CurrencySourceId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyPairs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CurrencyPairs_Sources_CurrencySourceId",
+                        column: x => x.CurrencySourceId,
+                        principalTable: "Sources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyPairComponents",
+                columns: table => new
+                {
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ComponentType = table.Column<int>(nullable: false),
+                    QueryComponent = table.Column<string>(nullable: false),
+                    Value = table.Column<decimal>(nullable: false),
+                    CurrencyPairId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyPairComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CurrencyPairComponents_CurrencyPairs_CurrencyPairId",
+                        column: x => x.CurrencyPairId,
+                        principalTable: "CurrencyPairs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PartialCurrencyPairs",
+                columns: table => new
+                {
+                    CurrencyId = table.Column<long>(nullable: false),
+                    CurrencyPairId = table.Column<long>(nullable: false),
+                    IsMain = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartialCurrencyPairs", x => new { x.CurrencyPairId, x.IsMain });
+                    table.ForeignKey(
+                        name: "FK_PartialCurrencyPairs_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PartialCurrencyPairs_CurrencyPairs_CurrencyPairId",
+                        column: x => x.CurrencyPairId,
+                        principalTable: "CurrencyPairs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Currencies_CurrencySourceId",
+                table: "Currencies",
+                column: "CurrencySourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Currencies_CurrencyTypeId",
+                table: "Currencies",
+                column: "CurrencyTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyPairComponents_CurrencyPairId",
+                table: "CurrencyPairComponents",
+                column: "CurrencyPairId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyPairs_CurrencySourceId",
+                table: "CurrencyPairs",
+                column: "CurrencySourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartialCurrencyPairs_CurrencyId",
+                table: "PartialCurrencyPairs",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestComponents_RequestId",
+                table: "RequestComponents",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestProperties_RequestId",
+                table: "RequestProperties",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sources_Abbreviation",
+                table: "Sources",
+                column: "Abbreviation");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyPairComponents");
+
+            migrationBuilder.DropTable(
+                name: "PartialCurrencyPairs");
+
+            migrationBuilder.DropTable(
+                name: "RequestComponents");
+
+            migrationBuilder.DropTable(
+                name: "RequestProperties");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "CurrencyPairs");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "CurrencyTypes");
+
+            migrationBuilder.DropTable(
+                name: "Sources");
+        }
+    }
+}
