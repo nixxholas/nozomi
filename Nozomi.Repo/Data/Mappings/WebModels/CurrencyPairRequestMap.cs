eb5dcdaba86dbd85b4033fc11430f9f018a9102d
nixxholas <nicholas@slyck.net>
2018-06-20 12:24:27 +0700

    Nozomi.Repo: Data: Mappings: WebModels: CurrencyPairRequestMap: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/WebModels/CurrencyPairRequestMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.WebModels;
+
+namespace Nozomi.Repo.Data.Mappings.WebModels
+{
+    public class CurrencyPairRequestMap
+    {
+        public CurrencyPairRequestMap(EntityTypeBuilder<CurrencyPairRequest> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasMany(cpr => cpr.RequestComponents).WithOne(rc => rc.Request)
+                .HasForeignKey(r => r.RequestId);
+        }
+    }
+}
