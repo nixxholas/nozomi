c1aeda41548d4aae14db6ae696bf903de7e074db
nixxholas <nicholas@slyck.net>
2018-06-20 16:22:46 +0700

    Nozomi.Ticker: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Ticker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nozomi.Ticker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
