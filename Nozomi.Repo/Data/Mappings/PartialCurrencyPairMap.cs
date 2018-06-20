bd15ed9881852ca73f3d15ec4d8ac78d42ea2a1c
nixxholas <nicholas@slyck.net>
2018-06-20 11:25:12 +0700

    Nozomi.Repo: Data: Mappings: PartialCurrencyPairMap: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/PartialCurrencyPairMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Repo.Data.Mappings
+{
+    public class PartialCurrencyPairMap
+    {
+        public PartialCurrencyPairMap(EntityTypeBuilder<PartialCurrencyPair> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(pcp => new { pcp.CurrencyPairId, pcp.IsMain });
+
+            entityTypeBuilder.HasOne(pcp => pcp.Currency).WithMany(c => c.PartialCurrencyPairs)
+                .HasForeignKey(pcp => pcp.CurrencyId);
+            entityTypeBuilder.HasOne(pcp => pcp.CurrencyPair).WithMany(cp => cp.PartialCurrencyPairs)
+                .HasForeignKey(pcp => pcp.CurrencyPair);
+        }
+    }
+}
