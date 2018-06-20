58f3e3c0657fd26d39084399dcc5bc5fca57f501
nixxholas <nicholas@slyck.net>
2018-06-20 17:27:39 +0700

    Nozomi.Repo: Data: IDbContext: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/IDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nozomi.Repo.Data
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        
+        int SaveChanges(long userId = 0);
+
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<int> SaveChangesAsync(long userId,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        void AddTimestamps(long userId = 0);
+    }
+}
