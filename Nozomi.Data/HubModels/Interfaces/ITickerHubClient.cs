0370e54993292a57aaeb4da0439a9af34e1fadd0
nixxholas <nicholas@slyck.net>
2018-06-21 18:44:33 +0700

    Nozomi.Data: HubModels: Further TickerHub Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Data/HubModels/Interfaces/ITickerHubClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Nozomi.Data.HubModels.Interfaces
+{
+    public interface ITickerHubClient
+    {
+        Task ReturnPayload(string payload);
+    }
+}
---
+++ Nozomi.Data/HubModels/TickerHubHeader.cs
@@ -0,0 +1,8 @@
+﻿namespace Nozomi.Data.HubModels
+{
+    public enum TickerHubHeader
+    {
+        Ping = 0,
+        Subscription = 1
+    }
+}
