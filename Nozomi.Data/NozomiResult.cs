09317e8aee367ef6787b903d9362b5e1c154bca4
nixxholas <nicholas@slyck.net>
2018-06-21 15:07:25 +0700

    Nozomi.Data: NozomiResult: Initial Implementation
    This entity will be used as a base return object for all requests made to Nozomi.
    
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Data/NozomiResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Nozomi.Data
+{
+    public class NozomiResult<TEntity> where TEntity : class
+    {
+        public bool Success { get; set; } = false;
+        
+        public IEnumerable<TEntity> Data { get; set; }
+    }
+}
