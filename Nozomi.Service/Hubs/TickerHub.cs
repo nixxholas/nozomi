79867a64248cd25084fadfbe349038caeba58275
nixxholas <nicholas@slyck.net>
2018-06-21 18:44:55 +0700

    Nozomi.Service: Hubs: SignalR hubs initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/Hubs/TickerHub.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Nozomi.Data.HubModels.Interfaces;
+
+namespace Nozomi.Service.Hubs
+{
+    public class TickerHub : Hub, ITickerHubClient
+    {
+        public TickerHub()
+        {
+            
+        }
+        
+        // We can use this to return a payload
+        public async Task ReturnPayload(string payload)
+        {
+            await Clients.Caller.SendCoreAsync("", "");
+        }
+    }
+}
