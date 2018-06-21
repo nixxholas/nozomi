596b812ec80ac75198b33c1794354d691c9a0650
nixxholas <nicholas@slyck.net>
2018-06-21 12:09:43 +0700

    Nozomi.Service: HostedServices: TickerSyncingService: Initial Migration
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/HostedServices/Interfaces/ITickerSyncingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Service.HostedServices.Interfaces
+{
+    public interface ITickerSyncingService
+    {
+        /// <summary>
+        /// Polls the data from externals.
+        /// 
+        /// Iterate from every service, returns the result in string.
+        /// </summary>
+        /// <returns>Returns the id of the pair and the result in decimal.</returns>
+        Task<Dictionary<long, decimal>> PollDataFromExternalsAsync(IEnumerable<CurrencyPair> cPairs);
+    }
+}
---
+++ Nozomi.Service/HostedServices/TickerSyncingService.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Counter.SDK.Utils;
+using Counter.SDK.Utils.Numerics;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Service.HostedServices.Interfaces;
+
+namespace Nozomi.Service.HostedServices
+{
+    public class TickerSyncingService : BaseHostedService, ITickerSyncingService
+    {
+        private ICurrencyPairService _cpService;
+        private ICurrencyPairComponentService _cpcService;
+        private ILogger<TickerSyncingService> _logger;
+
+        public TickerSyncingService(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            _cpService = _scope.ServiceProvider.GetRequiredService<ICurrencyPairService>();
+            _cpcService = _scope.ServiceProvider.GetRequiredService<ICurrencyPairComponentService>();
+            _logger = _scope.ServiceProvider.GetRequiredService<ILogger<TickerSyncingService>>();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("TickerSyncingService is starting.");
+
+            stoppingToken.Register(() => _logger.LogInformation("TickerSyncingService is stopping."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("TickerSyncingService is doing background work.");
+
+                // Retrieve all the active currency pairs.
+                var cPairs = _cpService.GetAllActive();
+
+                // Poll data.
+                Dictionary<long, decimal> result = await PollDataFromExternalsAsync(cPairs);
+
+                // Process and update it
+                foreach (var kvPair in result)
+                {
+                    _cpcService.UpdatePairValue(kvPair.Key, kvPair.Value);
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+
+            _logger.LogWarning("TickerSyncingService background task is stopping.");
+        }
+
+        public async Task<Dictionary<long, decimal>> PollDataFromExternalsAsync(IEnumerable<CurrencyPair> cPairs)
+        {
+            // Instantiate a dictionary
+            Dictionary<long, decimal> dataSet = new Dictionary<long, decimal>();
+            Dictionary<string, string> apiPayload = new Dictionary<string, string>();
+
+            var apiUrls = _cpService.GetAllCurrencyPairUrls();
+
+            // Query all the APIs first
+            foreach (var apiUrl in apiUrls)
+            {
+                if (!apiPayload.ContainsKey(apiUrl))
+                {
+                    // Setup the HTTP Client
+                    // https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+                    // 
+                    // In this case, we're assuming that CounterCore is always connected
+                    // to the internet. Take note of this 'possible' issue
+                    // https://stackoverflow.com/questions/28537916/check-internet-connection-available-or-not-in-c-sharp
+                    using (var client = new HttpClient())
+                    {
+                        // GET it
+                        var httpRes = await client.GetAsync(apiUrl);
+
+                        if (httpRes.IsSuccessStatusCode)
+                        {
+                            var httpContent = await httpRes.Content.ReadAsStringAsync();
+
+                            apiPayload.Add(apiUrl, httpContent);
+                        }
+                    }
+                }
+            }
+
+            // Foreach currency pair in the dataset
+            foreach (var pair in cPairs)
+            {
+                if (!string.IsNullOrEmpty(apiPayload[pair.APIUrl]))
+                {
+                    var httpContent = apiPayload[pair.APIUrl];
+
+                    var jsonToken = JToken.Parse(httpContent);
+
+                    // Is it an array or an object?
+                    // https://stackoverflow.com/questions/20620381/determine-if-json-results-is-object-or-array
+                    if (jsonToken is JArray)
+                    {
+                        // Pump in the array
+                        List<string> dataList = jsonToken.ToObject<List<string>>();
+
+                        // If the db really hodls a number,
+                        foreach (var component in pair.CurrencyPairComponents)
+                        {
+                            if (component.QueryComponent != null &&
+                                int.TryParse(component.QueryComponent, out int index))
+                            {
+                                // let's work it out
+                                if (index >= 0 && index < dataList.Count)
+                                {
+                                    // Number checks
+                                    // Make sure the datalist element we're targetting contains a proper value.
+                                    if (decimal.TryParse(dataList[index], out decimal val))
+                                    {
+                                        // Add the value in
+                                        dataSet.Add(component.Id, val);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (jsonToken is JObject)
+                    {
+                        // Pump in the object
+                        JObject obj = jsonToken.ToObject<JObject>();
+
+                        foreach (var component in pair.CurrencyPairComponents)
+                        {
+                            if (component.QueryComponent != null)
+                            {
+                                var rawData = (string) obj.SelectToken(component.QueryComponent);
+
+                                if (rawData != null)
+                                {
+                                    // https://stackoverflow.com/questions/23131414/culture-invariant-decimal-tryparse
+                                    var style = NumberStyles.Any;
+                                    if (ExponentHelper.IsExponentialFormat(rawData))
+                                    {
+                                        style = NumberStyles.Float;
+                                    }
+
+                                    // If it is an exponent
+                                    if (decimal.TryParse(rawData, style, CultureInfo.InvariantCulture,
+                                        out decimal val))
+                                    {
+                                        if (val > 0)
+                                        {
+                                            dataSet.Add(pair.Id, val);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Goodbye
+            return dataSet;
+        }
+    }
+}
