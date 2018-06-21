1c49c0fd695848cd98d4d01dcb45598479b88330
nixxholas <nicholas@slyck.net>
2018-06-21 14:53:53 +0700

    Nozomi.Service: Services: SourceService: Initial Migration Import
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/Services/Interfaces/ISourceService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Service.Services.Interfaces
+{
+    public interface ISourceService
+    {
+        IEnumerable<dynamic> GetAllNested();
+
+        IEnumerable<Source> GetAllActive(bool includeNested);
+
+        IEnumerable<dynamic> GetAllActiveObsc(bool includeNested);
+    }
+}
---
+++ Nozomi.Service/Services/SourceService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Repo.Data;
+using Nozomi.Repo.Repositories;
+using Nozomi.Service.Services.Interfaces;
+
+namespace Nozomi.Service.Services
+{
+    public class SourceService : BaseService<SourceService, NozomiDbContext>, ISourceService
+    {
+        public SourceService(ILogger<SourceService> logger, 
+            IUnitOfWork<NozomiDbContext> unitOfWork) : base(logger, unitOfWork)
+        {
+        }
+
+        public IEnumerable<Source> GetAllActive(bool includeNested = false)
+        {
+            if (includeNested) {
+                return _unitOfWork.GetRepository<Source>()
+                                  .GetQueryable()
+                                  .Where(cs => cs.DeletedAt == null)
+                                  .Where(cs => cs.IsEnabled)
+                                  .Include(cs => cs.Currencies)
+                                  .Include(cs => cs.CurrencyPairs);
+            } else {
+                return _unitOfWork.GetRepository<Source>()
+                                  .GetQueryable()
+                                  .Where(cs => cs.DeletedAt == null)
+                                  .Where(cs => cs.IsEnabled);
+            }
+        }
+
+        public IEnumerable<dynamic> GetAllActiveObsc(bool includeNested = false)
+        {
+            if (includeNested) {
+                return _unitOfWork.GetRepository<Source>()
+                    .GetQueryable()
+                    .Where(cs => cs.DeletedAt == null)
+                    .Where(cs => cs.IsEnabled)
+                    .Include(cs => cs.Currencies)
+                    .Include(cs => cs.CurrencyPairs)
+                    .Select(cs => new
+                    {
+                        id = cs.Id, 
+                        abbrv = cs.Abbreviation,
+                        name = cs.Name,
+                        currencies = cs.Currencies,
+                        currencyPairs = cs.CurrencyPairs
+                    });
+            } else {
+                return _unitOfWork.GetRepository<Source>()
+                    .GetQueryable()
+                    .Where(cs => cs.DeletedAt == null)
+                    .Where(cs => cs.IsEnabled)
+                    .Select(cs => new
+                    {
+                        id = cs.Id, 
+                        abbrv = cs.Abbreviation,
+                        name = cs.Name
+                    });
+            }
+        }
+
+        public IEnumerable<dynamic> GetAllNested()
+        {
+            return _unitOfWork.GetRepository<Source>()
+                .GetQueryable()
+                .AsNoTracking()
+                // Make sure all currency sources are not disabled or deleted
+                .Where(cs => cs.IsEnabled && cs.DeletedAt == null)
+                .Include(cs => cs.CurrencyPairs)
+                    .ThenInclude(cp => cp.PartialCurrencyPairs)
+                        .ThenInclude(pcp => pcp.Currency)
+                            .ThenInclude(c => c.CurrencyType)
+                .Where(cs => cs.CurrencyPairs
+                    // Make sure all currencypairs are not disabled or deleted
+                    .Any(cp => cp.IsEnabled && cp.DeletedAt == null
+                    &&
+                    // Make sure none of the currency pair's partial currency pair is not disabled or deleted
+                    cp.PartialCurrencyPairs
+                    .Any(pcp => pcp.Currency.IsEnabled && pcp.Currency.DeletedAt == null)))
+                .Select(cs => new {
+                    id = cs.Id,
+                    abbreviation = cs.Abbreviation,
+                    name = cs.Name,
+                    currencyPairs = cs.CurrencyPairs
+                        .Select(cp => new
+                        {
+                            id = cp.Id,
+                            partialCurrencyPairs = cp.PartialCurrencyPairs
+                                .Select(pcp => new
+                                {
+                                    currencyId = pcp.CurrencyId,
+                                    currency = new
+                                    {
+                                        abbrv = pcp.Currency.Abbrv,
+                                        currencyTypeId = pcp.Currency.CurrencyTypeId,
+                                        currencyType = new {
+                                            typeShortForm = pcp.Currency.CurrencyType.TypeShortForm,
+                                            name = pcp.Currency.CurrencyType.Name
+                                        },
+                                        name = pcp.Currency.Name,
+                                        walletTypeId = pcp.Currency.WalletTypeId
+                                    },
+                                    isMain = pcp.IsMain
+                                })
+                        })
+                });
+        }
+    }
+}
