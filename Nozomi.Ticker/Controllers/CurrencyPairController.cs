6ba91cdb21a3fcb689279384923b20ca28c550e8
nixxholas <nicholas@slyck.net>
2018-06-22 11:25:21 +0700

    Nozomi.Ticker: Controllers: CurrencyPairController: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Ticker/Controllers/CurrencyPairController.cs
+++ Nozomi.Ticker/Controllers/CurrencyPairController.cs
@@ -1,4 +1,28 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿using System.Collections.Generic;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Nozomi.Data;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Service.Hubs;
+
+namespace Nozomi.Ticker.Controllers
 {
-  public class $CLASS$ {$END$}
+    [ApiController]
+    public class CurrencyPairController : ControllerBase
+    {
+        private readonly IHubContext<TickerHub> _tickerHubContext;
+
+        public CurrencyPairController(IHubContext<TickerHub> tickerHubContext)
+        {
+            _tickerHubContext = tickerHubContext;
+        }
+        
+        [HttpPost]
+        public async Task CreateCurrencyPair(CurrencyPair currencyPair)
+        {
+            
+        }
+    }
 }
