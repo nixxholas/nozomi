b66e0fc9495438252d8de6deb0999f3a3643d001
nixxholas <nicholas@slyck.net>
2018-06-22 11:47:27 +0700

    Nozomi.Service: Services: Requests: RequestService: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/Services/Requests/Interfaces/IRequestService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Nozomi.Data.WebModels;
+
+namespace Nozomi.Service.Services.Requests.Interfaces
+{
+    public interface IRequestService
+    {
+        long Create(Request req, long userId = 0);
+
+        bool Update(Request req, long userId = 0);
+
+        bool SoftDelete(long reqId, long userId = 0);
+
+        IEnumerable<Request> GetAllActive(bool track = false);
+
+        IEnumerable<dynamic> GetAllActiveObsc(bool track = false);
+
+        IEnumerable<Request> GetAll(bool track = false);
+
+        IEnumerable<dynamic> GetAllObsc(bool track = false);
+    } 
+}
---
+++ Nozomi.Service/Services/Requests/RequestService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data.WebModels;
+using Nozomi.Repo.Data;
+using Nozomi.Repo.Repositories;
+using Nozomi.Service.Services.Requests.Interfaces;
+
+namespace Nozomi.Service.Services.Requests
+{
+    public class RequestService : BaseService<RequestService, NozomiDbContext>, IRequestService
+    {
+        public RequestService(ILogger<RequestService> logger, IUnitOfWork<NozomiDbContext> unitOfWork) 
+            : base(logger, unitOfWork)
+        {
+        }
+
+        public long Create(Request req, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Request req, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SoftDelete(long reqId, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Request> GetAllActive(bool track = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<dynamic> GetAllActiveObsc(bool track = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Request> GetAll(bool track = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<dynamic> GetAllObsc(bool track = false)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
