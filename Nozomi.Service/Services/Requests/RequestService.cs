094c9091742cad6cb976cda94cff541472654e9f
nixxholas <nicholas@slyck.net>
2018-06-22 11:55:49 +0700

    Nozomi.Service: Services: Requests: RequestService: Create API: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Service/Services/Requests/RequestService.cs
+++ Nozomi.Service/Services/Requests/RequestService.cs
@@ -16,7 +16,16 @@ namespace Nozomi.Service.Services.Requests
 
         public long Create(Request req, long userId = 0)
         {
-            throw new System.NotImplementedException();
+            // Safetynet
+            if (req != null && req.IsValid() && userId >= 0)
+            {
+                _unitOfWork.GetRepository<Request>().Add(req);
+                _unitOfWork.Commit(userId);
+
+                return req.Id;
+            }
+
+            return -1;
         }
 
         public bool Update(Request req, long userId = 0)
