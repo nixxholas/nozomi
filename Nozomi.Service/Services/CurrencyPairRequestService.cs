f2989aa71ffa44ebd4719f6e7d447f58b86d04e7
nixxholas <nicholas@slyck.net>
2018-06-22 10:56:43 +0700

    Nozomi.Service: Services: CurrencyPairRequestService: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/Services/CurrencyPairRequestService.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data.WebModels;
+using Nozomi.Repo.Data;
+using Nozomi.Repo.Repositories;
+using Nozomi.Service.Services.Interfaces;
+
+namespace Nozomi.Service.Services
+{
+    public class CurrencyPairRequestService : BaseService<CurrencyPairRequestService, NozomiDbContext>, ICurrencyPairRequestService
+    {
+        public CurrencyPairRequestService(ILogger<CurrencyPairRequestService> logger, IUnitOfWork<NozomiDbContext> unitOfWork) 
+            : base(logger, unitOfWork)
+        {
+        }
+
+        public long Create(CurrencyPairRequest cpRequest, long userId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CurrencyPairRequest Get(Expression<Func<CurrencyPairRequest, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetObsc(Expression<Func<CurrencyPairRequest, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CurrencyPairRequest cpRequest, long userId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SoftDelete(long cpRequestId, long userId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CurrencyPairRequest> GetAllActive(bool track = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Nozomi.Service/Services/Interfaces/ICurrencyPairRequestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Data.WebModels;
+
+namespace Nozomi.Service.Services.Interfaces
+{
+    public interface ICurrencyPairRequestService
+    {
+        long Create(CurrencyPairRequest cpRequest, long userId = 0);
+
+        CurrencyPairRequest Get(Expression<Func<CurrencyPairRequest, bool>> predicate);
+
+        dynamic GetObsc(Expression<Func<CurrencyPairRequest, bool>> predicate);
+
+        bool Update(CurrencyPairRequest cpRequest, long userId = 0);
+
+        bool SoftDelete(long cpRequestId, long userId = 0);
+        
+        IEnumerable<CurrencyPairRequest> GetAllActive(bool track = false);
+    }
+}
