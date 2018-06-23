6eb2550205a08cfb1e0222df340efa6240ef09cb
nixxholas <nicholas@slyck.net>
2018-06-23 10:46:16 +0700

    Nozomi.Service: Services: Requests: RequestLogService: Initial Implementation
    Nozomi.Service: Services: Requests: RequestLogService: Create API: Implementation
    
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/Services/Requests/RequestLogService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data.WebModels.LoggingModels;
+using Nozomi.Repo.Data;
+using Nozomi.Repo.Repositories;
+using Nozomi.Service.Services.Requests.Interfaces;
+
+namespace Nozomi.Service.Services.Requests
+{
+    public class RequestLogService : BaseService<RequestLogService, NozomiDbContext>, IRequestLogService
+    {
+        public RequestLogService(ILogger<RequestLogService> logger, IUnitOfWork<NozomiDbContext> unitOfWork) 
+            : base(logger, unitOfWork)
+        {
+        }
+
+        public long Create(RequestLog rLog)
+        {
+            if (rLog != null)
+            {
+                _unitOfWork.GetRepository<RequestLog>().Add(rLog);
+                _unitOfWork.Commit();
+                
+                return rLog.Id;
+            }
+
+            return -1;
+        }
+
+        public bool Update(RequestLog rLog, long userId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<dynamic> GetAllObsc(bool track = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RequestLog> GetAll(bool track = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RequestLog> GetAll(Expression<Func<RequestLog, bool>> predicate, bool track = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
