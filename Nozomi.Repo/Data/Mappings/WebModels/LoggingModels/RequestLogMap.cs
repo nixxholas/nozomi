73721d8aa791b0ca2462a0943d5daac9df7b329d
nixxholas <nicholas@slyck.net>
2018-06-23 10:36:48 +0700

    Nozomi.Repo: Data: Mappings: WebModels: LoggingModels: RequestLogMap: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/WebModels/LoggingModels/RequestLogMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Data.WebModels.LoggingModels;
+
+namespace Nozomi.Repo.Data.Mappings.WebModels.LoggingModels
+{
+    public class RequestLogMap
+    {
+        public RequestLogMap(EntityTypeBuilder<RequestLog> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(rl => rl.Id).HasName("RequestLog_PK_Id");
+            entityTypeBuilder.Property(rl => rl.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.Property(rl => rl.Type).IsRequired();
+            entityTypeBuilder.Property(rl => rl.RawPayload).IsRequired(false);
+
+            entityTypeBuilder.HasOne(rl => rl.Request).WithMany(r => r.RequestLogs)
+                .HasForeignKey(rl => rl.RequestId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
