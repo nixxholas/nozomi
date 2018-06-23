774c1e5fe7ab74a6363a59c5d09c9535476c659f
nixxholas <nicholas@slyck.net>
2018-06-23 10:36:05 +0700

    Nozomi.Data: WebModels: LoggingModels: RequestLog: RequestLogType Fallback Value
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/WebModels/LoggingModels/RequestLog.cs
+++ Nozomi.Data/WebModels/LoggingModels/RequestLog.cs
@@ -5,8 +5,8 @@ namespace Nozomi.Data.WebModels.LoggingModels
     public class RequestLog : BaseEntityModel
     {
         public long Id { get; set; }
-        
-        public RequestLogType Type { get; set; }
+
+        public RequestLogType Type { get; set; } = RequestLogType.Unknown;
         
         public string RawPayload { get; set; }
         
