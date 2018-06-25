4f2496df75339e54ef0d50d04abfbd60f3ae7ea5
nixxholas <nicholas@slyck.net>
2018-06-25 22:04:03 +0800

    Nozomi.Service: HostedServices: RequestTypes: HttpPostCurrencyPairRequestSyncingService: Initial Skeleton Commit
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Service/HostedServices/RequestTypes/HttpPostCurrencyPairRequestSyncingService.cs
@@ -0,0 +1,27 @@
+﻿using Nozomi.Data.WebModels;
+using Nozomi.Service.HostedServices.RequestTypes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nozomi.Service.HostedServices.RequestTypes
+{
+    public class HttpPostCurrencyPairRequestSyncingService : BaseHostedService, IHttpPostCurrencyPairRequestSyncingService
+    {
+        public HttpPostCurrencyPairRequestSyncingService(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public Task<bool> Process(CurrencyPairRequest req)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Nozomi.Service/HostedServices/RequestTypes/Interfaces/IHttpPostCurrencyPairRequestSyncingService.cs
@@ -0,0 +1,13 @@
+﻿using Nozomi.Data.WebModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nozomi.Service.HostedServices.RequestTypes.Interfaces
+{
+    public interface IHttpPostCurrencyPairRequestSyncingService
+    {
+        Task<bool> Process(CurrencyPairRequest req);
+    }
+}
