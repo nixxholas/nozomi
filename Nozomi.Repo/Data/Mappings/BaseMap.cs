7b60f5ef3d68d5767340c0ed75d09a35634b8243
nixxholas <nicholas@slyck.net>
2018-06-25 11:40:41 +0800

    Nozomi.Repo: Data: Mappings: BaseMap: Initial Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Data/Mappings/BaseMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nozomi.Repo.Data.Mappings
+{
+    public abstract class BaseMap<T> where T : class
+    {
+        public BaseMap(EntityTypeBuilder<T> entityTypeBuilder)
+        {
+            entityTypeBuilder.ForNpgsqlUseXminAsConcurrencyToken();
+        }
+    }
+}
