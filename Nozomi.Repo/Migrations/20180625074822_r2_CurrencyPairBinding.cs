e8efb7a49f1754d1b77485e591c302e6a9f815bb
nixxholas <nicholas@slyck.net>
2018-06-25 15:49:55 +0800

    Nozomi.Repo: Migrations: r2 - CurrencyPairBinding Migration
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Migrations/20180625074822_r2_CurrencyPairBinding.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r2_CurrencyPairBinding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CurrencyPairId",
+                table: "Requests",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_CurrencyPairId",
+                table: "Requests",
+                column: "CurrencyPairId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_CurrencyPairs_CurrencyPairId",
+                table: "Requests",
+                column: "CurrencyPairId",
+                principalTable: "CurrencyPairs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_CurrencyPairs_CurrencyPairId",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_CurrencyPairId",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "CurrencyPairId",
+                table: "Requests");
+        }
+    }
+}
