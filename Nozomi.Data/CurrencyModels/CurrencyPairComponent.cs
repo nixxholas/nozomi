20a6eaa5d0fcd311c1c3dbf8b880076dea4901cb
nixxholas <nicholas@slyck.net>
2018-06-26 12:09:27 +0800

    Nozomi.Data: CurrencyModels: CurrencyPairComponent: IsValueAbnormal: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/CurrencyModels/CurrencyPairComponent.cs
+++ Nozomi.Data/CurrencyModels/CurrencyPairComponent.cs
@@ -12,6 +12,21 @@ namespace Nozomi.Data.CurrencyModels
         public long CurrencyPairId { get; set; }
         public CurrencyPair CurrencyPair { get; set; }
 
+        public bool IsValueAbnormal(string incomingValue)
+        {
+            switch (ComponentType)
+            {
+                case ComponentType.Ask:
+                    decimal.TryParse(incomingValue, out decimal askRes);
+                    return (askRes <= 0);
+                case ComponentType.Bid:
+                    decimal.TryParse(incomingValue, out decimal bidRes);
+                    return bidRes <= 0;
+                default:
+                    return false;
+            }
+        }
+
         public bool IsValid()
         {
             return !string.IsNullOrEmpty(QueryComponent) && CurrencyPairId > 0;
