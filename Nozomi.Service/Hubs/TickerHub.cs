3148854b1b26eefa9ea8da2d9c223825d535012c
nixxholas <nicholas@slyck.net>
2018-06-29 11:48:37 +0800

    Nozomi.Service: Hubs: TickerHub: Tickers API: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Service/Hubs/TickerHub.cs
+++ Nozomi.Service/Hubs/TickerHub.cs
@@ -13,12 +13,24 @@ namespace Nozomi.Service.Hubs
     {
         private IEnumerable<CurrencyPair> _currencyPairs;
         private readonly ICurrencyPairService _cpService;
-        
+
         public TickerHub(ICurrencyPairService cpService)
         {
             _cpService = cpService;
         }
         
+        public async Task<NozomiResult<CurrencyPair>> Tickers(IEnumerable<CurrencyPair> currencyPairs = null)
+        {
+            var nozRes = new NozomiResult<CurrencyPair>()
+            {
+                Success = true,
+                ResultType = NozomiResultType.Success,
+                Data = currencyPairs
+            };
+            
+            return nozRes;
+        }
+
         // We can use this to return a payload
         public async Task<ChannelReader<NozomiResult<CurrencyPair>>> SubscribeToAll()
         {
@@ -38,30 +50,23 @@ namespace Nozomi.Service.Hubs
             // with the same semantic conventions while maintaining conformity.
             async Task WriteToChannel(ChannelWriter<NozomiResult<CurrencyPair>> writer)
             {
-                while (true)
+                // Pull in the latest data
+                _currencyPairs = _cpService.GetAllActive();
+
+                // Iterate them currency pairs
+                foreach (var cPair in _currencyPairs)
                 {
-                    // Pull in the latest data
-                    _currencyPairs = _cpService.GetAllActive();
-
-                    // Iterate them currency pairs
-                    foreach (var cPair in _currencyPairs)
+                    // Write one by one, and the client receives them one by one as well
+                    await writer.WriteAsync(new NozomiResult<CurrencyPair>()
                     {
-                        // Write one by one, and the client receives them one by one as well
-                        await writer.WriteAsync(new NozomiResult<CurrencyPair>()
-                        {
-                            Success = (cPair != null),
-                            ResultType = (cPair != null) ? NozomiResultType.Success : NozomiResultType.Failed,
-                            Data = new[] { cPair }
-                        });
-                    }
-
-                    writer.Complete();
-
-                    await Task.Delay(1000);
+                        Success = (cPair != null),
+                        ResultType = (cPair != null) ? NozomiResultType.Success : NozomiResultType.Failed,
+                        Data = new[] {cPair}
+                    });
                 }
-                
+
                 // Beep the client, telling them you're done
-                //writer.Complete();
+                writer.Complete();
             }
         }
     }
