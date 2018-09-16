f8bf2838bca86a585fc77a61159f7c3b69928449
nixxholas <nixholas@icloud.com>
2018-09-16 16:53:22 +0800

    Nozomi.Ticker: Program: Prep for service scoping
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/Program.cs
+++ Nozomi.Ticker/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Nozomi.Repo.Data;
 
 namespace Nozomi.Ticker
 {
@@ -14,11 +16,24 @@ namespace Nozomi.Ticker
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                // Retrieve your DbContext isntance here
+                var dbContext = scope.ServiceProvider.GetService<NozomiDbContext>();
+ 
+                // place your DB seeding code here
+                //DbSeeder.Seed(dbContext);
+            }
+            
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>();
     }
 }
