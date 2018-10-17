79589c5d502d2bc91c8841325c019622f3096178
nixxholas <nixholas@icloud.com>
2018-10-18 04:27:22 +0800

    Nozomi.Ticker: Controllers: v1: CurrencyPairController structuring
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/Controllers/v1/CurrencyPairController.cs
+++ Nozomi.Ticker/Controllers/v1/CurrencyPairController.cs
@@ -8,12 +8,13 @@ using Nozomi.Data;
 using Nozomi.Data.CurrencyModels;
 using Nozomi.Service.Hubs;
 using Nozomi.Service.Services.Interfaces;
+using Nozomi.Ticker.Controllers.v1.Interfaces;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Nozomi.Ticker.Controllers.v1
 {
     [ApiController]
-    public class CurrencyPairController : BaseController<CurrencyPairController>
+    public class CurrencyPairController : BaseController<CurrencyPairController>, ICurrencyPairController
     {
         private readonly ICurrencyPairService _currencyPairService;
         private readonly IHubContext<TickerHub> _tickerHubContext;
@@ -26,10 +27,32 @@ namespace Nozomi.Ticker.Controllers.v1
             _currencyPairService = currencyPairService;
         }
         
-        [HttpPost("Create")]
-        public async Task CreateCurrencyPair(CurrencyPair currencyPair)
+        [HttpPost("create")]
+        public async Task<NozomiResult<CurrencyPair>> CreateCurrencyPair(CurrencyPair currencyPair)
         {
-            var res = _currencyPairService.Create(currencyPair, currencyPair.CreatedBy);
+            if (_currencyPairService.Create(currencyPair, currencyPair.CreatedBy))
+            {
+                return new NozomiResult<CurrencyPair>()
+                {
+                    ResultType = NozomiResultType.Success
+                };
+            }
+            
+            return new NozomiResult<CurrencyPair>()
+            {
+                ResultType = NozomiResultType.Failed
+            };
+        }
+
+        [HttpGet("ticker")]
+        public Task Ticker(long id)
+        {
+            
+        }
+
+        public Task Ticker(string abbreviation)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
---
+++ Nozomi.Ticker/Controllers/v1/Interfaces/ICurrencyPairController.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Nozomi.Data;
+using Nozomi.Data.CurrencyModels;
+
+namespace Nozomi.Ticker.Controllers.v1.Interfaces
+{
+    public interface ICurrencyPairController
+    {
+        /// <summary>
+        /// Creates a currency pair with the following parameters and child objects.
+        /// </summary>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="currencyPair">Object containing the currency pair information.</param>
+        /// <returns>NozomiResult</returns>
+        Task<NozomiResult<CurrencyPair>> CreateCurrencyPair(CurrencyPair currencyPair);
+
+        /// <summary>
+        /// An explicitly-defined variation of the GET Ticker API, allows the user to explicitly call a single ticker
+        /// when the primary key is known to the caller.
+        /// </summary>
+        /// <param name="id">Unique identifier </param>
+        /// <returns></returns>
+        Task Ticker(long id);
+
+        /// <summary>
+        /// Returns a list of prices from various exchanges (if available) for the specific Ticker Abbreviation.
+        /// </summary>
+        /// <param name="abbreviation">The abbreviation of the ticker, i.e. ETHUSD</param>
+        /// <returns>Ticker Price Array</returns>
+        Task Ticker(string abbreviation);
+    }
+}
