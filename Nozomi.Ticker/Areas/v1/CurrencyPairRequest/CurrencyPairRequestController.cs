47ef785ddcbd5d85c96738034830b93bde9be348
nixxholas <nicholas@slyck.net>
2018-10-30 16:54:15 +0800

    Nozomi.Ticker: Areas: v1: CurrencyPairRequest: CurrencyPairRequestController: All API implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Ticker/Areas/v1/CurrencyPairRequest/CurrencyPairRequestController.cs
+++ Nozomi.Ticker/Areas/v1/CurrencyPairRequest/CurrencyPairRequestController.cs
@@ -1,13 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Data;
+using Nozomi.Data.AreaModels.v1.CurrencyPairRequest;
+using Nozomi.Service.Services.Interfaces;
 
 namespace Nozomi.Ticker.Areas.v1.CurrencyPairRequest
 {
     public class CurrencyPairRequestController : BaseController<CurrencyPairRequestController>, ICurrencyPairRequestController
     {
-        public CurrencyPairRequestController(ILogger<CurrencyPairRequestController> logger) : base(logger)
+        private ICurrencyPairRequestService _currencyPairRequestService;
+        
+        public CurrencyPairRequestController(ILogger<CurrencyPairRequestController> logger,
+            ICurrencyPairRequestService currencyPairRequestService) : base(logger)
         {
+            _currencyPairRequestService = currencyPairRequestService;
         }
-        
-        
+
+        public NozomiResult<JsonResult> All(bool includeNested)
+        {
+            var res = _currencyPairRequestService.GetAllActive();
+            
+            return new NozomiResult<JsonResult>()
+            {
+                ResultType = res != null ? NozomiResultType.Success : NozomiResultType.Failed,
+                Data = res != null ? new JsonResult(res) : 
+                    new JsonResult("We're currently unable to obtain all of the Currency Pair Requests.")
+            };
+        }
+
+        public NozomiResult<JsonResult> Create(CreateCurrencyPairRequest obj, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NozomiResult<JsonResult> Update(UpdateCurrencyPairRequest obj, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NozomiResult<JsonResult> Delete(long id, bool hardDelete = false, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NozomiResult<JsonResult> ManualPoll(long requestId, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
