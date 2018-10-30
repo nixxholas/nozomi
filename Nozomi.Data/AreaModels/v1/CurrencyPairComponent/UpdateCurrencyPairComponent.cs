d9be61e5ef09baba00b6a31cbbb89b08cf721267
nixxholas <nicholas@slyck.net>
2018-10-31 07:59:15 +0800

    Nozomi.Data: AreaModels: v1: UpdateCurrencyPairRequest refinement
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/AreaModels/v1/CurrencyPairComponent/UpdateCurrencyPairComponent.cs
+++ Nozomi.Data/AreaModels/v1/CurrencyPairComponent/UpdateCurrencyPairComponent.cs
@@ -2,6 +2,8 @@ namespace Nozomi.Data.AreaModels.v1.CurrencyPairComponent
 {
     public class UpdateCurrencyPairComponent : CreateCurrencyPairComponent
     {
+        public long Id { get; set; }
+        
         public bool ToBeDeleted()
         {
             return RequestId.Equals(0);
--- Nozomi.Data/AreaModels/v1/CurrencyPairRequest/UpdateCurrencyPairRequest.cs
+++ Nozomi.Data/AreaModels/v1/CurrencyPairRequest/UpdateCurrencyPairRequest.cs
@@ -8,6 +8,8 @@ namespace Nozomi.Data.AreaModels.v1.CurrencyPairRequest
 {
     public class UpdateCurrencyPairRequest
     {
+        public long Id { get; set; }
+        
         public long CurrencyPairId { get; set; }
         
         public RequestType RequestType { get; set; }
@@ -15,6 +17,8 @@ namespace Nozomi.Data.AreaModels.v1.CurrencyPairRequest
         public string DataPath { get; set; }
         
         public int Delay { get; set; }
+        
+        public bool IsEnabled { get; set; }
         
         public ICollection<UpdateCurrencyPairComponent> CurrencyPairComponents { get; set; }
         
