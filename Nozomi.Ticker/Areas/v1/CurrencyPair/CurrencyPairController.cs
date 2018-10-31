e649d4571a8dc558abbf3a121e8029a25b9db108
nixxholas <nicholas@slyck.net>
2018-10-31 11:42:32 +0700

    Nozomi.Ticker: Areas: v1: CurrencyPair: CurrencyPairController: Cleanup
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Ticker/Areas/v1/CurrencyPair/CurrencyPairController.cs
+++ Nozomi.Ticker/Areas/v1/CurrencyPair/CurrencyPairController.cs
@@ -18,16 +18,16 @@ namespace Nozomi.Ticker.Areas.v1.CurrencyPair
         private readonly IHubContext<TickerHub> _tickerHubContext;
 
         public CurrencyPairController(IHubContext<TickerHub> tickerHubContext, ICurrencyPairService currencyPairService,
-            ITickerService tickerService, ILogger<CurrencyPairController> logger) 
+            ITickerService tickerService, ILogger<CurrencyPairController> logger)
             : base(logger)
         {
             _tickerHubContext = tickerHubContext;
             _currencyPairService = currencyPairService;
             _tickerService = tickerService;
         }
-        
-        [HttpPost("create")]
-        public async Task<NozomiResult<Data.CurrencyModels.CurrencyPair>> Create(Data.CurrencyModels.CurrencyPair currencyPair)
+
+        [HttpPost]
+        public async Task<NozomiResult<Data.CurrencyModels.CurrencyPair>> Create([FromBody]Data.CurrencyModels.CurrencyPair currencyPair)
         {
             if (_currencyPairService.Create(currencyPair, currencyPair.CreatedBy))
             {
@@ -43,14 +43,14 @@ namespace Nozomi.Ticker.Areas.v1.CurrencyPair
             };
         }
 
-        [HttpGet("ticker")]
+        [HttpGet("{id}")]
         public Task Ticker(long id)
         {
             return _tickerService.GetById(id);
         }
 
-        [HttpGet("ticker-am")]
-        public NozomiResult<ICollection<DistinctiveTickerResponse>> Ticker(string abbreviation, string exchangeAbbrv = null)
+        [HttpGet("{abbreviation}")]
+        public NozomiResult<ICollection<DistinctiveTickerResponse>> Ticker(string abbreviation, [FromRoute]string exchangeAbbrv = null)
         {
             return _tickerService.GetByAbbreviation(abbreviation, exchangeAbbrv);
         }
