265baec3a915e6d0cddd4ff4ff4c354e80cbc790
nixxholas <nicholas@slyck.net>
2018-11-01 11:38:33 +0700

    Nozomi.Data: NozomiResult: Data-driven constructor
    Allows us to set the ResultType based on the data.
    
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/NozomiResult.cs
+++ Nozomi.Data/NozomiResult.cs
@@ -4,6 +4,14 @@ namespace Nozomi.Data
 {
     public class NozomiResult<TEntity> where TEntity : class
     {
+        public NozomiResult(){}
+
+        public NozomiResult(TEntity data)
+        {
+            ResultType = (data == null) ? NozomiResultType.Failed : NozomiResultType.Success;
+            Data = data;
+        }
+        
         public NozomiResultType ResultType { get; set; } = NozomiResultType.Unknown;
         
         public TEntity Data { get; set; }
