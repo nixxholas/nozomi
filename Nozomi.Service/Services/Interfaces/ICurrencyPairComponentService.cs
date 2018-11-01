db6bc3daa57df0a6aaad97af35234faa57b5a63c
nixxholas <nicholas@slyck.net>
2018-11-01 11:14:15 +0700

    Nozomi.Service: Services: Interfaces: ICurrencyPairComponentService: Scaffolding for further functions
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Service/Services/Interfaces/ICurrencyPairComponentService.cs
+++ Nozomi.Service/Services/Interfaces/ICurrencyPairComponentService.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using Nozomi.Data.AreaModels.v1.CurrencyPairComponent;
+using Nozomi.Data.WebModels;
+
 namespace Nozomi.Service.Services.Interfaces
 {
     public interface ICurrencyPairComponentService
     {
+        ICollection<RequestComponent> AllByRequestId(long requestId, bool includeNested = false);
+
+        ICollection<RequestComponent> All(bool includeNested = false);
+        
+        bool Create(CreateCurrencyPairComponent obj, long userId = 0);
+        
         bool UpdatePairValue(long id, decimal val);
+
+        bool Update(UpdateCurrencyPairComponent obj, long userId = 0);
+
+        bool Delete(long id, long userId = 0, bool hardDelete = false);
     }
 }
