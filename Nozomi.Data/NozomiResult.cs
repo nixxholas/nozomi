049da1c31fd44425b81463316afc481b75a6b535
nixxholas <nicholas@slyck.net>
2018-11-01 12:34:38 +0700

    Nozomi.Data: NozomiResult: Constructor divergence for further customizability
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/NozomiResult.cs
+++ Nozomi.Data/NozomiResult.cs
@@ -4,15 +4,26 @@ namespace Nozomi.Data
 {
     public class NozomiResult<TEntity> where TEntity : class
     {
+        // Manual Initialization
         public NozomiResult(){}
 
+        // Data-driven Initialization
         public NozomiResult(TEntity data)
         {
             ResultType = (data == null) ? NozomiResultType.Failed : NozomiResultType.Success;
             Data = data;
         }
-        
+
+        // Result-driven Initialization
+        public NozomiResult(NozomiResultType resultType, string msg)
+        {
+            ResultType = resultType;
+            Message = msg;
+        }
+
         public NozomiResultType ResultType { get; set; } = NozomiResultType.Unknown;
+        
+        public string Message { get; set; }
         
         public TEntity Data { get; set; }
     }
