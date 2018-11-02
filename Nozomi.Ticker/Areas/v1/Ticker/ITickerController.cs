c543a6d868868550c3ac2cf8c0828dfad06acafe
nixxholas <nicholas@slyck.net>
2018-11-03 01:40:30 +0800

    Nozomi.Ticker: Areas: v1: Ticker: ITickerController: Initial Scaffold
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Ticker/Areas/v1/Ticker/ITickerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Nozomi.Data;
+using Nozomi.Data.ResponseModels;
+
+namespace Nozomi.Ticker.Areas.v1.Ticker
+{
+    public interface ITickerController
+    {
+        NozomiResult<ICollection<DistinctiveTickerResponse>> Get(long currencySourceId, string symbol, bool includeNested = false);
+    }
+}
