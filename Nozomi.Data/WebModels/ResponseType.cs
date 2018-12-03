062d7bf4bf89205f6a076827e795f029bf74c7a6
nixxholas <nicholas@slyck.net>
2018-12-03 12:56:33 +0800

    Nozomi.Data: WebModels: ResponseType: Descriptions
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Data/WebModels/ResponseType.cs
+++ Nozomi.Data/WebModels/ResponseType.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace Nozomi.Data.WebModels
 {
     public enum ResponseType
     {
+        [Description("application/json")]
         Json = 1,
+        [Description("application/xml")]
         XML = 2
     }
 }
