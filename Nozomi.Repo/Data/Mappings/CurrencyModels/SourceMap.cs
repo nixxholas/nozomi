22d877ca67eb2ddc69e594629b7ee140c99caa29
nixxholas <nixholas@icloud.com>
2018-12-11 18:17:37 +0800

    Nozomi.Repo & Ticker: Source seeding migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Data/Mappings/CurrencyModels/SourceMap.cs
+++ Nozomi.Repo/Data/Mappings/CurrencyModels/SourceMap.cs
@@ -22,44 +22,6 @@ namespace Nozomi.Repo.Data.Mappings.CurrencyModels
             entityTypeBuilder.HasMany(s => s.CurrencyPairs).WithOne(cp => cp.CurrencySource)
                 .HasForeignKey(cp => cp.CurrencySourceId)
                 .HasConstraintName("Source_CurrencyPairs_Constraint");
-
-            entityTypeBuilder.HasData(
-                new Source()
-                {
-                    Id = 1,
-                    Abbreviation = "BFX",
-                    Name = "Bitfinex",
-                    APIDocsURL = "https://docs.bitfinex.com/docs/introduction"
-                },
-                new Source()
-                {
-                    Id = 2,
-                    Abbreviation = "HAKO",
-                    Name = "Coinhako",
-                    APIDocsURL = "None"
-                },
-                new Source()
-                {
-                    Id = 3,
-                    Abbreviation = "BNA",
-                    Name = "Binance",
-                    APIDocsURL = "https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md"
-                },
-                new Source()
-                {
-                    Id = 4,
-                    Abbreviation = "ECB",
-                    Name = "European Central Bank",
-                    APIDocsURL = "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
-                },
-                new Source()
-                {
-                    Id = 5,
-                    Abbreviation = "AVG",
-                    Name = "AlphaVantage",
-                    APIDocsURL = "https://www.alphavantage.co/documentation/"
-                }
-            );
         }
     }
 }
--- Nozomi.Ticker/StartupExtensions/DatabaseStartup.cs
+++ Nozomi.Ticker/StartupExtensions/DatabaseStartup.cs
@@ -1,7 +1,9 @@
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Nozomi.Data.CurrencyModels;
 using Nozomi.Repo.Data;
 
 namespace Nozomi.Ticker.StartupExtensions
@@ -23,6 +25,42 @@ namespace Nozomi.Ticker.StartupExtensions
                     }
                     
                     context.Database.EnsureCreated();
+                    
+                    if (!context.Sources.Any())
+                    {
+                        context.AddRange(
+                            new Source()
+                            {
+                                Abbreviation = "BFX",
+                                Name = "Bitfinex",
+                                APIDocsURL = "https://docs.bitfinex.com/docs/introduction"
+                            },
+                            new Source()
+                            {
+                                Abbreviation = "HAKO",
+                                Name = "Coinhako",
+                                APIDocsURL = "None"
+                            },
+                            new Source()
+                            {
+                                Abbreviation = "BNA",
+                                Name = "Binance",
+                                APIDocsURL = "https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md"
+                            },
+                            new Source()
+                            {
+                                Abbreviation = "ECB",
+                                Name = "European Central Bank",
+                                APIDocsURL = "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
+                            },
+                            new Source()
+                            {
+                                Abbreviation = "AVG",
+                                Name = "AlphaVantage",
+                                APIDocsURL = "https://www.alphavantage.co/documentation/"
+                            });
+                        context.SaveChanges();
+                    }
                 }
             }
         }
