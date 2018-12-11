04347905ca7d8e7206c19efa5712050ea9cddf74
nixxholas <nixholas@icloud.com>
2018-12-11 18:13:08 +0800

    Nozomi.Data: WebModels: RequestType: Description annotations
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Data/WebModels/RequestType.cs
+++ Nozomi.Data/WebModels/RequestType.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Nozomi.Data.WebModels
 {
     public enum RequestType
     {
+        [Description("HttpGet")]
         HttpGet = 0,
+        [Description("HttpPost")]
         HttpPost = 1,
+        [Description("WebSocket")]
         WebSocket = 50
     }
 }
