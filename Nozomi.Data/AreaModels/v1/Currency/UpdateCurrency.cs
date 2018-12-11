8fad46b0f61dd2a4de84f318e69730dbbd947a69
nixxholas <nicholas@slyck.net>
2018-12-12 07:38:16 +0800

    Nozomi.Data: AreaModels: v1: Currency: UpdateCurrency: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Data/AreaModels/v1/Currency/UpdateCurrency.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nozomi.Data.AreaModels.v1.Currency
+{
+    public class UpdateCurrency
+    {
+        [Required]
+        public long Id { get; set; }
+        
+        [Required]
+        public long CurrencyTypeId { get; set; }
+
+        [Required]
+        public string Abbrv { get; set; } // USD? MYR? IND?
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public long CurrencySourceId { get; set; }
+
+        // This will have a number if it is a crypto pair to peg to proper entities
+        public long WalletTypeId { get; set; } = 0;
+        
+        public ICollection<CurrencyModels.PartialCurrencyPair> PartialCurrencyPairs { get; set; }
+
+        public bool IsValid()
+        {
+            return CurrencyTypeId > 0 && !string.IsNullOrEmpty(Abbrv) && !string.IsNullOrEmpty(Name)
+                   && CurrencySourceId > 0;
+        }
+    }
+}
