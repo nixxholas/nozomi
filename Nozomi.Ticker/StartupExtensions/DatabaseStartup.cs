527f57a3f464dfd90c5e57e29e8c15c1c0b14028
nixxholas <nixholas@icloud.com>
2018-12-11 18:24:53 +0800

    Nozomi.Repo & Ticker: Currencies seeding migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/StartupExtensions/DatabaseStartup.cs
+++ Nozomi.Ticker/StartupExtensions/DatabaseStartup.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Nozomi.Data.CurrencyModels;
 using Nozomi.Repo.Data;
@@ -23,9 +24,9 @@ namespace Nozomi.Ticker.StartupExtensions
                     {
                         context.Database.EnsureDeleted();
                     }
-                    
+
                     context.Database.EnsureCreated();
-                    
+
                     if (!context.Sources.Any())
                     {
                         context.AddRange(
@@ -45,13 +46,15 @@ namespace Nozomi.Ticker.StartupExtensions
                             {
                                 Abbreviation = "BNA",
                                 Name = "Binance",
-                                APIDocsURL = "https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md"
+                                APIDocsURL =
+                                    "https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md"
                             },
                             new Source()
                             {
                                 Abbreviation = "ECB",
                                 Name = "European Central Bank",
-                                APIDocsURL = "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
+                                APIDocsURL =
+                                    "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
                             },
                             new Source()
                             {
@@ -59,6 +62,86 @@ namespace Nozomi.Ticker.StartupExtensions
                                 Name = "AlphaVantage",
                                 APIDocsURL = "https://www.alphavantage.co/documentation/"
                             });
+                        context.SaveChanges();
+                    }
+
+                    if (!context.Currencies.Any() && context.CurrencyTypes.Any() && context.Sources.Any())
+                    {
+                        context.Currencies.AddRange(
+                            new Currency()
+                            {
+                                CurrencyTypeId = 1,
+                                Abbrv = "USD",
+                                Name = "United States Dollar",
+                                CurrencySourceId = 1,
+                                WalletTypeId = 0
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 2,
+                                Abbrv = "ETH",
+                                Name = "Ethereum",
+                                CurrencySourceId = 1,
+                                WalletTypeId = 1 // As per CNWallet
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 2,
+                                Abbrv = "KNC",
+                                Name = "Kyber Network Coin",
+                                CurrencySourceId = 1,
+                                WalletTypeId = 4 // As per CNWallet
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 2,
+                                Abbrv = "KNC",
+                                Name = "Kyber Network Coin",
+                                CurrencySourceId = 3,
+                                WalletTypeId = 4 // As per CNWallet
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 2,
+                                Abbrv = "ETH",
+                                Name = "Ethereum",
+                                CurrencySourceId = 3,
+                                WalletTypeId = 1 // As per CNWallet
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 1,
+                                Abbrv = "EUR",
+                                Name = "Euro",
+                                CurrencySourceId = 4,
+                                WalletTypeId = 0
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 1,
+                                Abbrv = "USD",
+                                Name = "United States Dollar",
+                                CurrencySourceId = 4,
+                                WalletTypeId = 0
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 1,
+                                Abbrv = "EUR",
+                                Name = "Euro",
+                                CurrencySourceId = 5,
+                                WalletTypeId = 0
+                            },
+                            new Currency()
+                            {
+                                CurrencyTypeId = 1,
+                                Abbrv = "USD",
+                                Name = "United States Dollar",
+                                CurrencySourceId = 5,
+                                WalletTypeId = 0
+                            }
+                        );
+
                         context.SaveChanges();
                     }
                 }
