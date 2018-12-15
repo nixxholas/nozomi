56814a4c91737df995e83db25d1516de83122cd8
nixxholas <nicholas@slyck.net>
2018-12-15 19:17:38 +0800

    Nozomi.Base.Identity & Ticker: General .NET Core identity basic implementations
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Identity/Models/Identity/User.cs
+++ Nozomi.Base.Identity/Models/Identity/User.cs
@@ -1,7 +1,6 @@
-using IdentityServer4;
 using Microsoft.AspNetCore.Identity;
 
-namespace Nozomi.Base.Identity.Models
+namespace Nozomi.Base.Identity.Models.Identity
 {
     public class User : IdentityUser<long>
     {
--- Nozomi.Ticker/StartupExtensions/IdentityServerStartup.cs
+++ Nozomi.Ticker/StartupExtensions/IdentityServerStartup.cs
@@ -1,6 +1,11 @@
+using System;
 using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Nozomi.Base.Identity.Debugging;
 using Nozomi.Base.Identity.Models;
+using Nozomi.Base.Identity.Models.Identity;
+using Nozomi.Repo.Identity.Data;
 using Nozomi.Repo.Identity.Stores;
 using Nozomi.Service.Identity;
 
@@ -10,8 +15,45 @@ namespace Nozomi.Ticker.StartupExtensions
     {
         public static void ConfigureIdentityServer(this IServiceCollection services)
         {
-            services.AddTransient<IClientStore, ClientStore>();
-            services.AddTransient<IResourceStore, ResourceStore>();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            // Environment-specific configuration
+            if (!string.IsNullOrEmpty(env) && env.Equals("Production", StringComparison.InvariantCultureIgnoreCase))
+            {
+            } else
+            {
+            }
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<NozomiAuthContext>();
+            
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                // TODO: finish this
+                //options.LoginPath = "/Identity/Account/Login";
+                //options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
             
             services.AddIdentityServer()
                 //.AddSigningCredential(cert)
@@ -19,6 +61,10 @@ namespace Nozomi.Ticker.StartupExtensions
                 .AddClientStore<ClientStore>()
                 .AddAspNetIdentity<User>()
                 .AddProfileService<NozomiProfileService>();
+            
+            services.AddTransient<IClientStore, ClientStore>();
+            services.AddTransient<IResourceStore, ResourceStore>();
+            
         }
     }
 }
