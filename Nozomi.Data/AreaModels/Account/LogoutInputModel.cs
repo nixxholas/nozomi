9629852a28f9845263e6340774cf0a78b7e0bbb4
nixxholas <nixholas@icloud.com>
2018-12-17 00:27:27 +0800

    Nozomi.Data: AreaModels: Account: Implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Data/AreaModels/Account/LogoutInputModel.cs
@@ -0,0 +1,7 @@
+namespace Nozomi.Data.AreaModels.Account
+{
+    public class LogoutInputModel
+    {
+        public string LogoutId { get; set; }
+    }
+}
---
+++ Nozomi.Data/AreaModels/Account/LogoutViewModel.cs
@@ -0,0 +1,7 @@
+namespace Nozomi.Data.AreaModels.Account
+{
+    public class LogoutViewModel : LogoutInputModel
+    {
+        public bool ShowLogoutPrompt { get; set; }
+    }
+}
