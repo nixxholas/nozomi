bb9ecde9af604a4b748d05c8297d4a3057940784
nixxholas <nixholas@icloud.com>
2018-12-19 19:25:26 +0800

    Nozomi.Ticker: Middleware: NozomiExceptionMiddleware: Implementation and middleware injection
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Core/Responses/NozomiErrorResponse.cs
@@ -0,0 +1,9 @@
+namespace Nozomi.Core.Responses
+{
+    public class NozomiErrorResponse
+    {
+        public int StatusCode { get; set; }
+        
+        public string Message { get; set; }
+    }
+}
