05ae734715d614fdea29f0c2198e68dedda0808a
nixxholas <nixholas@icloud.com>
2018-12-21 06:01:42 +0800

    Nozomi.Infra.Service.Identity: Factories: NozomiUserClaimsPrincipalFactory: GenerateClaimsAsync overriding
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service.Identity/Factories/NozomiUserClaimsPrincipalFactory.cs
+++ Nozomi.Infra.Service.Identity/Factories/NozomiUserClaimsPrincipalFactory.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using IdentityModel;
 using Microsoft.AspNetCore.Identity;
@@ -12,16 +14,19 @@ namespace Nozomi.Service.Identity.Factories
     public class NozomiUserClaimsPrincipalFactory: UserClaimsPrincipalFactory<User, Role>
     {
         public new NozomiUserManager UserManager;
+        public new RoleManager<Role> RoleManager;
         
-        public NozomiUserClaimsPrincipalFactory( 
-            NozomiUserManager userManager,
-            RoleManager<Role> roleManager, 
-            IOptions<IdentityOptions> optionsAccessor) 
-            : base(userManager, roleManager, optionsAccessor)
+        public NozomiUserClaimsPrincipalFactory(NozomiUserManager userManager, RoleManager<Role> roleManager, 
+            IOptions<IdentityOptions> options) : base(userManager, roleManager, options)
         {
-            UserManager = userManager;
         }
- 
+        
+        /// <summary>
+        /// Creates a <see cref="T:System.Security.Claims.ClaimsPrincipal" /> from an user asynchronously.
+        /// </summary>
+        /// <param name="user">The user to create a <see cref="T:System.Security.Claims.ClaimsPrincipal" /> from.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous creation
+        /// operation, containing the created <see cref="T:System.Security.Claims.ClaimsPrincipal" />.</returns>
         public override async Task<ClaimsPrincipal> CreateAsync(User user)
         {
             var principal = await base.CreateAsync(user);
@@ -34,6 +39,35 @@ namespace Nozomi.Service.Identity.Factories
  
             identity.AddClaims(claims);
             return principal;
+        }
+        
+        /// <summary>Generate the claims for a user.</summary>
+        /// <param name="user">The user to create a <see cref="T:System.Security.Claims.ClaimsIdentity" /> from.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> that represents the asynchronous creation operation, containing the created <see cref="T:System.Security.Claims.ClaimsIdentity" />.</returns>
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
+        {
+            var userId = await UserManager.GetUserIdAsync(user);
+            var userNameAsync = await UserManager.GetUserNameAsync(user);
+            var id = new ClaimsIdentity("Identity.Application", 
+                this.Options.ClaimsIdentity.UserNameClaimType, this.Options.ClaimsIdentity.RoleClaimType);
+            id.AddClaim(new Claim(this.Options.ClaimsIdentity.UserIdClaimType, userId));
+            id.AddClaim(new Claim(this.Options.ClaimsIdentity.UserNameClaimType, userNameAsync));
+            ClaimsIdentity claimsIdentity;
+            if (this.UserManager.SupportsUserSecurityStamp)
+            {
+                claimsIdentity = id;
+                string type = this.Options.ClaimsIdentity.SecurityStampClaimType;
+                claimsIdentity.AddClaim(new Claim(type, await this.UserManager.GetSecurityStampAsync(user)));
+                claimsIdentity = (ClaimsIdentity) null;
+                type = (string) null;
+            }
+            if (this.UserManager.SupportsUserClaim)
+            {
+                claimsIdentity = id;
+                claimsIdentity.AddClaims((IEnumerable<Claim>) await this.UserManager.GetClaimsAsync(user));
+                claimsIdentity = (ClaimsIdentity) null;
+            }
+            return id;
         }
     }
 }
