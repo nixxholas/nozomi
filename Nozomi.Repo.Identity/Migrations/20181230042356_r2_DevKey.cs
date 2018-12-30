b1ad169ddb1a61dfed68de4f55039cf64bcb87b4
nixxholas <nixholas@icloud.com>
2018-12-30 12:24:11 +0800

    Nozomi.Repo.Identity: Migrations: r2 - DevKey
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Repo.Identity/Migrations/20181230042356_r2_DevKey.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nozomi.Repo.Identity.Migrations
+{
+    public partial class r2_DevKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiResources");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "IdentityResources");
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "Users",
+                type: "xid",
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.CreateTable(
+                name: "DevKey",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    UserId = table.Column<long>(nullable: false),
+                    Key = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevKey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DevKey_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevKey_UserId",
+                table: "DevKey",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevKey");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "ApiResources",
+                columns: table => new
+                {
+                    ApiResourceName = table.Column<string>(nullable: false),
+                    ApiResourceData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiResources", x => x.ApiResourceName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<string>(nullable: false),
+                    ClientData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentityResources",
+                columns: table => new
+                {
+                    IdentityResourceName = table.Column<string>(nullable: false),
+                    IdentityResourceData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityResources", x => x.IdentityResourceName);
+                });
+        }
+    }
+}
