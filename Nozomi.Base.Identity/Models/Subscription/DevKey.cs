c23a3122399f9de32747fb5d53121a0572f590f8
nixxholas <nicholas@slyck.net>
2019-01-03 06:30:01 +0800

    Nozomi.Base.Identity: Models: Subscription: DevKey: Proper generating segregation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Identity/Models/Subscription/DevKey.cs
+++ Nozomi.Base.Identity/Models/Subscription/DevKey.cs
@@ -7,22 +7,28 @@ namespace Nozomi.Base.Identity.Models.Subscription
 {
     public class DevKey : BaseEntityModel
     {
+        public DevKey() {}
+        
         public DevKey(long userId)
         {
             if (userId <= 0) throw new ArgumentException("Invalid UserId.");
             
             UserId = userId;
-            
-            var key = new byte[32];
-            using (var generator = RandomNumberGenerator.Create())
-                generator.GetBytes(key);
-            
-            Key = Convert.ToBase64String(key);
+            Key = GenerateAPIKey(userId.ToString());
         }
         
         public long Id { get; set; }
         public long UserId { get; set; }
         public User User { get; set; }
         public string Key { get; set; }
+        
+        public string GenerateAPIKey(string userId)
+        {
+            var key = new byte[32];
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(key);
+            }
+            return Convert.ToBase64String(key);
     }
 }
