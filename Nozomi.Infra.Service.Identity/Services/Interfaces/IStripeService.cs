c75ba1a8acfebe6368731633d89ea7acc60d4e2d
nixxholas <nicholas@slyck.net>
2019-01-04 18:44:56 +0800

    Nozomi.Infra.Service.Identity: Services: StripeService: CreatePlan API
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Service.Identity/Services/Interfaces/IStripeService.cs
+++ Nozomi.Infra.Service.Identity/Services/Interfaces/IStripeService.cs
@@ -5,6 +5,8 @@ namespace Nozomi.Service.Identity.Services.Interfaces
 {
     public interface IStripeService
     {
+        Task<bool> CreatePlan(PlanCreateOptions options);
+        
         Task<bool> CreateProduct(ProductCreateOptions options);
 
         Task<bool> CreateSubscription(SubscriptionCreateOptions options);
--- Nozomi.Infra.Service.Identity/Services/StripeService.cs
+++ Nozomi.Infra.Service.Identity/Services/StripeService.cs
@@ -21,6 +21,21 @@ namespace Nozomi.Service.Identity.Services
         }
 
 
+        public async Task<bool> CreatePlan(PlanCreateOptions options)
+        {
+            var planService = new PlanService();
+            var res = await planService.CreateAsync(options);
+
+            if (res != null)
+            {
+                _logger.LogInformation(ServiceName + ": Stripe plan creation successful.");
+                return true;
+            }
+
+            _logger.LogWarning(ServiceName + ": Stripe plan creation failed.");
+            return false;
+        }
+
         public async Task<bool> CreateProduct(ProductCreateOptions options)
         {
             var productService = new ProductService();
