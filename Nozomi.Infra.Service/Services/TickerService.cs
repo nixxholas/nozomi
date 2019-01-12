322f422a2b638f20779c6651d1c44cd88d5fb06a
nixxholas <nicholas@slyck.net>
2019-01-12 11:39:15 +0800

    Nozomi.Infra.Service: Services: TickerService: GetByAbbreviation: Static Update
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Service/Services/TickerService.cs
+++ Nozomi.Infra.Service/Services/TickerService.cs
@@ -99,52 +99,56 @@ namespace Nozomi.Service.Services
             try
             {
                 if (ticker.Length != 6) return null; // Invalid ticker length
+                
+                return new NozomiResult<ICollection<DistinctiveTickerResponse>>(
+                    NozomiServiceConstants.TickerSymbolDictionary[ticker].Where(i => i > 0).Select(
+                    i => NozomiServiceConstants.CurrencyPairDictionary[i]).ToList());
 
-                var query = _unitOfWork.GetRepository<CurrencyPair>()
-                    .GetQueryable()
-                    .AsNoTracking()
-                    .Where(cp => cp.DeletedAt == null && cp.IsEnabled)
-                    .Include(cp => cp.PartialCurrencyPairs)
-                    .ThenInclude(pcp => pcp.Currency)
-                    .Where(cp => (cp.PartialCurrencyPairs.SingleOrDefault(pcp => pcp.IsMain).Currency
-                                      .Abbrv // Make sure the first currency (main) is equal to the ticker's first
-                                  + cp.PartialCurrencyPairs.SingleOrDefault(pcp => !pcp.IsMain).Currency.Abbrv) // other way round
-                        .Equals(ticker, StringComparison.InvariantCultureIgnoreCase))
-                    .Include(cp => cp.CurrencySource)
-                    .Where(cp => cp.CurrencySource != null) // Make sure we have a source
-                    .Include(cp => cp.CurrencyPairRequests)
-                        .ThenInclude(cpr => cpr.RequestComponents)
-                            .ThenInclude(rc => rc.RequestComponentDatum)
-                    // Make sure there's something
-                    .Where(cp => cp.CurrencyPairRequests
-                        .Any(cpr => cpr.RequestComponents.Any(rc => rc.IsEnabled && rc.DeletedAt == null && 
-                                                                    rc.RequestComponentDatum != null)))
-                    .Select(cp => new DistinctiveTickerResponse()
-                    {
-                        Exchange = cp.CurrencySource.Name,
-                        ExchangeAbbrv = cp.CurrencySource.Abbreviation,
-                        LastUpdated = cp.CurrencyPairRequests.FirstOrDefault()
-                            .RequestComponents.FirstOrDefault()
-                            .RequestComponentDatum.ModifiedAt,
-                        Properties = cp.CurrencyPairRequests.FirstOrDefault()
-                            .RequestComponents
-                            .Select(rc => new KeyValuePair<string, string>(
-                                rc.ComponentType.ToString(), 
-                                rc.RequestComponentDatum.Value))
-                            .ToList()
-                    });
-
-                // Exchange-based filter
-                if (!exchangeAbbrv.IsNullOrEmpty())
-                {
-                    query = query.Where(cp => cp.ExchangeAbbrv.Equals(exchangeAbbrv));
-                }
-
-                return new NozomiResult<ICollection<DistinctiveTickerResponse>>()
-                {
-                    ResultType = NozomiResultType.Success,
-                    Data = query.ToList()
-                };
+//                var query = _unitOfWork.GetRepository<CurrencyPair>()
+//                    .GetQueryable()
+//                    .AsNoTracking()
+//                    .Where(cp => cp.DeletedAt == null && cp.IsEnabled)
+//                    .Include(cp => cp.PartialCurrencyPairs)
+//                    .ThenInclude(pcp => pcp.Currency)
+//                    .Where(cp => (cp.PartialCurrencyPairs.SingleOrDefault(pcp => pcp.IsMain).Currency
+//                                      .Abbrv // Make sure the first currency (main) is equal to the ticker's first
+//                                  + cp.PartialCurrencyPairs.SingleOrDefault(pcp => !pcp.IsMain).Currency.Abbrv) // other way round
+//                        .Equals(ticker, StringComparison.InvariantCultureIgnoreCase))
+//                    .Include(cp => cp.CurrencySource)
+//                    .Where(cp => cp.CurrencySource != null) // Make sure we have a source
+//                    .Include(cp => cp.CurrencyPairRequests)
+//                        .ThenInclude(cpr => cpr.RequestComponents)
+//                            .ThenInclude(rc => rc.RequestComponentDatum)
+//                    // Make sure there's something
+//                    .Where(cp => cp.CurrencyPairRequests
+//                        .Any(cpr => cpr.RequestComponents.Any(rc => rc.IsEnabled && rc.DeletedAt == null && 
+//                                                                    rc.RequestComponentDatum != null)))
+//                    .Select(cp => new DistinctiveTickerResponse()
+//                    {
+//                        Exchange = cp.CurrencySource.Name,
+//                        ExchangeAbbrv = cp.CurrencySource.Abbreviation,
+//                        LastUpdated = cp.CurrencyPairRequests.FirstOrDefault()
+//                            .RequestComponents.FirstOrDefault()
+//                            .RequestComponentDatum.ModifiedAt,
+//                        Properties = cp.CurrencyPairRequests.FirstOrDefault()
+//                            .RequestComponents
+//                            .Select(rc => new KeyValuePair<string, string>(
+//                                rc.ComponentType.ToString(), 
+//                                rc.RequestComponentDatum.Value))
+//                            .ToList()
+//                    });
+//
+//                // Exchange-based filter
+//                if (!exchangeAbbrv.IsNullOrEmpty())
+//                {
+//                    query = query.Where(cp => cp.ExchangeAbbrv.Equals(exchangeAbbrv));
+//                }
+//
+//                return new NozomiResult<ICollection<DistinctiveTickerResponse>>()
+//                {
+//                    ResultType = NozomiResultType.Success,
+//                    Data = query.ToList()
+//                };
             }
             catch (Exception ex)
             {
