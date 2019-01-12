b93ce8c88fd35f1da6d75c0eea9111bcfd2c3c0f
nixxholas <nicholas@slyck.net>
2019-01-12 11:01:20 +0800

    Nozomi.Infra.Service.Identity: Events: ApiTokenEvent: ApiTokensByUserId: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Infra.Service.Identity/Events/Auth/ApiTokenEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Nozomi.Base.Identity.Models.Identity;
+using Nozomi.Preprocessing.Abstracts;
+using Nozomi.Repo.BCL.Repository;
+using Nozomi.Repo.Identity.Data;
+using Nozomi.Service.Identity.Events.Auth.Interfaces;
+
+namespace Nozomi.Service.Identity.Events.Auth
+{
+    public class ApiTokenEvent : BaseEvent<ApiTokenEvent, NozomiAuthContext>, IApiTokenEvent
+    {
+        public ApiTokenEvent(ILogger<ApiTokenEvent> logger, IUnitOfWork<NozomiAuthContext> unitOfWork) 
+            : base(logger, unitOfWork)
+        {
+        }
+
+        public async Task<ICollection<ApiToken>> ApiTokensByUserId(long userId, bool onlyFunctional = false)
+        {
+            if (onlyFunctional)
+            {
+                return _unitOfWork.GetRepository<ApiToken>()
+                    .Get(at => at.UserId.Equals(userId) && at.IsEnabled && at.DeletedAt == null).ToList();
+            }
+            
+            return _unitOfWork.GetRepository<ApiToken>()
+                .Get(at => at.UserId.Equals(userId)).ToList();
+        }
+    }
+}
---
+++ Nozomi.Infra.Service.Identity/Events/Auth/Interfaces/IApiTokenEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nozomi.Base.Identity.Models.Identity;
+
+namespace Nozomi.Service.Identity.Events.Auth.Interfaces
+{
+    public interface IApiTokenEvent
+    {
+        Task<ICollection<ApiToken>> ApiTokensByUserId(long userId, bool onlyFunctional = false);
+    }
+}
