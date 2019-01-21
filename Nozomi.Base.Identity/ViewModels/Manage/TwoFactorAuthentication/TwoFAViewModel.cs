f81427153daf9a3df4d1e088a1baccf07219087d
nixxholas <nixholas@icloud.com>
2019-01-22 01:35:35 +0800

    Nozomi.Base.Identity: ViewModels: Manage: TwoFactorAuthentication: TwoFAViewModel: Merger update
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Identity/ViewModels/Manage/TwoFactorAuthentication/TwoFAViewModel.cs
+++ Nozomi.Base.Identity/ViewModels/Manage/TwoFactorAuthentication/TwoFAViewModel.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Nozomi.Base.Core;
 
 namespace Nozomi.Base.Identity.ViewModels.Manage.TwoFactorAuthentication
 {
     public class TwoFAViewModel : BaseViewModel
     {
+
+        public string SharedKey { get; set; }
+
+        public string AuthenticatorUri { get; set; }
+
+        [TempData]
+        public string[] RecoveryCodes { get; set; }
         public bool HasAuthenticator { get; set; }
 
         public int RecoveryCodesLeft { get; set; }
@@ -11,5 +20,11 @@ namespace Nozomi.Base.Identity.ViewModels.Manage.TwoFactorAuthentication
         public bool Is2faEnabled { get; set; }
 
         public bool IsMachineRemembered { get; set; }
+        
+        [Required]
+        [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Verification Code")]
+        public string Code { get; set; }
     }
 }
