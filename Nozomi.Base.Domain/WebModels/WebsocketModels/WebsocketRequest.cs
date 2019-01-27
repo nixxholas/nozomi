bd12c0f66587a7b90fce207721ad07ba0e626db8
nixxholas <nixholas@icloud.com>
2019-01-27 15:14:21 +0800

    Nozomi.Repo: Data: Mappings: WebModels: WebsocketModels: WebsocketRequestMap: Mapping
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/WebModels/WebsocketModels/WebsocketRequest.cs
+++ Nozomi.Base.Domain/WebModels/WebsocketModels/WebsocketRequest.cs
@@ -5,8 +5,6 @@ namespace Nozomi.Data.WebModels.WebsocketModels
 {
     public class WebsocketRequest : CurrencyPairRequest
     {
-        
-        
         public ICollection<WebsocketCommand> WebsocketCommands { get; set; }
     }
 }
---
+++ Nozomi.Repo/Data/Mappings/WebModels/WebsocketModels/WebsocketRequestMap.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Base.Core.Helpers.Mapping;
+using Nozomi.Data.WebModels.WebsocketModels;
+
+namespace Nozomi.Repo.Data.Mappings.WebModels.WebsocketModels
+{
+    public class WebsocketRequestMap : BaseMap<WebsocketRequest>
+    {
+        public WebsocketRequestMap(EntityTypeBuilder<WebsocketRequest> entityTypeBuilder) : base(entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(wsr => wsr.Id).HasName("WebsocketRequest_PK_Id");
+            entityTypeBuilder.Property(wsr => wsr.Id).ValueGeneratedOnAdd();
+
+            entityTypeBuilder.HasMany(wsr => wsr.WebsocketCommands)
+                .WithOne(wsc => wsc.WebsocketRequest).HasForeignKey(wsc => wsc.WebsocketRequestId);
+        }
+    }
+}
