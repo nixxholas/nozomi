c0457e3b8122a5aa838986878feda1bdb662310b
nixxholas <nixholas@icloud.com>
2019-01-27 20:23:52 +0800

    Nozomi.Repo: Migrations: r3 - RequestComponentIdentifier Migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20190127122315_r3_RequestComponentIdentifier.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r3_RequestComponentIdentifier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identifier",
+                table: "RequestComponents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "RequestComponents");
+        }
+    }
+}
