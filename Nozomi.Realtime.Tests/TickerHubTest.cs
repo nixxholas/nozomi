f96ea8a5e903cb41ef50e0510af939a45f7df896
nixxholas <nicholas@slyck.net>
2019-02-02 12:34:22 +0800

    Nozomi.Realtime.Tests: TickerHubTest: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Realtime.Tests/TickerHubTest.cs
+++ Nozomi.Realtime.Tests/TickerHubTest.cs
@@ -1,4 +1,9 @@
 using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Nozomi.Realtime.Infra.Service.Hubs;
+using Nozomi.Service.Events.Interfaces;
+using Nozomi.Service.Services.Interfaces;
 using Xunit;
 
 namespace Nozomi.Realtime.Tests
@@ -6,8 +11,14 @@ namespace Nozomi.Realtime.Tests
     public class TickerHubTest
     {
         [Fact]
-        public void Test1()
+        public void ConnectionTest()
         {
+            var mockTickerEvent = new Mock<ITickerEvent>();
+            var mockCurrencyPairService = new Mock<ICurrencyPairService>();
+            var mockLogger = new Mock<ILogger<TickerHub>>(); 
+                
+            var hub = new TickerHub(mockTickerEvent.Object, mockCurrencyPairService.Object, mockLogger.Object);
+            
         }
     }
 }
