9a93adf76a1c9e27c7680e65f0890d8feb45e042
nixxholas <nicholas@slyck.net>
2019-02-02 09:51:51 +0800

    Source: Nozomi.Realtime.Tests: Unit Test project implementation
    We need this in order to properly backtest all SignalR-related functionalities.
    
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Realtime.Tests/TickerHubTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Nozomi.Realtime.Tests
+{
+    public class TickerHubTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
