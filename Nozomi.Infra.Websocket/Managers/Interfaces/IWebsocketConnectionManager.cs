8fb077c51461c6dcb77805697e482d8caa996e40
nixxholas <nicholas@slyck.net>
2019-02-04 18:52:48 +0800

    Nozomi.Infra.Websocket: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Infra.Websocket/Managers/Interfaces/IWebsocketConnectionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace Nozomi.Infra.Websocket.Managers.Interfaces
+{
+    public interface IWebsocketConnectionManager
+    {
+        WebSocket GetSocketById(string id);
+
+        ConcurrentDictionary<string, WebSocket> GetAll();
+
+        string GetId(WebSocket socket);
+
+        void AddSocket(WebSocket socket);
+
+        Task RemoveSocket(string socketId);
+
+        string CreateConnectionId();
+    }
+}
