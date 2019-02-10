52c18a203cf83438f60a2816cd555a64943a28c3
nixxholas <nixholas@icloud.com>
2019-02-10 19:53:21 +0800

    Nozomi.Repo: Data: Mappings: WebModels: RcdHistoricItemMap: DbContext Mapping
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Data/Mappings/WebModels/RcdHistoricItemMap.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Base.Core.Helpers.Mapping;
+using Nozomi.Data.WebModels;
+
+namespace Nozomi.Repo.Data.Mappings.WebModels
+{
+    public class RcdHistoricItemMap : BaseMap<RcdHistoricItem>
+    {
+        
+        public RcdHistoricItemMap(EntityTypeBuilder<RcdHistoricItem> entityTypeBuilder) : base(entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(rcdhi => rcdhi.Id).HasName("RcdHistoricItem_PK_Id");
+            entityTypeBuilder.Property(rcdhi => rcdhi.Id).ValueGeneratedOnAdd();
+            
+            entityTypeBuilder.Property(rcdhi => rcdhi.Value).HasDefaultValue(string.Empty);
+
+            entityTypeBuilder.HasOne(rcdhi => rcdhi.RequestComponentDatum)
+                .WithMany(rcd => rcd.RcdHistoricItems)
+                .HasForeignKey(rcdhi => rcdhi.RequestComponentDatumId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
