141f541031ef635ed855539a5719b0bb2134966a
nixxholas <nixholas@icloud.com>
2019-02-24 04:15:27 +0800

    Nozomi.Infra.Service: Events: Memory: HistoricalDataEvent: Optimize
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Events/Memory/HistoricalDataEvent.cs
+++ Nozomi.Infra.Service/Events/Memory/HistoricalDataEvent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Nozomi.Data.CurrencyModels;
 using Nozomi.Data.ResponseModels;
@@ -14,71 +15,55 @@ namespace Nozomi.Service.Events.Memory
 {
     public class HistoricalDataEvent : BaseEvent<HistoricalDataEvent, NozomiDbContext>, IHistoricalDataEvent
     {
-        public HistoricalDataEvent(ILogger<HistoricalDataEvent> logger, IUnitOfWork<NozomiDbContext> unitOfWork) 
+        public HistoricalDataEvent(ILogger<HistoricalDataEvent> logger, IUnitOfWork<NozomiDbContext> unitOfWork)
             : base(logger, unitOfWork)
         {
         }
-        
+
         public ICollection<DistinctiveCurrencyResponse> GetSimpleCurrencyHistory(long sourceId, long days = 7)
         {
-            if (!_unitOfWork.GetRepository<Source>().GetQueryable().Any(s => s.Id.Equals(sourceId))) 
+            if (!_unitOfWork.GetRepository<Source>().GetQueryable().Any(s => s.Id.Equals(sourceId)))
                 return new List<DistinctiveCurrencyResponse>();
 
-//            var waprice = NozomiServiceConstants.Sources
-//                .SingleOrDefault(s => s.Id.Equals(sourceId))
-//                ?.Currencies
-//                .Select(c => c.PartialCurrencyPairs
-//                .Select(pcp => pcp.CurrencyPair)
-//                .SelectMany(cp => cp.CurrencyPairRequests
-//                    .SelectMany(cpr => cpr.RequestComponents
-//                        .SelectMany(rc => rc.RequestComponentDatum
-//                            .RcdHistoricItems
-//                            .Where(rcdhi => rcdhi.CreatedAt >
-//                                            DateTime.UtcNow.Subtract(TimeSpan.FromDays(days)))
-//                            .Select(rcdhi => decimal.Parse(rcdhi.Value))
-//                            .DefaultIfEmpty(0))
-//                    ))
-//                .Average())
-//            ;
-            
             return _unitOfWork.GetRepository<Source>().GetQueryable()
-                .SingleOrDefault(s => s.Id.Equals(sourceId))
-                ?.Currencies
-                .Select(c => new DistinctiveCurrencyResponse
-                    {
-                        Name = c.Name,
-                        Abbreviation = c.Abbrv,
-                        LastUpdated = c.ModifiedAt,
-                        WeeklyAvgPrice = c.PartialCurrencyPairs
-                            .Select(pcp => pcp.CurrencyPair)
-                            .SelectMany(cp => cp.CurrencyPairRequests
-                                .SelectMany(cpr => cpr.RequestComponents
-                                    .Where(rc => 
-                                        rc.ComponentType.Equals(ComponentType.Ask)
-                                    || rc.ComponentType.Equals(ComponentType.Bid))
-                                    .SelectMany(rc => rc.RequestComponentDatum
-                                        .RcdHistoricItems
-                                        .Where(rcdhi => rcdhi.CreatedAt >
-                                                        DateTime.UtcNow.Subtract(TimeSpan.FromDays(days)))
-                                        .Select(rcdhi => decimal.Parse(rcdhi.Value))
-                                        .DefaultIfEmpty(0))
-                                ))
-                            .Average(),
-                        DailyVolume = c.PartialCurrencyPairs
-                            .Select(pcp => pcp.CurrencyPair)
-                            .SelectMany(cp => cp.CurrencyPairRequests
-                                .SelectMany(cpr => cpr.RequestComponents
-                                    .Where(rc => rc.ComponentType.Equals(ComponentType.VOLUME))
-                                    .SelectMany(rc => rc.RequestComponentDatum
-                                        .RcdHistoricItems
-                                        .Where(rcdhi => rcdhi.CreatedAt >
-                                                        DateTime.UtcNow.Subtract(TimeSpan.FromDays(days)))
-                                        .Select(rcdhi => decimal.Parse(rcdhi.Value))
-                                        .DefaultIfEmpty(0))
-                                ))
-                            .Sum()
-                    })
-                .ToList() ?? null;
+                       .Include(s => s.Currencies)
+                       .ThenInclude(c => c.PartialCurrencyPairs)
+                       .ThenInclude(pcp => pcp.CurrencyPair)
+                       .ThenInclude(cp => cp.CurrencyPairRequests)
+                       .ThenInclude(cpr => cpr.RequestComponents)
+                       .ThenInclude(rc => rc.RequestComponentDatum)
+                       .ThenInclude(rcd => rcd.RcdHistoricItems)
+                       .SingleOrDefault(s => s.Id.Equals(sourceId))
+                       ?.Currencies
+                       .Select(c => new DistinctiveCurrencyResponse
+                       {
+                           Name = c.Name,
+                           Abbreviation = c.Abbrv,
+                           LastUpdated = c.ModifiedAt,
+                           WeeklyAvgPrice = c.PartialCurrencyPairs
+                               .Select(pcp => pcp.CurrencyPair)
+                               .SelectMany(cp => cp.CurrencyPairRequests
+                                   .SelectMany(cpr => cpr.RequestComponents
+                                       .Where(rc =>
+                                           rc.ComponentType.Equals(ComponentType.Ask)
+                                           || rc.ComponentType.Equals(ComponentType.Bid))
+                                       .SelectMany(rc => rc.RequestComponentDatum
+                                           .RcdHistoricItems
+                                           .Where(rcdhi => rcdhi.CreatedAt >
+                                                           DateTime.UtcNow.Subtract(TimeSpan.FromDays(days)))
+                                           .Select(rcdhi => decimal.Parse(rcdhi.Value))
+                                           .DefaultIfEmpty(0))
+                                   ))
+                               .Average(),
+                           DailyVolume = c.PartialCurrencyPairs
+                               .Select(pcp => pcp.CurrencyPair)
+                               .SelectMany(cp => cp.CurrencyPairRequests
+                                   .SelectMany(cpr => cpr.RequestComponents
+                                       .Where(rc => rc.ComponentType.Equals(ComponentType.VOLUME))
+                                       .Select(rc => decimal.Parse(rc.RequestComponentDatum.Value))))
+                               .FirstOrDefault()
+                       })
+                       .ToList() ?? null;
         }
     }
 }
