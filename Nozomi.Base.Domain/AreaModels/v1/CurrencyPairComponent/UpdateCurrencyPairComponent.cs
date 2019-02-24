ea3265ecbcf963614a8e2da3187278a22202f741
nixxholas <nixholas@icloud.com>
2019-02-24 11:56:56 +0800

    Revert "Nozomi.Base.Domain: AreaModels: v1: CurrencyPairComponent: UpdateCurrencyPairComponent: I think we're missing a parameter here"
    This reverts commit 6bff97a8fb206ba6ab43e2cb0981cb759fbf54d6.

--- Nozomi.Base.Domain/AreaModels/v1/CurrencyPairComponent/UpdateCurrencyPairComponent.cs
+++ Nozomi.Base.Domain/AreaModels/v1/CurrencyPairComponent/UpdateCurrencyPairComponent.cs
@@ -4,8 +4,6 @@ namespace Nozomi.Data.AreaModels.v1.CurrencyPairComponent
     {
         public long Id { get; set; }
         
-        public string QueryComponent { get; set; }
-        
         public bool ToBeDeleted()
         {
             return RequestId.Equals(0);
