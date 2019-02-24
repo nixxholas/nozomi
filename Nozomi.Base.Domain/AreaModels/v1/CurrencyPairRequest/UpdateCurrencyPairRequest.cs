2f431922a6f690df708d0f829fe5258658b8edf1
nixxholas <nixholas@icloud.com>
2019-02-24 12:16:02 +0800

    Nozomi.Base.Domain: AreaModels: v1: CurrencyPairRequest: UpdateCurrencyPairRequest: Utilize inheritance
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/AreaModels/v1/CurrencyPairRequest/UpdateCurrencyPairRequest.cs
+++ Nozomi.Base.Domain/AreaModels/v1/CurrencyPairRequest/UpdateCurrencyPairRequest.cs
@@ -6,22 +6,14 @@ using Nozomi.Data.WebModels;
 
 namespace Nozomi.Data.AreaModels.v1.CurrencyPairRequest
 {
-    public class UpdateCurrencyPairRequest
+    public class UpdateCurrencyPairRequest : CreateCurrencyPairRequest
     {
         public long Id { get; set; }
         
-        public long CurrencyPairId { get; set; }
-        
-        public RequestType RequestType { get; set; }
-        
-        public string DataPath { get; set; }
-        
-        public int Delay { get; set; }
-        
         public bool IsEnabled { get; set; }
         
-        public ICollection<UpdateCurrencyPairComponent> CurrencyPairComponents { get; set; }
+        public new ICollection<UpdateCurrencyPairComponent> RequestComponents { get; set; }
         
-        public ICollection<UpdateRequestProperty> RequestProperties { get; set; }
+        public new ICollection<UpdateRequestProperty> RequestProperties { get; set; }
     }
 }
