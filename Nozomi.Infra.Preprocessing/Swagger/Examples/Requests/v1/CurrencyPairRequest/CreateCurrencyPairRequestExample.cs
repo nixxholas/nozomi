5be785f318e2eddd1f94fab2b9ae9a1ede544fe8
nixxholas <nixholas@icloud.com>
2019-02-24 12:10:30 +0800

    Nozomi.Infra.Preprocessing: Swagger: Examples: Requests: v1: CurrencyPairRequest: CreateCurrencyPairRequestExample
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Infra.Preprocessing/Swagger/Examples/Requests/v1/CurrencyPairRequest/CreateCurrencyPairRequestExample.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Nozomi.Data.AreaModels.v1.CurrencyPairComponent;
+using Nozomi.Data.AreaModels.v1.CurrencyPairRequest;
+using Nozomi.Data.AreaModels.v1.RequestProperty;
+using Nozomi.Data.CurrencyModels;
+using Nozomi.Data.WebModels;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Nozomi.Preprocessing.Swagger.Examples.Requests.v1.CurrencyPairRequest
+{
+    public class CreateCurrencyPairRequestExample : IExamplesProvider<CreateCurrencyPairRequest>
+    {
+        public CreateCurrencyPairRequest GetExamples()
+        {
+            return new CreateCurrencyPairRequest()
+            {
+                CurrencyPairId = 1,
+                RequestType = RequestType.HttpPost,
+                DataPath = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
+                Delay = 3600000,
+                RequestComponents = new List<CreateCurrencyPairComponent>()
+                {
+                    new CreateCurrencyPairComponent()
+                    {
+                        ComponentType = ComponentType.Ask,
+                        QueryComponent = "Ask"
+                    }
+                },
+                RequestProperties = new List<CreateRequestProperty>()
+                {
+                    new CreateRequestProperty()
+                    {
+                        RequestPropertyType = RequestPropertyType.HttpBody,
+                        Key = "",
+                        Value = "Test"
+                    }
+                }
+            };
+        }
+    }
+}
