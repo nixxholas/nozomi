34f43684d9b32c6e9277abbc2c63499348fea40a
nixxholas <nixholas@icloud.com>
2019-02-24 12:48:59 +0800

    Nozomi.Infra.Preprocessing: Swagger: Examples: Requests: v1: RequestProperty: CreateRequestPropertyExample
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Infra.Preprocessing/Swagger/Examples/Requests/v1/RequestProperty/CreateRequestPropertyExample.cs
@@ -0,0 +1,19 @@
+using Nozomi.Data.AreaModels.v1.RequestProperty;
+using Nozomi.Data.WebModels;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Nozomi.Preprocessing.Swagger.Examples.Requests.v1.RequestProperty
+{
+    public class CreateRequestPropertyExample : IExamplesProvider<CreateRequestProperty>
+    {
+        public CreateRequestProperty GetExamples()
+        {
+            return new CreateRequestProperty()
+            {
+                RequestPropertyType = RequestPropertyType.HttpQuery,
+                Key = "Hello",
+                Value = "This is le value"
+            };
+        }
+    }
+}
