03e1a84f88cae6b9b98b36f9bc83dd3a9b70352c
nixxholas <nixholas@icloud.com>
2019-02-24 12:25:31 +0800

    Nozomi.Infra.Preprocessing: Swagger: Examples: Requests: v1: CurrencySource: UpdateSourceExample
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Infra.Preprocessing/Swagger/Examples/Requests/v1/CurrencySource/UpdateSourceExample.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Nozomi.Data.AreaModels.v1.CurrencySource;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Nozomi.Preprocessing.Swagger.Examples.Requests.v1.CurrencySource
+{
+    public class UpdateSourceExample : IExamplesProvider<UpdateSource>
+    {
+        public UpdateSource GetExamples()
+        {
+            return new UpdateSource()
+            {
+                Id = 1,
+                Name = "Bittrex",
+                Abbreviation = "BTRX",
+                ApiDocsUrl = "https://bittrex.zendesk.com/hc/en-us/articles/115003723911",
+                UpdateCurrencyPairs = new List<UpdateSource.UpdateCurrencyPair>()
+                {
+                    new UpdateSource.UpdateCurrencyPair()
+                    {
+                        Id = 1
+                    }
+                },
+                UpdateSourceCurrencies = new List<UpdateSource.UpdateSourceCurrency>()
+                {
+                    new UpdateSource.UpdateSourceCurrency()
+                    {
+                        Id = 3
+                    }
+                }
+            };
+        }
+    }
+}
