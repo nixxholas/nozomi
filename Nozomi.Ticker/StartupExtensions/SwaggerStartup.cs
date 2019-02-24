ecb9e7dd24151a9edd448cd0c2a20b667c8de55e
nixxholas <nixholas@icloud.com>
2019-02-24 13:31:25 +0800

    Source: Downgrade Swashbuckle for compatibility, enable all examples.
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/StartupExtensions/SwaggerStartup.cs
+++ Nozomi.Ticker/StartupExtensions/SwaggerStartup.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using Nozomi.Preprocessing.Swagger.Examples.Requests.v1.Currency;
 using Nozomi.Preprocessing.Swagger.Examples.Requests.v1.CurrencyPair;
 using Nozomi.Preprocessing.Swagger.Examples.Requests.v1.CurrencyPairComponent;
@@ -13,6 +12,7 @@ using Nozomi.Preprocessing.Swagger.Examples.Responses.Generic;
 using Nozomi.Ticker.Areas;
 using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.Swagger;
+using License = Swashbuckle.AspNetCore.Swagger.License;
 using UpdateCurrencyPairComponentExample = Nozomi.Preprocessing.Swagger.Examples.Requests.v1.CurrencyPairComponent.UpdateCurrencyPairComponentExample;
 
 namespace Nozomi.Ticker.StartupExtensions
@@ -21,6 +21,42 @@ namespace Nozomi.Ticker.StartupExtensions
     {
         public static void ConfigureSwagger(this IServiceCollection services)
         {
+            services.AddSwaggerGen(swaggerGenOptions =>
+            {
+                swaggerGenOptions.SwaggerDoc(GlobalApiVariables.CURRENT_API_VERSION, new Info()
+                {
+                    Version = GlobalApiVariables.CURRENT_API_VERSION,
+                    Title = "Nozomi API",
+                    Description = "Reference documentation for the usage of Nozomi.",
+                    TermsOfService = new Uri("https://detabox.com/privacy").ToString(),
+                    Contact = new Contact
+                    {
+                        Name = "Nicholas Chen",
+                        Email = "nicholas@counter.network",
+                        Url = new Uri("https://twitter.com/nixxholas").ToString()
+                    },
+                    License = new License
+                    {
+                        Name = "Copyright (C) Hayate Inc. - All Rights Reserved",
+                        Url = new Uri("https://detabox.com/license").ToString()
+                    }
+                });
+
+
+                swaggerGenOptions.ExampleFilters();
+                swaggerGenOptions.OperationFilter<DescriptionOperationFilter>();
+                // Adds an Upload button to endpoints which have [AddSwaggerFileUploadButton]
+                swaggerGenOptions.OperationFilter<AddFileParamTypesOperationFilter>(); 
+                swaggerGenOptions.OperationFilter<AddHeaderOperationFilter>();
+                swaggerGenOptions.OperationFilter<AddResponseHeadersFilter>(); // [SwaggerResponseHeader]
+                swaggerGenOptions.DescribeAllEnumsAsStrings();
+                // Adds "(Auth)" to the summary so that you can see which endpoints have Authorization
+                swaggerGenOptions.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+                swaggerGenOptions.OperationFilter<SecurityRequirementsOperationFilter>();
+                
+                swaggerGenOptions.EnableAnnotations();
+            });
+            
             // https://github.com/mattfrear/Swashbuckle.AspNetCore.Filters/issues/56
             // services.AddSwaggerExamplesFromAssemblyOf<MyExample>();
             services.AddSwaggerExamplesFromAssemblyOf<CreateCurrencyExample>();
@@ -38,35 +74,6 @@ namespace Nozomi.Ticker.StartupExtensions
             services.AddSwaggerExamplesFromAssemblyOf<UpdateRequestPropertyExample>();
             services.AddSwaggerExamplesFromAssemblyOf<NozomiJsonResultExample>();
             services.AddSwaggerExamplesFromAssemblyOf<NozomiStringResultExample>();
-            
-            services.AddSwaggerGen(swaggerGenOptions =>
-            {
-                swaggerGenOptions.SwaggerDoc(GlobalApiVariables.CURRENT_API_VERSION, new OpenApiInfo()
-                 {
-                     Version = GlobalApiVariables.CURRENT_API_VERSION,
-                     Title = "Nozomi API",
-                     Description = "Reference documentation for the usage of Nozomi.",
-                     TermsOfService = new Uri("https://detabox.com/privacy"),
-                     Contact = new OpenApiContact
-                     {
-                         Name = "Nicholas Chen",
-                         Email = "nicholas@counter.network",
-                         Url = new Uri("https://twitter.com/nixxholas")
-                     },
-                     License = new OpenApiLicense
-                     {
-                         Name = "Copyright (C) Hayate Inc. - All Rights Reserved",
-                         Url = new Uri("https://detabox.com/license")
-                     }
-                 });
-                    
-                swaggerGenOptions.OperationFilter<AppendAuthorizeToSummaryOperationFilter>(); // Adds "(Auth)" to the summary so that you can see which endpoints have Authorization
-                swaggerGenOptions.OperationFilter<AddFileParamTypesOperationFilter>(); // Adds an Upload button to endpoints which have [AddSwaggerFileUploadButton]
-                swaggerGenOptions.OperationFilter<AddResponseHeadersFilter>(); // [SwaggerResponseHeader]
-                 
-                swaggerGenOptions.ExampleFilters();
-                swaggerGenOptions.EnableAnnotations();
-             });
          }
      }
  }
