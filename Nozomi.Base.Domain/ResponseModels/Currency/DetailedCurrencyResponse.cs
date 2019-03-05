a34a30c53ce2d18068e92315f97d2aa144b5ca25
nixxholas <nicholas@slyck.net>
2019-03-06 06:31:55 +0800

    Nozomi.Base.Domain: ResponseModels: Currency: DetailedCurrencyResponse: ConfigureHistoricals implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/ResponseModels/Currency/DetailedCurrencyResponse.cs
+++ Nozomi.Base.Domain/ResponseModels/Currency/DetailedCurrencyResponse.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using Nozomi.Base.Core;
 using Nozomi.Data.Models.Currency;
+using Nozomi.Data.Models.Web;
 using Nozomi.Data.ResponseModels.RequestComponent;
 
 namespace Nozomi.Data.ResponseModels.Currency
 {
+    /// <summary>
+    /// Detailed Currency Response specific to a currency.
+    /// </summary>
     public class DetailedCurrencyResponse : DistinctiveCurrencyResponse
     {
         public DetailedCurrencyResponse()
@@ -40,7 +45,8 @@ namespace Nozomi.Data.ResponseModels.Currency
                                 .FirstOrDefault())
                             .FirstOrDefault().ModifiedAt)
                         .FirstOrDefault();
-                    
+                    ConfigureHistoricals(currencyPairs.ToList());
+
                     // TODO: take conversion rates into account
 //                    WeeklyAvgPrice = currencyPairs
 //                        .SelectMany(cp => cp.PartialCurrencyPairs)
@@ -57,7 +63,7 @@ namespace Nozomi.Data.ResponseModels.Currency
 //                        .Select(rcdhi => decimal.Parse(rcdhi.Value))
 //                        .DefaultIfEmpty()
 //                        .Average();
-                    
+
                     // TODO: take conversion rates into account
 //                    DailyVolume = currencyPairs
 //                        .SelectMany(cp => cp.PartialCurrencyPairs)
@@ -73,13 +79,6 @@ namespace Nozomi.Data.ResponseModels.Currency
 //                        .DefaultIfEmpty()
 //                        .Sum();
 
-                    // TODO: Resolve NRE
-                    var hist = currencyPairs
-                        .SelectMany(cp => cp.PartialCurrencyPairs)
-                        .Select(pcp => pcp.CurrencyPair)
-                        .SelectMany(cp => cp.CurrencyPairRequests)
-                        .SelectMany(cpr => cpr.RequestComponents);
-                    
 //                    Historical = currencyPairs
 //                        .SelectMany(cp => cp.PartialCurrencyPairs)
 //                        .Select(pcp => pcp.CurrencyPair)
@@ -103,6 +102,10 @@ namespace Nozomi.Data.ResponseModels.Currency
         {
             if (currencyPairs != null && currencyPairs.Any())
             {
+                #if DEBUG
+                var cPairs = currencyPairs.ToList();
+                #endif
+                
                 var curr = currencyPairs
                     .Select(cp => cp.PartialCurrencyPairs
                         .Select(pcp => pcp.Currency)
@@ -121,29 +124,39 @@ namespace Nozomi.Data.ResponseModels.Currency
                                 .FirstOrDefault())
                             .FirstOrDefault().ModifiedAt)
                         .FirstOrDefault();
+                    ConfigureHistoricals(currencyPairs.ToList());
+                }
+            }
+        }
 
-                    try
+        private void ConfigureHistoricals(List<Models.Currency.CurrencyPair> currencyPairs)
+        {
+            Historical = new Dictionary<ComponentType, List<ComponentHistoricalDatum>>();
+            
+            foreach (var reqComp in currencyPairs
+                .SelectMany(cp => cp.CurrencyPairRequests
+                    .SelectMany(cpr => cpr.RequestComponents)))
+            {
+                if (reqComp.RequestComponentDatum != null &&
+                    reqComp.RequestComponentDatum.RcdHistoricItems != null &&
+                    reqComp.RequestComponentDatum.RcdHistoricItems.Count > 0)
+                {
+                    var rcdhiList = reqComp.RequestComponentDatum.RcdHistoricItems;
+
+                    foreach (var rcdhi in rcdhiList)
                     {
-                        // TODO: Resolve NRE
-                        Historical = currencyPairs
-                            .SelectMany(cp => cp.PartialCurrencyPairs
-                                .Select(pcp => pcp.CurrencyPair))
-                            .SelectMany(cp => cp.CurrencyPairRequests)
-                            .SelectMany(cpr => cpr.RequestComponents)
-                            .ToDictionary(rc => rc.ComponentType,
-                                rc => rc.RequestComponentDatum
-                                    .RcdHistoricItems
-                                    .DefaultIfEmpty()
-                                    .Select(rcdhi => new ComponentHistoricalDatum
-                                    {
-                                        CreatedAt = rcdhi.CreatedAt,
-                                        Value = rcdhi.Value
-                                    })
-                                    .ToList());
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
+                        if (Historical.ContainsKey(reqComp.ComponentType))
+                        {
+                            Historical[reqComp.ComponentType].Add(new ComponentHistoricalDatum
+                            {
+                                CreatedAt = rcdhi.HistoricDateTime,
+                                Value = rcdhi.Value
+                            });
+                        }
+                        else
+                        {
+                            Historical.Add(reqComp.ComponentType, new List<ComponentHistoricalDatum>());
+                        }
                     }
                 }
             }
