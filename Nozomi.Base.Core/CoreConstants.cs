85c414bf5e4a157ca95a2b77c8d92753957d26e7
nixxholas <nicholas@slyck.net>
2019-03-14 07:29:29 +0800

    Nozomi.Base.Core: CoreConstants: GenericCurrency: Introduction
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Core/CoreConstants.cs
+++ Nozomi.Base.Core/CoreConstants.cs
@@ -2,6 +2,7 @@ namespace Nozomi.Base.Core
 {
     public static class CoreConstants
     {
+        public const string GenericCurrency = "USD";
         public const string GenericCounterCurrency = "USD";
     }
 }
