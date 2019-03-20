816e0789c5f11b142c1d09345aba402d0b158f42
nixxholas <nicholas@slyck.net>
2019-03-21 06:07:53 +0800

    Nozomi.Infra.Preprocessing.SignalR: Hubs: Interfaces: INozomiStreamClient: Prepare to stream currencies
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Preprocessing.SignalR/Hubs/Interfaces/INozomiStreamClient.cs
+++ Nozomi.Infra.Preprocessing.SignalR/Hubs/Interfaces/INozomiStreamClient.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Nozomi.Data.ResponseModels;
+using Nozomi.Data.ResponseModels.Currency;
 using Nozomi.Data.ResponseModels.Ticker;
 
 namespace Nozomi.Infra.Preprocessing.SignalR.Hubs.Interfaces
 {
     public interface INozomiStreamClient
     {
+        Task Currencies(ICollection<DetailedCurrencyResponse> currencies);
         /// <summary>
         /// Dispatches 
         /// </summary>
