e37c2ba9dd11744a600903432a1a220817ba1ede
nixxholas <nixholas@icloud.com>
2019-03-23 16:44:34 +0800

    Nozomi.Infra.Service: Events: CurrencyPairEvent: GetAllByCounterCurrency: Initial Implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Events/CurrencyPairEvent.cs
+++ Nozomi.Infra.Service/Events/CurrencyPairEvent.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Nozomi.Base.Core;
 using Nozomi.Data.Models.Currency;
@@ -18,7 +21,15 @@ namespace Nozomi.Service.Events
 
         public ICollection<CurrencyPair> GetAllByCounterCurrency(string counterCurrencyAbbrv = CoreConstants.GenericCounterCurrency)
         {
-            throw new System.NotImplementedException();
+            return _unitOfWork.GetRepository<CurrencyPair>()
+                .GetQueryable()
+                .AsNoTracking()
+                .Where(cp => cp.DeletedAt == null && cp.IsEnabled)
+                .Include(cp => cp.PartialCurrencyPairs)
+                .ThenInclude(pcp => pcp.Currency)
+                .Where(pcp => pcp.PartialCurrencyPairs.FirstOrDefault(cpcp => !cpcp.IsMain).Currency.Abbrv
+                    .Equals(counterCurrencyAbbrv, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
         }
     }
 }
