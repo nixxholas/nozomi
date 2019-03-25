81976f1f165c95bb11b62f7be06ba184a78f0c53
nixxholas <nixholas@icloud.com>
2019-03-25 23:13:13 +0800

    Nozomi.Ticker: Areas: Currency: View
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/Areas/CurrencyController.cs
+++ Nozomi.Ticker/Areas/CurrencyController.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Service.Events.Interfaces;
 using Nozomi.Service.Identity.Managers;
 
 namespace Nozomi.Ticker.Areas
 {
     public class CurrencyController : BaseViewController<CurrencyController>
     {
+        private ICurrencyEvent _currencyEvent { get; set; }
+        
         public CurrencyController(ILogger<CurrencyController> logger, NozomiSignInManager signInManager, 
-            NozomiUserManager userManager) 
+            NozomiUserManager userManager, ICurrencyEvent currencyEvent) 
             : base(logger, signInManager, userManager)
         {
+            _currencyEvent = currencyEvent;
         }
 
         [Route("/{abbrv}")]
         public IActionResult View(string abbrv)
         {
+            // First obtain all 'ABBRV' objects first, 
+            var currency = _currencyEvent.Get(abbrv, true);
+            
+            // DTO this sitch
+            
             return View();
         }
     }
