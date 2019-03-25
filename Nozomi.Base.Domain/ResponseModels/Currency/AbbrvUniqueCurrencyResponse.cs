b499f231f35630d7a196ab268c445853536473ac
nixxholas <nicholas@slyck.net>
2019-03-26 06:08:17 +0800

    Nozomi.Base.Domain: ResponseModels: Currency: AbbrvUniqueCurrencyResponse: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Base.Domain/ResponseModels/Currency/AbbrvUniqueCurrencyResponse.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Nozomi.Data.Models.Currency;
+using Nozomi.Data.Models.Web;
+using Nozomi.Data.Models.Web.Analytical;
+
+namespace Nozomi.Data.ResponseModels.Currency
+{
+    public class AbbrvUniqueCurrencyResponse : DistinctiveCurrencyResponse
+    {
+        public AbbrvUniqueCurrencyResponse() {}
+
+        public AbbrvUniqueCurrencyResponse(Models.Currency.Currency currency)
+        {
+            Abbreviation = currency.Abbrv;
+            Name = currency.Name;
+            LastUpdated = currency.ModifiedAt;
+            CurrencyType = currency.CurrencyType;
+            Denominations = currency.Denominations;
+            DenominationName = currency.DenominationName;
+            CurrencySource = currency.CurrencySource;
+            AnalysedComponents = currency.AnalysedComponents;
+            CurrencyRequests = currency.CurrencyRequests;
+            PartialCurrencyPairs = currency.PartialCurrencyPairs;
+        }
+        
+        public CurrencyType CurrencyType { get; set; }
+
+        public int Denominations { get; set; } = 0;
+        
+        public string DenominationName { get; set; }
+
+        public Models.Currency.Source CurrencySource { get; set; }
+        
+        public ICollection<AnalysedComponent> AnalysedComponents { get; set; }
+        
+        public ICollection<CurrencyRequest> CurrencyRequests { get; set; }
+
+        public ICollection<PartialCurrencyPair> PartialCurrencyPairs { get; set; }
+    }
+}
