98c7f2520447ed55ae563bf2adaf988e9da6a1f4
nixxholas <nixholas@icloud.com>
2019-03-26 22:43:04 +0800

    Nozomi.Repo: Migrations: r10 - Currency Property migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20190326144251_r10_CurrencyProperty.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r10_CurrencyProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Currencies",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyProperty",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: false),
+                    ModifiedBy = table.Column<long>(nullable: false),
+                    DeletedBy = table.Column<long>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(nullable: true),
+                    CurrencyId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyProperty", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CurrencyProperty_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyProperty_CurrencyId",
+                table: "CurrencyProperty",
+                column: "CurrencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyProperty");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Currencies");
+        }
+    }
+}
