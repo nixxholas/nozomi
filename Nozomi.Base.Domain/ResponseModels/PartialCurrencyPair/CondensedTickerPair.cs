0fd2d83c2850fd0046160cda348bd6d5744a547e
nixxholas <nicholas@slyck.net>
2019-03-27 06:27:18 +0800

    Nozomi.Base.Domain: ResponseModels: PartialCurrencyPair: CondensedTickerPair: CurrencyPairId implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/ResponseModels/PartialCurrencyPair/CondensedTickerPair.cs
+++ Nozomi.Base.Domain/ResponseModels/PartialCurrencyPair/CondensedTickerPair.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Nozomi.Data.ResponseModels.Source;
 
 namespace Nozomi.Data.ResponseModels.PartialCurrencyPair
 {
     public class CondensedTickerPair
     {
+        /// <summary>
+        /// Utilize CurrencyPairIds to properly construct the object.
+        /// </summary>
+        [HiddenInput(DisplayValue = false)]
+        public long CurrencyPairId { get; set; }
+        
         public string PairAbbreviation { get; set; }
         
         public ICollection<SourceResponse> Sources { get; set; }
