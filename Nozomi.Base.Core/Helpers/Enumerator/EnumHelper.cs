02b03e2953ea7afab4f7b852d0aaefdf7d6d19c0
nixxholas <nicholas@slyck.net>
2019-03-28 06:14:20 +0800

    Nozomi.Base.Core: Helpers: Enumerator: EnumHelper: GetEnumValuesAndDescriptions: Implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Core/Helpers/Enumerator/EnumHelper.cs
+++ Nozomi.Base.Core/Helpers/Enumerator/EnumHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
@@ -56,6 +57,26 @@ namespace Nozomi.Base.Core.Helpers.Enumerator
             }
             throw new ArgumentException("Not found.", "description");
             // or return default(T);
+        }
+        
+        public static List<KeyValuePair<string, int>> GetEnumValuesAndDescriptions<T>()
+        {
+            Type enumType = typeof (T);
+
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T is not System.Enum");
+
+            List<KeyValuePair<string, int>> enumValList = new List<KeyValuePair<string, int>>();
+
+            foreach (var e in Enum.GetValues(typeof(T)))
+            {
+                var fi = e.GetType().GetField(e.ToString());
+                var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                enumValList.Add(new KeyValuePair<string, int>((attributes.Length > 0) ? attributes[0].Description : e.ToString(), (int)e));
+            }
+
+            return enumValList;
         }
     }
 }
