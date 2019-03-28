53d1fa6a28e927dc119c9ccf570ff6fe057e6fa9
nixxholas <nixholas@icloud.com>
2019-03-29 02:13:38 +0800

    Nozomi.Base.Domain: Models: Web: Analytical: AnalysedComponentType: Descriptions
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/Models/Web/Analytical/AnalysedComponentType.cs
+++ Nozomi.Base.Domain/Models/Web/Analytical/AnalysedComponentType.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel;
+
 namespace Nozomi.Data.Models.Web.Analytical
 {
     public enum AnalysedComponentType
     {
         Unknown = 0,
+        [Description("Market Cap")]
         MarketCap = 1,
+        [Description("Price")]
         CurrentAveragePrice = 10,
+        [Description("Change")]
         DailyPriceChange = 50,
+        [Description("Weekly % Change")]
         WeeklyPriceChange = 51,
+        [Description("Monthly % Change")]
         MonthlyPriceChange = 52,
+        [Description("% Change")]
         DailyPricePctChange = 70,
+        [Description("Volume")]
         DailyVolume = 80
     }
 }
