f7ae9c8dbb5e76836ea3a8edd70f527dc6e22f21
nixxholas <nicholas@slyck.net>
2019-03-29 13:21:27 +0800

    Nozomi.Repo: Migrations: r11 - RequestComponent AnomalyIgnorance migration
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Migrations/20190329052113_r11_RequestComponentAnomalyIgnorance.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r11_RequestComponentAnomalyIgnorance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDenominated",
+                table: "RequestComponents",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AnomalyIgnorance",
+                table: "RequestComponents",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnomalyIgnorance",
+                table: "RequestComponents");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDenominated",
+                table: "RequestComponents",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+        }
+    }
+}
