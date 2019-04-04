cf57b3e9756b8bd74fa7b3214aefc2bb7d83a122
nixxholas <nixholas@icloud.com>
2019-04-04 23:54:39 +0800

    Nozomi.Infra.Analysis.Service: Services: AnalysedHistoricItemService: Push API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Analysis.Service/Services/Interfaces/IAnalysedHistoricItemService.cs
+++ Nozomi.Infra.Analysis.Service/Services/Interfaces/IAnalysedHistoricItemService.cs
@@ -5,5 +5,7 @@ namespace Nozomi.Infra.Analysis.Service.Services.Interfaces
     public interface IAnalysedHistoricItemService
     {
         long Create(AnalysedHistoricItem item, long userId = 0);
+
+        bool Push(AnalysedComponent analysedComponent, long userId = 0);
     }
 }
