165f972c20f89e9ab0899028f63a026d8c90748f
nixxholas <nicholas@slyck.net>
2019-04-08 06:14:07 +0800

    Source: Cleanup up statics
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Preprocessing/NozomiServiceConstants.cs
+++ Nozomi.Infra.Preprocessing/NozomiServiceConstants.cs
@@ -17,32 +17,6 @@ namespace Nozomi.Preprocessing
         public const string ApiTokenHeaderKey = "NozApiToken";
         
         /// <summary>
-        /// A dictionary with a key of CurrencySourceId and the Currency pair ticker symbol to return the
-        /// currency pair id in question.
-        /// </summary>
-        /// <returns>ID of the CurrencyPair's Request</returns>
-        public static Dictionary<Tuple<string, string>, long> CurrencySourceSymbolDictionary = 
-            new Dictionary<Tuple<string, string>, long>();
-
-        /// <summary>
-        /// A dictionary with a key of the ticker symbol and the value of an array of currency pairs ids
-        /// </summary>
-        public static Dictionary<string, LinkedList<long>> TickerSymbolDictionary = 
-            new Dictionary<string, LinkedList<long>>();
-        
-//        /// <summary>
-//        /// A dictionary with a key of the currencypairid and the value of the currencypair's ticker response.
-//        /// </summary>
-//        public static Dictionary<long, TickerByExchangeResponse> CurrencyPairDictionary = 
-//            new Dictionary<long, TickerByExchangeResponse>();
-        
-        /// <summary>
-        /// A collection containing ALL tickers unique to their source and ticker abbreviation.
-        /// </summary>
-        public static ICollection<UniqueTickerResponse> UniqueCurrencyPairs = 
-            new LinkedList<UniqueTickerResponse>();
-        
-        /// <summary>
         /// A collection containing ALL active and enabled sources.
         /// </summary>
         public static IEnumerable<Source> Sources { get; set; }
--- Nozomi.Ticker/StartupExtensions/StaticStartup.cs
+++ Nozomi.Ticker/StartupExtensions/StaticStartup.cs
@@ -19,86 +19,14 @@ namespace Nozomi.Ticker.StartupExtensions
     {
         public static void ConfigureStatics(this IApplicationBuilder app)
         {
-            using (var serviceScope = app.ApplicationServices
-                .GetRequiredService<IServiceScopeFactory>()
-                .CreateScope())
-            {
-                using (var context = serviceScope.ServiceProvider.GetService<NozomiDbContext>())
-                {
-                    var currencyPairs = context.CurrencyPairs
-                        .AsNoTracking()
-                        .Where(cp => cp.DeletedAt == null && cp.IsEnabled)
-                        .Include(cp => cp.CurrencySource)
-                        .Include(cp => cp.PartialCurrencyPairs)
-                            .ThenInclude(pcp => pcp.Currency)
-                        .Include(cp => cp.CurrencyPairRequests)
-                        .Where(cp => cp.CurrencyPairRequests.Any(cpr => cpr.DeletedAt == null && cpr.IsEnabled
-                                     && cpr.RequestComponents.Any(rc => rc.DeletedAt == null && rc.IsEnabled))
-                        && cp.PartialCurrencyPairs.Count == 2);
-
-                    foreach (var cp in currencyPairs)
-                    {
-                        var tickerSymbol = string.Join("", cp.PartialCurrencyPairs
-                            .OrderByDescending(pcp => pcp.IsMain)
-                            .Select(pcp => pcp.Currency.Abbrv));
-                            
-                        // Populate the CurrencySourceSymbolDictionary
-                        NozomiServiceConstants.CurrencySourceSymbolDictionary.Add(
-                            new Tuple<string, string>(cp.CurrencySource.Abbreviation, tickerSymbol), 
-                            cp.Id);
-                        
-                        // Populate the TickerSymbolDictionary
-                        
-                        // Check if the ticker exists in the collection
-                        if (NozomiServiceConstants.TickerSymbolDictionary.ContainsKey(tickerSymbol))
-                        {
-                            var longList = NozomiServiceConstants.TickerSymbolDictionary[tickerSymbol];
-
-                            // Make sure the list already has it
-                            if (!longList.Contains(cp.Id))
-                            {
-                                longList.AddLast(cp.Id);
-                            }
-                        }
-                        else // nope, let's add it in
-                        {
-                            NozomiServiceConstants.TickerSymbolDictionary.Add(tickerSymbol,
-                                new LinkedList<long>());
-
-                            NozomiServiceConstants.TickerSymbolDictionary[tickerSymbol].AddLast(cp.Id);
-                        }
-                    }
-
-//                    var dtrList = context.CurrencyPairRequests
-//                        .AsNoTracking()
-//                        .Where(r => r.DeletedAt == null && r.IsEnabled)
-//                        .Include(r => r.RequestComponents)
-//                            .ThenInclude(rc => rc.RequestComponentDatum)
-//                        .Include(r => r.CurrencyPair)
-//                            .ThenInclude(cp => cp.CurrencySource)
-//                        .Where(r => r.RequestComponents.Any(rc => rc.IsEnabled && rc.DeletedAt == null
-//                                                                  && rc.RequestComponentDatum != null))
-//                        .Select(cpr => new TickerByCurrencyPairResponse()
-//                        {
-//                            CurrencyPairId = cpr.CurrencyPairId,
-//                            Exchange = cpr.CurrencyPair.CurrencySource.Name,
-//                            ExchangeAbbrv = cpr.CurrencyPair.CurrencySource.Abbreviation,
-//                            LastUpdated = cpr.RequestComponents.First().RequestComponentDatum.ModifiedAt,
-//                            Properties = cpr.RequestComponents.Select(rc => 
-//                                new KeyValuePair<string,string>(rc.ComponentType.ToString(), 
-//                                    rc.RequestComponentDatum.Value)).ToList()
-//                        });
-//
-//                    foreach (var dtr in dtrList)
-//                    {
-//                        if (!NozomiServiceConstants.CurrencyPairDictionary.ContainsKey(dtr.CurrencyPairId))
-//                        {
-//                            // Add it in, value can be automatically casted down.
-//                            NozomiServiceConstants.CurrencyPairDictionary.Add(dtr.CurrencyPairId, dtr);
-//                        }
-//                    }
-                }
-            }
+//            using (var serviceScope = app.ApplicationServices
+//                .GetRequiredService<IServiceScopeFactory>()
+//                .CreateScope())
+//            {
+//                using (var context = serviceScope.ServiceProvider.GetService<NozomiDbContext>())
+//                {
+//                }
+//            }
         }
     }
 }
