33e046b7eaf279c69671b6ab80582379fb742df0
nixxholas <nixholas@icloud.com>
2019-04-07 23:23:07 +0800

    Nozomi.Repo: Migrations: r12 - Request FailureDelay migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Migrations/20190407151822_r12_RequestFailureDelay.Designer.cs
+++ Nozomi.Repo/Migrations/20190407151822_r12_RequestFailureDelay.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nozomi.Repo.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Nozomi.Repo.Migrations
 {
     [DbContext(typeof(NozomiDbContext))]
-    partial class NozomiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190407151822_r12_RequestFailureDelay")]
+    partial class r12_RequestFailureDelay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
