c5228fd4980502b117d2bc11d4c4d7cb3a7fe361
nixxholas <nicholas@slyck.net>
2019-04-08 06:35:23 +0800

    Nozomi.Base.Domain: Models: Web: Analytical: AnalysedComponentType: Hourly Average Price enum
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/Models/Web/Analytical/AnalysedComponentType.cs
+++ Nozomi.Base.Domain/Models/Web/Analytical/AnalysedComponentType.cs
@@ -7,16 +7,22 @@ namespace Nozomi.Data.Models.Web.Analytical
         Unknown = 0,
         [Description("Market Cap")]
         MarketCap = 1,
+        
         [Description("Price")]
         CurrentAveragePrice = 10,
+        [Description("Hourly Average Price")]
+        HourlyAveragePrice = 11,
+        
         [Description("Change")]
         DailyPriceChange = 50,
         [Description("Weekly % Change")]
         WeeklyPriceChange = 51,
         [Description("Monthly % Change")]
         MonthlyPriceChange = 52,
+        
         [Description("% Change")]
         DailyPricePctChange = 70,
+        
         [Description("Volume")]
         DailyVolume = 80
     }
