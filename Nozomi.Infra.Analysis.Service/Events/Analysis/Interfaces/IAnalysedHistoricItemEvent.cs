17512e526f5629d8b73e727c5a1e434fcdbbb0b7
nixxholas <nixholas@icloud.com>
2019-04-23 23:02:44 +0800

    Nozomi.Infra.Analysis.Service: Events: Analysis: AnalysedHistoricItemEvent: GetAll API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Analysis.Service/Events/Analysis/Interfaces/IAnalysedHistoricItemEvent.cs
+++ Nozomi.Infra.Analysis.Service/Events/Analysis/Interfaces/IAnalysedHistoricItemEvent.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Nozomi.Data.Models.Web.Analytical;
 
 namespace Nozomi.Infra.Analysis.Service.Events.Analysis.Interfaces
@@ -5,5 +7,7 @@ namespace Nozomi.Infra.Analysis.Service.Events.Analysis.Interfaces
     public interface IAnalysedHistoricItemEvent
     {
         AnalysedHistoricItem Latest(long analysedComponentId);
+
+        ICollection<AnalysedHistoricItem> GetAll(long analysedComponentId, TimeSpan since, int page = 0);
     }
 }
