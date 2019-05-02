7eac118e2e03a9bf9a6e709578cedc28d1ef3d6b
elvisyong <elvisyong@gmail.com>
2019-05-03 01:20:04 +0800

    Nozomi.Ticker: Areas: Admin: CurrencyController & View: Create Get Currencies API and view

--- Nozomi.Ticker/Areas/Admin/Controllers/CurrencyController.cs
+++ Nozomi.Ticker/Areas/Admin/Controllers/CurrencyController.cs
@@ -2,7 +2,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Base.Identity.ViewModels.Manage.Currency;
+using Nozomi.Data;
+using Nozomi.Data.AreaModels.v1.Currency;
+using Nozomi.Service.Events.Interfaces;
 using Nozomi.Service.Identity.Managers;
+using Nozomi.Service.Services.Interfaces;
 using Nozomi.Ticker.Controllers;
 
 namespace Nozomi.Ticker.Areas.Admin.Controllers
@@ -12,11 +17,81 @@ namespace Nozomi.Ticker.Areas.Admin.Controllers
     [Authorize(Roles = "Owner, Administrator, Staff")]
     public class CurrencyController : BaseViewController<CurrencyController>
     {
+        private readonly ICurrencyEvent _currencyEvent;
+        private readonly ICurrencyService _currencyService;
+        
         public CurrencyController(ILogger<CurrencyController> logger, NozomiSignInManager signInManager,
-            NozomiUserManager userManager)
+            NozomiUserManager userManager, ICurrencyEvent currencyEvent, ICurrencyService currencyService)
             : base(logger, signInManager, userManager)
         {
+            _currencyEvent = currencyEvent;
+            _currencyService = currencyService;
         }
+
+        #region Get Currencies
+
+        [HttpGet]
+        public async Task<IActionResult> Currencies()
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var vm = new CurrenciesViewModel
+            {
+                Currencies = _currencyEvent.GetAll(true)
+            };
+            
+            return View(vm);
+        }
+        
+        #endregion
+
+        #region POST EditCurrency
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditCurrency(long id, UpdateCurrency updateCurrency)
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (id != updateCurrency.Id)
+            {
+                return BadRequest();
+            }
+
+            if (_currencyService.Update(updateCurrency).ResultType.Equals(NozomiResultType.Success)) return Ok();
+
+            // Update failed.
+            return NotFound();
+        }
+        
+
+        #endregion
+
+        #region Delete DeleteCurrency
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCurrency(long id)
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if(_currencyService.Delete(id).ResultType.Equals(NozomiResultType.Success)) return Ok();
+
+            // Update failed.
+            return NotFound();
+        }
+
+        #endregion
 
         [HttpGet]
         public async Task<IActionResult> CreateCurrency()
