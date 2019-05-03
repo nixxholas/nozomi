4d026c0a5d9418a99e2e193a9ca8121363e324d9
nixxholas <nixholas@icloud.com>
2019-05-03 16:57:06 +0800

    Source: Generalise Build Timestamp
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Core/CoreConstants.cs
+++ Nozomi.Base.Core/CoreConstants.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace Nozomi.Base.Core
 {
     public static class CoreConstants
     {
         public const string GenericCurrency = "USD";
         public const string GenericCounterCurrency = "USD";
+
+        public static readonly DateTime BuildDateTime = DateTime.UtcNow;
     }
 }
