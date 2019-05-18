2009b833d905c6a22ff62a2c19a5b2a5a3040df1
nixxholas <nixholas@icloud.com>
2019-05-19 02:11:20 +0800

    Nozomi.Repo: Data: Mappings: WebModels: WebsocketModels: WebsocketRequestMap: Migrate seed data over
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Data/Mappings/WebModels/WebsocketModels/WebsocketRequestMap.cs
+++ Nozomi.Repo/Data/Mappings/WebModels/WebsocketModels/WebsocketRequestMap.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Nozomi.Base.Core.Helpers.Mapping;
@@ -16,6 +17,136 @@ namespace Nozomi.Repo.Data.Mappings.WebModels.WebsocketModels
                 .WithMany(cp => cp.WebsocketRequests).HasForeignKey(wsr => wsr.CurrencyPairId);
             entityTypeBuilder.HasMany(wsr => wsr.WebsocketCommands)
                 .WithOne(wsc => wsc.WebsocketRequest).HasForeignKey(wsc => wsc.WebsocketRequestId);
+
+            entityTypeBuilder.HasData(
+                // Binance's Websocket-based ticker data stream
+                new WebsocketRequest
+                {
+                    Id = 13,
+                    CurrencyPair = new CurrencyPair
+                    {
+                        CurrencyPairType = CurrencyPairType.EXCHANGEABLE,
+                        APIUrl = "wss://stream.binance.com:9443/stream?streams=!ticker@arr",
+                        DefaultComponent = "b",
+                        CurrencySourceId = 3,
+                        CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                        {
+                            new CurrencyCurrencyPair
+                            {
+                                CurrencyId = ethBna.Id
+                            },
+                            new CurrencyCurrencyPair
+                            {
+                                CurrencyId = btcBna.Id
+                            }
+                        },
+                        MainCurrency = ethBna.Abbrv,
+                        CounterCurrency = btcBna.Abbrv
+                    },
+                    Guid = Guid.NewGuid(),
+                    RequestType = RequestType.WebSocket,
+                    ResponseType = ResponseType.Json,
+                    DataPath = "wss://stream.binance.com:9443/stream?streams=!ticker@arr",
+                    Delay = 0,
+                    WebsocketCommands = new List<WebsocketCommand>(),
+                    RequestComponents = new List<RequestComponent>()
+                    {
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.VOLUME,
+                            Identifier = "data/s=>ETHBTC",
+                            QueryComponent = "v"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Ask,
+                            Identifier = "data/s=>ETHBTC",
+                            QueryComponent = "a"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Ask_Size,
+                            Identifier = "data/s=>ETHBTC",
+                            QueryComponent = "A"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Bid,
+                            Identifier = "data/s=>ETHBTC",
+                            QueryComponent = "b"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Bid_Size,
+                            Identifier = "data/s=>ETHBTC",
+                            QueryComponent = "B"
+                        }
+                    }
+                },
+                new WebsocketRequest
+                {
+                    Id = 14,
+                    CurrencyPair = new CurrencyPair
+                    {
+                        CurrencyPairType = CurrencyPairType.EXCHANGEABLE,
+                        APIUrl = "wss://stream.binance.com:9443/stream?streams=!ticker@arr",
+                        DefaultComponent = "b",
+                        CurrencySourceId = 3,
+                        CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                        {
+                            new CurrencyCurrencyPair
+                            {
+                                CurrencyId = kncBna.Id
+                            },
+                            new CurrencyCurrencyPair
+                            {
+                                CurrencyId = ethBna.Id
+                            }
+                        },
+                        MainCurrency = kncBna.Abbrv,
+                        CounterCurrency = ethBna.Abbrv
+                    },
+                    Guid = Guid.NewGuid(),
+                    RequestType = RequestType.WebSocket,
+                    ResponseType = ResponseType.Json,
+                    DataPath = "wss://stream.binance.com:9443/stream?streams=!ticker@arr",
+                    Delay = 0,
+                    WebsocketCommands = new List<WebsocketCommand>(),
+                    RequestComponents = new List<RequestComponent>()
+                    {
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.VOLUME,
+                            Identifier = "data/s=>KNCETH",
+                            QueryComponent = "v"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Ask,
+                            Identifier = "data/s=>KNCETH",
+                            QueryComponent = "a"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Ask_Size,
+                            Identifier = "data/s=>KNCETH",
+                            QueryComponent = "A"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Bid,
+                            Identifier = "data/s=>KNCETH",
+                            QueryComponent = "b"
+                        },
+                        new RequestComponent
+                        {
+                            ComponentType = ComponentType.Bid_Size,
+                            Identifier = "data/s=>KNCETH",
+                            QueryComponent = "B"
+                        }
+                    }
+                }
+            );
         }
     }
 }
