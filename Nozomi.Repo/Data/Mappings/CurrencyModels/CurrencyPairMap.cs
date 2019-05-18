981d5bdaded854a887f784d7b6570f8afdeb37cb
nixxholas <nixholas@icloud.com>
2019-05-18 20:26:52 +0800

    Nozomi.Repo: Data: Mappings: CurrencyModels: CurrencyPairMap: Migrate CurrencyPair seedings over
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Data/Mappings/CurrencyModels/CurrencyPairMap.cs
+++ Nozomi.Repo/Data/Mappings/CurrencyModels/CurrencyPairMap.cs
@@ -26,6 +26,177 @@ namespace Nozomi.Repo.Data.Mappings.CurrencyModels
                 .WithOne(pcp => pcp.CurrencyPair)
                 .HasForeignKey(pcp => pcp.CurrencyPairId)
                 .HasConstraintName("CurrencyPair_PartialCurrencyPairs_Constraint");
+
+            entityTypeBuilder.HasData(
+                new CurrencyPair()
+                {
+                    Id = 1,
+                    CurrencyPairType = CurrencyPairType.TRADEABLE,
+                    APIUrl = "https://api.ethfinex.com/v2/ticker/tETHUSD",
+                    DefaultComponent = "0",
+                    CurrencySourceId = 1,
+                    MainCurrency = "ETH",
+                    CounterCurrency = "USD",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = ethBfx.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = usdBfx.Id
+                        }
+                    }
+                },
+                new CurrencyPair
+                {
+                    Id = 2,
+                    CurrencyPairType = CurrencyPairType.TRADEABLE,
+                    APIUrl = "https://api.ethfinex.com/v2/ticker/tKNCUSD",
+                    DefaultComponent = "0",
+                    CurrencySourceId = 1,
+                    MainCurrency = "KNC",
+                    CounterCurrency = "USD",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = kncBfx.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = usdBfx.Id
+                        }
+                    }
+                },
+                new CurrencyPair()
+                {
+                    Id = 3,
+                    CurrencyPairType = CurrencyPairType.TRADEABLE,
+                    APIUrl = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
+                    DefaultComponent = "Cube",
+                    CurrencySourceId = 4,
+                    MainCurrency = "EUR",
+                    CounterCurrency = "USD",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = eurECB.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = usdECB.Id
+                        }
+                    }
+                },
+                new CurrencyPair
+                {
+                    Id = 4,
+                    CurrencyPairType = CurrencyPairType.TRADEABLE,
+                    APIUrl = "https://www.alphavantage.co/query",
+                    DefaultComponent = "Realtime Currency Exchange Rate/5. Exchange Rate",
+                    CurrencySourceId = 5,
+                    MainCurrency = "EUR",
+                    CounterCurrency = "USD",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = eurAVG.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = usdAVG.Id
+                        }
+                    }
+                },
+                new CurrencyPair
+                {
+                    Id = 5,
+                    CurrencyPairType = CurrencyPairType.EXCHANGEABLE,
+                    APIUrl = "https://poloniex.com/public?command=returnTicker",
+                    DefaultComponent = "BTC_BCN/lowestAsk",
+                    CurrencySourceId = 6,
+                    MainCurrency = "BTC",
+                    CounterCurrency = "BCN",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = btcPOLO.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = bcnPOLO.Id
+                        }
+                    }
+                },
+                new CurrencyPair
+                {
+                    Id = 6,
+                    CurrencyPairType = CurrencyPairType.EXCHANGEABLE,
+                    APIUrl = "https://poloniex.com/public?command=returnTicker",
+                    DefaultComponent = "BTC_BTS/lowestAsk",
+                    CurrencySourceId = 6,
+                    MainCurrency = "BTC",
+                    CounterCurrency = "BTS",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = btcPOLO.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = btsPOLO.Id
+                        }
+                    }
+                },
+                new CurrencyPair
+                {
+                    Id = 7,
+                    CurrencyPairType = CurrencyPairType.TRADEABLE,
+                    APIUrl = "https://api.bitfinex.com/v1/pubticker/etheur",
+                    DefaultComponent = "0",
+                    CurrencySourceId = 1,
+                    MainCurrency = "ETH",
+                    CounterCurrency = "EUR",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = ethBfx.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = eurBfx.Id
+                        }
+                    }
+                },
+                new CurrencyPair()
+                {
+                    Id = 8,
+                    CurrencyPairType = CurrencyPairType.EXCHANGEABLE,
+                    APIUrl = "https://poloniex.com/public?command=returnTicker",
+                    DefaultComponent = "USDT_BTC/lowestAsk",
+                    CurrencySourceId = 6,
+                    MainCurrency = "BTC",
+                    CounterCurrency = "USDT",
+                    CurrencyPairCurrencies = new List<CurrencyCurrencyPair>
+                    {
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = btcPOLO.Id
+                        },
+                        new CurrencyCurrencyPair
+                        {
+                            CurrencyId = usdtPOLO.Id
+                        }
+                    }
+                }
+            );
         }
     }
 }
