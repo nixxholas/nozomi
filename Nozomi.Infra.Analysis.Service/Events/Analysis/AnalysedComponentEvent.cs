14a6fe35afe249411fab6bea4d0d2047502cc531
nixxholas <nixholas@icloud.com>
2019-05-20 01:45:58 +0800

    Nozomi.Infra.Analysis.Service: Events: Analysis: AnalysedComponentEvent: GetAllByCurrency API: Compatibility Update
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Analysis.Service/Events/Analysis/AnalysedComponentEvent.cs
+++ Nozomi.Infra.Analysis.Service/Events/Analysis/AnalysedComponentEvent.cs
@@ -173,94 +173,123 @@ namespace Nozomi.Infra.Analysis.Service.Events.Analysis
             var qCurrency = _unitOfWork.GetRepository<Currency>()
                 .GetQueryable()
                 .AsNoTracking()
-                .SingleOrDefault(c => c.Id.Equals(currencyId));
+                .Where(c => c.Id.Equals(currencyId));
 
-            if (qCurrency == null) return null;
+            if (qCurrency.SingleOrDefault() == null) return null;
 
-            // Then we return
-            var finalQuery = _unitOfWork.GetRepository<CurrencyPair>()
-                .GetQueryable()
-                .AsNoTracking()
-                // Immediately filter first
-                .Where(cp =>
-                    // Make sure the main currencies are identical
-                    cp.CurrencyPairCurrencies.FirstOrDefault(ccp => ccp.CurrencyPair.MainCurrency
-                            .Equals(ccp.Currency.Abbreviation)).Currency.Abbreviation
-                        .Equals(qCurrency.Abbreviation, StringComparison.InvariantCultureIgnoreCase));
-            
-            #if DEBUG
-            // Debug and see if the correct currencies are present.
-            var correctCPairList = finalQuery.ToList();
-            #endif
+            qCurrency = qCurrency.Include(c => c.AnalysedComponents);
 
             if (ensureValid)
             {
-                finalQuery = finalQuery
-                    .Where(cp => cp.IsEnabled && cp.DeletedAt == null);
+                qCurrency = qCurrency.Where(c => c.DeletedAt == null && c.IsEnabled);
             }
 
-            if (!string.IsNullOrEmpty(counterCurrency))
+            if (track)
             {
-                finalQuery = finalQuery.Where(cp =>
-                    cp.CounterCurrency.Equals(counterCurrency, StringComparison.InvariantCultureIgnoreCase));
+                qCurrency = qCurrency.Include(c => c.AnalysedComponents)
+                    .ThenInclude(ac => ac.AnalysedHistoricItems);
             }
-            
-            #if DEBUG
-            var filteredCPairList = finalQuery.ToList();
-            #endif
 
-            var analysedComponents = finalQuery
-                .Include(cp => cp.CurrencyPairCurrencies)
-                .ThenInclude(pcp => pcp.Currency)
-                .Include(cp => cp.CurrencyPairRequests)
-                .ThenInclude(cpr => cpr.AnalysedComponents)
-                .ThenInclude(rc => rc.AnalysedHistoricItems)
-                .Where(cp => cp.CurrencyPairRequests.Any(cpr => cpr.IsEnabled && cpr.DeletedAt == null
-                                                                              && cpr.AnalysedComponents
-                                                                                  .Any(ac =>
-                                                                                      ac.IsEnabled && ac.DeletedAt ==
-                                                                                                   null
-                                                                                                   && ac.AnalysedHistoricItems.Count > 0)))
-                .SelectMany(cp => cp.CurrencyPairRequests)
-                .SelectMany(cpr => cpr.AnalysedComponents);
-
-#if DEBUG
-            if (currencyId.Equals(4))
-            {
-                var test = analysedComponents.ToList();
-                Console.WriteLine("Banf");
-            }
-#endif
-            
-            if (!finalQuery.Any()) return new List<AnalysedComponent>();
-
-            if (!track)
-            {
-                return analysedComponents
-                    .Select(ac => new AnalysedComponent
-                    {
-                        Id = ac.Id,
-                        ComponentType = ac.ComponentType,
-                        Value = ac.Value,
-                        Delay = ac.Delay,
-                        RequestId = ac.RequestId,
-                        CurrencyId = ac.CurrencyId
-                    })
-                    .ToList();
-            }
-            
-            return analysedComponents
+            return qCurrency
+                .SelectMany(c => c.AnalysedComponents)
                 .Select(ac => new AnalysedComponent
                 {
-                    AnalysedHistoricItems = ac.AnalysedHistoricItems,
                     Id = ac.Id,
                     ComponentType = ac.ComponentType,
                     Value = ac.Value,
+                    IsDenominated = ac.IsDenominated,
                     Delay = ac.Delay,
+                    UIFormatting = ac.UIFormatting,
                     RequestId = ac.RequestId,
-                    CurrencyId = ac.CurrencyId
+                    AnalysedHistoricItems = ac.AnalysedHistoricItems
                 })
                 .ToList();
+
+
+//            // Then we return
+//            var finalQuery = _unitOfWork.GetRepository<CurrencyPair>()
+//                .GetQueryable()
+//                .AsNoTracking()
+//                // Immediately filter first
+//                .Where(cp =>
+//                    // Make sure the main currencies are identical
+//                    cp.CurrencyPairCurrencies.FirstOrDefault(ccp => ccp.CurrencyPair.MainCurrency
+//                            .Equals(ccp.Currency.Abbreviation)).Currency.Abbreviation
+//                        .Equals(qCurrency.Abbreviation, StringComparison.InvariantCultureIgnoreCase));
+//            
+//            #if DEBUG
+//            // Debug and see if the correct currencies are present.
+//            var correctCPairList = finalQuery.ToList();
+//            #endif
+//
+//            if (ensureValid)
+//            {
+//                finalQuery = finalQuery
+//                    .Where(cp => cp.IsEnabled && cp.DeletedAt == null);
+//            }
+//
+//            if (!string.IsNullOrEmpty(counterCurrency))
+//            {
+//                finalQuery = finalQuery.Where(cp =>
+//                    cp.CounterCurrency.Equals(counterCurrency, StringComparison.InvariantCultureIgnoreCase));
+//            }
+//            
+//            #if DEBUG
+//            var filteredCPairList = finalQuery.ToList();
+//            #endif
+//
+//            var analysedComponents = finalQuery
+//                .Include(cp => cp.CurrencyPairCurrencies)
+//                .ThenInclude(pcp => pcp.Currency)
+//                .Include(cp => cp.CurrencyPairRequests)
+//                .ThenInclude(cpr => cpr.AnalysedComponents)
+//                .ThenInclude(rc => rc.AnalysedHistoricItems)
+//                .Where(cp => cp.CurrencyPairRequests.Any(cpr => cpr.IsEnabled && cpr.DeletedAt == null
+//                                                                              && cpr.AnalysedComponents
+//                                                                                  .Any(ac =>
+//                                                                                      ac.IsEnabled && ac.DeletedAt ==
+//                                                                                                   null
+//                                                                                                   && ac.AnalysedHistoricItems.Count > 0)))
+//                .SelectMany(cp => cp.CurrencyPairRequests)
+//                .SelectMany(cpr => cpr.AnalysedComponents);
+//
+//#if DEBUG
+//            if (currencyId.Equals(4))
+//            {
+//                var test = analysedComponents.ToList();
+//                Console.WriteLine("Banf");
+//            }
+//#endif
+//            
+//            if (!finalQuery.Any()) return new List<AnalysedComponent>();
+//
+//            if (!track)
+//            {
+//                return analysedComponents
+//                    .Select(ac => new AnalysedComponent
+//                    {
+//                        Id = ac.Id,
+//                        ComponentType = ac.ComponentType,
+//                        Value = ac.Value,
+//                        Delay = ac.Delay,
+//                        RequestId = ac.RequestId,
+//                        CurrencyId = ac.CurrencyId
+//                    })
+//                    .ToList();
+//            }
+//            
+//            return analysedComponents
+//                .Select(ac => new AnalysedComponent
+//                {
+//                    AnalysedHistoricItems = ac.AnalysedHistoricItems,
+//                    Id = ac.Id,
+//                    ComponentType = ac.ComponentType,
+//                    Value = ac.Value,
+//                    Delay = ac.Delay,
+//                    RequestId = ac.RequestId,
+//                    CurrencyId = ac.CurrencyId
+//                })
+//                .ToList();
         }
 
         public ICollection<AnalysedComponent> GetAllByCurrencyType(long currencyTypeId, bool track = false)
