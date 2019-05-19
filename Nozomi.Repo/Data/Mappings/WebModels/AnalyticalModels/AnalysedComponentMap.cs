4587fe57ca6b450e8bc118026dac815d436ec73d
nixxholas <nixholas@icloud.com>
2019-05-19 12:38:39 +0800

    Nozomi.Repo: Data: Mappings: WebModels: AnalyticalModels: AnalysedComponentMap: More component seeding
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Data/Mappings/WebModels/AnalyticalModels/AnalysedComponentMap.cs
+++ Nozomi.Repo/Data/Mappings/WebModels/AnalyticalModels/AnalysedComponentMap.cs
@@ -49,11 +49,83 @@ namespace Nozomi.Repo.Data.Mappings.WebModels.AnalyticalModels
                     DeletedAt = null,
                     CurrencyId = 3
                 },
+                new AnalysedComponent
+                {
+                    Id = 46,
+                    ComponentType = AnalysedComponentType.MarketCapChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 3
+                },
+                new AnalysedComponent
+                {
+                    Id = 47,
+                    ComponentType = AnalysedComponentType.MarketCapPctChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 3
+                },
+                new AnalysedComponent
+                {
+                    Id = 47,
+                    ComponentType = AnalysedComponentType.MarketCapHourlyPctChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 3
+                },
                 // KNC Bitfinex Market Cap
                 new AnalysedComponent
                 {
                     Id = 2,
                     ComponentType = AnalysedComponentType.MarketCap,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 4
+                },
+                new AnalysedComponent
+                {
+                    Id = 48,
+                    ComponentType = AnalysedComponentType.MarketCapChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 4
+                },
+                new AnalysedComponent
+                {
+                    Id = 49,
+                    ComponentType = AnalysedComponentType.MarketCapPctChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 4
+                },
+                new AnalysedComponent
+                {
+                    Id = 50,
+                    ComponentType = AnalysedComponentType.MarketCapHourlyPctChange,
                     Delay = 1000,
                     UIFormatting = "$ 0 a",
                     CreatedAt = DateTime.UtcNow,
@@ -71,6 +143,42 @@ namespace Nozomi.Repo.Data.Mappings.WebModels.AnalyticalModels
                     UIFormatting = "$ 0 a",
                     CreatedAt = DateTime.UtcNow,
                     ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    CurrencyId = 12
+                },
+                new AnalysedComponent
+                {
+                    Id = 51,
+                    ComponentType = AnalysedComponentType.MarketCapChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 12
+                },
+                new AnalysedComponent
+                {
+                    Id = 52,
+                    ComponentType = AnalysedComponentType.MarketCapPctChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
+                    DeletedAt = null,
+                    CurrencyId = 12
+                },
+                new AnalysedComponent
+                {
+                    Id = 53,
+                    ComponentType = AnalysedComponentType.MarketCapHourlyPctChange,
+                    Delay = 1000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    IsDenominated = true,
                     DeletedAt = null,
                     CurrencyId = 12
                 },
@@ -343,6 +451,234 @@ namespace Nozomi.Repo.Data.Mappings.WebModels.AnalyticalModels
                     ModifiedAt = DateTime.UtcNow,
                     DeletedAt = null,
                     RequestId = 12
+                },
+                // Coinhako BTCSGD
+                new AnalysedComponent
+                {
+                    Id = 28,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 15
+                },
+                // Coinhako BTCSGD
+                new AnalysedComponent
+                {
+                    Id = 29,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 15
+                },
+                // Coinhako BTCSGD
+                new AnalysedComponent()
+                {
+                    Id = 30,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 15
+                },
+                // Coinhako BTCUSD
+                new AnalysedComponent
+                {
+                    Id = 31,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 16
+                },
+                // Coinhako BTCUSD
+                new AnalysedComponent
+                {
+                    Id = 32,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 16
+                },
+                // Coinhako BTCUSD
+                new AnalysedComponent()
+                {
+                    Id = 33,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 16
+                },
+                // Coinhako ETHSGD
+                new AnalysedComponent
+                {
+                    Id = 34,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 17
+                },
+                // Coinhako ETHSGD
+                new AnalysedComponent
+                {
+                    Id = 35,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 17
+                },
+                // Coinhako ETHSGD
+                new AnalysedComponent()
+                {
+                    Id = 36,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 17
+                },
+                // Coinhako ETHUSD
+                new AnalysedComponent
+                {
+                    Id = 37,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 18
+                },
+                // Coinhako ETHUSD
+                new AnalysedComponent
+                {
+                    Id = 38,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 18
+                },
+                // Coinhako ETHUSD
+                new AnalysedComponent()
+                {
+                    Id = 39,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 18
+                },
+                // Coinhako LTCSGD
+                new AnalysedComponent
+                {
+                    Id = 40,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 19
+                },
+                // Coinhako LTCSGD
+                new AnalysedComponent
+                {
+                    Id = 41,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 19
+                },
+                // Coinhako LTCSGD
+                new AnalysedComponent()
+                {
+                    Id = 42,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 19
+                },
+                // Coinhako LTCUSD
+                new AnalysedComponent
+                {
+                    Id = 43,
+                    ComponentType = AnalysedComponentType.CurrentAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 20
+                },
+                // Coinhako LTCUSD
+                new AnalysedComponent
+                {
+                    Id = 44,
+                    ComponentType = AnalysedComponentType.HourlyAveragePrice,
+                    Delay = 10000,
+                    UIFormatting = "$ 0[.]00",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 20
+                },
+                // Coinhako LTCUSD
+                new AnalysedComponent()
+                {
+                    Id = 45,
+                    ComponentType = AnalysedComponentType.DailyPricePctChange,
+                    Delay = 10000,
+                    UIFormatting = "0[.]0",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    RequestId = 20
+                },
+                // Crypto Market Cap
+                new AnalysedComponent
+                {
+                    Id = 54,
+                    ComponentType = AnalysedComponentType.MarketCap,
+                    Delay = 3000,
+                    UIFormatting = "$ 0 a",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
+                    DeletedAt = null,
+                    CurrencyTypeId = 2
                 }
             );
         }
