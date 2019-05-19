aaff3667ec6c9f3dd9a41ba999399265435cd43c
nixxholas <nixholas@icloud.com>
2019-05-19 18:54:20 +0800

    Nozomi.Repo: Data: Mappings: CurrencyModels: CurrencySourceMap: Seed PK
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Data/Mappings/CurrencyModels/CurrencySourceMap.cs
+++ Nozomi.Repo/Data/Mappings/CurrencyModels/CurrencySourceMap.cs
@@ -28,120 +28,140 @@ namespace Nozomi.Repo.Data.Mappings.CurrencyModels
                 // Bitfinex USD
                 new CurrencySource
                 {
+                    Id = 1,
                     CurrencyId = 1,
                     SourceId = 1
                 },
                 // Bitfinex EUR
                 new CurrencySource
                 {
+                    Id = 2,
                     CurrencyId = 2,
                     SourceId = 1
                 },
                 // Bitfinex ETH
                 new CurrencySource
                 {
+                    Id = 3,
                     CurrencyId = 3,
                     SourceId = 1
                 },
                 // Bitfinex KNC
                 new CurrencySource
                 {
+                    Id = 4,
                     CurrencyId = 4,
                     SourceId = 1
                 },
                 // Binance KNC
                 new CurrencySource
                 {
+                    Id = 5,
                     CurrencyId = 5,
                     SourceId = 3
                 },
                 // Binance ETH
                 new CurrencySource
                 {
+                    Id = 6,
                     CurrencyId = 6,
                     SourceId = 3
                 },
                 // Binance BTC
                 new CurrencySource
                 {
+                    Id = 7,
                     CurrencyId = 7,
                     SourceId = 3
                 },
                 // European Central Bank EUR
                 new CurrencySource
                 {
+                    Id = 8,
                     CurrencyId = 2,
                     SourceId = 4
                 },
                 // European Central Bank USD
                 new CurrencySource
                 {
+                    Id = 9,
                     CurrencyId = 1,
                     SourceId = 4
                 },
                 // AlphaVantage EUR
                 new CurrencySource
                 {
+                    Id = 10,
                     CurrencyId = 2,
                     SourceId = 5
                 },
                 // AlphaVantage USD
                 new CurrencySource
                 {
+                    Id = 11,
                     CurrencyId = 1,
                     SourceId = 5
                 },
                 // Poloniex BTC
                 new CurrencySource
                 {
+                    Id = 12,
                     CurrencyId = 7,
                     SourceId = 6
                 },
                 // Poloniex BCN
                 new CurrencySource
                 {
+                    Id = 13,
                     CurrencyId = 8,
                     SourceId = 6
                 },
                 // Poloniex BTS
                 new CurrencySource
                 {
+                    Id = 14,
                     CurrencyId = 9,
                     SourceId = 6
                 },
                 // Poloniex USDT
                 new CurrencySource
                 {
+                    Id = 15,
                     CurrencyId = 10,
                     SourceId = 6
                 },
                 // Coinhako BTC
                 new CurrencySource
                 {
+                    Id = 16,
                     CurrencyId = 7,
                     SourceId = 2
                 },
                 // Coinhako SGD
                 new CurrencySource
                 {
+                    Id = 17,
                     CurrencyId = 11,
                     SourceId = 2
                 },
                 // Coinhako ETH
                 new CurrencySource
                 {
+                    Id = 18,
                     CurrencyId = 6,
                     SourceId = 2
                 },
                 // Coinhako USD
                 new CurrencySource
                 {
+                    Id = 19,
                     CurrencyId = 1,
                     SourceId = 2
                 },
                 // Coinhako LTC
                 new CurrencySource
                 {
+                    Id = 20,
                     CurrencyId = 12,
                     SourceId = 2
                 }
