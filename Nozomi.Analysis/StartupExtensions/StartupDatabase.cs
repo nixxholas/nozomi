b475f86f67bf5428fcddaae5323221737dd61951
nixxholas <nixholas@icloud.com>
2019-05-20 21:09:01 +0800

    Nozomi.Analysis: StartupDatabase Migration DI
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Analysis/StartupExtensions/StartupDatabase.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nozomi.Repo.Data;
+
+namespace Nozomi.Analysis.StartupExtensions
+{
+    public static class StartupDatabase
+    {
+        public static void UseAutoDbMigration(this IApplicationBuilder app, IHostingEnvironment env)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                var logger = serviceScope.ServiceProvider.GetService<ILogger<Startup>>();
+
+                using (var context = serviceScope.ServiceProvider.GetService<NozomiDbContext>())
+                {
+                    // Auto Wipe
+                    if (env.IsDevelopment())
+                    {
+                        //context.Database.EnsureDeleted();
+
+                        context.Database.EnsureCreated();
+                    }
+                    else
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+            }
+        }
+    }
+}
