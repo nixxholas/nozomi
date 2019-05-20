6e9089dcf6ce50b7e8a1ea31b13467c70ff8f99b
nixxholas <nixholas@icloud.com>
2019-05-21 00:58:21 +0800

    Nozomi.Base.Domain: Models: Web: RequestProperty: Equals Function
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/Models/Web/RequestProperty.cs
+++ Nozomi.Base.Domain/Models/Web/RequestProperty.cs
@@ -14,5 +14,25 @@ namespace Nozomi.Data.Models.Web
 
         public long RequestId { get; set; }
         public Request Request { get; set; }
+
+        protected bool Equals(RequestProperty other)
+        {
+            return RequestPropertyType == other.RequestPropertyType
+                   && string.Equals(Key, other.Key) && string.Equals(Value, other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int) RequestPropertyType;
+                hashCode = (hashCode * 397) ^ (Key != null ? Key.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Value != null ? Value.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ RequestId.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Request != null ? Request.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
