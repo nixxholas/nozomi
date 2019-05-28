e59bcebf70c5b0338c874a19881dcd2e27080398
elvisyong <elvisyong@gmail.com>
2019-05-29 03:25:10 +0800

    Nozomi.Base.Domain: AreaModels: v1: RequestComponent: UpdateRequestComponent: Create UpdateRequestComponent with id inside

--- Nozomi.Base.Domain/AreaModels/v1/RequestComponent/UpdateRequestComponent.cs
+++ Nozomi.Base.Domain/AreaModels/v1/RequestComponent/UpdateRequestComponent.cs
@@ -1,7 +1,7 @@
 namespace Nozomi.Data.AreaModels.v1.RequestComponent
 {
-    public class UpdateRequestComponent
+    public class UpdateRequestComponent: CreateRequestComponent
     {
-        
+        public long Id { get; set; }
     }
 }
