6ca80546777cc11c0b3b74de36d0566fef295b7c
nixxholas <nixholas@icloud.com>
2019-05-29 21:48:07 +0800

    Nozomi.Ticker: Controllers: APIs: v1: Currency: GetAllDetailed API
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Ticker/Controllers/APIs/v1/Currency/ICurrencyController.cs
+++ Nozomi.Ticker/Controllers/APIs/v1/Currency/ICurrencyController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Nozomi.Data;
 using Nozomi.Data.AreaModels.v1.Currency;
@@ -10,6 +11,8 @@ namespace Nozomi.Ticker.Controllers.APIs.v1.Currency
         ActionResult<NozomiResult<string>> Create(CreateCurrency createCurrency);
 
         NozomiResult<DetailedCurrencyResponse> Detailed(string abbreviation);
+
+        ICollection<DetailedCurrencyResponse> GetAllDetailed(string currencyType = "CRYPTO", int index = 0);
 
         NozomiResult<string> Update(UpdateCurrency updateCurrency);
     }
