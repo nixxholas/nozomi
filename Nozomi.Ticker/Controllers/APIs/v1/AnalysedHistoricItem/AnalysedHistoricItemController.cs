9b846c825919422a94a214b09893f3850a1d6eb0
nixxholas <nixholas@icloud.com>
2019-06-01 12:40:18 +0800

    Nozomi.Ticker: Controllers: APIs: v1: AnalysedHistoricItem: Implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Ticker/Controllers/APIs/v1/AnalysedHistoricItem/AnalysedHistoricItemController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data;
+using Nozomi.Service.Events.Analysis.Interfaces;
+using Nozomi.Service.Identity.Managers;
+
+namespace Nozomi.Ticker.Controllers.APIs.v1.AnalysedHistoricItem
+{
+    public class AnalysedHistoricItemController : BaseController<AnalysedHistoricItemController>, IAnalysedHistoricItemController
+    {
+        private readonly IAnalysedHistoricItemEvent _analysedHistoricItemEvent;
+        
+        public AnalysedHistoricItemController(ILogger<AnalysedHistoricItemController> logger, 
+            NozomiUserManager nozomiUserManager, IAnalysedHistoricItemEvent analysedHistoricItemEvent) 
+            : base(logger, nozomiUserManager)
+        {
+            _analysedHistoricItemEvent = analysedHistoricItemEvent;
+        }
+
+        public Task<NozomiResult<ICollection<Data.Models.Web.Analytical.AnalysedHistoricItem>>> GetAll(
+            long analysedComponentId, int index = 0)
+        {
+            return Task.FromResult(new NozomiResult<ICollection<Data.Models.Web.Analytical.AnalysedHistoricItem>>(
+                _analysedHistoricItemEvent.GetAll(analysedComponentId, TimeSpan.Zero, index)));
+        }
+    }
+}
---
+++ Nozomi.Ticker/Controllers/APIs/v1/AnalysedHistoricItem/IAnalysedHistoricItemController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nozomi.Data;
+
+namespace Nozomi.Ticker.Controllers.APIs.v1.AnalysedHistoricItem
+{
+    public interface IAnalysedHistoricItemController
+    {
+        Task<NozomiResult<ICollection<Data.Models.Web.Analytical.AnalysedHistoricItem>>> GetAll(long analysedComponentId, int index = 0);
+    }
+}
