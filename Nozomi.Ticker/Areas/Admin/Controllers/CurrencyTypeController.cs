ae1c01668a5dc4b8d623bc6b5c3fa2bac96a11a5
nixxholas <nicholas@slyck.net>
2019-06-06 21:01:46 +0800

    Nozomi.Ticker: Areas: Admin: Controllers: CurrencyType: Initial implementation
    - Create
    - Delete
    - GetAll
    
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Ticker/Areas/Admin/Controllers/CurrencyTypeController.cs
@@ -0,0 +1,83 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nozomi.Data;
+using Nozomi.Data.Models.Currency;
+using Nozomi.Service.Events.Interfaces;
+using Nozomi.Service.Identity.Managers;
+using Nozomi.Service.Services.Interfaces;
+using Nozomi.Ticker.Controllers;
+
+namespace Nozomi.Ticker.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Owner, Administrator, Staff")]
+    public class CurrencyTypeController : AreaBaseViewController<CurrencyTypeController>
+    {
+        private readonly ICurrencyTypeEvent _currencyTypeEvent;
+        private readonly ICurrencyTypeService _currencyTypeService;
+
+        public CurrencyTypeController(ILogger<CurrencyTypeController> logger, NozomiSignInManager signInManager,
+            NozomiUserManager userManager, ICurrencyTypeEvent currencyTypeEvent, 
+            ICurrencyTypeService currencyTypeService) 
+            : base(logger, signInManager, userManager)
+        {
+            _currencyTypeEvent = currencyTypeEvent;
+            _currencyTypeService = currencyTypeService;
+        }
+        
+        #region View all CurrencyTypes
+
+        [HttpGet]
+        public IActionResult CurrencyTypes()
+        {
+            return View(_currencyTypeEvent.GetAll());
+        }
+         
+        #endregion
+        
+        #region POST Create
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CurrencyType currencyType)
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var result = _currencyTypeService.Create(currencyType);
+
+            if (result > 0) return Ok(result);
+
+            // Create failed
+            return NotFound();
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            // TODO: Make full use of the hard delete option.
+            var result = _currencyTypeService.Delete(id, false, user.Id);
+
+            if (result) return Ok("Currency Type successfully deleted!");
+
+            // Create failed
+            return BadRequest("Invalid Currency Type.");
+        }
+
+        #endregion
+    }
+}
