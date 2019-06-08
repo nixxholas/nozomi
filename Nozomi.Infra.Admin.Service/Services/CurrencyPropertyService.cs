a1ecaa6f03668388627c9125d32343c679745ae6
nixxholas <nixholas@icloud.com>
2019-06-09 01:57:09 +0800

    Nozomi.Infra.Admin.Service: Services: CurrencyPropertyService: Scaffolding with interface
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Infra.Admin.Service/Services/CurrencyPropertyService.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Logging;
+using Nozomi.Data.Models.Currency;
+using Nozomi.Infra.Admin.Service.Services.Interfaces;
+using Nozomi.Preprocessing.Abstracts;
+using Nozomi.Repo.BCL.Repository;
+using Nozomi.Repo.Data;
+
+namespace Nozomi.Infra.Admin.Service.Services
+{
+    public class CurrencyPropertyService : BaseService<CurrencyPropertyService, NozomiDbContext>, ICurrencyPropertyService
+    {
+        public CurrencyPropertyService(ILogger<CurrencyPropertyService> logger, IUnitOfWork<NozomiDbContext> unitOfWork) : base(logger, unitOfWork)
+        {
+        }
+
+        public long Create(CurrencyProperty currencyProperty, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(CurrencyProperty currencyProperty, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(long currencyPropertyId, bool hardDelete = false, long userId = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
