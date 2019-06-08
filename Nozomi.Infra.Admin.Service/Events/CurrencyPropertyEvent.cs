f9eb4986faeec64a44276e3ecf0244ee5b8abc07
nixxholas <nixholas@icloud.com>
2019-06-09 01:49:33 +0800

    Nozomi.Infra.Admin.Service: Events: CurrencyPropertyEvent: GetAll API: Implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Admin.Service/Events/CurrencyPropertyEvent.cs
+++ Nozomi.Infra.Admin.Service/Events/CurrencyPropertyEvent.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Nozomi.Data.Models.Currency;
 using Nozomi.Infra.Admin.Service.Events.Interfaces;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.BCL.Repository;
 using Nozomi.Repo.Data;
@@ -17,7 +20,24 @@ namespace Nozomi.Infra.Admin.Service.Events
 
         public ICollection<CurrencyProperty> GetAll(int index = 0, bool track = false)
         {
-            throw new System.NotImplementedException();
+            if (index > 0)
+            {
+                var query = _unitOfWork.GetRepository<CurrencyProperty>()
+                    .GetQueryable()
+                    .AsNoTracking()
+                    .Skip(index * NozomiServiceConstants.CurrencyPropertyTakeoutLimit)
+                    .Take(NozomiServiceConstants.CurrencyPropertyTakeoutLimit);
+
+                if (track)
+                {
+                    query = query
+                        .Include(cp => cp.Currency);
+                }
+
+                return query.ToList();
+            }
+
+            return null;
         }
 
         public CurrencyProperty Get(long id, bool track = false)
