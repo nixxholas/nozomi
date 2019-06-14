6ff4f7c7df212223a92c17a04e973f845fafbfee
ElvisYong <elvisyong@gmail.com>
2019-06-15 01:56:13 +0800

    Nozomi.Base.Identity: ViewModels: Manage: CurrencyPair: CurrencyPairViewModel: ViewModel for CurrencyPair
    Signed-off-by: ElvisYong <elvisyong@gmail.com>

--- Nozomi.Base.Identity/ViewModels/Manage/CurrencyPair/CurrencyPairViewModel.cs
+++ Nozomi.Base.Identity/ViewModels/Manage/CurrencyPair/CurrencyPairViewModel.cs
@@ -5,5 +5,6 @@ namespace Nozomi.Base.Identity.ViewModels.Manage.CurrencyPair
     public class CurrencyPairViewModel
     {
         public ICollection<Data.Models.Currency.CurrencyPair> CurrencyPairs { get; set; }
+        public IEnumerable<Data.Models.Currency.Currency> Currencies { get; set; }
     }
 }
