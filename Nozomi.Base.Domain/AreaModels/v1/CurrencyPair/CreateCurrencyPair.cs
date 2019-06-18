9d5815b80ddbd3a00b081a0f405da52a3e1b35c7
ElvisYong <elvisyong@gmail.com>
2019-06-18 17:33:50 +0800

    Nozomi.Base.Domain: AreaModels: v1: CurrencyPair: CreateCurrencyPair: Change maincurrency and countercurrency to take in abbrv instead.
    Signed-off-by: elvisyong <elvisyong@gmail.com>

--- Nozomi.Base.Domain/AreaModels/v1/CurrencyPair/CreateCurrencyPair.cs
+++ Nozomi.Base.Domain/AreaModels/v1/CurrencyPair/CreateCurrencyPair.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using Nozomi.Data.AreaModels.v1.CurrencyPairRequest;
-using Nozomi.Data.AreaModels.v1.PartialCurrencyPair;
+using Nozomi.Base.Core.Helpers.Native.Collections;
 using Nozomi.Data.Models.Currency;
 
 namespace Nozomi.Data.AreaModels.v1.CurrencyPair
@@ -19,14 +13,16 @@ namespace Nozomi.Data.AreaModels.v1.CurrencyPair
         
         public long SourceId { get; set; }
         
-        public Models.Currency.Currency MainCurrency{ get; set; }
+        public string MainCurrencyAbbrv{ get; set; }
         
-        public Models.Currency.Currency CounterCurrency{ get; set; }
+        public string CounterCurrencyAbbrv { get; set; }
+        
+        public bool IsEnabled { get; set; }
 
         public bool IsValid()
         {
             return CurrencyPairType >= 0 && !string.IsNullOrEmpty(APIUrl) && SourceId > 0
-                   && MainCurrency != null && CounterCurrency != null;
+                   && !MainCurrencyAbbrv.IsNullOrEmpty() && !CounterCurrencyAbbrv.IsNullOrEmpty();
         }
     }
 }
