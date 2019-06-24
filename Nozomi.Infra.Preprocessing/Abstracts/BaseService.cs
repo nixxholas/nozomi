dc7cdd1c05f67307b9bc89a2c2d29614559e0edb
nixxholas <nicholas@slyck.net>
2019-06-25 06:05:09 +0800

    Nozomi.Infra.Preprocessing: Abstracts: BaseService: Further increase property security
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Preprocessing/Abstracts/BaseService.cs
+++ Nozomi.Infra.Preprocessing/Abstracts/BaseService.cs
@@ -8,8 +8,8 @@ namespace Nozomi.Preprocessing.Abstracts
         where T : class
         where TContext : IDbContext
     {
-        protected ILogger<T> _logger;
-        protected IUnitOfWork<TContext> _unitOfWork;
+        protected readonly ILogger<T> _logger;
+        protected readonly IUnitOfWork<TContext> _unitOfWork;
         protected readonly string _serviceName;
 
         public BaseService(ILogger<T> logger, IUnitOfWork<TContext> unitOfWork)
