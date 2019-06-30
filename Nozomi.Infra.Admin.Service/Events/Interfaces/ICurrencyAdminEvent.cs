a5618910728fc26c54b54896601b07d71576b110
nixxholas <nixholas@icloud.com>
2019-06-30 12:12:47 +0800

    Nozomi.Infra.Admin.Service: Events: CurrencyAdminEvent: GetCurrencyBySlug API: Tracking support
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Admin.Service/Events/Interfaces/ICurrencyAdminEvent.cs
+++ Nozomi.Infra.Admin.Service/Events/Interfaces/ICurrencyAdminEvent.cs
@@ -6,7 +6,7 @@ namespace Nozomi.Infra.Admin.Service.Events.Interfaces
 {
     public interface ICurrencyAdminEvent
     {
-        Currency GetCurrencyBySlug(string slug);
+        Currency GetCurrencyBySlug(string slug, bool track = false);
 
         ICollection<Currency> GetAll(bool track = false);
     }
