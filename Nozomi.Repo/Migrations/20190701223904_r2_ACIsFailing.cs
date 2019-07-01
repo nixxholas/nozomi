5a65bbe7acc6af76e064621fbf4407a8657e36f3
nixxholas <nicholas@slyck.net>
2019-07-02 06:39:18 +0800

    Nozomi.Repo: Migrations: r2 - AC IsFailing Migration
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Migrations/20190701223904_r2_ACIsFailing.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r2_ACIsFailing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFailing",
+                table: "AnalysedComponents",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFailing",
+                table: "AnalysedComponents");
+        }
+    }
+}
