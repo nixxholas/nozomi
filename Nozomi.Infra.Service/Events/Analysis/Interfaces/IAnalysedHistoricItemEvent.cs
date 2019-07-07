515e12cb95db09122e5197c93d07877d4fd0d775
nixholas <nixholas@icloud.com>
2019-07-08 00:23:38 +0800

    Nozomi.Infra.Service: Events: Analysis: AnalysedHistoricItemEvent: TraverseRelatedHistory API: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Events/Analysis/Interfaces/IAnalysedHistoricItemEvent.cs
+++ Nozomi.Infra.Service/Events/Analysis/Interfaces/IAnalysedHistoricItemEvent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Nozomi.Base.Core.Responses;
 using Nozomi.Data.Models.Web.Analytical;
 
 namespace Nozomi.Service.Events.Analysis.Interfaces
@@ -20,5 +21,15 @@ namespace Nozomi.Service.Events.Analysis.Interfaces
 
         ICollection<AnalysedHistoricItem> GetRelevantHistorics(long analysedComponentId, 
             Expression<Func<AnalysedHistoricItem, bool>> predicate, int index = 0);
+
+        /// <summary>
+        /// This enables the caller to enter the AnalysedComponent in question, traverse to its parent in search for
+        /// the related component in question.
+        /// </summary>
+        /// <param name="analysedComponentId">The component we're utilising to look for the right type</param>
+        /// <param name="componentType">The type of the component we're looking for.</param>
+        /// <returns>Historicals of the target.</returns>
+        NozomiPaginatedResult<AnalysedHistoricItem> TraverseRelatedHistory(long analysedComponentId,
+            AnalysedComponentType componentType, int page = 0);
     }
 }
