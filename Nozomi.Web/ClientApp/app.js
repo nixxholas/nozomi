efb80976ba62f50b1b109218dd4b121c119c279a
nixxholas <nicholas@slyck.net>
2019-07-20 19:23:51 +0800

    Nozomi.Web: ClientApp: Axios resolution
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/app.js
+++ Nozomi.Web/ClientApp/app.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-import axios from 'axios'
-import router from './router/index'
-import store from './store'
-import { sync } from 'vuex-router-sync'
-import App from 'components/app-root'
-import { FontAwesomeIcon } from './icons'
+import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import axios from 'axios';
+import router from './router/index';
+import store from './store';
+import { sync } from 'vuex-router-sync';
+import App from 'components/app-root';
+import { FontAwesomeIcon } from './icons';''
 
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon);
@@ -14,7 +14,10 @@ Vue.component('icon', FontAwesomeIcon);
 // Registration of Buefy
 Vue.use(Buefy);
 
+// Registration of Axios
+// https://stackoverflow.com/questions/51374367/axios-is-not-defined-in-vue-js-cli
 Vue.prototype.$http = axios;
+Vue.prototype.$axios = axios;
 
 sync(store, router);
 
--- Nozomi.Web/ClientApp/components/home-page.vue
+++ Nozomi.Web/ClientApp/components/home-page.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
+      <b-table
+        :data="data"
+        :loading="loading"
 
+        paginated
+        backend-pagination
+        :total="total"
+        :per-page="perPage"
+        @page-change="onPageChange"
+        aria-next-label="Next page"
+        aria-previous-label="Previous page"
+        aria-page-label="Page"
+        aria-current-label="Current page">
+
+      </b-table>
     </div>
 </template>
 
@@ -16,26 +30,29 @@ export default {
     }
   },
   methods: {
-    async loadData() {
+    loadData() {
       this.loading = true;
 
       // Optionally the request above could also be done as
-      let result = await axios.get(
-        '/user', {
-        params: {
-          ID: 12345
-        }
-      })
+      this.$axios.get('/api/Currency/GetAllDetailed/' + (this.page - 1))
         .then(function (response) {
-          console.log(response);
+          console.dir(response);
         })
         .catch(function (error) {
-          console.log(error);
+          console.dir(error);
         })
         .then(function () {
           // always executed
         });
+    },
+    onPageChange(page) {
+      this.page = page;
+      this.loadData()
     }
+  },
+  mounted() {
+    console.dir("Loading");
+    this.loadData();
   }
 }
 </script>
