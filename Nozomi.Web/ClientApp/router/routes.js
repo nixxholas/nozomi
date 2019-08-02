2fbf0154ce1fb4a266e1f8961c708033a8ccea3b
nixxholas <nicholas@slyck.net>
2019-08-02 18:36:55 +0800

    Nozomi.Web: ClientApp: Remove a crappy ass route
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/router/routes.js
+++ Nozomi.Web/ClientApp/router/routes.js
@@ -1,5 +1,4 @@
 import CounterExample from 'components/counter-example';
-import FetchData from 'components/fetch-data';
 import HomePage from 'components/home-page';
 import About from 'components/about';
 
@@ -9,7 +8,6 @@ export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
   { name: 'about', path: '/about', component: About, display: 'About', icon: 'info' },
   { name: 'counter', path: '/counter', component: CounterExample, display: 'Counter', icon: 'graduation-cap' },
-  { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Data', icon: 'list' },
   // Currency-specific routing
   { name: 'view-currency', path: '/currency/:slug', props: true, component: ViewCurrency }
 ];
