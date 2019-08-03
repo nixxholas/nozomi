75f6857760467234187d8763922f8f49e2ea4faa
nixholas <nixholas@icloud.com>
2019-08-03 16:55:57 +0800

    Nozomi.Web: Controllers: APIs: v1: Secure some APIs
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Web/Controllers/APIs/v1/ComponentType/ComponentTypeController.cs
+++ Nozomi.Web/Controllers/APIs/v1/ComponentType/ComponentTypeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,6 +19,7 @@ namespace Nozomi.Web.Controllers.APIs.v1.ComponentType
             _componentTypeEvent = componentTypeEvent;
         }
 
+        [Authorize(Roles = "Staff")]
         [HttpGet]
         public NozomiResult<JsonResult> All()
         {
--- Nozomi.Web/Controllers/APIs/v1/Request/RequestController.cs
+++ Nozomi.Web/Controllers/APIs/v1/Request/RequestController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -19,6 +20,7 @@ namespace Nozomi.Web.Controllers.APIs.v1.Request
             _requestEvent = requestEvent;
         }
 
+        [Authorize(Roles = "Staff")]
         [HttpGet]
         public NozomiResult<JsonResult> All(bool includeNested)
         {
