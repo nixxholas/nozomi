5675b8cfaaa475bf080c8a66db769264fd471602
nixholas <nixholas@icloud.com>
2019-08-06 17:05:38 +0800

    Nozomi.Base.Domain: ResponseModels: Currency: DetailedCurrencyResponse: Deepen Currency integration
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ResponseModels/Currency/DetailedCurrencyResponse.cs
+++ Nozomi.Base.Domain/ResponseModels/Currency/DetailedCurrencyResponse.cs
@@ -5,6 +5,7 @@ using Nozomi.Base.Core.Helpers.Enumerator;
 using Nozomi.Base.Core.Helpers.Native.Numerals;
 using Nozomi.Data.AreaModels.v1.RequestComponent;
 using Nozomi.Data.Models.Web.Analytical;
+using Nozomi.Data.ResponseModels.CurrencyProperty;
 using Nozomi.Data.ResponseModels.RequestComponent;
 using Nozomi.Data.ResponseModels.TickerPair;
 
@@ -12,15 +13,20 @@ namespace Nozomi.Data.ResponseModels.Currency
 {
     public class DetailedCurrencyResponse : GeneralisedCurrencyResponse
     {
+        public string Description { get; set; }
+        
         public new List<DateValuePair<decimal>> AveragePriceHistory { get; set; }
         
         public ICollection<RequestComponentResponse> RequestComponents { get; set; }
+        
+        public ICollection<CurrencyPropertyResponse> Properties { get; set; }
         
         public DetailedCurrencyResponse(Models.Currency.Currency currency, ICollection<CurrencyTickerPair> currencyTickerPairs)
         {
             // Aggregate non-compounded properties first
             Name = currency.Name;
             Abbreviation = currency.Abbreviation;
+            Description = currency.Description;
             Slug = currency.Slug;
             LastUpdated = currency.ModifiedAt;
             LogoPath = currency.LogoPath;
@@ -92,6 +98,7 @@ namespace Nozomi.Data.ResponseModels.Currency
             // Aggregate non-compounded properties first
             Name = currency.Name;
             Abbreviation = currency.Abbreviation;
+            Description = currency.Description;
             Slug = currency.Slug;
             LastUpdated = currency.ModifiedAt;
             LogoPath = currency.LogoPath;
@@ -165,6 +172,100 @@ namespace Nozomi.Data.ResponseModels.Currency
                     })
                         .ToList();
                 }
+            }
+        }
+        public DetailedCurrencyResponse(Models.Currency.Currency currency, 
+            ICollection<Models.Web.RequestComponent> requestComponents, 
+            ICollection<Models.Currency.CurrencyProperty> properties)
+        {
+            // Aggregate non-compounded properties first
+            Name = currency.Name;
+            Abbreviation = currency.Abbreviation;
+            Description = currency.Description;
+            Slug = currency.Slug;
+            LastUpdated = currency.ModifiedAt;
+            LogoPath = currency.LogoPath;
+
+            foreach (var ac in currency.AnalysedComponents)
+            {
+                switch (ac.ComponentType)
+                {
+                    case AnalysedComponentType.DailyVolume:
+                        DailyVolume = decimal.Parse(ac.Value ?? "0");
+                        break;
+                    case AnalysedComponentType.MarketCap:
+                        MarketCap = decimal.Parse(ac.Value ?? "0");
+                        break;
+                    case AnalysedComponentType.CurrentAveragePrice:
+                        AveragePrice = decimal.Parse(ac.Value ?? "0");
+                        break;
+                    case AnalysedComponentType.HourlyAveragePrice:
+                        if (ac.AnalysedHistoricItems != null && ac.AnalysedHistoricItems.Count > 0)
+                        {
+                            AveragePriceHistory = ac.AnalysedHistoricItems
+                                .Where(ahi => NumberHelper.IsNumericDecimal(ahi.Value)
+                                              && ahi.HistoricDateTime > DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)))
+                                .OrderBy(ahi => ahi.HistoricDateTime)
+                                .Select(ahi => new DateValuePair<decimal>() {
+                                    Time = ahi.HistoricDateTime, 
+                                    Value = decimal.Parse(ahi.Value)
+                                })
+                                .ToList();
+                        }
+
+                        break;
+                    case AnalysedComponentType.DailyPricePctChange:
+                        DailyAvgPctChange = decimal.Parse(ac.Value ?? "0");
+                        break;
+                    default:
+                        if (Historical == null)
+                            Historical = new Dictionary<AnalysedComponentType, List<ComponentHistoricalDatum>>();
+
+                        Historical.Add(ac.ComponentType, new List<ComponentHistoricalDatum>());
+                        
+                        Historical[ac.ComponentType].Add(new ComponentHistoricalDatum()
+                        {
+                            CreatedAt = ac.ModifiedAt,
+                            Value = ac.Value
+                        });
+                        
+                        if (ac.AnalysedHistoricItems != null && ac.AnalysedHistoricItems.Count > 0)
+                        {
+                            Historical[ac.ComponentType].AddRange(ac.AnalysedHistoricItems
+                                .Select(ahi => new ComponentHistoricalDatum()
+                                {
+                                    CreatedAt = ahi.HistoricDateTime,
+                                    Value = ahi.Value
+                                }));
+                        }
+
+                        Historical[ac.ComponentType] = Historical[ac.ComponentType]
+                            .OrderByDescending(chd => chd.CreatedAt)
+                            .ToList();
+                        break;
+                }
+            }
+
+            if (requestComponents != null && requestComponents.Count > 0)
+            {
+                RequestComponents = requestComponents.Select(rc => new RequestComponentResponse()
+                    {
+                        Name = EnumHelper.GetDescription(rc.ComponentType),
+                        Timestamp = rc.ModifiedAt,
+                        Value = rc.Value
+                    })
+                    .ToList();
+            }
+
+            if (properties != null && properties.Count > 0)
+            {
+                Properties = properties
+                    .Select(p => new CurrencyPropertyResponse
+                    {
+                        Type = p.Type.GetDescription(),
+                        Value = p.Value
+                    })
+                    .ToList();
             }
         }
     }
---
+++ Nozomi.Base.Domain/ResponseModels/CurrencyProperty/CurrencyPropertyResponse.cs
@@ -0,0 +1,9 @@
+namespace Nozomi.Data.ResponseModels.CurrencyProperty
+{
+    public class CurrencyPropertyResponse
+    {
+        public string Type { get; set; }
+        
+        public string Value { get; set; }
+    }
+}
