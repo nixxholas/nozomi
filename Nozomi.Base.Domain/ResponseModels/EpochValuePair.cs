f663001737fd2823dbae60853e4464202326bee4
nixholas <nixholas@icloud.com>
2019-08-06 20:30:28 +0800

    Nozomi.Base.Domain: ResponseModels: EpochValuePair: Generics hotfixing
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ResponseModels/EpochValuePair.cs
+++ Nozomi.Base.Domain/ResponseModels/EpochValuePair.cs
@@ -1,6 +1,6 @@
 namespace Nozomi.Data.ResponseModels
 {
-    public class EpochValuePair<T> : DateValuePair<T> where T : class
+    public class EpochValuePair<T> : DateValuePair<T>
     {
         public new double Time { get; set; }
     }
