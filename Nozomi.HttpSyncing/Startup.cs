189979a1989a81fef2bf1cd346dcea28c7df3d54
nixholas <nixholas@icloud.com>
2019-08-22 23:40:56 +0800

    Nozomi.HttpSyncing: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.HttpSyncing/Startup.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nozomi.Infra.Analysis.Service.HostedServices.RequestTypes;
+using Nozomi.Repo.BCL.Context;
+using Nozomi.Repo.BCL.Repository;
+using Nozomi.Repo.Data;
+using Nozomi.Service.Events;
+using Nozomi.Service.Events.Interfaces;
+using Nozomi.Service.Services;
+using Nozomi.Service.Services.Interfaces;
+using Nozomi.Service.Services.Requests;
+using Nozomi.Service.Services.Requests.Interfaces;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace Nozomi.HttpSyncing
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+        
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (HostingEnvironment.IsDevelopment())
+            {
+                // Greet the beloved dev
+                Console.WriteLine(@"Welcome to the dev environment, your machine is named: " + Environment.MachineName);
+
+                // Postgres DB Setup
+                var str = Configuration.GetConnectionString("Local:" + @Environment.MachineName);
+
+                services
+                    .AddEntityFrameworkNpgsql()
+                    .AddDbContext<NozomiDbContext>(options =>
+                        {
+                            options.UseNpgsql(str);
+                            options.EnableSensitiveDataLogging();
+                            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                        },
+                        ServiceLifetime.Transient);
+            }
+            else
+            {
+                var vaultToken = Configuration["vaultToken"];
+
+                if (string.IsNullOrEmpty(vaultToken))
+                    throw new SystemException("Invalid vault token.");
+
+                var authMethod = new TokenAuthMethodInfo(vaultToken);
+                var vaultClientSettings = new VaultClientSettings("http://vault.nozomi.one:8200", authMethod);
+                var vaultClient = new VaultClient(vaultClientSettings);
+
+                var nozomiVault = vaultClient.V1.Secrets.Cubbyhole.ReadSecretAsync("nozomi")
+                    .GetAwaiter()
+                    .GetResult().Data;
+
+                var mainDb = (string) nozomiVault["main"];
+                if (string.IsNullOrEmpty(mainDb))
+                    throw new SystemException("Invalid main database configuration");
+                // Database
+                services.AddDbContext<NozomiDbContext>(options =>
+                {
+                    options.UseNpgsql(mainDb
+                        , builder =>
+                        {
+                            builder.EnableRetryOnFailure();
+                        }
+                    );
+                    options.EnableSensitiveDataLogging(false);
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                }, ServiceLifetime.Transient);
+            }
+            
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddTransient<IUnitOfWork<NozomiDbContext>, UnitOfWork<NozomiDbContext>>();
+            services.AddTransient<IDbContext, NozomiDbContext>();
+            
+            services.AddScoped<IRequestEvent, RequestEvent>();
+            services.AddTransient<IRcdHistoricItemService, RcdHistoricItemService>();
+            services.AddTransient<IRequestComponentService, RequestComponentService>();
+            services.AddTransient<IRequestService, RequestService>();
+            services.AddHostedService<HttpGetRequestSyncingService>();
+            services.AddHostedService<HttpPostRequestSyncingService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+        }
+    }
+}
