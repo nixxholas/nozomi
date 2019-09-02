abde161c913e10f4b505bdb3af48cd56303b0092
nixholas <nixholas@icloud.com>
2019-09-02 22:43:58 +0800

    Nozomi.Repo: Migrations: r8 - ACLastCheckedRefactor Migration
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20190902143700_r8_ACLastCheckedRefactor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r8_ACLastCheckedRefactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastChecked",
+                table: "AnalysedComponents");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastChecked",
+                table: "AnalysedComponents",
+                nullable: true);
+        }
+    }
+}
