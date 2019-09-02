45e9e5ee96f1ca5b6a8be1acdac60696f8a66a67
nixxholas <nicholas@slyck.net>
2019-09-03 06:26:37 +0800

    Nozomi.Web: ClientApp: components: home-page: Test logic
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/components/home-page.vue
+++ Nozomi.Web/ClientApp/components/home-page.vue
@@ -234,6 +234,41 @@ export default {
 
       }
     },
+    async authWeb3() {
+        try {
+            // Modern dapp browsers...
+            if (window.ethereum) {
+                window.web3 = new Web3(ethereum);
+                try {
+                    await ethereum.enable();
+                    var accounts= await web3.eth.getAccounts();
+                    var option={from: accounts[0] };
+                    var myContract = new web3.eth.Contract(abi, contractAddress);
+                    myContract.methods.RegisterInstructor('11','Ali')
+                        .send(option,function(error,result){
+                            if (! error)
+                                console.log(result);
+                            else
+                                console.log(error);
+                        });
+                } catch (error) {
+                    // User denied account access...
+                }
+            }
+            // Legacy dapp browsers...
+            else if (window.web3) {
+                window.web3 = new Web3(web3.currentProvider);
+                // Acccounts always exposed
+                web3.eth.sendTransaction({/* ... */});
+            }
+            // Non-dapp browsers...
+            else {
+                console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+            }
+        } catch (e) {
+            console.dir("oops: " + e.toString());
+        }
+    },
     onPageChange(page) {
       this.cryptoTable.page = page;
       this.loadData();
@@ -243,12 +278,7 @@ export default {
     //this.loadData();
     this.loadFiatData();
     this.loadCryptoData();
-
-    try {
-        let web3 = new this.$Web3(this.$Web3.givenProvider);
-    } catch (e) {
-        console.dir("oops: " + e.toString());
-    }
+    //this.authWeb3();
   }
 
 }
