afdaea30e5fd30492654049461e9017c64e8e0c7
nixxholas <nicholas@slyck.net>
2019-09-03 19:12:42 +0800

    Nozomi.Web: ClientApp: components: Migrate some functions over properly
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/components/nav-menu.vue
+++ Nozomi.Web/ClientApp/components/nav-menu.vue
@@ -71,14 +71,47 @@
       methods: {
         toggleCollapsed: function () {
         },
-        login() {
+          async authWeb3() {
+              // Modern dapp browsers...
+              if (window.ethereum) {
+                  try {
+                      window.web3 = new Web3(ethereum);
+                      await ethereum.enable();
+                      let accounts = await web3.eth.getAccounts();
+                      let option = { from: accounts[0] };
+                      let myContract = new web3.eth.Contract(abi, contractAddress);
+                      myContract.methods.RegisterInstructor('11','Ali')
+                          .send(option,function(error,result){
+                              if (! error)
+                                  console.log(result);
+                              else
+                                  console.log(error);
+                          });
+                  } catch (error) {
+                      // User denied account access...
+                  }
+              }
+              // Legacy dapp browsers...
+              else if (window.web3) {
+                  window.web3 = new Web3(web3.currentProvider);
+                  // Acccounts always exposed
+                  web3.eth.sendTransaction({/* ... */});
+              }
+              // Non-dapp browsers...
+              else {
+                  console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+              }
+          },
+        async login() {
           this.$buefy.notification.open({
             duration: 5000,
             message: `Authentication functionality is coming soon!`,
             position: 'is-bottom-right',
             type: 'is-warning',
             hasIcon: true
-          })
+          });
+
+          await this.authWeb3();
         }
       }
     }
