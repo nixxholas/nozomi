c1131268771a721369b44a02e790e3a7827da9e5
nixxholas <nicholas@slyck.net>
2019-09-10 23:39:49 +0800

    Nozomi.Infra.Auth: Services: Address: Create API: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Infra.Auth/Services/Address/AddressService.cs
+++ Nozomi.Infra.Auth/Services/Address/AddressService.cs
@@ -1,4 +1,8 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Nozomi.Base.Auth.Models;
+using Nozomi.Base.Auth.Models.Wallet;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.Auth.Data;
 using Nozomi.Repo.BCL.Repository;
@@ -10,6 +14,31 @@ namespace Nozomi.Infra.Auth.Services.Address
         public AddressService(ILogger<AddressService> logger, IUnitOfWork<AuthDbContext> unitOfWork) 
             : base(logger, unitOfWork)
         {
+        }
+
+        public string Create(string userId, string address, AddressType type)
+        {
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(address))
+                return string.Empty;
+
+            var user = _unitOfWork.GetRepository<User>()
+                .GetQueryable()
+                .AsNoTracking()
+                .SingleOrDefault(u => u.Id.Equals(userId));
+
+            if (user == null)
+                return string.Empty;
+
+            var addr = new Base.Auth.Models.Wallet.Address()
+            {
+                UserId = userId,
+                Hash = address,
+                Type = type
+            };
+            
+            _unitOfWork.GetRepository<Base.Auth.Models.Wallet.Address>().Add(addr);
+
+            return addr.Id;
         }
     }
 }
