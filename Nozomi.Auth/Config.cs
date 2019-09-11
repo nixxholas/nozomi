94b981e254438c3c1bf18086dbdce8062be639fe
nixxholas <nicholas@slyck.net>
2019-09-11 18:49:54 +0800

    Nozomi.Auth: Config: TestUser collection implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Auth/Config.cs
+++ Nozomi.Auth/Config.cs
@@ -2,10 +2,14 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Security.Claims;
 using IdentityModel;
+using IdentityServer4;
 using Nozomi.Base.Auth.Global;
+using Nozomi.Base.Auth.Models;
 
 namespace Nozomi.Auth
 {
@@ -70,59 +74,126 @@ namespace Nozomi.Auth
             return new[]
             {
                 // client credentials flow client
-                new Client
-                {
-                    ClientId = "client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
-
-                    AllowedScopes = {"api1"}
-                },
+//                new Client
+//                {
+//                    ClientId = "client",
+//                    ClientName = "Client Credentials Client",
+//
+//                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+//                    ClientSecrets = {new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
+//
+//                    AllowedScopes = {"api1"}
+//                },
 
                 // MVC client using hybrid flow
-                new Client
-                {
-                    ClientId = "mvc",
-                    ClientName = "MVC Client",
-
-                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                    ClientSecrets = {new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256())},
-
-                    RedirectUris = {"http://localhost:5001/signin-oidc"},
-                    FrontChannelLogoutUri = "http://localhost:5001/signout-oidc",
-                    PostLogoutRedirectUris = {"http://localhost:5001/signout-callback-oidc"},
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = {"openid", "profile", "api1"}
-                },
+//                new Client
+//                {
+//                    ClientId = "mvc",
+//                    ClientName = "MVC Client",
+//
+//                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+//                    ClientSecrets = {new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256())},
+//
+//                    RedirectUris = {"http://localhost:5001/signin-oidc"},
+//                    FrontChannelLogoutUri = "http://localhost:5001/signout-oidc",
+//                    PostLogoutRedirectUris = {"http://localhost:5001/signout-callback-oidc"},
+//
+//                    AllowOfflineAccess = true,
+//                    AllowedScopes = {"openid", "profile", "api1"}
+//                },
 
                 // SPA client using code flow + pkce
+//                new Client
+//                {
+//                    ClientId = "spa",
+//                    ClientName = "SPA Client",
+//                    ClientUri = "http://identityserver.io",
+//
+//                    AllowedGrantTypes = GrantTypes.Code,
+//                    RequirePkce = true,
+//                    RequireClientSecret = false,
+//
+//                    RedirectUris =
+//                    {
+//                        "http://localhost:5002/index.html",
+//                        "http://localhost:5002/callback.html",
+//                        "http://localhost:5002/silent.html",
+//                        "http://localhost:5002/popup.html",
+//                    },
+//
+//                    PostLogoutRedirectUris = {"http://localhost:5002/index.html"},
+//                    AllowedCorsOrigins = {"http://localhost:5002"},
+//
+//                    AllowedScopes = {"openid", "profile", "api1"}
+//                }
+
                 new Client
                 {
-                    ClientId = "spa",
-                    ClientName = "SPA Client",
-                    ClientUri = "http://identityserver.io",
+                    ClientId = "nozweb",
+                    ClientName = "Nozomi Web Client",
+                    ClientUri = "https://nozomi.one",
 
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    RequireClientSecret = false,
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
 
-                    RedirectUris =
+                    RedirectUris =           { "http://localhost:5001/index.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5001/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5001" },
+
+                    AllowedScopes =
                     {
-                        "http://localhost:5002/index.html",
-                        "http://localhost:5002/callback.html",
-                        "http://localhost:5002/silent.html",
-                        "http://localhost:5002/popup.html",
-                    },
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
 
-                    PostLogoutRedirectUris = {"http://localhost:5002/index.html"},
-                    AllowedCorsOrigins = {"http://localhost:5002"},
-
-                    AllowedScopes = {"openid", "profile", "api1"}
+                        "nozomiapi.read_only"
+                    }
                 }
             };
         }
+            
+        public static List<User> TestUsers = new List<User>()
+        {
+            new User
+            {
+                Id = Guid.NewGuid().ToString(), 
+                UserName = "alice", 
+                EmailConfirmed = true,
+                PasswordHash = "alice", 
+                UserClaims = new List<UserClaim>(){
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.Name, 
+                        ClaimValue = "Alice Smith" 
+                    },
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.GivenName, 
+                        ClaimValue = "Alice"
+                    },
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.FamilyName, 
+                        ClaimValue = "Smith"
+                    },
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.Email,
+                        ClaimValue = "AliceSmith@email.com"
+                    },
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.WebSite, 
+                        ClaimValue = "http://alice.com"
+                    },
+                    new UserClaim
+                    {
+                        ClaimType = JwtClaimTypes.Address, 
+                        ClaimValue = 
+                            @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }"
+                    }
+                }
+            }
+        };
     }
 }
