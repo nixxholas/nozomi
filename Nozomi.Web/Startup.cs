b89403350c037ad681a14b0340871da92c265fe0
nixxholas <nicholas@slyck.net>
2019-09-23 21:52:31 +0800

    Nozomi.Web: Startup: Configure Authentication with OIDC
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/Startup.cs
+++ Nozomi.Web/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
@@ -124,11 +127,46 @@ namespace Nozomi.Web
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddAuthentication()
-                .AddJwtBearer(options =>
+            services.AddAuthentication(options =>
                 {
-                    options.Audience = "nozomi.web";
-                    options.Authority = "https://localhost:6001";
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    options.SlidingExpiration = false;
+                    options.ExpireTimeSpan = TimeSpan.FromHours(2);
+                })
+//                .AddJwtBearer(options =>
+//                {
+//                    options.Audience = "nozomi.web";
+//                    options.Authority = "https://localhost:6001";
+//                })
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+
+                    if (env.Equals("production", StringComparison.OrdinalIgnoreCase))
+                    {
+                        options.Authority = "https://auth.nozomi.one";
+                        options.RequireHttpsMetadata = true;
+                    }
+                    else // Even if it is null, just do localhost..
+                    {
+                        options.Authority = "https://localhost:6001";
+                        options.RequireHttpsMetadata = true;
+                    }
+
+                    options.ClientId = "nozomi.vue";
+                    options.ClientSecret = "super-secret";
+                    options.ResponseType = "code id_token";
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("nozomi.web.read_only");
+                    options.Scope.Add("offline_access");
+                    //options.ClaimActions.MapJsonKey("website", "website");
                 });
 
             services.AddAuthorization(options =>
