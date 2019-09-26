52a13a28bd85d5ef093920fe7a92031288c22980
nixxholas <nicholas@slyck.net>
2019-09-27 06:26:08 +0800

    Nozomi.Web: Propagate the conventional implicit auth.
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/StartupExtensions/StartupIdentity.cs
+++ Nozomi.Web/StartupExtensions/StartupIdentity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -16,55 +17,59 @@ namespace Nozomi.Web.StartupExtensions
     {
         public static void ConfigureNozomiAuth(this IServiceCollection services, string env)
         {
-            // Turn off the JWT claim type mapping to allow well-known claims (e.g. ‘sub’ and ‘idp’) to flow through unmolested
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-            if (!env.Equals("production", StringComparison.OrdinalIgnoreCase))
-                IdentityModelEventSource.ShowPII = true;
-
-            services.AddAuthentication(options =>
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
                 {
-                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = "oidc";
-                })
-                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-                {
-                    options.SlidingExpiration = false;
-                    options.ExpireTimeSpan = TimeSpan.FromHours(2);
-                })
-//                .AddJwtBearer(options =>
-//                {
-//                    options.Audience = "nozomi.web";
-//                    options.Authority = "https://localhost:6001";
-//                })
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.SignInScheme = "Cookies";
-
-                    if (env.Equals("production", StringComparison.OrdinalIgnoreCase))
-                    {
-                        options.Authority = "https://auth.nozomi.one";
-                        options.RequireHttpsMetadata = true;
-                    }
-                    else // Even if it is null, just do localhost..
-                    {
-                        options.Authority = "https://localhost:6001";
-                        options.RequireHttpsMetadata = true;
-                    }
-
-                    options.CallbackPath = "/auth-oidc";
-
-                    options.ClientId = "nozomi.vue";
-                    options.ClientSecret = "super-secret";
-                    options.ResponseType = "code id_token";
-
-                    options.SaveTokens = true;
-                    options.GetClaimsFromUserInfoEndpoint = true;
-
-                    options.Scope.Add("nozomi.web.read_only");
-                    options.Scope.Add("offline_access");
-                    //options.ClaimActions.MapJsonKey("website", "website");
+                    options.Authority = "https://localhost:6001/";
+                    options.RequireHttpsMetadata = true;
+                    options.ApiName = "nozomi.web";
                 });
+
+            // Turn off the JWT claim type mapping to allow well-known claims (e.g. ‘sub’ and ‘idp’) to flow through unmolested
+//            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+//
+//            if (!env.Equals("production", StringComparison.OrdinalIgnoreCase))
+//                IdentityModelEventSource.ShowPII = true;
+//
+//            services.AddAuthentication(options =>
+//                {
+//                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//                    options.DefaultChallengeScheme = "oidc";
+//                })
+//                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+//                {
+//                    options.SlidingExpiration = false;
+//                    options.ExpireTimeSpan = TimeSpan.FromHours(2);
+//                })
+//                .AddOpenIdConnect("oidc", options =>
+//                {
+//                    options.SignInScheme = "Cookies";
+//
+//                    if (env.Equals("production", StringComparison.OrdinalIgnoreCase))
+//                    {
+//                        options.Authority = "https://auth.nozomi.one";
+//                        options.RequireHttpsMetadata = true;
+//                    }
+//                    else // Even if it is null, just do localhost..
+//                    {
+//                        options.Authority = "https://localhost:6001";
+//                        options.RequireHttpsMetadata = true;
+//                    }
+//
+//                    options.CallbackPath = "/auth-oidc";
+//
+//                    options.ClientId = "nozomi.vue";
+//                    options.ClientSecret = "super-secret";
+//                    options.ResponseType = "code id_token";
+//
+//                    options.SaveTokens = true;
+//                    options.GetClaimsFromUserInfoEndpoint = true;
+//
+//                    options.Scope.Add("nozomi.web.read_only");
+//                    options.Scope.Add("offline_access");
+//                    //options.ClaimActions.MapJsonKey("website", "website");
+//                });
         }
     }
 }
