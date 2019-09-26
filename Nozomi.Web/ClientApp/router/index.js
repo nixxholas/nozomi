6cb2c4733b22099209c0456dff134986189099e0
nixxholas <nicholas@slyck.net>
2019-09-27 06:46:49 +0800

    Nozomi.Web: ClientApp: router: index: Router guard
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/router/index.js
+++ Nozomi.Web/ClientApp/router/index.js
@@ -7,6 +7,23 @@ Vue.use(VueRouter);
 let router = new VueRouter({
   mode: 'history',
   routes
+})
+// Before each route is accessed,
+.beforeEach(async (to, from, next) => {
+  let app = router.app.$data || {isAuthenticated: false} ;
+  if (app.isAuthenticated) {
+    // already signed in, we can navigate anywhere
+    next()
+  } else if (to.matched.some(record => record.meta.requiresAuth)) {
+    // authentication is required. Trigger the sign in process, including the return URI
+    router.app.authenticate(to.path).then(() => {
+      console.log('authenticating a protected url:' + to.path);
+      next();
+    });
+  } else {
+    //No auth required. We can navigate
+    next()
+  }
 });
 
 export default router
