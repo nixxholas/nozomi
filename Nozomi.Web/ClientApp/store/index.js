f91b9e69ec8f0904aee8054638b9359494710970
nixxholas <nicholas@slyck.net>
2019-09-28 09:25:37 +0800

    Nozomi.Web: ClientApp: I actually realised that the stores already have Vuex active
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/store/index.js
+++ Nozomi.Web/ClientApp/store/index.js
@@ -1,29 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 // TYPES
-const MAIN_SET_COUNTER = 'MAIN_SET_COUNTER'
+const MAIN_SET_COUNTER = 'MAIN_SET_COUNTER';
 
 // STATE
 const state = {
   counter: 1
-}
+};
 
 // MUTATIONS
 const mutations = {
   [MAIN_SET_COUNTER] (state, obj) {
     state.counter = obj.counter
   }
-}
+};
 
 // ACTIONS
 const actions = ({
+  login({commit}, user){
+    return new Promise((resolve, reject) => {
+      commit('auth_request');
+      axios({url: 'http://localhost:3000/login', data: user, method: 'POST' })
+        .then(resp => {
+          const token = resp.data.token;
+          const user = resp.data.user;
+          localStorage.setItem('token', token);
+          axios.defaults.headers.common['Authorization'] = token;
+          commit('auth_success', token, user);
+          resolve(resp)
+        })
+        .catch(err => {
+          commit('auth_error');
+          localStorage.removeItem('token');
+          reject(err)
+        })
+    })
+  },
   setCounter ({ commit }, obj) {
     commit(MAIN_SET_COUNTER, obj)
   }
-})
+});
 
 export default new Vuex.Store({
   state,
