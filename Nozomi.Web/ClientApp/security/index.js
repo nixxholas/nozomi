cd43f844c85ac926d222d4c2ff546876791be5b1
nixholas <nixholas@icloud.com>
2019-10-06 02:32:44 +0800

    Nozomi.Web: ClientApp: security: index: env support
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Web/ClientApp/security/index.js
+++ Nozomi.Web/ClientApp/security/index.js
@@ -2,12 +2,12 @@
 import Oidc from 'oidc-client';
 
 let mgr = new Oidc.UserManager({
-  authority: 'https://localhost:6001',
+  authority: process.env.NODE_ENV === "production" ? "https://auth.nozomi.one" : 'https://localhost:6001',
   client_id: 'nozomi.spa',
-  redirect_uri: 'https://localhost:5001/callback',
+  redirect_uri: process.env.NODE_ENV === "production" ? "https://nozomi.one/auth-oidc" : 'https://localhost:5001/auth-oidc',
   response_type: 'id_token token',
   scope: 'openid profile nozomi.api',
-  post_logout_redirect_uri: 'https://localhost:5001/',
+  post_logout_redirect_uri: process.env.NODE_ENV === "production" ? "https://nozomi.one" : 'https://localhost:5001/',
   userStore: new Oidc.WebStorageStateStore({ store: window.localStorage }),
 });
 
