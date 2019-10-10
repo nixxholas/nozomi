ed19f2c2bf2f83673f05ed834a7da2c3a1ccf3b9
nixxholas <nicholas@slyck.net>
2019-10-11 06:44:14 +0800

    Nozomi.Web: ClientApp: store: config: declare production configurations explicitly.
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/store/config.js
+++ Nozomi.Web/ClientApp/store/config.js
@@ -3,11 +3,13 @@ import { WebStorageStateStore } from 'oidc-client';
 export const oidcSettings = {
   authority: process.env.NODE_ENV === "production" ? "https://auth.nozomi.one" : 'https://localhost:6001',
   client_id: 'nozomi.spa',
-  redirect_uri: window.location.origin + "/oidc-callback",
+  redirect_uri: process.env.NODE_ENV === "production" ? "https://nozomi.one/oidc-callback"
+    : window.location.origin + "/oidc-callback",
   response_type: 'id_token token',
   scope: 'openid profile nozomi.web.read_only',
-  post_logout_redirect_uri: window.location.origin,
-  silent_redirect_uri: window.location.origin + '/oidc-silent-renew',
+  post_logout_redirect_uri: process.env.NODE_ENV === "production" ? "https://nozomi.one" : window.location.origin,
+  silent_redirect_uri: process.env.NODE_ENV === "production" ? "https://nozomi.one/oidc-silent-renew"
+    : window.location.origin + '/oidc-silent-renew',
   accessTokenExpiringNotificationTime: 10,
   automaticSilentRenew: true,
   filterProtocolClaims: true,
