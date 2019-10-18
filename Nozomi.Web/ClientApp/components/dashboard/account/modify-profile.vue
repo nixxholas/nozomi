8bce1dd8d4af88910e48954c2e7627b039065281
nixxholas <nicholas@slyck.net>
2019-10-18 14:32:15 +0800

    Nozomi.Web: ClientApp: components: dashboard: account: modify-profile: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Web/ClientApp/components/dashboard/account/modify-profile.vue
@@ -0,0 +1,37 @@
+<template>
+{{ profile }}
+</template>
+
+<script>
+    import { mapGetters, mapActions } from 'vuex';
+
+    export default {
+        name: "Modify Profile",
+        computed: {
+            ...mapGetters([
+                'oidcIsAuthenticated',
+                'oidcUser',
+                'oidcAccessToken',
+                'oidcAccessTokenExp',
+                'oidcIdToken',
+                'oidcIdTokenExp',
+                'oidcAuthenticationIsChecked',
+                'oidcError'
+            ]),
+            hasAccess: function() {
+                return this.oidcIsAuthenticated || this.$route.meta.isPublic
+            }
+        },
+        data: function () {
+            return {
+                profile: function() {
+                    return this.oidcUser();
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
