0f33fd938ad2be9cb648ca25470c52fdb8043203
nixxholas <nicholas@slyck.net>
2019-10-20 23:23:31 +0800

    Nozomi.Web: ClientApp: components: dashboard: forms: create-request-modal: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Web/ClientApp/components/dashboard/forms/create-request-modal.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <button class="button is-primary"
+            @click="isCreateRequestModalActive = true">
+      Create
+    </button>
+
+    <b-modal has-modal-card trap-focus :active.sync="isCreateRequestModalActive">
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Create a request</p>
+          </header>
+          <section class="modal-card-body">
+            <form action="">
+            <b-field label="URL">
+              <b-input
+                type="url"
+                placeholder="https://nozomi.one/api/Ticker/GetAllAsync/0"
+                v-bind="form.apiUrl"
+                required
+                expanded>
+              </b-input>
+            </b-field>
+            <b-field>
+              <template slot="label">
+                Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
+              </template>
+              <b-input
+                type="number"
+                placeholder="Delay between each update in milliseconds"
+                v-bind="form.delay"
+                expanded
+                required>
+              </b-input>
+            </b-field>
+            <b-field>
+              <template slot="label">
+                Retry Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
+              </template>
+              <b-input
+                type="number"
+                placeholder="Retry attempt delay in milliseconds"
+                v-bind="form.failureDelay"
+                required
+                expanded>
+              </b-input>
+            </b-field>
+            </form>
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button" type="button" @click="$parent.close()">Close</button>
+            <button class="button is-primary">Submit</button>
+          </footer>
+        </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "create-request-modal",
+        props: {
+            // Nothing yet
+        },
+        data: function() {
+            return {
+                isCreateRequestModalActive: false,
+                form: {
+                    apiUrl: '',
+                    delay: 0,
+                    failureDelay: 0,
+                },
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
