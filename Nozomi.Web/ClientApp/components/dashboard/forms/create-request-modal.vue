83db5d5d727efabafc40a9f28adf4d8a46b79969
nixxholas <nicholas@slyck.net>
2019-10-22 21:24:00 +0800

    Nozomi.Web: ClientApp: components: dashboard: forms: create-request-modal: deepen implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/components/dashboard/forms/create-request-modal.vue
+++ Nozomi.Web/ClientApp/components/dashboard/forms/create-request-modal.vue
@@ -6,14 +6,14 @@
     </button>
 
     <b-modal has-modal-card trap-focus :active.sync="isCreateRequestModalActive">
-        <div class="modal-card">
-          <header class="modal-card-head">
-            <p class="modal-card-title">Create a request</p>
-          </header>
-          <section class="modal-card-body">
-            <form action="">
-              <RequestTypeDrowdown v-model="form.type"></RequestTypeDrowdown>
-              <ResponseTypeDropdown v-model="form.responseType"></ResponseTypeDropdown>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Create a request</p>
+        </header>
+        <section class="modal-card-body">
+          <form action="">
+            <RequestTypeDrowdown v-model="form.type"></RequestTypeDrowdown>
+            <ResponseTypeDropdown v-model="form.responseType"></ResponseTypeDropdown>
             <b-field label="URL">
               <b-input
                 type="url"
@@ -47,13 +47,22 @@
                 expanded>
               </b-input>
             </b-field>
-            </form>
-          </section>
-          <footer class="modal-card-foot">
-            <button class="button" type="button" @click="$parent.close()">Close</button>
-            <button class="button is-primary">Submit</button>
-          </footer>
-        </div>
+            <b-tabs v-model="form.requestParentType" expanded class="has-text-dark">
+              <b-tab-item label="Currency">
+                <b-field>
+
+                </b-field>
+              </b-tab-item>
+              <b-tab-item label="Currency Pair"></b-tab-item>
+              <b-tab-item label="Currency Type"></b-tab-item>
+            </b-tabs>
+          </form>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button" type="button" @click="isCreateRequestModalActive = false">Close</button>
+          <button class="button is-primary">Submit</button>
+        </footer>
+      </div>
     </b-modal>
   </div>
 </template>
@@ -64,20 +73,27 @@
 
     export default {
         name: "create-request-modal",
-        components: { ResponseTypeDropdown, RequestTypeDrowdown },
+        components: {ResponseTypeDropdown, RequestTypeDrowdown},
         props: {
             // Nothing yet
         },
-        data: function() {
+        beforeCreate: {
+
+        },
+        data: function () {
             return {
                 isCreateRequestModalActive: false,
                 form: {
+                    activeTab: 0, // For requestParentType
                     type: 0,
-                    responseType: 0,
+                    responseType: 1,
                     apiUrl: '',
                     delay: 0,
                     failureDelay: 0,
+                    requestParentType: 0
                 },
+                formHelper: {},
+                currencies: []
             }
         }
     }
