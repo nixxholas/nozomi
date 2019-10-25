19ba4ae2a74e685c21c16a969bd12f8d43db76f9
nixxholas <nicholas@slyck.net>
2019-10-25 10:40:26 +0800

    Nozomi.Web: ClientApp: components: dashboard: forms: create-request-modal: Resolve underlying bugs, implement proper submission
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/components/dashboard/forms/create-request-modal.vue
+++ Nozomi.Web/ClientApp/components/dashboard/forms/create-request-modal.vue
@@ -6,106 +6,111 @@
     </button>
 
     <b-modal has-modal-card trap-focus :active.sync="isCreateRequestModalActive">
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Create a request</p>
-        </header>
-        <section class="modal-card-body">
-          <form action="">
-            <RequestTypeDrowdown v-model="form.type"></RequestTypeDrowdown>
-            <ResponseTypeDropdown v-model="form.responseType"></ResponseTypeDropdown>
-            <b-field label="URL">
-              <b-input
-                type="url"
-                placeholder="https://nozomi.one/api/Ticker/GetAllAsync/0"
-                v-bind="form.apiUrl"
-                required
-                expanded>
-              </b-input>
-            </b-field>
-            <b-field>
-              <template slot="label">
-                Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
-              </template>
-              <b-input
-                type="number"
-                placeholder="Delay between each update in milliseconds"
-                v-bind="form.delay"
-                expanded
-                required>
-              </b-input>
-            </b-field>
-            <b-field>
-              <template slot="label">
-                Retry Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
-              </template>
-              <b-input
-                type="number"
-                placeholder="Retry attempt delay in milliseconds"
-                v-bind="form.failureDelay"
-                required
-                expanded>
-              </b-input>
-            </b-field>
+      <b-loading :active.sync="isModalLoading" :can-cancel="false"></b-loading>
+      <!--https://stackoverflow.com/questions/48028718/using-event-modifier-prevent-in-vue-to-submit-form-without-redirection-->
+        <form v-on:submit.prevent="create()">
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">Create a request</p>
+            </header>
+            <section class="modal-card-body">
+              <RequestTypeDrowdown v-model="form.type"></RequestTypeDrowdown>
+              <ResponseTypeDropdown v-model="form.responseType"></ResponseTypeDropdown>
+              <b-field label="URL">
+                <b-input
+                  type="url"
+                  placeholder="https://nozomi.one/api/Ticker/GetAllAsync/0"
+                  v-model="form.dataPath"
+                  required
+                  expanded>
+                </b-input>
+              </b-field>
+              <b-field>
+                <template slot="label">
+                  Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
+                </template>
+                <b-input
+                  type="number"
+                  placeholder="Delay between each update in milliseconds"
+                  v-model="form.delay"
+                  expanded
+                  required>
+                </b-input>
+              </b-field>
+              <b-field>
+                <template slot="label">
+                  Retry Delay <span class="has-text-primary is-italic">(In milliseconds)</span>
+                </template>
+                <b-input
+                  type="number"
+                  placeholder="Retry attempt delay in milliseconds"
+                  v-model="form.failureDelay"
+                  required
+                  expanded>
+                </b-input>
+              </b-field>
 
-            <b-tabs v-model="form.requestParentType" expanded class="has-text-dark">
-              <b-tab-item label="Currency">
-                <b-field label="Find a currency">
-                  <b-autocomplete
-                    rounded
-                    v-model="form.currencySlug"
-                    :data="currencies"
-                    placeholder="e.g. EUR"
-                    icon="magnify"
-                    @select="option => form.currencySlug = option">
-                    <template slot="empty">No results found</template>
-                  </b-autocomplete>
-                </b-field>
-              </b-tab-item>
-              <b-tab-item label="Currency Pair">
-                <b-field>
-                  <b-autocomplete
-                    :data="currencyPairs"
-                    placeholder="e.g. EURUSD"
-                    field="id"
-                    :custom-formatter="getCurrencyPairTickerPairStr"
-                    :loading="currencyPairsIsLoading"
-                    @select="option => form.currencyPair = option">
+              <b-tabs v-model="form.requestParentType" expanded class="has-text-dark">
+                <b-tab-item label="Currency">
+                  <b-field label="Find a currency">
+                    <b-autocomplete
+                      rounded
+                      v-model="form.currencySlug"
+                      :data="currencies"
+                      placeholder="e.g. EUR"
+                      icon="magnify"
+                      @select="option => form.currencySlug = option">
+                      <template slot="empty">No results found</template>
+                    </b-autocomplete>
+                  </b-field>
+                </b-tab-item>
+                <b-tab-item label="Currency Pair">
+                  <b-field>
+                    <b-autocomplete
+                      :data="currencyPairs"
+                      v-model="form.currencyPairId"
+                      placeholder="e.g. EURUSD"
+                      field="id"
+                      :custom-formatter="getCurrencyPairTickerPairStr"
+                      :loading="currencyPairsIsLoading"
+                      @select="option => form.currencyPair = option">
 
-                    <template slot-scope="props">
-                      <b-taglist attached>
-                        <b-tag type="is-dark">
-                          {{ props.option.mainTicker }}{{ props.option.counterTicker }}
-                        </b-tag>
-                        <b-tag type="is-info">
-                          <b>{{ props.option.sourceName }}</b>
-                        </b-tag>
-                      </b-taglist>
-                    </template>
-                  </b-autocomplete>
-                </b-field>
-              </b-tab-item>
-              <b-tab-item label="Currency Type">
-                <b-field v-if="currencyTypes !== null && currencyTypes.length > 0">
-                  <b-select placeholder="Select a currency type" v-model="form.currencyType">
-                    <option v-for="ct in currencyTypes" :value="ct.id">{{ ct.name }}</option>
-                  </b-select>
-                </b-field>
-              </b-tab-item>
-            </b-tabs>
-          </form>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button" type="button" @click="isCreateRequestModalActive = false">Close</button>
-          <button class="button is-primary">Submit</button>
-        </footer>
-      </div>
+                      <template slot-scope="props">
+                        <b-taglist attached>
+                          <b-tag type="is-dark">
+                            {{ props.option.mainTicker }}{{ props.option.counterTicker }}
+                          </b-tag>
+                          <b-tag type="is-info">
+                            <b>{{ props.option.sourceName }}</b>
+                          </b-tag>
+                        </b-taglist>
+                      </template>
+                    </b-autocomplete>
+                  </b-field>
+                </b-tab-item>
+                <b-tab-item label="Currency Type">
+                  <b-field v-if="currencyTypes !== null && currencyTypes.length > 0">
+                    <b-select placeholder="Select a currency type" v-model="form.currencyType">
+                      <option v-for="ct in currencyTypes" :value="ct.id">{{ ct.name }}</option>
+                    </b-select>
+                  </b-field>
+                </b-tab-item>
+              </b-tabs>
+            </section>
+
+            <footer class="modal-card-foot">
+              <button class="button" type="button" @click="isCreateRequestModalActive = false">Close</button>
+              <button class="button is-primary" type="submit">Submit</button>
+            </footer>
+          </div>
+        </form>
     </b-modal>
   </div>
 </template>
 
 <script>
     import store from '../../../store/index';
+    import { mapActions } from 'vuex';
     import RequestTypeDrowdown from '../../elements/request-type-dropdown';
     import ResponseTypeDropdown from "../../elements/response-type-dropdown";
 
@@ -113,7 +118,33 @@
         name: "create-request-modal",
         components: {ResponseTypeDropdown, RequestTypeDrowdown},
         props: {
-            // Nothing yet
+            currentRoute: window.location.href // https://forum.vuejs.org/t/how-to-get-path-from-route-instance/26934/2
+        },
+        methods: {
+            ...mapActions('oidcStore', ['authenticateOidc', 'signOutOidc']),
+            getCurrencyPairTickerPairStr: function(obj) {
+                return obj.mainTicker + obj.counterTicker + " (" + obj.sourceName + ")";
+            },
+            create: function() {
+                this.isModalLoading = true;
+
+                let self = this;
+                this.$axios.post('/api/Request/Create', this.form, {
+                    headers: {
+                        Authorization: "Bearer " + store.state.oidcStore.access_token
+                    }
+                })
+                    .then(function (response) {
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+                    .finally(function () {
+                    // always executed
+                    self.isModalLoading = false;
+                });
+            }
         },
         beforeCreate: function () {
             let self = this;
@@ -125,7 +156,8 @@
                 })
                 .catch(function (error) {
                     // handle error
-                    console.log(error);
+                    console.dir(self.methods);
+                    self.methods.authenticateOidc(self.currentRoute);
                 })
                 .finally(function () {
                     // always executed
@@ -141,11 +173,12 @@
                 }
             })
                 .then(function (response) {
+                    console.dir(response.data);
                     self.currencyPairs = response.data;
                 })
                 .catch(function (error) {
                     // handle error
-                    console.log(error);
+                    self.methods.authenticateOidc(self.currentRoute);
                 })
                 .finally(function () {
                     // always executed
@@ -174,32 +207,26 @@
         data: function () {
             return {
                 isCreateRequestModalActive: false,
+                isModalLoading: false,
                 form: {
                     activeTab: 0, // For requestParentType
                     type: 0,
                     responseType: 1,
-                    apiUrl: '',
+                    dataPath: "",
                     delay: 0,
                     failureDelay: 0,
                     requestParentType: 0,
                     currencySlug: '',
                     currencyPair: {},
-                    currencyType: 0,
-                    storeHistorical: false,
-                    isDenominated: false,
-                    anomalyIgnorance: false
+                    currencyPairId: null,
+                    currencyType: 0
                 },
                 formHelper: {},
                 currencies: [],
                 currencyPairs: [],
                 currencyPairsIsLoading: false,
                 currencyTypes: [],
-                currencyTypesIsLoading: false,
-            }
-        },
-        methods: {
-            getCurrencyPairTickerPairStr: function(obj) {
-                return obj.mainTicker + obj.counterTicker + " (" + obj.sourceName + ")";
+                currencyTypesIsLoading: false
             }
         }
     }
