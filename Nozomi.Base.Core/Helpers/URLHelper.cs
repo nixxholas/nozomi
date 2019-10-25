5678765786e245e76277ea975057091053302880
nixxholas <nicholas@slyck.net>
2019-10-25 14:42:51 +0800

    Nozomi.Base.Core: Helpers: URLHelper: docs
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Core/Helpers/URLHelper.cs
+++ Nozomi.Base.Core/Helpers/URLHelper.cs
@@ -4,6 +4,7 @@ namespace Nozomi.Base.Core.Helpers
 {
     public static class URLHelper
     {
+        // https://stackoverflow.com/questions/7578857/how-to-check-whether-a-string-is-a-valid-http-url
         public static bool IsUrl(string uriName)
         {
             return Uri.TryCreate(uriName, UriKind.Absolute, out var uriResult)
