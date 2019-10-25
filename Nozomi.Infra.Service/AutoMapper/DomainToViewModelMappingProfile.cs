d61a62518b6218fdfb6a4ca013449f24a3f535b3
nixxholas <nicholas@slyck.net>
2019-10-25 16:13:42 +0800

    Nozomi.Infra.Service: AutoMapper: Further implementation/Scaffolding
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Infra.Service/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Nozomi.Data.Models;
+using Nozomi.Service.ViewModels;
+
+namespace Nozomi.Service.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            //CreateMap<Request, RequestViewModel>();
+        }
+    }
+}
