8a3aed5fb8138bcdc97901a71976bb6aa55dd038
nixholas <nixholas@icloud.com>
2019-10-27 01:57:51 +0800

    Revert "Nozomi.Infra.Service: Services: NewRequestService: Initial implementation"
    This reverts commit df727a4261762d7dd5a4a970c5103e3c939d8900.

--- Nozomi.Infra.Service/Services/Interfaces/INewRequestService.cs
+++
@@ -1,9 +0,0 @@
-using Nozomi.Service.ViewModels;
-
-namespace Nozomi.Service.Services.Interfaces
-{
-    public interface INewRequestService
-    {
-        void Create(CreateRequestViewModel vm);
-    }
-}
