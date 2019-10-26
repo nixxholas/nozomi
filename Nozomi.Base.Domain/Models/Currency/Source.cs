51dee6fcf206b314c72d3f54480649a372e0db1c
nixholas <nixholas@icloud.com>
2019-10-27 01:57:43 +0800

    Revert "Nozomi.Base.Domain: Get commands, events & models ready for source creation"
    This reverts commit 817f9fe49858204d59028604fe70e7ce16bc899e.

--- Nozomi.Base.Domain/Models/Currency/Source.cs
+++ Nozomi.Base.Domain/Models/Currency/Source.cs
@@ -8,15 +8,6 @@ namespace Nozomi.Data.Models.Currency
 {
     public class Source : Entity
     {
-        public Source () {}
-
-        public Source(string abbreviation, string name, string apiDocsUrl)
-        {
-            Abbreviation = abbreviation;
-            Name = name;
-            APIDocsURL = apiDocsUrl;
-        }
-        
         public long Id { get; set; }
 
         // Short form for the currency source if needed.
