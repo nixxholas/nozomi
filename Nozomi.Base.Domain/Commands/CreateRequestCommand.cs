f27aaa5021916f98ed64cd374fffd806c2786ece
nixholas <nixholas@icloud.com>
2019-10-27 01:58:05 +0800

    Revert "Nozomi.Base.Domain: Commands: CreateRequestCommand: Compat changes"
    This reverts commit 5ca46cd713c15e9a84eb32c55cf19e38471b2afb.

--- Nozomi.Base.Domain/Commands/CreateRequestCommand.cs
+++ Nozomi.Base.Domain/Commands/CreateRequestCommand.cs
@@ -18,21 +18,6 @@ namespace Nozomi.Data.Commands
             CurrencyTypeId = currencyTypeId;
         }
         
-        public CreateRequestCommand(RequestType requestType, ResponseType responseType, string dataPath, int delay,
-            int failureDelay, string currencySlug, long currencyPairId, long currencyTypeId)
-        {
-            RequestType = requestType;
-            ResponseType = responseType;
-            DataPath = dataPath;
-            Delay = delay;
-            FailureDelay = failureDelay;
-            CurrencySlug = currencySlug;
-            CurrencyPairId = currencyPairId;
-            CurrencyTypeId = currencyTypeId;
-        }
-        
-        public string CurrencySlug { get; private set; }
-        
         public override bool IsValid()
         {
             ValidationResult = new CreateRequestValidation().Validate(this);
