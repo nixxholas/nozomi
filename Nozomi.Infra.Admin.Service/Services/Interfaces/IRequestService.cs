be7e54158d3c86db1f9c84c34e658d16f6c9b734
nixholas <nixholas@icloud.com>
2019-10-28 09:58:37 +0800

    Nozomi.Infra.Admin.Service: Services: RequestServices: Migrate it here
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.Infra.Admin.Service/Services/Interfaces/IRequestService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Nozomi.Data;
+using Nozomi.Data.AreaModels.v1.Requests;
+using Nozomi.Data.Models.Web;
+using Nozomi.Data.ViewModels.Request;
+
+namespace Nozomi.Infra.Admin.Service.Services.Interfaces
+{
+    public interface IRequestService
+    {
+        long Create(Request request, string userId = null);
+        
+        NozomiResult<string> Create(CreateRequest createRequest, string userId = null);
+        
+        bool Delay(Request request, TimeSpan duration);
+
+        bool HasUpdated(long requestId);
+
+        bool HasUpdated(ICollection<Request> requests);
+
+        NozomiResult<string> Update(UpdateRequest updateRequest, string userId = null);
+
+        NozomiResult<string> Delete(long reqId, bool hardDelete = false, string userId = null);
+
+        bool ManualPoll(long id, string userId = null);
+    } 
+}
