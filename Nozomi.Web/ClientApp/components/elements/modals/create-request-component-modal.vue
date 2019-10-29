e9a3d9dabf49ae580e6bd62523ee4c9542d56d62
nixxholas <nicholas@slyck.net>
2019-10-29 22:36:07 +0800

    Nozomi.Web: ClientApp: components: elements: modals: create-request-component-modal: get ready for the backend
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/ClientApp/components/elements/modals/create-request-component-modal.vue
+++ Nozomi.Web/ClientApp/components/elements/modals/create-request-component-modal.vue
@@ -39,7 +39,6 @@
                 type="text"
                 placeholder=""
                 v-model="form.queryComponent"
-                required
                 expanded>
               </b-input>
             </b-field>
