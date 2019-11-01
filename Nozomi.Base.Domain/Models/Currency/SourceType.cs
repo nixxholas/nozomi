84eb7263d5246350e22706a9f8562995933f0586
nixxholas <nicholas@slyck.net>
2019-11-01 17:03:34 +0800

    Nozomi.Base.Domain: Models: Currency: SourceType: Constructor extension
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/Models/Currency/SourceType.cs
+++ Nozomi.Base.Domain/Models/Currency/SourceType.cs
@@ -4,6 +4,14 @@ namespace Nozomi.Data.Models.Currency
 {
     public class SourceType : Entity
     {
+        public SourceType() {}
+
+        public SourceType(string abbreviation, string name)
+        {
+            Abbreviation = abbreviation;
+            Name = name;
+        }
+    
         public string Abbreviation { get; set; }
         
         public string Name { get; set; }
