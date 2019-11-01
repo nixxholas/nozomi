f31ab280100fc1e500e935290571a05daa3cdc27
nixxholas <nicholas@slyck.net>
2019-11-01 18:25:03 +0800

    Source: Nozomi.Repo: SourceType migrations
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Repo/Migrations/20191101093248_r10_SourceType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r10_SourceType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SourceTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    CreatedById = table.Column<string>(nullable: true),
+                    ModifiedById = table.Column<string>(nullable: true),
+                    DeletedById = table.Column<string>(nullable: true),
+                    Guid = table.Column<Guid>(nullable: false),
+                    Abbreviation = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    xmin = table.Column<uint>(type: "xid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("SourceType_Id_PK", x => x.Id);
+                    table.UniqueConstraint("AK_SourceTypes_Guid", x => x.Guid);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SourceTypes_Abbreviation",
+                table: "SourceTypes",
+                column: "Abbreviation",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SourceTypes");
+        }
+    }
+}
