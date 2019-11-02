7fd110fbda32c3e523a4602bc9300b2796e02de5
nixxholas <nicholas@slyck.net>
2019-11-02 14:35:56 +0800

    Nozomi.Web: Controllers: APIs: v1: Source: Create API: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Web/Controllers/APIs/v1/Source/ISourceController.cs
+++ Nozomi.Web/Controllers/APIs/v1/Source/ISourceController.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Nozomi.Data;
+using Nozomi.Data.ViewModels.Source;
 
 namespace Nozomi.Web.Controllers.APIs.v1.Source
  {
      public interface ISourceController
      {
+         IActionResult Create(CreateSourceViewModel vm);
+
          NozomiResult<ICollection<Data.Models.Currency.Source>> All();
 
          NozomiResult<ICollection<Data.Models.Currency.Source>> GetCurrencySources(string slug, int page = 0);
