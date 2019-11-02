eb84e26c7ace908ad567703ccf1db7d8479d7ee3
nixxholas <nicholas@slyck.net>
2019-11-02 12:16:37 +0800

    Nozomi.Base.Domain: ResponseModels: SourceType: CreateSourceTypeViewModel: FluentValidation implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/ResponseModels/SourceType/CreateSourceTypeViewModel.cs
+++ Nozomi.Base.Domain/ResponseModels/SourceType/CreateSourceTypeViewModel.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace Nozomi.Data.ResponseModels.SourceType
 {
     public class CreateSourceTypeViewModel
@@ -5,5 +7,20 @@ namespace Nozomi.Data.ResponseModels.SourceType
         public string Abbreviation { get; set; }
         
         public string Name { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new CreateSourceTypeValidator();
+            return validator.Validate(this).IsValid;
+        }
+        
+        public class CreateSourceTypeValidator : AbstractValidator<CreateSourceTypeViewModel>
+        {
+            public CreateSourceTypeValidator()
+            {
+                RuleFor(st => st.Abbreviation).NotNull().NotEmpty();
+                RuleFor(st => st.Name).NotNull().NotEmpty();
+            }
+        }
     }
 }
