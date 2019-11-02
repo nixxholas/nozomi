9d459cb4f1472068eaa31a6c570bde6057e6ca37
nixxholas <nicholas@slyck.net>
2019-11-02 12:13:20 +0800

    Nozomi.Base.Domain: ResponseModels: SourceType: CreateSourceTypeViewModel: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Base.Domain/ResponseModels/SourceType/CreateSourceTypeViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nozomi.Data.ResponseModels.SourceType
+{
+    public class CreateSourceTypeViewModel
+    {
+        public string Abbreviation { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
