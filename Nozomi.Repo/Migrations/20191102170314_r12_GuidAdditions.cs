4533c2df8366c93632ca59975d0751b4bc6aaea1
nixholas <nixholas@icloud.com>
2019-11-03 01:16:13 +0800

    Nozomi.Repo: Migrations: r12 - GuidAdditions migration
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20191102170314_r12_GuidAdditions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r12_GuidAdditions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                table: "Sources",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                table: "CurrencyTypes",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sources_Guid",
+                table: "Sources",
+                column: "Guid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyTypes_Guid",
+                table: "CurrencyTypes",
+                column: "Guid",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sources_Guid",
+                table: "Sources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CurrencyTypes_Guid",
+                table: "CurrencyTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "Sources");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "CurrencyTypes");
+        }
+    }
+}
