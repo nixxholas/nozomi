bc1f4e7c4dedfae867ee44aa9d401ba0feb17de6
nixxholas <nicholas@slyck.net>
2019-11-02 14:43:15 +0800

    Nozomi.Base.Domain: Models: Currency: Source: Guid property initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/Models/Currency/Source.cs
+++ Nozomi.Base.Domain/Models/Currency/Source.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Nozomi.Base.Core;
@@ -17,6 +18,8 @@ namespace Nozomi.Data.Models.Currency
         }
         
         public long Id { get; set; }
+
+        public Guid Guid { get; set; }
 
         // Short form for the currency source if needed.
         public string Abbreviation { get; set; }
