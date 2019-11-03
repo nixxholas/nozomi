75d2d388c396b3bb0e5d212414a837083ba9d299
nixxholas <nicholas@slyck.net>
2019-11-04 06:23:27 +0800

    Nozomi.Base.Domain: ViewModels: Currency: CreateCurrencyViewModel: Initial implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

---
+++ Nozomi.Base.Domain/ViewModels/Currency/CreateCurrencyViewModel.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Nozomi.Data.ViewModels.Currency
+{
+    public class CreateCurrencyViewModel
+    {
+        public Guid CurrencyTypeGuid { get; set; }
+        
+        public string Abbreviation { get; set; }
+        
+        public string Slug { get; set; }
+        
+        public string Name { get;set; }
+        
+        public string LogoPath { get; set; }
+        
+        public string Description { get; set; }
+
+        public int Denominations { get; set; } = 0;
+        
+        public string DenominationName { get; set; }
+    }
+}
