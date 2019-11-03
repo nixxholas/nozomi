9ffe4797a1c3b379070ef69d5575cf08f087907b
nixholas <nixholas@icloud.com>
2019-11-03 21:21:49 +0800

    Nozomi.Web: ClientApp: components: nav-menu: cleanup
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Web/ClientApp/components/nav-menu.vue
+++ Nozomi.Web/ClientApp/components/nav-menu.vue
@@ -54,7 +54,7 @@
 <script>
     import { routes } from '../router/routes';
     import { mapActions, mapGetters } from 'vuex';
-    import Web3 from 'web3';
+    // import Web3 from 'web3';
 
     export default {
         data() {
@@ -79,88 +79,6 @@
         },
         methods: {
             ...mapActions('oidcStore', ['authenticateOidc', 'signOutOidc']),
-            async authWeb3() {
-                this.loginLoading = true;
-
-                try {
-                    // Modern dapp browsers...
-                    if (window.ethereum) {
-                        // Propagate Web3
-                        window.web3 = new Web3(ethereum);
-                        await window.ethereum.enable();
-                        window.ethereum.autoRefreshOnNetworkChange = false;
-
-                        // Obtain the user accounts
-                        let authMsg = 'This is a Nozomi auth message';
-                        let accounts = await window.web3.eth.getAccounts();
-
-                        // Ensure that the user is holding the wallet/s by asking him to unlock his
-                        // account with a random message.
-                        // https://ethereum.stackexchange.com/questions/48489/how-to-prove-that-a-user-owns-their-public-key-for-free=
-                        if (accounts != null && accounts.length > 0) {
-                            let self = this;
-
-                            // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-                            // await Promise.all(accounts.map(async (account) => {
-                            // }));
-
-                            // let shaMsg = window.web3.utils.sha3(authMsg);
-                            let signed = await window.web3.eth.personal.sign(authMsg, accounts[0],
-                                function (err, sig) {
-                                    if (err) {
-                                        self.$buefy.notification.open({
-                                            duration: 3000,
-                                            message: `Hey! Don't manipulate any authentication data!`,
-                                            position: 'is-bottom-right',
-                                            type: 'is-danger',
-                                            hasIcon: true
-                                        });
-                                    }
-                                });
-
-                            let web3Payload = {
-                                "claimerAddress": accounts[0],
-                                "signature": signed,
-                                "rawMessage": authMsg
-                            };
-
-                            store.dispatch('login', web3Payload);
-                        }
-                    }
-                    // Legacy dapp browsers...
-                    else if (window.web3) {
-                        window.web3 = new Web3(web3.currentProvider);
-                        // Acccounts always exposed
-                        web3.eth.sendTransaction({/* ... */});
-                    }
-                    // Non-dapp browsers...
-                    else {
-                        this.$buefy.notification.open({
-                            duration: 5000,
-                            message: `Non-Ethereum browser detected. You should consider trying MetaMask!`,
-                            position: 'is-bottom-right',
-                            type: 'is-warning',
-                            hasIcon: true
-                        });
-                    }
-
-                    // Then run it
-                    // store.dispatch('login');
-                } catch (error) {
-                    console.dir(error);
-
-                    // User denied account access...
-                    this.$buefy.notification.open({
-                        duration: 5000,
-                        message: `Your browser may not be supporting Web3 properly.`,
-                        position: 'is-bottom-right',
-                        type: 'is-danger',
-                        hasIcon: true
-                    });
-                }
-
-                this.loginLoading = false;
-            },
             hasWeb3() {
                 try {
                     return window.ethereum || window.web3;
