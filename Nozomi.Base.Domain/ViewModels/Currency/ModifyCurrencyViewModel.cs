58e7123740899a662109895d93fd3bb33249212b
nixxholas <nicholas@slyck.net>
2019-11-05 06:23:08 +0800

    Nozomi.Base.Domain: ViewModels: Currency: ModifyCurrencyViewModel: Validation implementation
    Signed-off-by: nixxholas <nicholas@slyck.net>

--- Nozomi.Base.Domain/ViewModels/Currency/ModifyCurrencyViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/Currency/ModifyCurrencyViewModel.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using FluentValidation;
+
 namespace Nozomi.Data.ViewModels.Currency
 {
     public class ModifyCurrencyViewModel : CurrencyViewModel
@@ -5,5 +8,22 @@ namespace Nozomi.Data.ViewModels.Currency
         public long Id { get; set; }
         
         public bool IsEnabled { get; set; }
+        
+        public class ModifyCurrencyValidator : AbstractValidator<ModifyCurrencyViewModel>
+        {
+            public ModifyCurrencyValidator()
+            {
+                RuleFor(e => e.Id).GreaterThan(0);
+                RuleFor(e => e.IsEnabled).NotNull();
+                RuleFor(e => e.CurrencyTypeGuid).NotNull();
+                RuleFor(e => e.Abbreviation).NotNull().NotEmpty();
+                RuleFor(e => e.Slug).NotNull().NotEmpty();
+                RuleFor(e => e.Name).NotNull().NotEmpty();
+                RuleFor(e => e.LogoPath);
+                RuleFor(e => e.Description);
+                RuleFor(e => e.Denominations).GreaterThanOrEqualTo(0);
+                RuleFor(e => e.DenominationName);
+            }
+        }
     }
 }
