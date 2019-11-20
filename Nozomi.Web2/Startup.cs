558abd53e2c48d504291aa2af2c978798c3cccd8
Nicholas Chen <nixholas@icloud.com>
2019-11-20 22:42:20 +0800

    Nozomi.Web2: DI up to NozomiDbContext along with is propagation.
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Web2/Startup.cs
+++ Nozomi.Web2/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Nozomi.Web2.Extensions;
 using VueCliMiddleware;
 
 namespace Nozomi.Web2
@@ -23,6 +25,23 @@ namespace Nozomi.Web2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddOptions();
+
+            services.AddResponseCompression();
+
+            services.AddDbContextInjections();
+
+            services.AddMemoryCache();
+
+            services.AddHealthChecks();
+            
             services.AddControllersWithViews();
 
             // Add AddRazorPages if the app uses Razor Pages.
