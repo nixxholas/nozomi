437653aaabe51b33c3363b55df088704937be9f0
Nicholas Chen <nixholas@icloud.com>
2019-11-20 23:23:12 +0800

    Nozomi.Web2: Startup: Complete DI migration to from Nozomi.Web
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Web2/Startup.cs
+++ Nozomi.Web2/Startup.cs
@@ -1,11 +1,18 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Nozomi.Preprocessing;
+using Nozomi.Preprocessing.Filters;
 using Nozomi.Web2.Extensions;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using VueCliMiddleware;
 
 namespace Nozomi.Web2
@@ -18,8 +25,8 @@ namespace Nozomi.Web2
             Environment = environment;
         }
 
-        public static IConfiguration Configuration { get; set;  }
-        
+        public static IConfiguration Configuration { get; set; }
+
         public static IWebHostEnvironment Environment { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -41,17 +48,47 @@ namespace Nozomi.Web2
             services.AddMemoryCache();
 
             services.AddHealthChecks();
-            
-            services.AddControllersWithViews();
 
-            // Add AddRazorPages if the app uses Razor Pages.
+            services.AddControllersWithViews(options => { options.Filters.Add(typeof(HttpGlobalExceptionFilter)); })
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            // Add AddRazorPages
             services.AddRazorPages();
 
-            // In production, the Vue files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
+            if (Environment.IsProduction())
             {
-                configuration.RootPath = "ClientApp/dist";
+                services.AddHsts(opt =>
+                {
+                    opt.Preload = true;
+                    opt.IncludeSubDomains = true;
+                    opt.MaxAge = TimeSpan.FromDays(60);
+                });
+            }
+
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                // options.HttpsPort = 5001;
             });
+
+            // In production, the Vue files will be served from this directory
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+
+            // UoW-Repository injection
+            services.ConfigureRepoLayer();
+
+            // Service layer injections
+            services.ConfigureInfra();
+
+            // Swashbuckle Swagger
+            services.ConfigureSwagger();
+
+            // Auth
+            services.ConfigureNozomiAuth();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,11 +103,51 @@ namespace Nozomi.Web2
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
+                
+                app.UseResponseCompression();
             }
+            
+            // Always enforce HTTPS for development and production
+            app.UseHttpsRedirection();
+
+            // https://github.com/IdentityServer/IdentityServer4/issues/1331
+            var forwardOptions = new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+                RequireHeaderSymmetry = false
+            };
+
+            forwardOptions.KnownNetworks.Clear();
+            forwardOptions.KnownProxies.Clear();
+
+            // ref: https://github.com/aspnet/Docs/issues/2384
+            app.UseForwardedHeaders(forwardOptions);
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.UseCookiePolicy();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.DocumentTitle = "Nozomi API";
+                // https://stackoverflow.com/questions/39116047/how-to-change-base-url-of-swagger-in-asp-net-core
+                c.RoutePrefix = "docs";
+                c.SwaggerEndpoint("/swagger/" + GlobalApiVariables.CURRENT_API_VERSION + "/swagger.json", "Nozomi API");
+                
+                c.DocExpansion(DocExpansion.None);
+                c.EnableDeepLinking();
+                c.EnableFilter();
+            });
 
             app.UseRouting();
 
@@ -79,12 +156,16 @@ namespace Nozomi.Web2
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+                
+                // Health check up!!!
+                // https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-3.0#basic-health-probe
+                endpoints.MapHealthChecks("/health");
 
                 if (env.IsDevelopment())
                 {
                     endpoints.MapToVueCliProxy(
                         "{*path}",
-                        new SpaOptions { SourcePath = "ClientApp" },
+                        new SpaOptions {SourcePath = "ClientApp"},
                         npmScript: "serve",
                         regex: "Compiled successfully");
                 }
@@ -93,10 +174,7 @@ namespace Nozomi.Web2
                 endpoints.MapRazorPages();
             });
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-            });
+            app.UseSpa(spa => { spa.Options.SourcePath = "ClientApp"; });
         }
     }
 }
