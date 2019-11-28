af7659aef4efca807cdd4f68f0ad8cc7fdaf842b
nixxholas <nixholas@icloud.com>
2019-11-28 22:49:21 +0800

    Nozomi.Repo: Migrations: r14 - AnalysedComponentGuid Migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo/Migrations/20191128144811_r14_AnalysedComponentGuid.Designer.cs
+++ Nozomi.Repo/Migrations/20191128144811_r14_AnalysedComponentGuid.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nozomi.Repo.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Nozomi.Repo.Migrations
 {
     [DbContext(typeof(NozomiDbContext))]
-    partial class NozomiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191128144811_r14_AnalysedComponentGuid")]
+    partial class r14_AnalysedComponentGuid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
