68d171463dca33f92319513991cfa0dcac15fe78
nixxholas <nixholas@icloud.com>
2019-11-28 09:09:11 +0800

    Nozomi.Web2: Controllers: v1: AnalysedComponentType: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/Controllers/v1/AnalysedComponentType/AnalysedComponentTypeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nozomi.Preprocessing;
+
+namespace Nozomi.Web2.Controllers.v1.AnalysedComponentType
+{
+    public class AnalysedComponentTypeController : BaseApiController<AnalysedComponentTypeController>,
+        IAnalysedComponentTypeController
+    {
+        public AnalysedComponentTypeController(ILogger<AnalysedComponentTypeController> logger) : base(logger)
+        {
+        }
+
+        public IActionResult All()
+        {
+            return Ok(NozomiServiceConstants.AnalysedComponentTypeMap);
+        }
+    }
+}
