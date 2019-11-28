a0ec1f77daf34bf49c6a76c22369b9f8ead83341
nixxholas <nixholas@icloud.com>
2019-11-29 06:14:00 +0800

    Nozomi.Web2: ClientApp: src: services: AnalysedComponentService: getTypes API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
+++ Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
@@ -13,16 +13,13 @@ export default {
             });
         });
     },
-    
+
     getTypes() {
         return new Promise((resolve, reject) => {
-            axios.get('/api/AnalysedComponentType/All', {
-                headers: {
-                    Authorization: "Bearer " + store.state.oidcStore.access_token
-                }
-            }).then(function (response) {
-                resolve(response.data);
-            }).catch(function (error) {
+            axios.get('/api/AnalysedComponentType/All')
+                .then(function (response) {
+                    resolve(response.data);
+                }).catch(function (error) {
                 reject(error);
             });
         });
