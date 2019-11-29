69ef9d4e313fca39c570cda005d3290446cfcf38
nixxholas <nixholas@icloud.com>
2019-11-30 02:20:55 +0800

    Nozomi.Web2: ClientApp: src: services: SourceService: listByCurrency API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/SourceService.js
+++ Nozomi.Web2/ClientApp/src/services/SourceService.js
@@ -9,5 +9,20 @@ export default {
         reject(error);
       });
     });
+  },
+
+  listByCurrency(guid, page, itemsPerPage) {
+    return new Promise((resolve, reject) => {
+      axios.get('/api/Source/ListByCurrency/' + guid, {
+        params: {
+          page: page,
+          itemsPerPage: itemsPerPage
+        }
+      }).then(function (response) {
+        resolve(response.data);
+      }).catch(function (error) {
+        reject(error);
+      });
+    });
   }
 }
