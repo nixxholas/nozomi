c999946f1c03f10b18eacef593c66e49e3605b6b
nixxholas <nixholas@icloud.com>
2019-12-03 23:24:09 +0800

    Nozomi.Web2: ClientApp: src: components: tables: currencies-table: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/ClientApp/src/components/tables/currencies-table.vue
@@ -0,0 +1,142 @@
+<template>
+    <div>
+        <b-table
+                :data="data"
+                :current-page.sync="currentPage"
+                :per-page="perPage"
+                :loading="dataLoading"
+
+                paginated
+                backend-pagination
+                :total="dataCount"
+                @page-change="onPageChange"
+
+                backend-sorting
+                :default-sort-direction="defaultSortOrder"
+                :default-sort="[sortField, sortOrder]"
+                @sort="onSort"
+
+                default-sort="name"
+                aria-next-label="Next page"
+                aria-previous-label="Previous page"
+                aria-page-label="Page"
+                aria-current-label="Current page">
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>
+                            <b-icon
+                                    icon="frown"
+                                    size="is-large">
+                            </b-icon>
+                        </p>
+                        <p>Nothing here.</p>
+                    </div>
+                </section>
+            </template>
+
+            <template slot-scope="props">
+                <b-table-column field="name" label="Name" sortable>
+                    <router-link :to="`/currency/${props.row.slug}`">
+                        <img v-if="props.row.logoPath != null"
+                             :src="props.row.logoPath" class="mr-1"
+                             style="width: 24px; height: 24px; vertical-align: bottom;"/>
+                        {{ props.row.name }}
+                    </router-link>
+                </b-table-column>
+
+                <b-table-column field="currencyType" label="Type" sortable>
+                    {{ getType(props.row.currencyTypeGuid).name }}
+                </b-table-column>
+            </template>
+        </b-table>
+        <b-loading :active.sync="dataLoading"></b-loading>
+    </div>
+</template>
+
+<script>
+    import CurrencyService from "@/services/CurrencyService";
+    import CurrencyTypeService from "@/services/CurrencyTypeService";
+    
+    export default {
+        name: "currencies-table",
+        props: {
+            type: null,
+            perPage: 50
+        },
+        data: function () {
+            return {
+                currentPage: 1,
+                dataLoading: true,
+                data: [],
+                dataCount: 0,
+                sortField: 'name',
+                defaultSortOrder: 'desc',
+                sortOrder: 'desc',
+                typeData: []
+            }
+        },
+        methods: {
+            getType(guid) {
+                const types = this.typeData;
+                if (types && types.length > 0)
+                    for (var i = 0; i < types.length; i++) {
+                        if (types[i].guid === guid)
+                            return types[i];
+                    }
+
+                return "Null"
+            },
+            onPageChange: function (page) {
+                let self = this;
+                self.dataLoading = true;
+
+                CurrencyService.listAll(page - 1, self.perPage)
+                    .then(function (res) {
+                        self.data = res;
+
+                        self.dataLoading = false;
+                    });
+            },
+            onSort(field, order) {
+                let self = this;
+                self.dataLoading = true;
+
+                self.sortField = field;
+                self.sortOrder = order;
+
+                let sortAscending = order === "asc";
+
+                CurrencyService.listAll(self.currentPage - 1, self.perPage, sortAscending, self.sortField)
+                    .then(function (res) {
+                        self.data = res;
+
+                        self.dataLoading = false;
+                    });
+            },
+        },
+        mounted: function() {
+            let self = this;
+            CurrencyService.getCurrencyCount(self.props.type)
+                .then(function (res) {
+                    self.dataCount = res;
+                });
+
+            CurrencyService.listAll()
+                .then(function (res) {
+                    self.data = res;
+                });
+
+            CurrencyTypeService.getAll()
+                .then(function (res) {
+                    self.typeData = res;
+
+                    self.dataLoading = false;
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
