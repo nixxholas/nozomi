bfc8fda6fe3194366d01d08a1c55d75d15d9ec67
nixxholas <nixholas@icloud.com>
2019-12-03 22:03:55 +0800

    Nozomi.Web2: ClientApp: src: views: About: Timeline?
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/views/About.vue
+++ Nozomi.Web2/ClientApp/src/views/About.vue
@@ -2,26 +2,95 @@
   <div>
     <section class="hero is-medium">
       <div class="hero-body">
-        <div class="container">
-          <div class="columns is-centered">
-            <div class="column is-5 is-offset-1 landing-caption">
-              <h1 class="title is-1 is-bold is-spaced">
-                Poll, Aggregate.
-              </h1>
-              <h2 class="subtitle is-5 is-muted">We’re an API-first data company delivering retrograde market data and
-                aggregating APIs to products that require a strong and reliable data backbone.</h2>
-              <p>
-                <b-button rounded type="is-dark">Get Started</b-button>
-              </p>
-            </div>
-            <div class="column is-5">
-              <figure class="image">
-                <img src="@/assets/svg/illustrations/knowledgebase-community.png">
-              </figure>
-            </div>
-            <div class="column is-1"></div>
+        <div class="columns is-centered">
+          <div class="column is-5 is-offset-1 landing-caption">
+            <h1 class="title is-1 is-bold is-spaced">
+              Poll, Aggregate.
+            </h1>
+            <h2 class="subtitle is-5 is-muted">We’re an API-first data company delivering retrograde market data and
+              aggregating APIs to products that require a strong and reliable data backbone.</h2>
+            <p>
+              <b-button rounded type="is-dark">Get Started</b-button>
+            </p>
           </div>
+          <div class="column is-5">
+            <figure class="image">
+              <img src="@/assets/svg/illustrations/knowledgebase-community.png">
+            </figure>
+          </div>
+          <div class="column is-1"></div>
         </div>
+      </div>
+    </section>
+    
+    <section class="hero is-medium">
+      <div class="hero-body">
+        <h1 class="title is-1 is-bold is-spaced pb-3">
+          Timeline
+        </h1>
+          <div class="timeline is-centered">
+            <header class="timeline-header">
+              <span class="tag is-medium is-primary">Genesis</span>
+            </header>
+            <div class="timeline-item">
+              <div class="timeline-marker is-primary"></div>
+              <div class="timeline-content">
+                <p class="heading">April 2018</p>
+                <p>Side project kickoff</p>
+              </div>
+            </div>
+            <div class="timeline-item">
+              <div class="timeline-marker is-info is-icon">
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="timeline-content">
+                <p class="heading">October 2018</p>
+                <p>Wrapping engine completion</p>
+              </div>
+            </div>
+            <header class="timeline-header">
+              <span class="tag is-primary">2019</span>
+            </header>
+            <div class="timeline-item">
+              <div class="timeline-marker is-info is-icon">
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="timeline-content">
+                <p class="heading">January 2019</p>
+                <p>Compute engine completion</p>
+              </div>
+            </div>
+            <div class="timeline-item">
+              <div class="timeline-marker is-info is-icon">
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="timeline-content">
+                <p class="heading">April 2019</p>
+                <p>Initial UI</p>
+              </div>
+            </div>
+            <div class="timeline-item">
+              <div class="timeline-marker is-primary is-icon">
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="timeline-content">
+                <p class="heading">October 2019</p>
+                <p>Public preview</p>
+              </div>
+            </div>
+            <div class="timeline-item">
+              <div class="timeline-marker is-primary is-icon">
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="timeline-content">
+                <p class="heading">March 2020</p>
+                <p>Public beta</p>
+              </div>
+            </div>
+            <header class="timeline-header">
+              <span class="tag is-medium is-primary">World domination</span>
+            </header>
+          </div>
       </div>
     </section>
 
