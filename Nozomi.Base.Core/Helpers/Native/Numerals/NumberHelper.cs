145a64305eaa95143f4e8c9e04a24ee6478e6822
nixxholas <nixholas@icloud.com>
2019-12-07 14:10:57 +0800

    Nozomi.Base.Core: Helpers: Native: Numerals: NumberHelper: Extend check
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Core/Helpers/Native/Numerals/NumberHelper.cs
+++ Nozomi.Base.Core/Helpers/Native/Numerals/NumberHelper.cs
@@ -2,6 +2,7 @@ namespace Nozomi.Base.Core.Helpers.Native.Numerals
 {
     public static class NumberHelper
     {
-        public static bool IsNumericDecimal(string val) => decimal.TryParse(val, out _);
+        public static bool IsNumericDecimal(string val) => 
+            string.IsNullOrWhiteSpace(val) && decimal.TryParse(val, out _);
     }
 }
