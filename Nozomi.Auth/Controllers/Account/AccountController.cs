bf3217f3f005d00c2ee8d21fcf225f67b3122b53
nixxholas <nixholas@icloud.com>
2019-12-09 00:51:50 +0800

    Nozomi.Auth: Controllers: Account: Web3Create API: Role propagation support
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Auth/Controllers/Account/AccountController.cs
+++ Nozomi.Auth/Controllers/Account/AccountController.cs
@@ -130,9 +130,29 @@ namespace Nozomi.Auth.Controllers.Account
 
             if (result.Succeeded)
             {
-                var createdAddress = _addressService.Create(user.Id, address, AddressType.Ethereum);
+                // Default role binding
+                var roleResult = await _userManager.AddToRoleAsync(user, "User");
 
-                return !string.IsNullOrWhiteSpace(createdAddress) ? user : null;
+                if (roleResult.Succeeded)
+                {
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
+                    // Send an email with this link
+//                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+//                    var callbackUrl = Url.Action("ConfirmEmail", "Account", 
+//                        new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+//                    await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
+//                        "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //_logger.LogInformation(3, "User created a new account with password.");
+                    
+                    //return Redirect(model.ReturnUrl);
+                    
+                    var createdAddress = _addressService.Create(user.Id, address, AddressType.Ethereum);
+
+                    return !string.IsNullOrWhiteSpace(createdAddress) ? user : null;
+                }
+                    
+                ModelState.AddModelError(user.Id, AccountOptions.FailedToJoinRole);
             }
 
             return null;
