a098fee8434a5466874bdcf93480064818c0f0e1
nixxholas <nixholas@icloud.com>
2019-12-15 16:41:33 +0800

    Nozomi.Web2: ClientApp: src: services: CoreService: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/ClientApp/src/services/CoreService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import store from '../store/index';
+
+export default {
+    getCurrentBuildTime() {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Core/GetCurrentBuildTime/').then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+
+    getUserDetails() {
+        return new Promise((resolve, reject) => {
+            axios.post('/api/Core/GetUserDetails', vm, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+}
