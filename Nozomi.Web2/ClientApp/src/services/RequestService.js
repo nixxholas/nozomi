ac622684603934216c9f2e727897ad36c223f52c
nixxholas <nixholas@icloud.com>
2019-12-17 06:26:45 +0800

    Nozomi.Web2: ClientApp: src: services: RequestService: GetAllForUser API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/RequestService.js
+++ Nozomi.Web2/ClientApp/src/services/RequestService.js
@@ -2,6 +2,20 @@ import axios from 'axios';
 import store from '../store/index';
 
 export default {
+    getAllForUser() {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Request/GetAll', {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+    
     create(vm) {
         if (!vm)
             throw new Error("Invalid payload. Please try again.");
