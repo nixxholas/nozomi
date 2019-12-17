26405c779942980c489fd110a48340b5f2730a42
nixxholas <nixholas@icloud.com>
2019-12-17 23:33:51 +0800

    Nozomi.Web2: ClientApp: src: services: CurrencyTypeService: ListAll API implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/CurrencyTypeService.js
+++ Nozomi.Web2/ClientApp/src/services/CurrencyTypeService.js
@@ -2,13 +2,32 @@ import axios from 'axios';
 import Converter from '../helpers/converter';
 
 export default {
-  getAll() {
-    return new Promise((resolve, reject) => {
-      axios.get('/api/CurrencyType/All').then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  }
+    getAll() {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/CurrencyType/All').then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+
+    listAll(page = 0, itemsPerPage = 50, orderAscending = true, 
+            orderingParam = "TypeShortForm") {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/CurrencyType/ListAll', {
+              params: {
+                page: page, 
+                itemsPerPage: itemsPerPage, 
+                orderAscending: orderAscending,
+                orderingParam: orderingParam
+              }
+            })
+                .then(function (response) {
+                    resolve(response.data);
+                }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 }
