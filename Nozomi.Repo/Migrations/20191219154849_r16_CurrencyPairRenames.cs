ee77b47dc7bbb5f2a0caf2985635af0ca9c1efe2
nixxholas <nixholas@icloud.com>
2019-12-19 23:53:11 +0800

    Nozomi.Repo: Migrations: r16 - CurrencyPairRenames migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20191219154849_r16_CurrencyPairRenames.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r16_CurrencyPairRenames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "CurrencyPair_AK_MainCurrency_CounterCurrency_Source",
+                table: "CurrencyPairs");
+
+            migrationBuilder.DropColumn(
+                name: "CounterCurrencyAbbrv",
+                table: "CurrencyPairs");
+
+            migrationBuilder.DropColumn(
+                name: "MainCurrencyAbbrv",
+                table: "CurrencyPairs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CounterTicker",
+                table: "CurrencyPairs",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainTicker",
+                table: "CurrencyPairs",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "CurrencyPair_AK_MainCurrency_CounterCurrency_Source",
+                table: "CurrencyPairs",
+                columns: new[] { "MainTicker", "CounterTicker", "SourceId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "CurrencyPair_AK_MainCurrency_CounterCurrency_Source",
+                table: "CurrencyPairs");
+
+            migrationBuilder.DropColumn(
+                name: "CounterTicker",
+                table: "CurrencyPairs");
+
+            migrationBuilder.DropColumn(
+                name: "MainTicker",
+                table: "CurrencyPairs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CounterCurrencyAbbrv",
+                table: "CurrencyPairs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainCurrencyAbbrv",
+                table: "CurrencyPairs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "CurrencyPair_AK_MainCurrency_CounterCurrency_Source",
+                table: "CurrencyPairs",
+                columns: new[] { "MainCurrencyAbbrv", "CounterCurrencyAbbrv", "SourceId" });
+        }
+    }
+}
