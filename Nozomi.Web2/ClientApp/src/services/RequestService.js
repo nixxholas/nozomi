709e4ca9a45bd78100838ecb48ee7bf92ff29fe0
nixxholas <nixholas@icloud.com>
2019-12-19 21:30:47 +0800

    Nozomi.Web2: ClientApp: src: services: RequestService: Update API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/RequestService.js
+++ Nozomi.Web2/ClientApp/src/services/RequestService.js
@@ -32,4 +32,21 @@ export default {
             });
         });
     },
+
+    update(vm) {
+        if (!vm)
+            throw new Error("Invalid payload. Please try again.");
+
+        return new Promise((resolve, reject) => {
+            axios.put('/api/Request/Update', vm, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 }
