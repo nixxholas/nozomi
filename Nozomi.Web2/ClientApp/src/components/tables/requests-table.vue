4316c44d265c2ff5225c8c4ce478e7222ca68a81
nixxholas <nixholas@icloud.com>
2019-12-19 21:35:08 +0800

    Nozomi.Web2: ClientApp: src: components: tables: requests-table: micro improvements for UI
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/components/tables/requests-table.vue
+++ Nozomi.Web2/ClientApp/src/components/tables/requests-table.vue
@@ -1,95 +1,103 @@
 <template>
-  <b-table
-    :loading="isLoading"
-    :data="requestData"
-    :columns="requestColumns"
-    detailed
-    detail-key="guid">
-    <template slot-scope="props">
-      <b-table-column field="requestType" label="Type">
-        <b-tag type="is-dark">
-          {{ getRequestType(props.row.requestType) }}
-        </b-tag>
-      </b-table-column>
-      <b-table-column field="responseType" label="Response Type">
-        <b-tag type="is-dark">
-          {{ getResponseType(props.row.responseType) }}
-        </b-tag>
-      </b-table-column>
-      <!--  https://github.com/buefy/buefy/issues/278#issuecomment-349536701  -->
-      <b-table-column field="dataPath" label="API Url" style="word-break:break-all;">
-        <a class="has-text-info" :href="props.row.dataPath">{{ props.row.dataPath }}</a>
-      </b-table-column>
-      <b-table-column field="delay" label="Delay">
-        <b-tag type="is-info">
-          {{ props.row.delay }} ms
-        </b-tag>
-      </b-table-column>
-      <b-table-column field="failureDelay" label="Failure Delay">
-        <b-tag type="is-warning">
-          {{ props.row.failureDelay }} ms
-        </b-tag>
-      </b-table-column>
-      <b-table-column field="actions" label="">
-        <div class="buttons">
-          <b-button type="is-danger"
-                    icon-left="trash">
-            Delete
-          </b-button>
-        </div>
-      </b-table-column>
-    </template>
-    <template slot="detail" slot-scope="props">
-      <b-taglist attached>
-        <b-tag type="is-dark">Unique ID</b-tag>
-        <b-tag type="is-info">{{ props.row.guid }}</b-tag>
-      </b-taglist>
-      <nav class="level is-mobile">
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Status</p>
-            <p class="title"
-               v-bind:class="{ 'has-text-danger': !props.row.isEnabled,
+    <b-table
+            :loading="isLoading"
+            :data="requestData"
+            :columns="requestColumns"
+            detailed
+            detail-key="guid">
+        <template slot-scope="props">
+            <b-table-column field="requestType" label="Type">
+                <b-tag type="is-dark">
+                    {{ getRequestType(props.row.requestType) }}
+                </b-tag>
+            </b-table-column>
+            <b-table-column field="responseType" label="Response Type">
+                <b-tag type="is-dark">
+                    {{ getResponseType(props.row.responseType) }}
+                </b-tag>
+            </b-table-column>
+            <!--  https://github.com/buefy/buefy/issues/278#issuecomment-349536701  -->
+            <b-table-column field="dataPath" label="API Url" style="word-break:break-all;">
+                <a class="has-text-info" :href="props.row.dataPath">{{ props.row.dataPath }}</a>
+            </b-table-column>
+            <b-table-column field="delay" label="Delay">
+                <b-tag type="is-info">
+                    {{ props.row.delay }} ms
+                </b-tag>
+            </b-table-column>
+            <b-table-column field="failureDelay" label="Failure Delay">
+                <b-tag type="is-warning">
+                    {{ props.row.failureDelay }} ms
+                </b-tag>
+            </b-table-column>
+            <b-table-column field="actions" label="">
+                <div class="buttons">
+                  <RequestModal :request="props.row"/>
+                    <b-button type="is-danger"
+                              icon-left="trash">
+                        Delete
+                    </b-button>
+                </div>
+            </b-table-column>
+        </template>
+        <template slot="detail" slot-scope="props">
+            <b-taglist attached>
+                <b-tag type="is-dark">Unique ID</b-tag>
+                <b-tag type="is-info">{{ props.row.guid }}</b-tag>
+            </b-taglist>
+            <nav class="level is-mobile">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Status</p>
+                        <p class="title"
+                           v-bind:class="{ 'has-text-danger': !props.row.isEnabled,
                            'has-text-success': props.row.isEnabled }">
-              {{ props.row.isEnabled ? "Active" : "Disabled" }}
-            </p>
-          </div>
-        </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <p class="heading">Request Component</p>
-            <p>
-              <CreateRCComponent v-bind:guid="props.row.guid"></CreateRCComponent>
-            </p>
-          </div>
-        </div>
-      </nav>
-    </template>
-    <template slot="empty">
-      <section class="section">
-        <div class="content has-text-grey has-text-centered">
-          <p>
-            <b-icon
-              icon="sad-cry"
-              size="is-large">
-            </b-icon>
-          </p>
-          <p>Nothing here.</p>
-        </div>
-      </section>
-    </template>
-  </b-table>
+                            {{ props.row.isEnabled ? "Active" : "Disabled" }}
+                        </p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Request Component</p>
+                        <p>
+                            <CreateRCComponent v-bind:guid="props.row.guid" />
+                        </p>
+                    </div>
+                </div>
+            </nav>
+        </template>
+        <template slot="empty">
+            <section class="section">
+                <div class="content has-text-grey has-text-centered">
+                    <p>
+                        <b-icon
+                                icon="sad-cry"
+                                size="is-large">
+                        </b-icon>
+                    </p>
+                    <p>Nothing here.</p>
+                </div>
+            </section>
+        </template>
+    </b-table>
 </template>
 
 <script>
     import store from '@/store/index';
     // Request Component imports
     import CreateRCComponent from '@/components/modals/create-request-component-modal';
+    import RequestModal from '@/components/modals/request-modal'
     import RequestService from "@/services/RequestService";
 
     export default {
         name: "requests-table",
-        components: { CreateRCComponent },
+        components: { CreateRCComponent, RequestModal },
+        props: {
+            request: {
+                default: null,
+                type: Object
+            }
+        },
         data: function () {
             return {
                 isLoading: true,
@@ -150,10 +158,10 @@
 
                 this.isLoading = false;
             },
-            getRequestType: function(val) {
+            getRequestType: function (val) {
                 let result = "-";
 
-                this.requestTypes.forEach(function(item){
+                this.requestTypes.forEach(function (item) {
                     if (item.value === val) {
                         result = item.key;
                     }
@@ -161,10 +169,10 @@
 
                 return result;
             },
-            getResponseType: function(val) {
+            getResponseType: function (val) {
                 let result = "-";
 
-                this.responseTypes.forEach(function(item){
+                this.responseTypes.forEach(function (item) {
                     if (item.value === val) {
                         result = item.key;
                     }
@@ -173,7 +181,7 @@
                 return result;
             }
         },
-        beforeMount: function() {
+        beforeMount: function () {
             this.updateRequests();
 
             let self = this;
