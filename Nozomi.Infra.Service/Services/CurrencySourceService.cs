0a6ee88b73b7bbca357fcaddd97f5e2677ff0c70
nixxholas <nixholas@icloud.com>
2019-12-20 12:41:46 +0800

    Nozomi.Infra.Service: Services: CurrencySource: EnsurePairIsCreated API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Services/CurrencySourceService.cs
+++ Nozomi.Infra.Service/Services/CurrencySourceService.cs
@@ -8,15 +8,19 @@ using Nozomi.Data.Models.Currency;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.BCL.Repository;
 using Nozomi.Repo.Data;
+using Nozomi.Service.Events.Interfaces;
 using Nozomi.Service.Services.Interfaces;
 
 namespace Nozomi.Service.Services
 {
     public class CurrencySourceService : BaseService<CurrencySourceService, NozomiDbContext>, ICurrencySourceService
     {
-        public CurrencySourceService(ILogger<CurrencySourceService> logger,
+        private readonly ICurrencyEvent _currencyEvent;
+        
+        public CurrencySourceService(ILogger<CurrencySourceService> logger, ICurrencyEvent currencyEvent, 
             IUnitOfWork<NozomiDbContext> unitOfWork) : base(logger, unitOfWork)
         {
+            _currencyEvent = currencyEvent;
         }
 
         public NozomiResult<string> Create(CreateCurrencySource currencySource, string userId = null)
@@ -44,6 +48,46 @@ namespace Nozomi.Service.Services
             {
                 return new NozomiResult<string>(NozomiResultType.Failed, ex.ToString());
             }
+        }
+
+        public bool EnsurePairIsCreated(string mainTicker, string counterTicker, long sourceId,
+            string userId = null)
+        {
+            if (!_unitOfWork.GetRepository<CurrencySource>()
+                .GetQueryable()
+                .AsNoTracking()
+                .Include(cs => cs.Currency)
+                .Any(cs => cs.DeletedAt == null && cs.IsEnabled 
+                                                  && cs.Currency != null 
+                                                  && cs.Currency.Slug.Equals(mainTicker)))
+            {
+                // Since it doesn't exist, symlink the new currency source
+                var mainCurrencySource = new CurrencySource(sourceId);
+                var mainCurrency = _currencyEvent.GetBySlug(mainTicker);
+                mainCurrencySource.CurrencyId = mainCurrency.Id;
+                
+                _unitOfWork.GetRepository<CurrencySource>().Add(mainCurrencySource);
+                _unitOfWork.Commit(userId);
+            }
+            
+            if (!_unitOfWork.GetRepository<CurrencySource>()
+                .GetQueryable()
+                .AsNoTracking()
+                .Include(cs => cs.Currency)
+                .Any(cs => cs.DeletedAt == null && cs.IsEnabled 
+                                                && cs.Currency != null 
+                                                && cs.Currency.Slug.Equals(counterTicker)))
+            {
+                // Since it doesn't exist, symlink the new currency source
+                var counterCurrencySource = new CurrencySource(sourceId);
+                var counterCurrency = _currencyEvent.GetBySlug(counterTicker);
+                counterCurrencySource.CurrencyId = counterCurrency.Id;
+                
+                _unitOfWork.GetRepository<CurrencySource>().Add(counterCurrencySource);
+                _unitOfWork.Commit(userId);
+            }
+
+            return true;
         }
 
         public NozomiResult<string> Delete(long id, string userId = null)
--- Nozomi.Infra.Service/Services/Interfaces/ICurrencySourceService.cs
+++ Nozomi.Infra.Service/Services/Interfaces/ICurrencySourceService.cs
@@ -7,6 +7,10 @@ namespace Nozomi.Service.Services.Interfaces
     public interface ICurrencySourceService
     {
         NozomiResult<string> Create(CreateCurrencySource currencySource, string userId = null);
+
+        bool EnsurePairIsCreated(string mainTicker, string counterTicker, long sourceId, 
+            string userId);
+        
         NozomiResult<string> Delete(long id, string userId = null);
     }
 }
