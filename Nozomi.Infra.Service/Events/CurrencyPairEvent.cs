b2df669b2f508b01cc1ffacb4d25b4811fd8e43f
nixxholas <nixholas@icloud.com>
2019-12-20 22:58:20 +0800

    Nozomi.Infra.Service: Events: CurrencyPairEvent: All API: Optimise
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Events/CurrencyPairEvent.cs
+++ Nozomi.Infra.Service/Events/CurrencyPairEvent.cs
@@ -110,8 +110,9 @@ namespace Nozomi.Service.Events
                 .Where(cp => cp.IsEnabled && cp.DeletedAt == null && cp.SourceId > 0);
 
             if (!string.IsNullOrEmpty(mainTicker))
-                query = query.Where(cp => cp.MainTicker.Equals(mainTicker));
-            
+                query = query.Where(cp => cp.MainTicker.ToUpper()
+                    .Equals(mainTicker.ToUpper()));
+
             if (!string.IsNullOrEmpty(sourceGuid) && Guid.TryParse(sourceGuid, out var parsedSourceGuid))
                 query = query
                     .Include(c => c.Source)
