925e41625846904d7e200ae42527da24653d8ac3
nixxholas <nixholas@icloud.com>
2019-12-21 22:56:53 +0800

    Nozomi.Web2: ClientApp: src: services: CurrencyService: list API
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/CurrencyService.js
+++ Nozomi.Web2/ClientApp/src/services/CurrencyService.js
@@ -3,106 +3,117 @@ import store from '../store/index';
 import Converter from '../helpers/converter';
 
 export default {
-  getCurrencyData(page = 1, itemsPerPage = 50, type = "CRYPTO", sortType = "MarketCap", 
-                  typesToTake = ["MarketCap"], descendingOrder = true) {
-    if (!typesToTake)
-      typesToTake = ["MarketCap"];
-    
-    return new Promise((resolve, reject) => {
-      axios.get('/api/Currency/All?' +
-        Converter.arrayToString("typesToTake", typesToTake), {
-        params: {
-          currencyType: type,
-          itemsPerIndex: itemsPerPage,
-          index: (page - 1),
-          sortType: sortType, // 1 = Market cap
-          orderDescending: descendingOrder,
-        }
-      }).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  },
+    getCurrencyData(page = 1, itemsPerPage = 50, type = "CRYPTO", sortType = "MarketCap",
+                    typesToTake = ["MarketCap"], descendingOrder = true) {
+        if (!typesToTake)
+            typesToTake = ["MarketCap"];
 
-  listAll(page = 0, itemsPerPage = 50, currencyTypeName = null, orderAscending = false, 
-          orderingParam = "Abbreviation") {
-    return new Promise((resolve, reject) => {
-      axios.get('/api/Currency/ListAll', {
-        params: {
-          currencyTypeName: currencyTypeName,
-          page: page,
-          itemsPerPage: itemsPerPage,
-          orderAscending: orderAscending,
-          orderingParam: orderingParam
-        }
-      }).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  },
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Currency/All?' +
+                Converter.arrayToString("typesToTake", typesToTake), {
+                params: {
+                    currencyType: type,
+                    itemsPerIndex: itemsPerPage,
+                    index: (page - 1),
+                    sortType: sortType, // 1 = Market cap
+                    orderDescending: descendingOrder,
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 
-  getCurrencyCount(type) {
-    return new Promise((resolve, reject) => {
-      axios.get('/api/Currency/GetCountByType', {
-        params: {
-          currencyType: type,
-        }
-      }).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  },
+    list(slug = null) {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Currency/List?' + slug)
+                .then(function (response) {
+                    resolve(response.data);
+                }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 
-  getPairCount(slug) {
-    if (!slug)
-      return 0;
-    
-    return new Promise((resolve, reject) => {
-      axios.get('/api/Currency/GetPairCount/' + slug).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  },
-  
-  create(vm) {
-    if (!vm)
-      throw new Error("Invalid payload. Please try again.");
+    listAll(page = 0, itemsPerPage = 50, currencyTypeName = null, orderAscending = false,
+            orderingParam = "Abbreviation") {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Currency/ListAll', {
+                params: {
+                    currencyTypeName: currencyTypeName,
+                    page: page,
+                    itemsPerPage: itemsPerPage,
+                    orderAscending: orderAscending,
+                    orderingParam: orderingParam
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 
-    return new Promise((resolve, reject) => {
-      axios.post('/api/Currency/Create', vm, {
-        headers: {
-          Authorization: "Bearer " + store.state.oidcStore.access_token
-        }
-      }).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  },
+    getCurrencyCount(type) {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Currency/GetCountByType', {
+                params: {
+                    currencyType: type,
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
 
-  edit(vm) {
-    if (!vm)
-      throw new Error("Invalid payload. Please try again.");
+    getPairCount(slug) {
+        if (!slug)
+            return 0;
 
-    return new Promise((resolve, reject) => {
-      axios.put('/api/Currency/Edit', vm, {
-        headers: {
-          Authorization: "Bearer " + store.state.oidcStore.access_token
-        }
-      }).then(function (response) {
-        resolve(response.data);
-      }).catch(function (error) {
-        reject(error);
-      });
-    });
-  }
+        return new Promise((resolve, reject) => {
+            axios.get('/api/Currency/GetPairCount/' + slug).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+
+    create(vm) {
+        if (!vm)
+            throw new Error("Invalid payload. Please try again.");
+
+        return new Promise((resolve, reject) => {
+            axios.post('/api/Currency/Create', vm, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+
+    edit(vm) {
+        if (!vm)
+            throw new Error("Invalid payload. Please try again.");
+
+        return new Promise((resolve, reject) => {
+            axios.put('/api/Currency/Edit', vm, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    }
 }
