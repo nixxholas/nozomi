87210d3196ae1f0fcbf2dfe93eaf51e47a761150
nixxholas <nixholas@icloud.com>
2019-12-29 04:12:15 +0800

    Nozomi.Web2: ClientApp: src: views: Changelog: preview 6 log!
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/views/Changelog.vue
+++ Nozomi.Web2/ClientApp/src/views/Changelog.vue
@@ -63,6 +63,17 @@
                                 </ul>
                             </div>
                         </div>
+                        <div class="timeline-item is-primary">
+                            <div class="timeline-marker is-primary"></div>
+                            <div class="timeline-content">
+                                <p class="heading">29 December 2019 - <b>preview 6</b></p>
+                                <ul>
+                                    <li>.NET Core 3.1 framework compatibility update and hotfixing</li>
+                                    <li>Backend sorting is now working as intended</li>
+                                    <li>We literally added a huge supercharger to every part of the app</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
