6a0eebeb233bd6ead34e0e4055e046ccf99ce7e0
nixxholas <nixholas@icloud.com>
2019-12-30 18:26:29 +0800

    Nozomi.Web2: ClientApp: src: services: ComponentService: Proper scaffold
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/ComponentService.js
+++ Nozomi.Web2/ClientApp/src/services/ComponentService.js
@@ -1,9 +1,68 @@
-
+import axios from 'axios';
+import store from "../store";
 
 export default {
-  getAll() {
-    
+  all(index = 0, itemsPerPage = 0,
+      includeNested = false) {
+
+    return new Promise((resolve, reject) => {
+      axios.get('/api/ComponentType/All', {
+        headers: {
+          Authorization: "Bearer " + store.state.oidcStore.access_token
+        },
+        params: {
+          index: index,
+          itemsPerPage: itemsPerPage,
+          includeNested: includeNested
+        }
+      }).then(function (response) {
+        resolve(response);
+      }).catch(function (error) {
+        reject(error);
+      });
+    });
   },
+  
+  allByRequest(requestGuid, index = 0, itemsPerPage = 0,
+      includeNested = false) {
+
+    return new Promise((resolve, reject) => {
+      axios.get('/api/ComponentType/AllByRequest', {
+        headers: {
+          Authorization: "Bearer " + store.state.oidcStore.access_token
+        },
+        params: {
+          requestGuid: requestGuid,
+          index: index,
+          itemsPerPage: itemsPerPage,
+          includeNested: includeNested
+        }
+      }).then(function (response) {
+        resolve(response);
+      }).catch(function (error) {
+        reject(error);
+      });
+    });
+  },
+
+  create(vm) {
+    if (!vm)
+      throw new Error("Invalid payload. Please try again.");
+
+    return new Promise((resolve, reject) => {
+      axios.post('/api/Component/Create', vm, {
+        headers: {
+          Authorization: "Bearer " + store.state.oidcStore.access_token
+        }
+      }).then(function (response) {
+        resolve(response);
+      }).catch(function (error) {
+        reject(error);
+      });
+    });
+  },
+  
+  // Ad-hoc functions
   
   getComponentValue(dataset, type) {
     if (dataset && dataset.length > 0) {
