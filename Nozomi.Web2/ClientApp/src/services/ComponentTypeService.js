680e32d30e3fb1aeb5f5bfcc6a9935d89185fe9e
nixxholas <nixholas@icloud.com>
2020-01-02 17:30:09 +0800

    Nozomi.Web2: ClientApp: src: services: ComponentTypeService: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/ClientApp/src/services/ComponentTypeService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import store from "../store";
+
+export default {
+    all(index = 0, itemsPerPage = 0,
+        includeNested = false) {
+
+        return new Promise((resolve, reject) => {
+            axios.get('/api/ComponentType/All', {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+}
