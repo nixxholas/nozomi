aa0ec9a950c74d098e444ce18886b1ef5318d316
nixxholas <nixholas@icloud.com>
2020-01-04 09:52:43 +0800

    Nozomi.Web2: ClientApp: src: views: Pricing: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/ClientApp/src/views/Pricing.vue
@@ -0,0 +1,101 @@
+<template>
+    <section class="hero">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Pricing
+                </h1>
+                <h2 class="subtitle">
+                    Hero subtitle
+                </h2>
+                
+                <div class="pricing-table">
+                    <div class="pricing-plan">
+                        <div class="plan-header">Railcar</div>
+                        <div class="plan-price"><span class="plan-price-amount">Free!</span></div>
+                        <div class="plan-items">
+                            <div class="plan-item">1k API calls</div>
+                            <div class="plan-item">2 Requests</div>
+                            <div class="plan-item">4 Computes</div>
+                            <div class="plan-item">5s Speed limit</div>
+                            <div class="plan-item"><b>Publicly Distributed</b></div>
+                        </div>
+                    </div>
+
+                    <div class="pricing-plan is-warning">
+                        <div class="plan-header">Monorail</div>
+                        <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>5</span>/month</div>
+                        <div class="plan-items">
+                            <div class="plan-item">10k API calls</div>
+                            <div class="plan-item">5 Requests</div>
+                            <div class="plan-item">20 Computes</div>
+                            <div class="plan-item">3s Speed Limit</div>
+                            <div class="plan-item"><b>Publicly Distributed</b></div>
+                        </div>
+<!--                        <div class="plan-footer">-->
+<!--                            <button class="button is-fullwidth">Choose</button>-->
+<!--                        </div>-->
+                    </div>
+
+                    <div class="pricing-plan is-info">
+                        <div class="plan-header">Rapid</div>
+                        <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>20</span>/month</div>
+                        <div class="plan-items">
+                            <div class="plan-item">75k API calls</div>
+                            <div class="plan-item">10 Requests</div>
+                            <div class="plan-item">20 Computes</div>
+                            <div class="plan-item">1s Speed Limit</div>
+                            <div class="plan-item">2 Historical Requests*</div>
+                            <div class="plan-item"><b>Publicly Distributed</b></div>
+                        </div>
+<!--                        <div class="plan-footer">-->
+<!--                            <button class="button is-fullwidth">Choose</button>-->
+<!--                        </div>-->
+                    </div>
+
+                    <div class="pricing-plan is-info">
+                        <div class="plan-header">Shinkansen</div>
+                        <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>50</span>/month</div>
+                        <div class="plan-items">
+                            <div class="plan-item">200k API calls</div>
+                            <div class="plan-item">15 Requests</div>
+                            <div class="plan-item">30 Computes</div>
+                            <div class="plan-item">1s Speed Limit</div>
+                            <div class="plan-item">10 Historical Requests*</div>
+                            <div class="plan-item"><b>Publicly Distributed</b></div>
+                        </div>
+                        <!--                        <div class="plan-footer">-->
+                        <!--                            <button class="button is-fullwidth">Choose</button>-->
+                        <!--                        </div>-->
+                    </div>
+                </div>
+                
+                <div class="pricing-table">
+
+
+                    <div class="pricing-plan is-danger">
+                        <div class="plan-header">Maglev</div>
+                        <div class="plan-price">
+                            Starting from
+                            <br /><span class="plan-price-amount"><span class="plan-price-currency">$</span>300</span>/month</div>
+                        <div class="plan-items">
+                            <div class="plan-item">From 1.5m API calls</div>
+                            <div class="plan-item">From 50 Requests</div>
+                            <div class="plan-item">From 50 Computes</div>
+                            <div class="plan-item">100ms Speed Limit</div>
+                            <div class="plan-item">10 Historical Requests*</div>
+                            <div class="plan-item"><b>Privately Distributed</b></div>
+                        </div>
+                        <div class="plan-footer">
+                            <button class="button is-fullwidth">Contact us</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <ul>
+                    <li><i>* Data retention of up to 7 days</i></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
