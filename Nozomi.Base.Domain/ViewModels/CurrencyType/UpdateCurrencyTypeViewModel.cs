f762fa3dd03caa838582b3f1926b10a8144c77c1
nixxholas <nixholas@icloud.com>
2020-01-11 16:33:35 +0800

    Nozomi.Base.Domain: ViewModels: CurrencyType: UpdateCurrencyTypeViewModel: FluentValidation implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/CurrencyType/UpdateCurrencyTypeViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/CurrencyType/UpdateCurrencyTypeViewModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using FluentValidation;
 
 namespace Nozomi.Data.ViewModels.CurrencyType
 {
@@ -6,5 +7,21 @@ namespace Nozomi.Data.ViewModels.CurrencyType
     {
         [DefaultValue(false)]
         public bool Delete { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new UpdateCurrencyTypeValidator();
+            return validator.Validate(this).IsValid;
+        }
+        
+        public class UpdateCurrencyTypeValidator : AbstractValidator<UpdateCurrencyTypeViewModel>
+        {
+            public UpdateCurrencyTypeValidator()
+            {
+                RuleFor(st => st.TypeShortForm).NotNull().NotEmpty()
+                    .Must(e => e.Length <= 12);
+                RuleFor(st => st.Name).NotNull().NotEmpty();
+            }
+        }
     }
 }
