4535332bf8756f0d3562d32f2ceba77fdb4c8f76
nixxholas <nixholas@icloud.com>
2020-01-11 16:57:16 +0800

    Nozomi.Infra.Preprocessing: Filters: HttpGlobalExceptionFilter: IWebHostEnvironment usage
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Preprocessing/Filters/HttpGlobalExceptionFilter.cs
+++ Nozomi.Infra.Preprocessing/Filters/HttpGlobalExceptionFilter.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Nozomi.Base.BCL;
 using Nozomi.Preprocessing.ActionResults;
@@ -11,10 +12,10 @@ namespace Nozomi.Preprocessing.Filters
 {
     public class HttpGlobalExceptionFilter : IExceptionFilter
     {
-        private readonly IHostingEnvironment env;
+        private readonly IWebHostEnvironment env;
         private readonly ILogger<HttpGlobalExceptionFilter> logger;
 
-        public HttpGlobalExceptionFilter(IHostingEnvironment env, ILogger<HttpGlobalExceptionFilter> logger)
+        public HttpGlobalExceptionFilter(IWebHostEnvironment env, ILogger<HttpGlobalExceptionFilter> logger)
         {
             this.env = env;
             this.logger = logger;
