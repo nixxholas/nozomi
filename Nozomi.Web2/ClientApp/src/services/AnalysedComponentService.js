5ba096648cbc7fd5c86b16490f5e904bc5c96083
nixxholas <nixholas@icloud.com>
2020-01-12 21:20:23 +0800

    Nozomi.Web2: ClientApp: src: services: AnalysedComponentService: get API: Initial introduction
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
+++ Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
@@ -2,6 +2,21 @@ import store from '../store/index';
 import axios from 'axios';
 
 export default {
+    get(guid) {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/AnalysedComponent/Get/' + guid, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            })
+                .then(function (response) {
+                    resolve(response);
+                }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+
     // TODO: Haven't started lol
     getAll() {
         return new Promise((resolve, reject) => {
