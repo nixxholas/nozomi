02d224d990946187ca6102c4cc39d0fb8aa123c9
nixxholas <nixholas@icloud.com>
2020-01-12 23:01:34 +0800

    Nozomi.Web2: ClientApp: src: services: AnalysedComponentService: all API: AllByIdentifier API implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
+++ Nozomi.Web2/ClientApp/src/services/AnalysedComponentService.js
@@ -2,6 +2,42 @@ import store from '../store/index';
 import axios from 'axios';
 
 export default {
+    all(currencySlug, currencyPairGuid, currencyTypeShortForm, 
+        index = 0, itemsPerPage = 200) {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/AnalysedComponent/AllByIdentifier', {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                },
+                params: {
+                    currencySlug: currencySlug,
+                    currencyPairGuid: currencyPairGuid,
+                    currencyTypeShortForm: currencyTypeShortForm,
+                    index: index,
+                    itemsPerPage: itemsPerPage
+                }
+            })
+                .then(function (response) {
+                    resolve(response);
+                }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+    
+    create(payload) {
+        axios.post('/api/AnalysedComponent/Create', payload, {
+            headers: {
+                Authorization: "Bearer " + store.state.oidcStore.access_token
+            }
+        })
+            .then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+            reject(error);
+        });
+    },
+
     get(guid) {
         return new Promise((resolve, reject) => {
             axios.get('/api/AnalysedComponent/Get/' + guid, {
