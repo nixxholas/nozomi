82ec9ad71aea64523723f07ae7e925cc83814cf0
nixxholas <nixholas@icloud.com>
2020-01-12 18:35:30 +0800

    Nozomi.Web2: ClientApp: src: components: autocompletes: currency-pairs-autocomplete: finally everything works gracefully
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/components/autocompletes/currency-pairs-autocomplete.vue
+++ Nozomi.Web2/ClientApp/src/components/autocompletes/currency-pairs-autocomplete.vue
@@ -6,17 +6,16 @@
                 placeholder="e.g. EURUSD"
                 :custom-formatter="getCurrencyPairTickerPairStr"
                 :loading="isLoading"
-                @select="option => selected = option"
+                @select="option => selected ? selected = option.guid : selected = null"
                 @typing="getAsyncData"
                 @infinite-scroll="getMoreAsyncData">
-
             <template slot-scope="props">
                 <div class="media">
                     <div class="media-content">
                         {{ props.option.mainTicker + props.option.counterTicker }}
                         <br>
-                        <small v-if="props.option.source">
-                            From <b><i v-if="props.option.source.name">{{ props.option.source.name }}</i></b>
+                        <small v-if="props.option.sourceGuid && getSource(props.option.sourceGuid)">
+                            From <b><i>{{ getSource(props.option.sourceGuid).name }}</i></b>
                         </small>
                     </div>
                 </div>
@@ -26,85 +25,107 @@
 </template>
 
 <script>
-    import debounce from 'lodash/debounce';
     import CurrencyPairService from "@/services/CurrencyPairService";
+    import SourceService from "@/services/SourceService";
 
-export default {
-    name: 'currency-pairs-autocomplete',
-    props: {
-        incomingCurrencyPairGuid: {
-            type: String,
-            default: null
-        }
-    },
-    data: function () {
-        return {
-            isLoading: false,
-            data: [],
-            page: 0,
-            query: '',
-            selected: null,
-            currencyPairGuid: null,
-        }
-    },
-    watch: {
-        selected: function (newVal) {
-            // Safetynet
-            if (newVal && newVal.guid) {
-                this.currencyPairGuid = newVal.guid;
-
-                // https://www.telerik.com/blogs/how-to-emit-data-in-vue-beyond-the-vuejs-documentation
-                this.$emit('input', newVal.guid); // Emit the value out for v-model support
+    export default {
+        name: 'currency-pairs-autocomplete',
+        props: {
+            incomingCurrencyPairGuid: {
+                type: String,
+                default: null
             }
-        }
-    },
-    methods: {
-        getCurrencyPairTickerPairStr: function (obj) {
-            if (!obj)
+        },
+        data: function () {
+            return {
+                isLoading: false,
+                data: [],
+                page: 0,
+                query: '',
+                selected: null,
+                currencyPairGuid: null,
+                sources: []
+            }
+        },
+        watch: {
+            selected: function (newVal) {
+                // Safetynet
+                if (newVal && newVal.guid) {
+                    this.currencyPairGuid = newVal.guid;
+
+                    // https://www.telerik.com/blogs/how-to-emit-data-in-vue-beyond-the-vuejs-documentation
+                    this.$emit('input', newVal.guid); // Emit the value out for v-model support
+                }
+            }
+        },
+        methods: {
+            getCurrencyPairTickerPairStr: function (obj) {
+                if (obj && obj.mainTicker && obj.counterTicker && obj.sourceGuid) {
+                    // + obj.source.name + ""
+                    let source = this.getSource(obj.sourceGuid);
+
+                    if (source)
+                        return obj.mainTicker + obj.counterTicker + " from " + source.name;
+                    else
+                        return obj.mainTicker + obj.counterTicker;
+                }
+
+                // Reset everything if something is off
+                this.selected = null;
+                this.currencyPairGuid = null;
                 return '';
+            },
+            getAsyncData: function (query) {
+                let self = this;
+                self.isLoading = true;
 
-            return obj.mainTicker + obj.counterTicker + " from " + obj.source.name + "";
+                CurrencyPairService.search(query, this.page, 50)
+                    .then(function (data) {
+                        self.data = data;
+
+                        // self.page++;
+                        // this.totalPages = data.total_pages
+                    })
+                    .catch((error) => {
+                        throw error;
+                    })
+                    .finally(() => {
+                        self.isLoading = false;
+                    })
+            },
+            getMoreAsyncData: function () {
+                this.getAsyncData(this.query);
+            },
+            getSource: function(guid) {
+                if (this.sources && this.sources.length > 0) {
+                    return this.sources.filter(e => e.guid === guid)[0];
+                }
+                
+                return null;
+            }
         },
-        getAsyncData: function (query) {
+        created: function () {
             let self = this;
+
+            // Synchronously call for data
             self.isLoading = true;
-
-            console.dir(query);
-            CurrencyPairService.search(query, this.page, 50)
-                .then(function (data) {
-                    self.data = data;
-                    
-                    self.page++;
-                    // this.totalPages = data.total_pages
+            CurrencyPairService.all()
+                .then(function (response) {
+                    self.data = response;
                 })
-                .catch((error) => {
-                    throw error;
+                .catch(function (error) {
+                    // handle error
+                    self.methods.authenticateOidc(self.currentRoute);
                 })
-                .finally(() => {
+                .finally(function () {
+                    // always executed
                     self.isLoading = false;
+                });
+        
+            SourceService.getAll()
+                .then(function (response) {
+                    self.sources = response;
                 })
-        },
-        getMoreAsyncData: function () {
-            this.getAsyncData(this.query);
         }
-    },
-    created: function () {
-        let self = this;
-
-        // Synchronously call for data
-        self.isLoading = true;
-        CurrencyPairService.all()
-            .then(function (response) {
-                self.data = response;
-            })
-            .catch(function (error) {
-                // handle error
-                self.methods.authenticateOidc(self.currentRoute);
-            })
-            .finally(function () {
-                // always executed
-                self.isLoading = false;
-            });
     }
-}
 </script>
