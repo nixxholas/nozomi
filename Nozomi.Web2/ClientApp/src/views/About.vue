e8e48717758723cfff3ee6ecbc2f54356dd7955f
nixxholas <nixholas@icloud.com>
2020-01-13 23:16:26 +0800

    Nozomi.Web2: ClientApp: more content
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/views/About.vue
+++ Nozomi.Web2/ClientApp/src/views/About.vue
@@ -1,212 +1,204 @@
 <template>
-  <div>
-    <section class="hero is-medium">
-      <div class="hero-body">
-        <div class="columns is-centered">
-          <div class="column is-5 is-offset-1 landing-caption">
-            <h1 class="title is-1 is-bold is-spaced">
-              Poll, Aggregate.
-            </h1>
-            <h2 class="subtitle is-5 is-muted">We’re an API-first data company delivering retrograde market data and
-              aggregating APIs to products that require a strong and reliable data backbone.</h2>
-            <p>
-              <b-button rounded type="is-dark">Get Started</b-button>
-            </p>
-          </div>
-          <div class="column is-5">
-            <figure class="image">
-              <img src="@/assets/svg/illustrations/knowledgebase-community.png">
-            </figure>
-          </div>
-          <div class="column is-1"></div>
-        </div>
-      </div>
-    </section>
-    
-    <section class="hero is-medium">
-      <div class="hero-body">
-        <h1 class="title is-1 is-bold is-spaced pb-3">
-          Timeline
-        </h1>
-          <div class="timeline is-centered">
-            <header class="timeline-header">
-              <span class="tag is-medium is-primary">Genesis</span>
-            </header>
-            <div class="timeline-item">
-              <div class="timeline-marker is-primary"></div>
-              <div class="timeline-content">
-                <p class="heading">April 2018</p>
-                <p>Side project kickoff</p>
-              </div>
+    <div>
+        <section class="hero is-medium">
+            <div class="hero-body">
+                <div class="columns is-centered">
+                    <div class="column is-5 is-offset-1 landing-caption">
+                        <h1 class="title is-1 is-bold is-spaced">
+                            Poll, Aggregate.
+                        </h1>
+                        <h2 class="subtitle is-5 is-muted">We’re an API-first data company delivering retrograde market
+                            data and
+                            aggregating APIs to products that require a strong and reliable data backbone.</h2>
+                        <p>
+                            <b-button rounded type="is-dark">Get Started</b-button>
+                        </p>
+                    </div>
+                    <div class="column is-5">
+                        <figure class="image">
+                            <img src="@/assets/svg/illustrations/knowledgebase-community.png">
+                        </figure>
+                    </div>
+                    <div class="column is-1"></div>
+                </div>
             </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-info is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">October 2018</p>
-                <p>Wrapping engine completion</p>
-              </div>
-            </div>
-            <header class="timeline-header">
-              <span class="tag is-primary">2019</span>
-            </header>
-            <div class="timeline-item">
-              <div class="timeline-marker is-info is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">January 2019</p>
-                <p>Compute engine completion</p>
-              </div>
-            </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-info is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">April 2019</p>
-                <p>Initial UI</p>
-              </div>
-            </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-primary is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">October 2019</p>
-                <p>Public preview</p>
-              </div>
-            </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-primary is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">February 2020</p>
-                <p>Seed funding round</p>
-              </div>
-            </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-primary is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">March 2020</p>
-                <p>Public beta</p>
-              </div>
-            </div>
-            <div class="timeline-item">
-              <div class="timeline-marker is-primary is-icon">
-                <i class="fa fa-plus"></i>
-              </div>
-              <div class="timeline-content">
-                <p class="heading">September 2020</p>
-                <p>Public Release Candidate</p>
-              </div>
-            </div>
-            <header class="timeline-header">
-              <span class="tag is-medium is-primary">World domination</span>
-            </header>
-          </div>
-      </div>
-    </section>
+        </section>
 
-    <section class="hero is-primary is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1">
-            Our team
-          </h1>
-          
-          <div class="row">
-            <div class="columns is-desktop">
-              <div class="column">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by4">
-                      <img src="/assets/img/people/nicholas.jpg">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-content">
-                        <p class="title is-4 has-text-black">Nicholas Chen</p>
-                        <p class="subtitle is-6 has-text-black">Founder</p>
-                      </div>
+        <section class="hero is-medium">
+            <div class="hero-body">
+                <h1 class="title is-1 is-bold is-spaced pb-3">
+                    Timeline
+                </h1>
+                <div class="timeline is-centered">
+                    <header class="timeline-header">
+                        <span class="tag is-medium is-primary">Genesis</span>
+                    </header>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-primary"></div>
+                        <div class="timeline-content">
+                            <p class="heading">April 2018</p>
+                            <p>Side project kickoff</p>
+                        </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div class="column">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by4">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-content">
-                        <p class="title is-4 has-text-black">Qi Xiang</p>
-                        <p class="subtitle is-6 has-text-black">Backend Engineer</p>
-                      </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-info is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">October 2018</p>
+                            <p>Wrapping engine completion</p>
+                        </div>
                     </div>
-                  </div>
+                    <header class="timeline-header">
+                        <span class="tag is-primary">2019</span>
+                    </header>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-info is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">January 2019</p>
+                            <p>Compute engine completion</p>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-info is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">April 2019</p>
+                            <p>Initial UI</p>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-primary is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">October 2019</p>
+                            <p>Public preview</p>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-primary is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">February 2020</p>
+                            <p>Seed funding round</p>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-primary is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">March 2020</p>
+                            <p>Public beta</p>
+                        </div>
+                    </div>
+                    <div class="timeline-item">
+                        <div class="timeline-marker is-primary is-icon">
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        <div class="timeline-content">
+                            <p class="heading">September 2020</p>
+                            <p>Public Release Candidate</p>
+                        </div>
+                    </div>
+                    <header class="timeline-header">
+                        <span class="tag is-medium is-primary">World domination</span>
+                    </header>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
 
-    <section class="hero is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1">
-            Never forgotten.
-          </h1>
+        <section class="hero is-light is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">
+                        Our team
+                    </h1>
 
-          <div class="row">
-            <div class="columns is-desktop">
-              <div class="column">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-content">
-                        <p class="title is-4 has-text-black">Jasper Chua</p>
-                        <p class="subtitle is-6 has-text-black">Contributions to Nozomi's First Business Plan</p>
-                      </div>
+                    <div class="tile is-ancestor">
+                        <div class="tile is-vertical is-4">
+                            <div class="tile is-parent">
+                                <div class="tile is-child">
+                                    <div class="image is-384x384">
+                                        <img src="/assets/img/people/nicholas.jpg" />
+                                    </div>
+                                    <p class="title is-4 pt-2"><b>Nicholas Chen</b></p>
+                                    <p class="subtitle">Engineering</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile is-vertical is-4">
+                            <div class="tile is-parent">
+                                <div class="tile is-child">
+                                    <div class="image is-384x384">
+                                        <img src="/assets/img/people/qixiang.jpg" />
+                                    </div>
+                                    <p class="title is-4 pt-2"><b>Cho Qi Xiang</b></p>
+                                    <p class="subtitle">Partner</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile is-vertical is-4">
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-              <div class="column">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-content">
-                        <p class="title is-4 has-text-black">Elvis Yong</p>
-                        <p class="subtitle is-6 has-text-black">Contributions to Nozomi's First Web App</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+        </section>
+
+        <section class="hero is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-3">
+                        Contributors
+                    </h1>
+
+                    <div class="row">
+                        <div class="columns is-desktop">
+                            <div class="column">
+                                <div class="card">
+                                    <div class="card-content">
+                                        <div class="media">
+                                            <div class="media-content">
+                                                <p class="title is-5 has-text-black">Jasper Chua</p>
+                                                <p class="subtitle is-6 has-text-black">Contributions to Nozomi's First
+                                                    Business Plan</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="card">
+                                    <div class="card-content">
+                                        <div class="media">
+                                            <div class="media-content">
+                                                <p class="title is-5 has-text-black">Elvis Yong</p>
+                                                <p class="subtitle is-6 has-text-black">Contributions to Nozomi's First
+                                                    Web App</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {}
+    export default {
+        data() {
+            return {}
+        }
     }
-  }
 </script>
 
 <style>
