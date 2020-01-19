09005555a1bbf42c41831c2545e3558c0dbe5a7c
nixxholas <nixholas@icloud.com>
2020-01-19 14:38:53 +0800

    Nozomi.Base.Auth: ViewModels: Account: RetryEmailConfirmationInputModel: IsValid implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Auth/ViewModels/Account/RetryEmailConfirmationInputModel.cs
+++ Nozomi.Base.Auth/ViewModels/Account/RetryEmailConfirmationInputModel.cs
@@ -5,6 +5,12 @@ namespace Nozomi.Base.Auth.ViewModels.Account
     public class RetryEmailConfirmationInputModel
     {
         public string Email { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new RetryEmailConfirmationValidator().Validate(this);
+            return validator.IsValid;
+        }
         
         public class RetryEmailConfirmationValidator : AbstractValidator<RetryEmailConfirmationInputModel>
         {
