e90ab54ef46d9bf59dcca2467682d2d4e294ba49
nixxholas <nixholas@icloud.com>
2020-01-22 06:40:49 +0800

    Nozomi.Web2: ClientApp: src: services: NozomiAuthService: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ Nozomi.Web2/ClientApp/src/services/NozomiAuthService.js
@@ -0,0 +1,22 @@
+import store from '../store/index';
+import { oidcSettings } from '../store/config';
+import axios from 'axios';
+
+export default {
+    update(payload) {
+        if (!vm)
+            throw new Error("Invalid payload. Please try again.");
+
+        return new Promise((resolve, reject) => {
+            axios.put(oidcSettings.authority + '/Account/Update', vm, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    }
+}
