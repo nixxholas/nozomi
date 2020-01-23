634120aa03ba0f0c176b52cee9c3d2e0b8e8862e
nixxholas <nixholas@icloud.com>
2020-01-23 13:51:22 +0800

    Nozomi.Base.BCL: KeyValuePairObject: Mark as obselete
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.BCL/KeyValuePairObject.cs
+++ Nozomi.Base.BCL/KeyValuePairObject.cs
@@ -1,5 +1,15 @@
+using System;
+
 namespace Nozomi.Base.BCL
 {
+    /// <summary>
+    /// KeyValuePairObject
+    ///
+    /// A simple and easy replacement to IDictionary of string, string in Javascript
+    /// 
+    /// Let's try to deprecate this as soon as possible.
+    /// </summary>
+    [Obsolete]
     public class KeyValuePairObject
     {
         public string Key { get; set; }
