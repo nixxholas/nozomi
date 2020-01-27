9de1429fd4d451523fdc55c6e757bcbe3bf2d466
nixxholas <nixholas@icloud.com>
2020-01-28 01:12:37 +0800

    Nozomi.Web2: omit https for now
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Program.cs
+++ Nozomi.Web2/Program.cs
@@ -18,31 +18,6 @@ namespace Nozomi.Web2
                 .UseKestrel(options =>
                 {
                     options.AddServerHeader = false;
-
-                    if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")) 
-                        && Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
-                            .Equals("production", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        var hasHttpsPortConfigured = int.TryParse(Environment.GetEnvironmentVariable("HTTPS_PORT")
-                            , out var port);
-                        var certPassword = Environment.GetEnvironmentVariable("SSLCERT_PASSWORD");
-                        if (!string.IsNullOrEmpty(certPassword))
-                            certPassword = "123456"; // Deefault
-                        
-                        if (!hasHttpsPortConfigured)
-                        {
-                            port = 5001; // Default port
-
-                            Console.WriteLine("HTTPS port not configured! Self configuring to 443.");
-                        }
-                        
-                        options.Listen(IPAddress.Loopback, port, listenOptions =>
-                        {
-                            var cert = new X509Certificate2("nozomi.pfx", certPassword);
-
-                            listenOptions.UseHttps(cert);
-                        });
-                    }
                 })
                 .UseStartup<Startup>();
     }
