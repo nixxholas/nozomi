21e08bf7cb9a6982f5e16327b508551ff5b8d4ff
nixxholas <nixholas@icloud.com>
2020-01-28 23:11:52 +0800

    Nozomi.Web2: Startup: 404 backup routing
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Startup.cs
+++ Nozomi.Web2/Startup.cs
@@ -135,6 +135,22 @@ namespace Nozomi.Web2
             // ref: https://github.com/aspnet/Docs/issues/2384
             // app.UseForwardedHeaders();
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                // If there's no available file and the request doesn't contain an extension,
+                // we're probably trying to access a page. Rewrite request to use app root
+                if (context.Response.StatusCode == 404 
+                    && !Path.HasExtension(context.Request.Path.Value)
+                    && !context.Request.Path.Value.StartsWith("/api"))
+                {
+                    context.Request.Path = "/index.html";
+                    context.Response.StatusCode = 200; // Make sure we update the status code, otherwise it returns 404
+                    await next();
+                }
+            });
+
             app.UseDefaultFiles();
             // app.UseStaticFiles();
             app.UseSpaStaticFiles();
