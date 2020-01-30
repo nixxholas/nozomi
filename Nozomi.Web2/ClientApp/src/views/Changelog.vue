ace9affd5dd8c2903782614223b4f9dede0b583a
nixxholas <nixholas@icloud.com>
2020-01-30 19:09:23 +0800

    Nozomi.Web2: ClientApp: src: views: Changelog: Let everyone know that we updated some crap
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/views/Changelog.vue
+++ Nozomi.Web2/ClientApp/src/views/Changelog.vue
@@ -170,6 +170,16 @@
                                 </ul>
                             </div>
                         </div>
+                        <div class="timeline-item is-info">
+                            <div class="timeline-marker is-info"></div>
+                            <div class="timeline-content">
+                                <p class="heading">30 January 2019 - <b>beta 7</b></p>
+                                <ul>
+                                    <li><b>Auth</b> - All connections to Nozomi are now end-to-end encrypted</li>
+                                    <li><b>Web</b> - All connections to Nozomi are now end-to-end encrypted</li>
+                                </ul>
+                            </div>
+                        </div>
                         <header class="timeline-header">
                             <span class="tag is-primary">Release Candidate</span>
                         </header>
