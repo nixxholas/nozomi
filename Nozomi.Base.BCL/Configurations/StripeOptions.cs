e2d2723d1bcb976a51f88f8b03dcda6b16f7b591
nixxholas <nixholas@icloud.com>
2020-01-31 19:59:50 +0800

    Nozomi.Base.BCL: Configurations: StripeOptions: ProductId: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.BCL/Configurations/StripeOptions.cs
+++ Nozomi.Base.BCL/Configurations/StripeOptions.cs
@@ -4,5 +4,9 @@ namespace Nozomi.Base.BCL.Configurations
     {
         public string SecretKey { get; set; }
         public string PublishableKey { get; set; }
+        /// <summary>
+        /// The product id that is stashed in Stripe, an entity that stores all pricing plans
+        /// </summary>
+        public string ProductId { get; set; }
     }
 }
