3da4bef28ff4d74b0030a2808c0155f23efc1d9a
nixxholas <nixholas@icloud.com>
2020-01-31 23:36:41 +0800

    Nozomi.Web2: Startup: Remove the hardcoded middleware
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Startup.cs
+++ Nozomi.Web2/Startup.cs
@@ -134,22 +134,6 @@ namespace Nozomi.Web2
             // ref: https://github.com/aspnet/Docs/issues/2384
             app.UseForwardedHeaders();
 
-            app.Use(async (context, next) =>
-            {
-                await next();
-            
-                // If there's no available file and the request doesn't contain an extension,
-                // we're probably trying to access a page. Rewrite request to use app root
-                if ((context.Response.StatusCode == 404 
-                    || !Path.HasExtension(context.Request.Path.Value))
-                    && !context.Request.Path.Value.StartsWith("/api"))
-                {
-                    context.Request.Path = "/index.html";
-                    context.Response.StatusCode = 200; // Make sure we update the status code, otherwise it returns 404
-                    await next();
-                }
-            });
-
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
