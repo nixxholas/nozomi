386f72e6626d47aa18e28f87eb7c94f6628d0516
nixxholas <nixholas@icloud.com>
2020-01-31 20:15:55 +0800

    Nozomi.Infra.Auth: Events: Stripe: Plans API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Auth/Events/Stripe/IStripeEvent.cs
+++ Nozomi.Infra.Auth/Events/Stripe/IStripeEvent.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stripe;
+
 namespace Nozomi.Infra.Auth.Events.Stripe
 {
     public interface IStripeEvent
     {
-        
+        /// <summary>
+        /// Obtain all plans stashed for our product
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Plan>> Plans(bool activeOnly = true);
     }
 }
--- Nozomi.Infra.Auth/Events/Stripe/StripeEvent.cs
+++ Nozomi.Infra.Auth/Events/Stripe/StripeEvent.cs
@@ -1,15 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Nozomi.Base.BCL.Configurations;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.Auth.Data;
 using Nozomi.Repo.BCL.Repository;
+using Stripe;
 
 namespace Nozomi.Infra.Auth.Events.Stripe
 {
     public class StripeEvent : BaseEvent<StripeEvent, AuthDbContext>, IStripeEvent
     {
-        public StripeEvent(ILogger<StripeEvent> logger, IUnitOfWork<AuthDbContext> unitOfWork) 
+        private readonly Product _stripeProduct;
+        
+        public StripeEvent(ILogger<StripeEvent> logger, IUnitOfWork<AuthDbContext> unitOfWork,
+            IOptions<StripeOptions> stripeConfiguration) 
             : base(logger, unitOfWork)
         {
+            // apiKey = Secret Key
+            StripeConfiguration.ApiKey = stripeConfiguration.Value.SecretKey;
+
+            var productService = new ProductService();
+            _stripeProduct = productService.Get(stripeConfiguration.Value.ProductId);
+        }
+
+        public async Task<IEnumerable<Plan>> Plans(bool activeOnly = true)
+        {
+            if (_stripeProduct != null)
+            {
+                var planService = new PlanService();
+                var planListOptions = new PlanListOptions
+                {
+                    Active = activeOnly,
+                    Product = _stripeProduct.Id
+                };
+                
+                var plans = await planService.ListAsync(planListOptions);
+                if (plans.StripeResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    _logger.LogWarning($"{_eventName} plans: Unable to load, plans cannot be " +
+                                       $"retrieved from Stripe.");
+                    throw new NullReferenceException($"{_eventName} plans: Unable to load, plans cannot be " +
+                                                     $"retrieved from Stripe.");
+                }
+
+                return plans.Data;
+            }
+
+            throw new NullReferenceException($"{_eventName} plans: Unable to load, Product is not configured.");
         }
     }
 }
