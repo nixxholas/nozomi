72400561a793843afdece3965e3133dd197f3fcc
Qi Xiang <qixyqix@outlook.com>
2020-02-01 12:54:24 +0800

    Nozomi.Base.Auth: Global: Added stripeSubscriptionId to NozomiJwtClaimTypes

--- Nozomi.Base.Auth/Global/NozomiJwtClaimTypes.cs
+++ Nozomi.Base.Auth/Global/NozomiJwtClaimTypes.cs
@@ -11,5 +11,10 @@ namespace Nozomi.Base.Auth.Global
         /// A set of array for the cards the user has.
         /// </summary>
         public const string StripeCustomerCardId = "stripe_cust_card_id";
+        /// <summary>
+        /// The Id of the user's current subscription
+        /// </summary>
+        public const string StripeSubscriptionId = "stripe_sub_id";
+        
     }
 }
