f1c88514a52679fb9e91eee28f39ecca54463508
nixxholas <nixholas@icloud.com>
2020-02-03 00:37:46 +0800

    Nozomi.Web2: ClientApp: src: services: auth: PaymentService: addPaymentMethod intro
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/auth/PaymentService.js
+++ Nozomi.Web2/ClientApp/src/services/auth/PaymentService.js
@@ -3,6 +3,20 @@ import { oidcSettings } from '@/store/config';
 import axios from 'axios';
 
 export default {
+    addPaymentMethod(pmToken) {
+        return new Promise((resolve, reject) => {
+            axios.post(oidcSettings.authority + '/Payment/AddPaymentMethod', pmToken, {
+                headers: {
+                    Authorization: "Bearer " + store.state.oidcStore.access_token
+                }
+            }).then(function (response) {
+                resolve(response);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+    
     getStripePubKey() {
         return new Promise((resolve, reject) => {
             axios.get(oidcSettings.authority + '/Payment/GetStripePubKey', {
@@ -16,7 +30,7 @@ export default {
             });
         });
     },
-
+    
     getStripeCustId() {
         return new Promise((resolve, reject) => {
             axios.get(oidcSettings.authority + '/Payment/GetStripeCustId', {
