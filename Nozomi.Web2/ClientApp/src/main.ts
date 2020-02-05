397cd4d4cb224bc02c317aefcc29cb9a8555f4fb
nixxholas <nixholas@icloud.com>
2020-02-05 17:52:41 +0800

    Nozomi.Web2: ClientApp: src: main: capitalize filter usage
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/main.ts
+++ Nozomi.Web2/ClientApp/src/main.ts
@@ -17,6 +17,7 @@ import App from './App.vue';
 // @ts-ignore
 import router from '@/routing/router';
 import store from '@/store/index';
+import capitalizeFilter from '@/filters/capitalize.filter';
 import dateFilter from '@/filters/date.filter';
 
 // FontAwesome!!!
@@ -28,15 +29,15 @@ Vue.component('vue-fontawesome', FontAwesomeIcon);
 
 // Font awesome solid icon configurations
 import { faAngleLeft, faAngleRight, faArrowUp, faAtlas, faBug, faCaretDown, 
-  faCheck, faCheckCircle, faChevronDown, faCog,
+  faCaretUp, faCheck, faCheckCircle, faChevronDown, faCog,
   faCoins, faColumns, faCreditCard, faDumpster, faEnvelope, faExclamationCircle, 
   faExclamationTriangle,
   faEye, faFingerprint, faFrown, faHome, faInfo, faInfoCircle, faKey, faLandmark, 
   faMoneyBill, faMoneyBillWave, faSadCry, faSpinner,
   faSearch, faSignOutAlt,faTicketAlt, faTimes, faTrash, faUniversity, faUser } 
   from '@fortawesome/free-solid-svg-icons';
-library.add(faAngleLeft, faAngleRight, faArrowUp, faAtlas, faBug, faCaretDown, 
-    faCheck, faCheckCircle, faChevronDown, faCog,
+library.add(faAngleLeft, faAngleRight, faArrowUp, faAtlas, faBug, faCaretDown,
+    faCaretUp, faCheck, faCheckCircle, faChevronDown, faCog,
     faCoins, faColumns, faCreditCard, faDumpster, faEnvelope, faExclamationCircle, 
     faExclamationTriangle,
     faEye, faFingerprint, faFrown, faHome, faInfo, faInfoCircle, faKey, faLandmark, 
@@ -64,6 +65,7 @@ Vue.prototype.$numeral = numeral;
 
 Vue.config.productionTip = false;
 
+Vue.filter('capitalize', capitalizeFilter);
 Vue.filter('date', dateFilter);
 
 new Vue({
