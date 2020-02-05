8346ea5b71d753a0338fe83d33bc37755a98f45f
nixxholas <nixholas@icloud.com>
2020-02-05 22:33:29 +0800

    Nozomi.Base.BCL: Configurations: StripeOptions: DefaultPlanId Implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.BCL/Configurations/StripeOptions.cs
+++ Nozomi.Base.BCL/Configurations/StripeOptions.cs
@@ -8,5 +8,9 @@ namespace Nozomi.Base.BCL.Configurations
         /// The product id that is stashed in Stripe, an entity that stores all pricing plans
         /// </summary>
         public string ProductId { get; set; }
+        /// <summary>
+        /// The id of the default plan 
+        /// </summary>
+        public string DefaultPlanId { get; set; }
     }
 }
