63ee0d27fd67a9f5cc157cb46e52680e6a6b1706
nixxholas <nixholas@icloud.com>
2020-02-13 06:14:37 +0800

    Nozomi.Web2: Controllers: v1: RequestProperty: Get API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Controllers/v1/RequestProperty/RequestPropertyController.cs
+++ Nozomi.Web2/Controllers/v1/RequestProperty/RequestPropertyController.cs
@@ -1,18 +1,39 @@
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Nozomi.Data.ViewModels.RequestProperty;
+using Nozomi.Service.Events.Interfaces;
+using Nozomi.Service.Services.Interfaces;
 
 namespace Nozomi.Web2.Controllers.v1.RequestProperty
 {
     public class RequestPropertyController : BaseApiController<RequestPropertyController>, IRequestPropertyController
     {
-        public RequestPropertyController(ILogger<RequestPropertyController> logger) : base(logger)
+        private readonly IRequestPropertyEvent _requestPropertyEvent;
+        private readonly IRequestPropertyService _requestPropertyService;
+        
+        public RequestPropertyController(ILogger<RequestPropertyController> logger,
+            IRequestPropertyEvent requestPropertyEvent, IRequestPropertyService requestPropertyService) : base(logger)
         {
+            _requestPropertyEvent = requestPropertyEvent;
+            _requestPropertyService = requestPropertyService;
         }
 
+        [Authorize]
+        [HttpGet("{guid}")]
         public IActionResult Get(string guid)
         {
-            throw new System.NotImplementedException();
+            var sub = ((ClaimsIdentity) User.Identity)
+                .Claims.SingleOrDefault(c => c.Type.Equals(JwtClaimTypes.Subject))?.Value;
+
+            // Since we get the sub,
+            if (!string.IsNullOrWhiteSpace(sub))
+                return Ok(_requestPropertyEvent.GetByGuid(guid, sub));
+
+            return BadRequest("Please re-authenticate again");
         }
 
         public IActionResult GetAllByRequest(string requestGuid)
