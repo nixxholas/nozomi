3970355d18c9e9587e8305747f13395af05f5df1
nixxholas <nixholas@icloud.com>
2020-02-12 23:00:36 +0800

    Nozomi.Infra.Service: Services: RequestPropertyService: Update API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Services/RequestPropertyService.cs
+++ Nozomi.Infra.Service/Services/RequestPropertyService.cs
@@ -16,19 +16,23 @@ namespace Nozomi.Service.Services
     public class RequestPropertyService : BaseService<RequestPropertyService, NozomiDbContext>, IRequestPropertyService
     {
         private readonly IRequestEvent _requestEvent;
+        private readonly IRequestPropertyEvent _requestPropertyEvent;
         
         public RequestPropertyService(ILogger<RequestPropertyService> logger, IUnitOfWork<NozomiDbContext> unitOfWork,
-            IRequestEvent requestEvent) 
+            IRequestEvent requestEvent, IRequestPropertyEvent requestPropertyEvent) 
             : base(logger, unitOfWork)
         {
             _requestEvent = requestEvent;
+            _requestPropertyEvent = requestPropertyEvent;
         }
 
         public RequestPropertyService(IHttpContextAccessor contextAccessor, ILogger<RequestPropertyService> logger, 
-            IUnitOfWork<NozomiDbContext> unitOfWork, IRequestEvent requestEvent) 
+            IUnitOfWork<NozomiDbContext> unitOfWork, IRequestEvent requestEvent, 
+            IRequestPropertyEvent requestPropertyEvent) 
             : base(contextAccessor, logger, unitOfWork)
         {
             _requestEvent = requestEvent;
+            _requestPropertyEvent = requestPropertyEvent;
         }
 
         public Task Create(CreateRequestPropertyInputModel inputModel, string userId)
@@ -67,7 +71,30 @@ namespace Nozomi.Service.Services
 
         public Task Update(UpdateRequestPropertyInputModel inputModel, string userId)
         {
-            throw new System.NotImplementedException();
+            if (inputModel.IsValid())
+            {
+                var requestProperty = _requestPropertyEvent.GetByGuid(inputModel.Guid, userId, 
+                    true, true);
+                
+                if (requestProperty != null)
+                {
+                    // Update the Request property
+                    requestProperty.RequestPropertyType = inputModel.Type;
+                    requestProperty.Key = inputModel.Key;
+                    requestProperty.Value = inputModel.Value;
+                    // Push
+                    _unitOfWork.GetRepository<RequestProperty>().Update(requestProperty);
+                    // Commit
+                    _unitOfWork.Commit(userId);
+                    // Complete
+                    _logger.LogInformation($"{_serviceName} Update: user {userId} has successfully " +
+                                           $"updated the request property {inputModel.Guid}.");
+                    return Task.CompletedTask;
+                }
+                
+            }
+
+            throw new ArgumentNullException($"{_serviceName} Update: Invalid input model.");
         }
 
         public Task Delete(string guid, string userId)
