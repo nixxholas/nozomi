981557de894e6226c3bd5c4967c7d5bdfd639448
nixxholas <nixholas@icloud.com>
2020-02-13 18:06:19 +0800

    Nozomi.Web2: ClientApp: src: services: RequestPropertyService: Get API
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/RequestPropertyService.ts
+++ Nozomi.Web2/ClientApp/src/services/RequestPropertyService.ts
@@ -2,9 +2,19 @@ import axios from "axios";
 
 const baseUrl = 'api/RequestProperty/';
 export default {
-    getAllByRequest(guid: string) {
+    get(guid: string) {
         return new Promise((resolve, reject) => {
-            axios.get(baseUrl  + 'GetAllByRequest/' + guid).then(function (response) {
+            axios.get(baseUrl  + 'Get/' + guid).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    },
+    
+    getAllByRequest(requestGuid: string) {
+        return new Promise((resolve, reject) => {
+            axios.get(baseUrl  + 'GetAllByRequest/' + requestGuid).then(function (response) {
                 resolve(response.data);
             }).catch(function (error) {
                 reject(error);
