814d5fa2c6555af727327947c214e027896cb91b
nixxholas <nixholas@icloud.com>
2020-02-15 20:16:58 +0800

    Nozomi.Base.Domain: ViewModels: WebsocketCommandProperty: WebsocketCommandPropertyViewModel: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/WebsocketCommandProperty/WebsocketCommandPropertyViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/WebsocketCommandProperty/WebsocketCommandPropertyViewModel.cs
@@ -1,9 +1,30 @@
+using System;
 using Nozomi.Data.Models.Web.Websocket;
 
 namespace Nozomi.Data.ViewModels.WebsocketCommandProperty
 {
     public class WebsocketCommandPropertyViewModel : CreateWebsocketCommandPropertyInputModel
     {
+        public WebsocketCommandPropertyViewModel() {}
+
+        public WebsocketCommandPropertyViewModel(Guid guid, CommandPropertyType type, string key, string value)
+        {
+            Guid = guid;
+            Type = type;
+            Key = key;
+            Value = value;
+        }
+
+        public WebsocketCommandPropertyViewModel(long id, CommandPropertyType type, string key, string value)
+        {
+            Id = id;
+            Type = type;
+            Key = key;
+            Value = value;
+        }
+        
         public long Id { get; set; }
+        
+        public Guid Guid { get; set; }
     }
 }
