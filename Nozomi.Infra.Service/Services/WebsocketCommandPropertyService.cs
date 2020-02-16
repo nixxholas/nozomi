9d450b1b1cca70d617a85d73db55d316d5735415
nixxholas <nixholas@icloud.com>
2020-02-16 14:48:35 +0800

    Nozomi.Infra.Service: Services: WebsocketCommandPropertyService: Create API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Service/Services/WebsocketCommandPropertyService.cs
+++ Nozomi.Infra.Service/Services/WebsocketCommandPropertyService.cs
@@ -1,9 +1,12 @@
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Nozomi.Data.Models.Web.Websocket;
 using Nozomi.Data.ViewModels.WebsocketCommandProperty;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.BCL.Repository;
 using Nozomi.Repo.Data;
+using Nozomi.Service.Events.Interfaces;
 using Nozomi.Service.Services.Interfaces;
 
 namespace Nozomi.Service.Services
@@ -11,21 +14,51 @@ namespace Nozomi.Service.Services
     public class WebsocketCommandPropertyService : BaseService<WebsocketCommandPropertyService, NozomiDbContext>,
         IWebsocketCommandPropertyService
     {
+        private readonly IWebsocketCommandEvent _websocketCommandEvent;
+        private readonly IWebsocketCommandPropertyEvent _websocketCommandPropertyEvent;
+
         public WebsocketCommandPropertyService(ILogger<WebsocketCommandPropertyService> logger, 
-            IUnitOfWork<NozomiDbContext> unitOfWork) 
+            IUnitOfWork<NozomiDbContext> unitOfWork, IWebsocketCommandEvent websocketCommandEvent, 
+            IWebsocketCommandPropertyEvent websocketCommandPropertyEvent) 
             : base(logger, unitOfWork)
         {
+            _websocketCommandEvent = websocketCommandEvent;
+            _websocketCommandPropertyEvent = websocketCommandPropertyEvent;
         }
 
         public WebsocketCommandPropertyService(IHttpContextAccessor contextAccessor, 
-            ILogger<WebsocketCommandPropertyService> logger, IUnitOfWork<NozomiDbContext> unitOfWork) 
+            ILogger<WebsocketCommandPropertyService> logger, IUnitOfWork<NozomiDbContext> unitOfWork, 
+            IWebsocketCommandEvent websocketCommandEvent, IWebsocketCommandPropertyEvent websocketCommandPropertyEvent) 
             : base(contextAccessor, logger, unitOfWork)
         {
+            _websocketCommandEvent = websocketCommandEvent;
+            _websocketCommandPropertyEvent = websocketCommandPropertyEvent;
         }
 
         public void Create(CreateWebsocketCommandPropertyInputModel vm, string userId)
         {
-            throw new System.NotImplementedException();
+            // If it is a solo create
+            if (vm.IsValidDependant())
+            {
+                var property = new WebsocketCommandProperty
+                {
+                    WebsocketCommandId = vm.CommandId,
+                    CommandPropertyType = vm.Type,
+                    Key = vm.Key,
+                    Value = vm.Value
+                };
+                
+                // Populate the property through the GUID method
+                if (Guid.TryParse(vm.CommandGuid, out var parsedGuid))
+                    property.WebsocketCommandId = _websocketCommandEvent.Get(parsedGuid).Id;
+                
+                _unitOfWork.GetRepository<WebsocketCommandProperty>().Add(property); // Add
+                _unitOfWork.Commit(userId); // Commit
+                return; // Exit
+            }
+            
+            _logger.LogInformation($"{_serviceName} Create: Invalid view model.");
+            throw new InvalidOperationException("Invalid payload!");
         }
 
         public void Update(UpdateWebsocketCommandPropertyInputModel vm, string userId)
