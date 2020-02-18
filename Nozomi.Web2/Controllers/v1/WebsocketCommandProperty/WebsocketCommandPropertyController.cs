72306571e952b71dd1385acb752dbc106347332e
nixxholas <nixholas@icloud.com>
2020-02-18 23:53:06 +0800

    Nozomi.Web2: Controllers: v1: WebsocketCommandProperty: Get API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Controllers/v1/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
+++ Nozomi.Web2/Controllers/v1/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
@@ -1,19 +1,45 @@
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Base.BCL.Helpers.Enumerator;
 using Nozomi.Data.ViewModels.WebsocketCommandProperty;
+using Nozomi.Preprocessing.Statics;
+using Nozomi.Service.Events.Interfaces;
 
 namespace Nozomi.Web2.Controllers.v1.WebsocketCommandProperty
 {
     public class WebsocketCommandPropertyController : BaseApiController<WebsocketCommandPropertyController>,
     IWebsocketCommandPropertyController
     {
-        public WebsocketCommandPropertyController(ILogger<WebsocketCommandPropertyController> logger) : base(logger)
+        private readonly IWebsocketCommandPropertyEvent _websocketCommandPropertyEvent;
+        
+        public WebsocketCommandPropertyController(ILogger<WebsocketCommandPropertyController> logger,
+            IWebsocketCommandPropertyEvent websocketCommandPropertyEvent) : base(logger)
         {
+            _websocketCommandPropertyEvent = websocketCommandPropertyEvent;
         }
         
         public IActionResult Get(string guid)
         {
-            throw new System.NotImplementedException();
+            var sub = ((ClaimsIdentity) User.Identity)
+                .Claims.SingleOrDefault(c => c.Type.Equals(JwtClaimTypes.Subject))?.Value;
+
+            // Since we get the sub,
+            if (!string.IsNullOrWhiteSpace(sub))
+            {
+                var roles = ((ClaimsIdentity) User.Identity)
+                    .Claims.Where(c => c.Type.Equals(JwtClaimTypes.Role));
+                
+                if (roles.Any(r => NozomiPermissions.AllStaffRoles
+                    .Any(s => s.GetDescription().Equals(r.Value))))
+                    return Ok(_websocketCommandPropertyEvent.View(guid, false));
+                    
+                return Ok(_websocketCommandPropertyEvent.View(guid, true, sub));
+            }
+
+            return BadRequest("Please re-authenticate again");
         }
 
         public IActionResult GetByCommand(string commandGuid)
