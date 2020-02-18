d08964278b0f36c3494e81974239b98cb31e6f56
nixxholas <nixholas@icloud.com>
2020-02-18 23:57:21 +0800

    Nozomi.Web2: Controllers: v1: WebsocketCommandProperty: Update API: Initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Web2/Controllers/v1/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
+++ Nozomi.Web2/Controllers/v1/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
@@ -91,9 +91,22 @@ namespace Nozomi.Web2.Controllers.v1.WebsocketCommandProperty
             return BadRequest("Please re-authenticate again");
         }
 
+        [Authorize]
+        [HttpPut]
         public IActionResult Update(UpdateWebsocketCommandPropertyInputModel vm)
         {
-            throw new System.NotImplementedException();
+            var sub = ((ClaimsIdentity) User.Identity)
+                .Claims.SingleOrDefault(c => c.Type.Equals(JwtClaimTypes.Subject))?.Value;
+
+            // Since we get the sub,
+            if (!string.IsNullOrWhiteSpace(sub))
+            {
+                _websocketCommandPropertyService.Update(vm, sub);
+                
+                return Ok("Websocket Command property successfully updated!");
+            }
+
+            return BadRequest("Please re-authenticate again");
         }
 
         public IActionResult Delete(string websocketCommandPropertyGuid)
