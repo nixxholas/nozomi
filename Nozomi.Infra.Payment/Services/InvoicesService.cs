ad555b81ce32fa5a2fd15cd96603efbea1adfa0a
Qi Xiang <Qixyqix@outlook.com>
2020-02-18 15:52:31 +0800

    Nozomi.Infra.Payment: Services: Added files for invoice & quota services

---
+++ Nozomi.Infra.Payment/Services/InvoicesService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Nozomi.Base.Auth.Global;
+using Nozomi.Base.BCL.Configurations;
+using Nozomi.Infra.Payment.Services.Interfaces;
+using Nozomi.Preprocessing.Abstracts;
+using Nozomi.Repo.Auth.Data;
+using Nozomi.Repo.BCL.Repository;
+using Stripe;
+
+namespace Nozomi.Infra.Payment.Services
+{
+    class InvoicesService : IInvoicesService
+    {
+        public Task InvoiceCreated(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InvoiceFinalized(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InvoicePaid(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InvoicePaymentFailed(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InvoiceUpcoming(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Nozomi.Infra.Payment/Services/QuotaService.cs
@@ -0,0 +1,26 @@
+﻿using Nozomi.Infra.Payment.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nozomi.Infra.Payment.Services
+{
+    class QuotaService : IQuotaService
+    {
+        public Task DowngradeQuota()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ResetQuota()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpgradeQuota()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
