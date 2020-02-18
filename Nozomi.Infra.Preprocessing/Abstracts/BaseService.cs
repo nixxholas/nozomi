27b86a50b6e716815e066aaf512413c1c71953e6
nixxholas <nixholas@icloud.com>
2020-02-18 22:55:16 +0800

    Nozomi.Infra.Preprocessing: Abstracts: BaseService: Have a generic containing 0 DB dependency
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Preprocessing/Abstracts/BaseService.cs
+++ Nozomi.Infra.Preprocessing/Abstracts/BaseService.cs
@@ -5,6 +5,33 @@ using Nozomi.Repo.BCL.Repository;
 
 namespace Nozomi.Preprocessing.Abstracts
 {
+    public abstract class BaseService<T>
+        where T : class
+    {
+        // https://stackoverflow.com/questions/38571032/how-to-get-httpcontext-current-in-asp-net-core
+        private readonly IHttpContextAccessor _contextAccessor;
+        protected readonly ILogger<T> _logger;
+        protected readonly string _serviceName;
+
+        public BaseService(ILogger<T> logger)
+        {
+            _logger = logger;
+            _serviceName = typeof(T).FullName;
+        }
+
+        public BaseService(IHttpContextAccessor contextAccessor, ILogger<T> logger)
+        {
+            _contextAccessor = contextAccessor;
+            _logger = logger;
+            _serviceName = typeof(T).FullName;
+        }
+
+        public IHttpContextAccessor CurrentAccessor()
+        {
+            return _contextAccessor;
+        }
+    }
+    
     public abstract class BaseService<T, TContext>
         where T : class
         where TContext : IDbContext
