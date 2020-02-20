79c1d88f1aaa98df7a7e021c09c9951be502164b
Bing Huang <bing2609@gmail.com>
2020-02-20 21:09:42 +0800

    Nozomi.Base.Domain: ViewModels: Trello: Added Trello POCO

---
+++ Nozomi.Base.Domain/ViewModels/Trello/CheckItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Nozomi.Data.ViewModels.Trello
+{
+    public class CheckItemViewModel
+    {
+        public string ID { get; set; }
+
+        public string State { get; set; }
+
+        public string Name { get; set; }
+    }
+}
