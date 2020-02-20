2fb67916d51a11d1c13761f4022f8b375f27f919
Bing Huang <bing2609@gmail.com>
2020-02-20 21:09:42 +0800

    Nozomi.Base.Domain: ViewModels: Trello: Added Trello POCO

---
+++ Nozomi.Base.Domain/ViewModels/Trello/BoardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Nozomi.Data.ViewModels.Trello
+{
+    public class BoardViewModel
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string DateLastActivity { get; set; }
+
+        public IEnumerable<ListViewModel> Lists { get; set; }
+    }
+}
