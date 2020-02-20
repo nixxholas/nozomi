22312e4eb073d04b43bb0cf7d1fcb2c4cd6df713
nixxholas <nixholas@icloud.com>
2020-02-21 00:35:44 +0800

    Nozomi.Base.Domain: ViewModels: WebsocketCommand: UpdateWebsocketCommandInputModel: Proper ID validation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/WebsocketCommand/UpdateWebsocketCommandInputModel.cs
+++ Nozomi.Base.Domain/ViewModels/WebsocketCommand/UpdateWebsocketCommandInputModel.cs
@@ -16,7 +16,13 @@ namespace Nozomi.Data.ViewModels.WebsocketCommand
                 RuleFor(c => c.Type).IsInEnum();
                 RuleFor(c => c.Name).NotNull().NotEmpty();
                 RuleFor(c => c.Delay).GreaterThanOrEqualTo(-1);
-                RuleFor(c => c.RequestGuid).NotEmpty().NotNull();
+
+                RuleFor(c => c.Id).GreaterThan(0)
+                    .Unless(e => 
+                        System.Guid.TryParse(e.Guid, out var parsedGuid));
+                RuleFor(c => c.Guid).Must(guid => 
+                    System.Guid.TryParse(guid, out var parsedGuid))
+                    .Unless(c => c.Id > 0);
             }
         }
 
