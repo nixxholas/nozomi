2ed5c0e915e4dadba4ca9c02e0302a0b8eb2451f
Qi Xiang <Qixyqix@outlook.com>
2020-02-21 11:23:13 +0800

    Nozomi.Infra.Payment: Services: Added implementation for InvoicePaid & InvoicePaymentFailed

--- Nozomi.Infra.Payment/Services/InvoicesService.cs
+++ Nozomi.Infra.Payment/Services/InvoicesService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Nozomi.Base.Auth.Global;
 using Nozomi.Base.BCL.Configurations;
 using Nozomi.Infra.Auth.Events.Stripe;
@@ -13,10 +14,13 @@ using Stripe;
 
 namespace Nozomi.Infra.Payment.Services
 {
-    class InvoicesService : IInvoicesService
+    class InvoicesService : BaseService<InvoicesService>, IInvoicesService
     {
-        private readonly StripeEvent _stripeEvent;
-        public InvoicesService() {
+        private readonly IStripeEvent _stripeEvent;
+        private readonly IQuotaService _quotaService;
+        public InvoicesService(ILogger<InvoicesService> logger, IStripeEvent stripeEvent, IQuotaService quotaService) : base(logger) {
+            _stripeEvent = stripeEvent;
+            _quotaService = quotaService;
         }
 
         public async Task InvoiceCreated(Invoice invoice)
@@ -31,21 +35,52 @@ namespace Nozomi.Infra.Payment.Services
 
         public async Task InvoicePaid(Invoice invoice)
         {
-            if (invoice.Paid) {
-                //Call quota service to update user's quota
-            }
+            var methodName = "InvoicePaid";
+            PerformInvoicePrecheck(invoice, methodName);
+
+            if (!invoice.Paid)
+                throw new InvalidOperationException($"{_serviceName} {methodName}: Invoice is not paid.");
+
+
+            var user = await _stripeEvent.GetUserByCustomerId(invoice.CustomerId);
+
+            if(user == null)
+                throw new NullReferenceException($"{_serviceName} {methodName}: Unable to find user tied to customer id.");
+
+            await _quotaService.ResetQuota();
 
             return;
         }
 
         public async Task InvoicePaymentFailed(Invoice invoice)
         {
-            throw new NotImplementedException();
+            var methodName = "InvoicePaymentFailed";
+            PerformInvoicePrecheck(invoice, methodName);
+
+            if (invoice.Paid)
+                throw new InvalidOperationException($"{_serviceName} {methodName}: Invoice is paid.");
+
+
+            var user = await _stripeEvent.GetUserByCustomerId(invoice.CustomerId);
+
+            if (user == null)
+                throw new NullReferenceException($"{_serviceName} {methodName}: Unable to find user tied to customer id.");
+
+            await _quotaService.DowngradeQuota();
+
+            return;
         }
 
         public async Task InvoiceUpcoming(Invoice invoice)
         {
             throw new NotImplementedException();
+        }
+
+        private void PerformInvoicePrecheck(Invoice invoice, string methodName) {
+            if (invoice == null)
+                throw new NullReferenceException($"{_serviceName} {methodName}: Invoice is null.");
+            if (string.IsNullOrEmpty(invoice.CustomerId))
+                throw new NullReferenceException($"{_serviceName} {methodName}: Customer id is null");
         }
     }
 }
