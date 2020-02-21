78570779d07518fe823da175b9494e08c70f66db
nixxholas <nixholas@icloud.com>
2020-02-21 10:25:05 +0800

    Nozomi.Infra.Analysis.Service: HostedServices: RequestTypes: WSRequestSyncingService: Some formatting
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Analysis.Service/HostedServices/RequestTypes/WSRequestSyncingService.cs
+++ Nozomi.Infra.Analysis.Service/HostedServices/RequestTypes/WSRequestSyncingService.cs
@@ -56,7 +56,8 @@ namespace Nozomi.Infra.Analysis.Service.HostedServices.RequestTypes
                 //============================= Update Sockets to keep =============================// 
 
                 // We will need to resync the Request collection to make sure we're polling only the ones we want to poll
-                var dataEndpoints = _websocketRequestEvent.GetAllByRequestTypeUniqueToURL(RequestType.WebSocket);
+                var dataEndpoints = _websocketRequestEvent
+                    .GetAllByRequestTypeUniqueToURL(RequestType.WebSocket);
 
                 if (dataEndpoints.Count > 0)
                 {
@@ -83,7 +84,8 @@ namespace Nozomi.Infra.Analysis.Service.HostedServices.RequestTypes
                                 // Pre-request processing
                                 newSocket.OnOpen += (sender, args) =>
                                 {
-                                    var wsCommands = dataEndpoint.Value.SelectMany(de => de.WebsocketCommands).ToList();
+                                    var wsCommands = dataEndpoint.Value
+                                        .SelectMany(de => de.WebsocketCommands).ToList();
 
                                     foreach (var wsCommand in wsCommands)
                                     {
