1c1def20c6921379a8b22bb78f06605c03129423
Qi Xiang <qixyqix@outlook.com>
2020-02-22 12:21:42 +0800

    Nozomi.Infra.Auth:Services: QuotaClaims: Added interface for quotaclaims service

---
+++ Nozomi.Infra.Auth/Services/QuotaClaims/IQuotaClaimsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Nozomi.Infra.Auth.Services.QuotaClaims
+{
+    public interface IQuotaClaimsService
+    {
+        Task SetQuota(Base.Auth.Models.User user);
+
+        Task AddUsage(Base.Auth.Models.User user);
+        
+        Task RestUsage(Base.Auth.Models.User user);
+        
+    }
+}
