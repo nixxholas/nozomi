f1693bc1edec1c6a887ff5a8c42a2fd43636ea14
nixxholas <nixholas@icloud.com>
2020-02-22 17:05:22 +0800

    Nozomi.Infra.Compute: Events: ComputeValue: IsOutdated API: string-based initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Compute/Events/ComputeValueEvent.cs
+++ Nozomi.Infra.Compute/Events/ComputeValueEvent.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Nozomi.Data.Models.Web;
 using Nozomi.Infra.Compute.Events.Interfaces;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.BCL.Repository;
@@ -16,7 +19,23 @@ namespace Nozomi.Infra.Compute.Events
 
         public bool IsOutdated(string computeGuid)
         {
-            throw new NotImplementedException();
+            if (Guid.TryParse(computeGuid, out var parsedGuid))
+            {
+                return _unitOfWork.GetRepository<ComputeValue>()
+                    .GetQueryable()
+                    .AsNoTracking()
+                    // Ensure it tallies and is active
+                    .Where(cv => cv.DeletedAt == null && cv.IsEnabled && cv.Guid.Equals(parsedGuid))
+                    .Include(cv => cv.Compute)
+                    // Always check against the most recent
+                    .OrderByDescending(cv => cv.CreatedAt)
+                    .Take(1)
+                    .AsEnumerable()
+                    .Select(cv => cv.CreatedAt.AddMilliseconds(cv.Compute.Delay))
+                    .FirstOrDefault() <= DateTime.UtcNow;
+            }
+
+            return false;
         }
 
         public bool IsOutdated(Guid computeGuid)
