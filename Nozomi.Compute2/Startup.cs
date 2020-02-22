d2247f57632afb1a5573d099a251c9fb591ae173
nixxholas <nixholas@icloud.com>
2020-02-22 14:24:15 +0800

    Nozomi.Compute2 & Nozomi.Infra.Compute: Get ready for Nozomi.Repo.Compute
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Compute2/Startup.cs
+++ Nozomi.Compute2/Startup.cs
@@ -5,17 +5,114 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Nozomi.Repo.Compute.Data;
+using Nozomi.Repo.Data;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
 
 namespace Nozomi.Compute2
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            if (WebHostEnvironment.IsDevelopment())
+            {
+                // Greet the beloved dev
+                Console.WriteLine(@"Welcome to the dev environment, your machine is named: " + Environment.MachineName);
+
+                // Postgres DB Setup
+                var str = Configuration.GetConnectionString("Local:" + @Environment.MachineName);
+                var computeStr = Configuration.GetConnectionString("LocalCompute:" 
+                                                                   + @Environment.MachineName);
+
+                services
+                    .AddEntityFrameworkNpgsql()
+                    .AddDbContextPool<NozomiDbContext>(options =>
+                        {
+                            options.UseNpgsql(str);
+                            options.EnableSensitiveDataLogging();
+                            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                        });
+                
+                services
+                    .AddEntityFrameworkNpgsql()
+                    .AddDbContext<NozomiComputeDbContext>(options =>
+                        {
+                            options.UseNpgsql(computeStr);
+                            options.EnableSensitiveDataLogging();
+                            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                        },
+                        ServiceLifetime.Transient);
+            }
+            else
+            {
+                var vaultUrl = Configuration["vaultUrl"];
+                var vaultToken = Configuration["vaultToken"];
+
+                if (string.IsNullOrEmpty(vaultToken))
+                    throw new SystemException("Invalid vault token.");
+
+                var authMethod = new TokenAuthMethodInfo(vaultToken);
+                var vaultClientSettings = new VaultClientSettings(
+                    !string.IsNullOrWhiteSpace(vaultUrl) ? vaultUrl : "https://blackbox.nozomi.one:8200", 
+                    authMethod);
+                var vaultClient = new VaultClient(vaultClientSettings);
+
+                var nozomiVault = vaultClient.V1.Secrets.Cubbyhole.ReadSecretAsync("nozomi")
+                    .GetAwaiter()
+                    .GetResult().Data;
+
+                var mainDb = (string) nozomiVault["main"];
+                if (string.IsNullOrEmpty(mainDb))
+                    throw new SystemException("Invalid main database configuration");
+                // Database
+                services
+                    .AddDbContextPool<NozomiDbContext>(options =>
+                {
+                    options.UseNpgsql(mainDb
+                        , builder =>
+                        {
+                            builder.EnableRetryOnFailure();
+                        }
+                    );
+                    options.EnableSensitiveDataLogging();
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                });
+                
+                var computeDb = (string) nozomiVault["compute"];
+                if (string.IsNullOrEmpty(computeDb))
+                    throw new SystemException("Invalid main database configuration");
+                services
+                    .AddEntityFrameworkNpgsql()
+                    .AddDbContext<NozomiComputeDbContext>(options =>
+                        {
+                            options.UseNpgsql(computeDb
+                                , builder =>
+                                {
+                                    builder.EnableRetryOnFailure();
+                                }
+                            );
+                            options.EnableSensitiveDataLogging();
+                            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                        },
+                        ServiceLifetime.Transient);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
