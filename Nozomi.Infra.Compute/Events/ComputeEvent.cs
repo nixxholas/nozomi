38bbc4dd72a5c720a9b4d75d4d23ce795d937d31
nixxholas <nixholas@icloud.com>
2020-02-22 13:44:17 +0800

    Nozomi.Infra.Compute: Events: Compute: Get API: string-based initial implementation
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Compute/Events/ComputeEvent.cs
+++ Nozomi.Infra.Compute/Events/ComputeEvent.cs
@@ -40,7 +40,27 @@ namespace Nozomi.Infra.Compute.Events
         public Data.Models.Web.Compute Get(string guid, bool includeChildren = false, 
             bool ensureNotDeletedOrDisabled = true)
         {
-            throw new NotImplementedException();
+            if (!Guid.TryParse(guid, out var parsedGuid))
+            {
+                _logger.LogWarning($"{_eventName} Get (string): Invalid guid.");
+                return null;
+            }
+            
+            var query = _unitOfWork.GetRepository<Data.Models.Web.Compute>()
+                .GetQueryable()
+                .Where(c => c.Guid.Equals(parsedGuid));
+
+            if (ensureNotDeletedOrDisabled)
+                query = query.Where(c => c.DeletedAt == null && c.IsEnabled);
+
+            if (includeChildren)
+                query = query.Include(c => c.Expressions)
+                    .Include(c => c.ParentComputes)
+                    .ThenInclude(pc => pc.ParentCompute)
+                    .Include(c => c.ChildComputes)
+                    .ThenInclude(cc => cc.ChildCompute);
+
+            return query.SingleOrDefault();
         }
 
         public IEnumerable<Data.Models.Web.Compute> GetByParent(Guid parentGuid, bool includeChildren = false, 
