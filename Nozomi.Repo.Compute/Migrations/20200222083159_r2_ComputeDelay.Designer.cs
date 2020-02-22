534967f645be399806f4421b01c2bfd2f0ad1a3e
nixxholas <nixholas@icloud.com>
2020-02-22 16:33:35 +0800

    Nozomi.Repo: Migrations: r2 - ComputeDelay migration
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Repo.Compute/Migrations/20200222083159_r2_ComputeDelay.Designer.cs
+++ Nozomi.Repo.Compute/Migrations/20200222083159_r2_ComputeDelay.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nozomi.Repo.Compute.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Nozomi.Repo.Compute.Migrations
 {
     [DbContext(typeof(NozomiComputeDbContext))]
-    partial class NozomiComputeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200222083159_r2_ComputeDelay")]
+    partial class r2_ComputeDelay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
