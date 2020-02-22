229cd561b074576129da9b67ec959be75fa94721
Bing Huang <bing2609@gmail.com>
2020-02-23 00:30:53 +0800

    Nozomi.Web2: ClientApp: src: views: Bugs.vue: Removed commented codes and added TrelloListTabs component

--- Nozomi.Web2/ClientApp/src/views/Bugs.vue
+++ Nozomi.Web2/ClientApp/src/views/Bugs.vue
@@ -7,33 +7,8 @@
                         Bugs & Issues
                     </h1>
                     
-<!--                    <article class="media">-->
-<!--                        <figure class="media-left">-->
-<!--                            <b-icon size="is-large" icon="fingerprint"/>-->
-<!--                        </figure>-->
-<!--                        <div class="media-content">-->
-<!--                            <div class="content">-->
-<!--                                <p>-->
-<!--                                    <b-tag type="is-dark">Auth</b-tag><strong></strong> <small>@johnsmith</small> <small>31m</small>-->
-<!--                                    <br>-->
-<!--                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.-->
-<!--                                </p>-->
-<!--                            </div>-->
-<!--                            <nav class="level is-mobile">-->
-<!--                                <div class="level-left">-->
-<!--                                    <a class="level-item">-->
-<!--                                        <span class="icon is-small"><i class="fas fa-reply"></i></span>-->
-<!--                                    </a>-->
-<!--                                    <a class="level-item">-->
-<!--                                        <span class="icon is-small"><i class="fas fa-retweet"></i></span>-->
-<!--                                    </a>-->
-<!--                                    <a class="level-item">-->
-<!--                                        <span class="icon is-small"><i class="fas fa-heart"></i></span>-->
-<!--                                    </a>-->
-<!--                                </div>-->
-<!--                            </nav>-->
-<!--                        </div>-->
-<!--                    </article>-->
+                    <TrelloListTabs />
+                    
                 </div>
             </div>
         </section>
@@ -41,10 +16,15 @@
 </template>
 
 <script>
+    import TrelloListTabs from "@/components/trello/list-type-tab";
+    
     export default {
         name: "bugs-and-issues",
         data: function() {
             return {}
+        },
+        components: {
+            TrelloListTabs
         }
     }
 </script>
