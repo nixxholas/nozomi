d868378dbb21e64ca18d0a3a3d800f74af276b57
Bing Huang <bing2609@gmail.com>
2020-02-22 16:50:01 +0800

    Nozomi.Infra.Service: Events: Interfaces: Trello events now verify accessible boards on every request

--- Nozomi.Infra.Service/Events/TrelloEvent.cs
+++ Nozomi.Infra.Service/Events/TrelloEvent.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Nozomi.Base.BCL.Helpers.Enumerator;
+using Nozomi.Data.Enums.Trello;
 using Nozomi.Data.ViewModels.Trello;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.BCL.Repository;
@@ -33,21 +36,53 @@ namespace Nozomi.Service.Events
             _httpClient = client;
         }
 
+        public async Task<List<ListViewModel>> GetPublicList(BoardType boardType, string boardId)
+        {
+            if (string.IsNullOrWhiteSpace((boardId)))
+                throw new ArgumentNullException("Parameter 'boardId' is supposed to contain a valid string.");
+            
+            // Enum description contains boardId and "," separated
+            List<string> publicBoards = boardType.GetDescription().Split(",").ToList<string>();
+            if (!publicBoards.Any(s => boardId.Contains((s))))
+                throw new ArgumentException("Unauthorized access to board");
 
-        public async Task<List<ListViewModel>> GetListsAsync(string boardId)
+            return await GetListsAsync(boardId);
+        }
+        
+        public async Task<List<CardViewModel>> GetPublicCard(BoardType boardType, string boardId, string listId)
+        {
+            List<ListViewModel> trelloList = await GetPublicList(boardType, boardId);
+
+            if (!trelloList.Any(t => listId.Contains(t.ID)))
+                throw new ArgumentException("Unauthorized access to list");
+
+            return await GetCardsAsync(listId);
+        }
+
+        public async Task<List<CheckListViewModel>> GetPublicChecklist(BoardType boardType, string boardId,
+            string listId, string cardId)
+        {
+            List<CardViewModel> cardList = await GetPublicCard(boardType, boardId, listId);
+
+            if (!cardList.Any(c => cardId.Contains(c.ID)))
+                throw new ArgumentException("Unauthorized access to card");
+            
+            return await GetCheckListsAsync(cardId);
+        }
+
+        private async Task<List<ListViewModel>> GetListsAsync(string boardId)
         {
             if (string.IsNullOrWhiteSpace(boardId))
                 throw new ArgumentNullException("Parameter 'boardId' is supposesd to contain a valid string.");
-
+        
             HttpResponseMessage response = await _httpClient.GetAsync($"/1/boards/{boardId}/lists?key={_publicApiKey}&token={_authToken}");
             response.EnsureSuccessStatusCode();
-
+        
             string resultString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<ListViewModel>>(resultString);
         }
 
-
-        public async Task<List<CardViewModel>> GetCardsAsync(string listId)
+        private async Task<List<CardViewModel>> GetCardsAsync(string listId)
         {
             if (string.IsNullOrWhiteSpace(listId))
                 throw new ArgumentNullException("Parameter 'listId' is supposed to contain a valid string");
@@ -63,8 +98,7 @@ namespace Nozomi.Service.Events
             return JsonConvert.DeserializeObject<List<CardViewModel>>(resultString);
         }
 
-
-        public async Task<List<CheckListViewModel>> GetCheckListsAsync(string cardId)
+        private async Task<List<CheckListViewModel>> GetCheckListsAsync(string cardId)
         {
             if (string.IsNullOrWhiteSpace(cardId))
                 throw new ArgumentNullException("Parameter 'cardId' is supposed to contain a valid string");
