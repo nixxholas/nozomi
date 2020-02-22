d1e78082b723a112768517b9417b7705840b0bea
Bing Huang <bing2609@gmail.com>
2020-02-22 12:07:16 +0800

    Nozomi.Infra.Service: Events: Added Trello events

---
+++ Nozomi.Infra.Service/Events/Interfaces/ITrelloEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nozomi.Data.ViewModels.Trello;
+
+namespace Nozomi.Service.Events.Interfaces
+{
+    public interface ITrelloEvent
+    {
+        Task<List<ListViewModel>> GetListsAsync(string boardId = null);
+
+        Task<List<CardViewModel>> GetCardsAsync(string listId = null);
+
+        Task<List<CheckListViewModel>> GetCheckListsAsync(string cardId = null);
+    }
+}
