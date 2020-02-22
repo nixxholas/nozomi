eb1a45337ccd8f1a0561785e12f47b48a910163e
Qi Xiang <qixyqix@outlook.com>
2020-02-23 00:46:35 +0800

    Nozomi.Infra.Auth: Events: UserEvent: Added interface for userEvent

---
+++ Nozomi.Infra.Auth/Events/UserEvent/IUserEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stripe;
+
+namespace Nozomi.Infra.Auth.Events.UserEvent
+{
+    public interface IUserEvent
+    {
+        bool HasStripe(string userId);
+
+        Task AddPaymentMethod(string userId, string paymentMethodId);
+
+        Task RemovePaymentMethod(string userId, string paymentMethodId);
+    }
+}
