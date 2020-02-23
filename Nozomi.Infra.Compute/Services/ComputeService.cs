22f8a7ac637e672a7cd5710daa34f5712e1055da
nixxholas <nixholas@icloud.com>
2020-02-23 10:34:27 +0800

    Nozomi.Infra.Compute: Services: Compute: Modified APIs: initial implementations
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Compute/Services/ComputeService.cs
+++ Nozomi.Infra.Compute/Services/ComputeService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Nozomi.Infra.Compute.Services.Interfaces;
 using Nozomi.Preprocessing.Abstracts;
@@ -10,22 +12,50 @@ namespace Nozomi.Infra.Compute.Services
 {
     public class ComputeService : BaseService<ComputeService, NozomiComputeDbContext>, IComputeService
     {
-        public ComputeService(ILogger<ComputeService> logger, IUnitOfWork<NozomiComputeDbContext> unitOfWork) : base(logger, unitOfWork)
+        public ComputeService(ILogger<ComputeService> logger, IUnitOfWork<NozomiComputeDbContext> unitOfWork) 
+            : base(logger, unitOfWork)
         {
         }
 
-        public ComputeService(IHttpContextAccessor contextAccessor, ILogger<ComputeService> logger, IUnitOfWork<NozomiComputeDbContext> unitOfWork) : base(contextAccessor, logger, unitOfWork)
+        public ComputeService(IHttpContextAccessor contextAccessor, ILogger<ComputeService> logger, 
+            IUnitOfWork<NozomiComputeDbContext> unitOfWork) 
+            : base(contextAccessor, logger, unitOfWork)
         {
         }
 
         public void Modified(Guid guid, string userId = null)
         {
-            throw new NotImplementedException();
+            var compute = _unitOfWork.GetRepository<Data.Models.Web.Compute>()
+                .GetQueryable()
+                .AsTracking()
+                .SingleOrDefault(c => c.Guid.Equals(guid));
+            
+            if (compute == null)
+                throw new NullReferenceException($"{_serviceName} Modified (GUID): Invalid guid.");
+            
+            compute.ModifiedAt = DateTime.UtcNow;
+            
+            _unitOfWork.GetRepository<Data.Models.Web.Compute>().Update(compute);
+            _unitOfWork.Commit(userId);
         }
 
         public void Modified(string guid, string userId = null)
         {
-            throw new NotImplementedException();
+            if (!Guid.TryParse(guid, out var parsedGuid))
+                throw new ArgumentException($"{_serviceName} Modified (string): Invalid guid string.");
+            
+            var compute = _unitOfWork.GetRepository<Data.Models.Web.Compute>()
+                .GetQueryable()
+                .AsTracking()
+                .SingleOrDefault(c => c.Guid.Equals(parsedGuid));
+            
+            if (compute == null)
+                throw new NullReferenceException($"{_serviceName} Modified (string): Invalid guid.");
+            
+            compute.ModifiedAt = DateTime.UtcNow;
+            
+            _unitOfWork.GetRepository<Data.Models.Web.Compute>().Update(compute);
+            _unitOfWork.Commit(userId);
         }
     }
 }
