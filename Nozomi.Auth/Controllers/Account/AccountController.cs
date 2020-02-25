fa47af8e98384404788e3e5c149436a01ec2342c
Bing Huang <bing2609@gmail.com>
2020-02-25 21:39:37 +0800

    Nozomi.Auth: Controllers: Account: Added default error message when empty registration form is submitted

--- Nozomi.Auth/Controllers/Account/AccountController.cs
+++ Nozomi.Auth/Controllers/Account/AccountController.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -200,6 +200,11 @@ namespace Nozomi.Auth.Controllers.Account
                             ModelState.AddModelError(string.Empty, resultError.Description);    
                         }
                     }
+                }
+                else
+                {
+                    // Default error state when empty form is being submitted
+                    ModelState.AddModelError(string.Empty, AccountOptions.InvalidCredentialsErrorMessage);
                 }
             }
             else if (!string.IsNullOrEmpty(button) && button.Equals("cancel") && !string.IsNullOrEmpty(model.ReturnUrl))
