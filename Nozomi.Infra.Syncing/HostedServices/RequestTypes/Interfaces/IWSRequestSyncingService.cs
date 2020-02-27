6a3a334c34507f438d2952dfc2bb10731ca44fba
nixxholas <nixholas@icloud.com>
2020-02-28 06:21:50 +0800

    Nozomi.Infra.Syncing: HostedServices: RequestTypes: Setup for System.Text.Json usage
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- Nozomi.Infra.Syncing/HostedServices/RequestTypes/Interfaces/IWSRequestSyncingService.cs
+++ Nozomi.Infra.Syncing/HostedServices/RequestTypes/Interfaces/IWSRequestSyncingService.cs
@@ -7,7 +7,7 @@ namespace Nozomi.Infra.Syncing.HostedServices.RequestTypes.Interfaces
 {
     public interface IWSRequestSyncingService
     {
-        bool Process(ICollection<Request> cpr, string payload);
+        Task<bool> Process(ICollection<Request> cpr, string payload);
 
         bool Update(JToken token, ResponseType resType, IEnumerable<Component> requestComponents);
     }
