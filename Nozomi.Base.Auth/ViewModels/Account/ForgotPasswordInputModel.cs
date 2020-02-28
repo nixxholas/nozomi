18362d638b413b46389f35df81cb73ce5c46a9b9
Bing Huang <bing2609@gmail.com>
2020-02-28 16:11:21 +0800

    Nozomi.Base.Auth: ViewModels: Account: Added custom validation for ForgotPasswordInputValidator

--- Nozomi.Base.Auth/ViewModels/Account/ForgotPasswordInputModel.cs
+++ Nozomi.Base.Auth/ViewModels/Account/ForgotPasswordInputModel.cs
@@ -6,5 +6,28 @@ namespace Nozomi.Base.Auth.ViewModels.Account
     {
         [Required] [EmailAddress] public string Email { get; set; }
         public string ReturnUrl { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new ForgotPasswordInputValidator().Validate(this);
+            return validator.IsValid;
+        }
+
+        public class ForgotPasswordInputValidator : AbstractValidator<ForgotPasswordInputModel>
+        {
+            public ForgotPasswordInputValidator()
+            {
+                RuleFor(e => e.Email).NotEmpty().NotNull().EmailAddress();
+                RuleFor(e => e.ReturnUrl).NotEmpty().NotNull()
+                    .Custom((url, context) =>
+                    {
+                        if (!string.IsNullOrEmpty(url) && !Uri.TryCreate(url, UriKind.Absolute, out var uriResult) 
+                                                       && uriResult.Scheme == Uri.UriSchemeHttp)
+                        {
+                            context.AddFailure("Invalid URL.");
+                        }
+                    });
+            }
+        }
     }
 }
