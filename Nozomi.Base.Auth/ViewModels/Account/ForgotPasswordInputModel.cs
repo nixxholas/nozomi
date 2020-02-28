2e2fcdf20705c0d4ca93f26127f41160c9a813c7
Bing Huang <bing2609@gmail.com>
2020-02-28 16:09:08 +0800

    Nozomi.Base.Auth: ViewModels: Account: Added ReturnUrl in ForgotPasswordInputModel

--- Nozomi.Base.Auth/ViewModels/Account/ForgotPasswordInputModel.cs
+++ Nozomi.Base.Auth/ViewModels/Account/ForgotPasswordInputModel.cs
@@ -5,5 +5,6 @@ namespace Nozomi.Base.Auth.ViewModels.Account
     public class ForgotPasswordInputModel
     {
         [Required] [EmailAddress] public string Email { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
