91556fdb9888472142c4b9b34ec7179e0db0b2f6
Nicholas Chen <nixholas@icloud.com>
2020-02-29 12:39:03 +0800

    Nozomi.Compute2: Let's make seeding work and implement Web middleware support
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Compute2/Program.cs
+++ Nozomi.Compute2/Program.cs
@@ -1,5 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
@@ -10,11 +18,12 @@ namespace Nozomi.Compute2
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .UseSerilog((context, configuration) =>
                 {
                     configuration
@@ -27,6 +36,32 @@ namespace Nozomi.Compute2
                             "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
                             theme: AnsiConsoleTheme.Literate);
                 })
-                .UseStartup<Startup>();
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .ConfigureKestrel((context, options) => { options.AllowSynchronousIO = true; })
+                        .UseStartup<Startup>();
+                });
+        
+        static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                 WebHost.CreateDefaultBuilder(args)
+                     .UseContentRoot(Directory.GetCurrentDirectory())
+                     .UseSerilog((context, configuration) =>
+                     {
+                         configuration
+                             .MinimumLevel.Debug()
+                             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                             .MinimumLevel.Override("System", LogEventLevel.Warning)
+                             .Enrich.FromLogContext()
+                             .WriteTo.Console(
+                                 outputTemplate:
+                                 "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                                 theme: AnsiConsoleTheme.Literate);
+                     })
+                     .UseKestrel(options =>
+                     {
+                         options.AddServerHeader = false;
+                     })
+                     .UseStartup<Startup>();
     }
 }
--- Nozomi.Compute2/Startup.cs
+++ Nozomi.Compute2/Startup.cs
@@ -149,8 +149,8 @@ namespace Nozomi.Compute2
             services.AddTransient<IComputeExpressionService, ComputeExpressionService>();
             
             // The actual hosted services
-            services.AddHostedService<ComputeExpressionHostedService>();
             services.AddHostedService<ComputeHostedService>();
+            services.AddHostedService<ComputeExpressionHostedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -160,6 +160,8 @@ namespace Nozomi.Compute2
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseAutoDbMigration(WebHostEnvironment);
 
             app.UseRouting();
 
