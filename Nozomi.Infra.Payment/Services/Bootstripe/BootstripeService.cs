76661c30379071c3ffc8bfad3ad9c83a66c80ae9
Qi Xiang <qixyqix@outlook.com>
2020-03-01 01:07:25 +0800

    Nozomi.Infra.Payment: Services: Bootstripe: Change Plan implementation

--- Nozomi.Infra.Payment/Services/Bootstripe/BootstripeService.cs
+++ Nozomi.Infra.Payment/Services/Bootstripe/BootstripeService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -232,9 +233,66 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
             _quotaClaimsService.SetQuota(user.Id, quotaValue);
         }
 
-        public Task ChangePlan(string planId, User user)
+        public async Task ChangePlan(string planId, User user)
         {
-            throw new System.NotImplementedException();
+            const string methodName = "ChangePlan";
+            PerformUserPrecheck(user, methodName);
+
+            var plan = await _planService.GetAsync(planId);
+            
+            if(plan == null)
+                throw new StripeException($"{_serviceName} {methodName}: An error occured while retrieving plan by id {planId}");
+
+            var activeSubscriptionId = _userEvent.GetUserActiveSubscriptionId(user.Id);
+            var stripeCustomerId = _userEvent.GetStripeCustomerId(user.Id);
+            
+            if(activeSubscriptionId == null)
+                throw new InvalidOperationException($"{_serviceName} {methodName}: Active subscription Id does not exist");
+
+            var subscription = await _subscriptionService.GetAsync(planId);
+            
+            if(subscription == null)
+                throw new StripeException($"{_serviceName} {methodName}: Could not retrieve subscription by id {activeSubscriptionId}");
+            
+            if(!subscription.Status.ToLower().Equals("active"))
+                throw new InvalidOperationException($"{_serviceName} {methodName}: Subscription id {activeSubscriptionId} is not active");
+
+            if (!subscription.CustomerId.Equals(stripeCustomerId))
+                throw new InvalidOperationException($"{_serviceName} {methodName}: Subscription {activeSubscriptionId} does not belong to customer {stripeCustomerId}");
+
+            if (!subscription.Plan.Id.Equals(planId))
+                throw new InvalidOperationException($"{_serviceName} {methodName}: No need to update subscription plan");
+
+            var subscriptionChangeOptions = new SubscriptionUpdateOptions {
+                Items = new List<SubscriptionItemOptions> {
+                    new SubscriptionItemOptions
+                    {
+                        Plan = planId
+                    }
+                }
+            };
+            
+            subscription = await _subscriptionService.UpdateAsync(subscription.Id, subscriptionChangeOptions);
+            
+            if (!subscription.Plan.Id.Equals(planId)) {
+                _logger.LogInformation($"{_serviceName} {methodName}: There was an issue " +
+                                       $"changing user {user.Id} plan to {planId}");
+                throw new StripeException($"{_serviceName} {methodName}: There was an " +
+                                          $"issue changing user {user.Id} plan to {planId}");
+            }
+            
+            _logger.LogInformation($"{_serviceName} ChangeSubscription: {user.Id} " +
+                                   "successfully changed to new plan {subscription.Plan.Id}");
+            
+            var quotaValue = 0;
+            var quotaString = plan.Metadata["Quota"];
+            if (!int.TryParse(quotaString, out quotaValue))
+                throw new FormatException($"{_serviceName} {methodName}: Failed to parse plan quota to int");
+            
+            _quotaClaimsService.SetQuota(user.Id, quotaValue);
+            
+            _logger.LogInformation($"{_serviceName} {methodName}: Updated user {user.Id} quota to {quotaValue}");
+            return;
         }
 
         private void PerformUserPrecheck(User user, string methodName)
