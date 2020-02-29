a131e0dad926f2830f44222a6b2820868532442f
Nicholas Chen <nixholas@icloud.com>
2020-02-29 12:13:12 +0800

    Nozomi.Compute2: Ensure that this service is a web service rather than a console app
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Compute2/Program.cs
+++ Nozomi.Compute2/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
@@ -19,8 +13,8 @@ namespace Nozomi.Compute2
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseSerilog((context, configuration) =>
                 {
                     configuration
@@ -33,6 +27,6 @@ namespace Nozomi.Compute2
                             "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
                             theme: AnsiConsoleTheme.Literate);
                 })
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .UseStartup<Startup>();
     }
 }
