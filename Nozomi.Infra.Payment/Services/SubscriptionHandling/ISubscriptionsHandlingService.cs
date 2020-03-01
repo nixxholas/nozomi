9eed667c2dc55888f0e7554d738a9ef539e620d5
Qi Xiang <qixyqix@outlook.com>
2020-03-01 14:19:51 +0800

    Nozomi.Infra.Payment: Services: SubscriptionHandling: Added subscription handling interface

---
+++ Nozomi.Infra.Payment/Services/SubscriptionHandling/ISubscriptionsHandlingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Stripe;
+
+namespace Nozomi.Infra.Payment.Services.SubscriptionHandling
+{
+    public interface ISubscriptionsHandlingService
+    {
+        Task SubscriptionCancelled(Subscription subscription);
+    }
+}
