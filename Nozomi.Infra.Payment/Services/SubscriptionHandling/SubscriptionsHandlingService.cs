14d16eb65754dc73596b6edbb901a9f0348cb9b1
Qi Xiang <qixyqix@outlook.com>
2020-03-01 14:24:37 +0800

    Nozomi.Infra.Payment: Services: SubscriptionHandling: Added subscriptions handling service

---
+++ Nozomi.Infra.Payment/Services/SubscriptionHandling/SubscriptionsHandlingService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Nozomi.Infra.Auth.Events.UserEvent;
+using Nozomi.Infra.Auth.Services.QuotaClaims;
+using Nozomi.Infra.Payment.Services.Interfaces;
+using Nozomi.Preprocessing.Abstracts;
+using Stripe;
+
+namespace Nozomi.Infra.Payment.Services.SubscriptionHandling
+{
+    public class SubscriptionsHandlingService: BaseService<SubscriptionsHandlingService>, ISubscriptionsHandlingService
+    {
+        private readonly IQuotaClaimsService _quotaClaimsService;
+        private readonly IUserEvent _userEvent;
+        
+        public SubscriptionsHandlingService(ILogger<SubscriptionsHandlingService> logger, IQuotaClaimsService quotaClaimsService, IUserEvent userEvent) : base(logger)
+        {
+            _quotaClaimsService = quotaClaimsService;
+            _userEvent = userEvent;
+        }
+
+        public Task SubscriptionCancelled(Subscription subscription)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
