ce2a3a0ca6b87df8ec32d72c390bb68b9d332238
Qi Xiang <qixyqix@outlook.com>
2020-03-01 15:27:38 +0800

    Nozomi.Infra.Payment: Services: SubscriptionHandling: subscription cancelled initial implementation

--- Nozomi.Infra.Payment/Services/SubscriptionHandling/SubscriptionsHandlingService.cs
+++ Nozomi.Infra.Payment/Services/SubscriptionHandling/SubscriptionsHandlingService.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Nozomi.Infra.Auth.Events.Stripe;
 using Nozomi.Infra.Auth.Events.UserEvent;
 using Nozomi.Infra.Auth.Services.QuotaClaims;
 using Nozomi.Infra.Payment.Services.Interfaces;
@@ -13,16 +15,39 @@ namespace Nozomi.Infra.Payment.Services.SubscriptionHandling
     {
         private readonly IQuotaClaimsService _quotaClaimsService;
         private readonly IUserEvent _userEvent;
+        private readonly IStripeEvent _stripeEvent;
         
-        public SubscriptionsHandlingService(ILogger<SubscriptionsHandlingService> logger, IQuotaClaimsService quotaClaimsService, IUserEvent userEvent) : base(logger)
+        public SubscriptionsHandlingService(ILogger<SubscriptionsHandlingService> logger, IQuotaClaimsService quotaClaimsService, IUserEvent userEvent, IStripeEvent stripeEvent) : base(logger)
         {
             _quotaClaimsService = quotaClaimsService;
             _userEvent = userEvent;
+            _stripeEvent = stripeEvent;
         }
 
-        public Task SubscriptionCancelled(Subscription subscription)
+        public async Task SubscriptionCancelled(Subscription subscription)
         {
-            throw new System.NotImplementedException();
+            const string methodName = "SubscriptionCancelled";
+            PerformSubscriptionPrecheck(subscription, methodName);
+            
+            if(!subscription.Status.ToLower().Equals("canceled "))
+                throw new InvalidOperationException($"{_serviceName} {methodName}: ");
+
+            var customerId = subscription.CustomerId;
+            var user = await _stripeEvent.GetUserByCustomerId(customerId);
+
+            if (user == null)
+                throw new NullReferenceException(
+                    $"{_serviceName} {methodName}: Unable to retrieve user based on customer id {customerId}");
+            
+            //TODO: Get default plan quota
+            var quota = 5000;
+            _quotaClaimsService.SetQuota(user.Id, quota);
+        }
+
+        private void PerformSubscriptionPrecheck(Subscription subscription, string methodName)
+        {
+            if(subscription == null)
+                throw new ArgumentNullException($"{_serviceName} {methodName}: Subscription is null");
         }
     }
 }
