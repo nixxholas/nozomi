d7ede9888fc6d044e1a429b8f9845804cc218506
Nicholas Chen <nixholas@icloud.com>
2020-03-03 20:19:20 +0800

    Nozomi.Repo: Migrations: r14 - Request FailureCount migration
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

---
+++ Nozomi.Repo/Migrations/20200303121913_r14_Request_FailureCount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Migrations
+{
+    public partial class r14_Request_FailureCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FailureCount",
+                table: "Requests",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailureCount",
+                table: "Requests");
+        }
+    }
+}
