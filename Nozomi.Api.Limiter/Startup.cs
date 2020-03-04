114fdc1275bd1ce4eb22d2f06c48b76bca02181a
Nicholas Chen <nixholas@icloud.com>
2020-03-04 21:13:26 +0800

    Nozomi.Api.Limiter: Initial DI for Redis ConnectionMultiplexer
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Api.Limiter/Startup.cs
+++ Nozomi.Api.Limiter/Startup.cs
@@ -10,22 +10,55 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Nozomi.Api.Limiter.Extensions;
+using StackExchange.Redis;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
 
 namespace Nozomi.Api.Limiter
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            if (WebHostEnvironment.IsStaging() || WebHostEnvironment.IsProduction())
+            {
+                var vaultUrl = Configuration["vaultUrl"];
+                var vaultToken = Configuration["vaultToken"];
+
+                if (string.IsNullOrEmpty(vaultToken))
+                    throw new SystemException("Invalid vault token.");
+
+                var authMethod = new TokenAuthMethodInfo(vaultToken);
+                var vaultClientSettings = new VaultClientSettings(
+                    !string.IsNullOrWhiteSpace(vaultUrl) ? vaultUrl : "https://blackbox.nozomi.one:8200",
+                    authMethod);
+                var vaultClient = new VaultClient(vaultClientSettings);
+                
+                var vault = vaultClient.V1.Secrets.Cubbyhole.ReadSecretAsync("api-limiter")
+                    .GetAwaiter()
+                    .GetResult().Data;
+                
+                services.ConfigureRedis((string) vault["redis"]);
+            }
+            else
+            {
+                var redisStr = Configuration.GetConnectionString("LocalRedis:" + @Environment.MachineName);
+                services.ConfigureRedis(redisStr);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
