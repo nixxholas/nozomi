0795938c38aeaca70b50e23b701385fb73d85e25
Nicholas Chen <nixholas@icloud.com>
2020-03-05 22:54:33 +0800

    Nozomi.Infra.Api.Limiter: Events: ApiKeyRedisAction: CanPour API: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Api.Limiter/Events/ApiKeyRedisActionEvent.cs
+++ Nozomi.Infra.Api.Limiter/Events/ApiKeyRedisActionEvent.cs
@@ -1,15 +1,34 @@
+using System;
 using Microsoft.Extensions.Logging;
 using Nozomi.Infra.Api.Limiter.Events.Interfaces;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.Auth.Data;
+using StackExchange.Redis;
 
 namespace Nozomi.Infra.Api.Limiter.Events
 {
     public class ApiKeyRedisActionEvent : BaseEvent<ApiKeyRedisActionEvent, AuthDbContext>, IApiKeyRedisActionEvent
     {
-        public ApiKeyRedisActionEvent(ILogger<ApiKeyRedisActionEvent> logger, AuthDbContext context) 
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        
+        public ApiKeyRedisActionEvent(ILogger<ApiKeyRedisActionEvent> logger, AuthDbContext context,
+            IConnectionMultiplexer connectionMultiplexer) 
             : base(logger, context)
         {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public bool CanPour(string key)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                var redisValue = _connectionMultiplexer.GetDatabase((int) NozomiRedisDatabase.BlockedApiKeys)
+                    .StringGet(key);
+
+                return redisValue.IsNullOrEmpty;
+            }
+            
+            throw new NullReferenceException("Invalid key.");
         }
     }
 }
--- Nozomi.Infra.Api.Limiter/Events/Interfaces/IApiKeyRedisActionEvent.cs
+++ Nozomi.Infra.Api.Limiter/Events/Interfaces/IApiKeyRedisActionEvent.cs
@@ -2,6 +2,11 @@ namespace Nozomi.Infra.Api.Limiter.Events.Interfaces
 {
     public interface IApiKeyRedisActionEvent
     {
-        
+        /// <summary>
+        /// Can this API Key spill into the leaky bucket of the user?
+        /// </summary>
+        /// <param name="key">The API Key in question</param>
+        /// <returns>True if we can 'pour' more into the user's bucket</returns>
+        bool CanPour(string key);
     }
 }
