5a1e4cbcf1e8cae849c037db3d5dfa1f66f40254
Nicholas Chen <nixholas@icloud.com>
2020-03-06 14:38:12 +0800

    Nozomi.Infra.Api.Limiter: Events: NozomiRedisEvent: Exists API: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Api.Limiter/Events/Interfaces/INozomiRedisEvent.cs
+++ Nozomi.Infra.Api.Limiter/Events/Interfaces/INozomiRedisEvent.cs
@@ -1,7 +1,9 @@
+using Nozomi.Preprocessing;
+
 namespace Nozomi.Infra.Api.Limiter.Events.Interfaces
 {
     public interface INozomiRedisEvent
     {
-        
+        bool Exists(string key, RedisDatabases redisDatabase = RedisDatabases.BlockedApiKeys);
     }
 }
--- Nozomi.Infra.Api.Limiter/Events/NozomiRedisEvent.cs
+++ Nozomi.Infra.Api.Limiter/Events/NozomiRedisEvent.cs
@@ -1,14 +1,33 @@
+using System;
 using Microsoft.Extensions.Logging;
 using Nozomi.Infra.Api.Limiter.Events.Interfaces;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.Auth.Data;
+using StackExchange.Redis;
 
 namespace Nozomi.Infra.Api.Limiter.Events
 {
     public class NozomiRedisEvent : BaseEvent<NozomiRedisEvent, AuthDbContext>, INozomiRedisEvent
     {
-        public NozomiRedisEvent(ILogger<NozomiRedisEvent> logger, AuthDbContext context) : base(logger, context)
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        
+        public NozomiRedisEvent(ILogger<NozomiRedisEvent> logger, AuthDbContext context,
+            IConnectionMultiplexer connectionMultiplexer) : base(logger, context)
         {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public bool Exists(string key, RedisDatabases redisDatabase = RedisDatabases.BlockedApiKeys)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                var database = _connectionMultiplexer.GetDatabase((int) redisDatabase);
+
+                return database.KeyExists(key);
+            }
+
+            throw new NullReferenceException("Invalid key.");
         }
     }
 }
