1377d8b945d0a80371838e471fbaa2dbd5780f03
Nicholas Chen <nixholas@icloud.com>
2020-03-06 18:42:11 +0800

    Nozomi.Base.Auth: Global: NozomiJwtClaimTypes: ApiKeys type
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.Auth/Global/NozomiJwtClaimTypes.cs
+++ Nozomi.Base.Auth/Global/NozomiJwtClaimTypes.cs
@@ -3,25 +3,35 @@ namespace Nozomi.Base.Auth.Global
     public static class NozomiJwtClaimTypes
     {
         public const string DefaultWallet = "default_wallet_hash";
+
         /// <summary>
         /// The unique identifier of the user on stripe.
         /// </summary>
         public const string StripeCustomerId = "stripe_cust_id";
+
         /// <summary>
         /// A set of array for the payment methods the user has.
         /// </summary>
         public const string StripeCustomerPaymentMethodId = "stripe_cust_payment_method_id";
+
         /// <summary>
         /// The Id of the user's default payment method
         /// </summary>
         public const string StripeCustomerDefaultPaymentId = "stripe_cust_default_payment_method_id";
+
         /// <summary>
         /// The Id of the user's current subscription
         /// </summary>
         public const string StripeSubscriptionId = "stripe_sub_id";
+
         /// <summary>
         /// The Ids of the user's previous subscriptions
         /// </summary>
         public const string PreviousStripeSubscriptionId = "ex_stripe_sud_id";
+
+        /// <summary>
+        /// The API Keys of the user
+        /// </summary>
+        public const string ApiKeys = "api_keys";
     }
 }
