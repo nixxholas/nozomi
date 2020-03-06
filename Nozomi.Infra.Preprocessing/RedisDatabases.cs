0147facef1d5ab86a48f6903ba1903aa1ee7f36c
Nicholas Chen <nixholas@icloud.com>
2020-03-06 19:31:02 +0800

    Nozomi.Infra.Preprocessing: RedisDatabase: Docs
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Preprocessing/RedisDatabases.cs
+++ Nozomi.Infra.Preprocessing/RedisDatabases.cs
@@ -3,8 +3,26 @@ namespace Nozomi.Preprocessing
     public enum RedisDatabases
     {
         Default = -1,
+        /// <summary>
+        /// Unrecorded API Key Events Database
+        ///
+        /// Keys are the Event's critical information
+        /// Values is the weight of the event.
+        /// </summary>
         UnrecordedApiKeyEvents = 0,
+        /// <summary>
+        /// ApiKeyUser Database
+        ///
+        /// Keys are APIs that map to its respective user.
+        /// Values are the user's ID.
+        /// </summary>
         ApiKeyUser = 2,
+        /// <summary>
+        /// BlockedUserApiKeys Database
+        /// 
+        /// Keys are UserIds
+        /// Values are API Keys
+        /// </summary>
         BlockedUserApiKeys = 3,
     }
 }
