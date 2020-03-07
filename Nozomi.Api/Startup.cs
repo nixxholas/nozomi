1016d70a8e519e5f5fccc6868d2150e21f86b367
Nicholas Chen <nixholas@icloud.com>
2020-03-07 17:42:07 +0800

    Nozomi.Api & Api.Limiter: Redis compat changes, Db injections for Nozomi.Api
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Api/Startup.cs
+++ Nozomi.Api/Startup.cs
@@ -6,33 +6,88 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Nozomi.Api.Extensions;
 using Nozomi.Infra.Api.Limiter.Events;
 using Nozomi.Infra.Api.Limiter.Events.Interfaces;
 using Nozomi.Infra.Api.Limiter.Services;
 using Nozomi.Infra.Api.Limiter.Services.Interfaces;
 using Nozomi.Preprocessing;
+using Nozomi.Repo.Auth.Data;
 using Nozomi.Service.Events;
 using Nozomi.Service.Events.Interfaces;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
 
 namespace Nozomi.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (WebHostEnvironment.IsStaging() || WebHostEnvironment.IsProduction())
+            {
+                var vaultUrl = Configuration["vaultUrl"];
+                var vaultToken = Configuration["vaultToken"];
+
+                if (string.IsNullOrEmpty(vaultToken))
+                    throw new SystemException("Invalid vault token.");
+
+                var authMethod = new TokenAuthMethodInfo(vaultToken);
+                var vaultClientSettings = new VaultClientSettings(
+                    !string.IsNullOrWhiteSpace(vaultUrl) ? vaultUrl : "https://blackbox.nozomi.one:8200",
+                    authMethod);
+                var vaultClient = new VaultClient(vaultClientSettings);
+                
+                var vault = vaultClient.V1.Secrets.Cubbyhole.ReadSecretAsync("api-limiter")
+                    .GetAwaiter()
+                    .GetResult().Data;
+                
+                services.ConfigureRedis((string) vault["redis"]);
+
+                services.AddDbContextPool<AuthDbContext>(options =>
+                {
+                    options.UseNpgsql((string) vault["auth"], 
+                        nozomiDbContextBuilder => { nozomiDbContextBuilder.EnableRetryOnFailure(); }
+                    );
+                    options.EnableSensitiveDataLogging(false);
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                });
+            }
+            else
+            {
+                Console.WriteLine(@"Welcome to the dev environment, your machine is named: " + Environment.MachineName);
+                
+                var redisStr = Configuration.GetConnectionString("LocalRedis:" + Environment.MachineName);
+                
+                services.ConfigureRedis(redisStr);
+
+                services.AddDbContextPool<AuthDbContext>(options =>
+                {
+                    options.UseNpgsql(Configuration.GetConnectionString("LocalAuth:" + Environment.MachineName), 
+                        nozomiDbContextBuilder => { nozomiDbContextBuilder.EnableRetryOnFailure(); }
+                    );
+                    options.EnableSensitiveDataLogging(false);
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                });
+            }
+            
             services.AddControllers();
 
             services.AddSwaggerGen(config =>
