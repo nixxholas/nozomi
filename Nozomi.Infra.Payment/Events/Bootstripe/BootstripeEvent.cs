312d32396900356b370772f1d954c58732604b90
Qi Xiang <qixyqix@outlook.com>
2020-03-07 13:45:57 +0800

    Nozomi.Infra.Payment: Added BootstripeEvent fles

--- Nozomi.Infra.Payment/Events/Bootstripe/BootstripeEvent.cs
+++ Nozomi.Infra.Payment/Events/Bootstripe/BootstripeEvent.cs
@@ -1,7 +1,56 @@
-﻿namespace Nozomi.Infra.Payment.Events.Bootstripe
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Nozomi.Base.Auth.Models;
+using Nozomi.Preprocessing.Abstracts;
+using Stripe;
+
+namespace Nozomi.Infra.Payment.Events.Bootstripe
 {
-    public class BootstripeEvent
+    public class BootstripeEvent : BaseEvent<BootstripeEvent>, IBootstripeEvent
     {
-        
+        public BootstripeEvent(ILogger<BootstripeEvent> logger) : base(logger)
+        {
+        }
+
+        public bool IsDefaultPlan(string planId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Plan>> Plans(bool activeOnly = true)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Plan Plan(string planId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool PlanExists(string planId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Card>> Cards(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CardExists(string stripeUserId, string cardId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<PaymentMethod>> ListPaymentMethods(string stripeUserId, string paymentMethodType = "card")
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool PaymentMethodExists(string stripeUserId, string paymentMethodId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Nozomi.Infra.Payment/Events/Bootstripe/IBootstripeEvent.cs
+++ Nozomi.Infra.Payment/Events/Bootstripe/IBootstripeEvent.cs
@@ -1,7 +1,25 @@
-﻿namespace Nozomi.Infra.Payment.Events.Bootstripe
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stripe;
+
+namespace Nozomi.Infra.Payment.Events.Bootstripe
 {
     public interface IBootstripeEvent
     {
+        bool IsDefaultPlan(string planId);
         
+        Task<IEnumerable<Plan>> Plans(bool activeOnly = true);
+
+        Plan Plan(string planId);
+
+        bool PlanExists(string planId);
+
+        Task<IEnumerable<Card>> Cards(Base.Auth.Models.User user);
+
+        bool CardExists(string stripeUserId, string cardId);
+        
+        Task<IEnumerable<PaymentMethod>> ListPaymentMethods(string stripeUserId, string paymentMethodType = "card");
+
+        bool PaymentMethodExists(string stripeUserId, string paymentMethodId);
     }
 }
