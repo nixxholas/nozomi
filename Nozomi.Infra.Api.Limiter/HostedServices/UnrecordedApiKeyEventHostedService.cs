8f095f25e843fbffc420fd3fe65002a65197d023
Nicholas Chen <nixholas@icloud.com>
2020-03-07 12:21:27 +0800

    Nozomi.Infra.Api.Limiter: HostedServices: UnrecordedApiKeyEventHostedService: About to get started
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Api.Limiter/HostedServices/UnrecordedApiKeyEventHostedService.cs
+++ Nozomi.Infra.Api.Limiter/HostedServices/UnrecordedApiKeyEventHostedService.cs
@@ -2,10 +2,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
+using StackExchange.Redis;
 
 namespace Nozomi.Infra.Api.Limiter.HostedServices
 {
+    /// <summary>
+    /// UnrecordedApiKeyEventHostedService
+    ///
+    /// Maintains the UnrecordedApiKeyEvents Redis Cache.
+    /// 1. Pops the cached items as soon as possible
+    /// 2. Processes the user claims for quota usage with the popped items
+    /// </summary>
     public class UnrecordedApiKeyEventHostedService : BaseHostedService<UnrecordedApiKeyEventHostedService>
     {
         public UnrecordedApiKeyEventHostedService(IServiceScopeFactory scopeFactory) : base(scopeFactory)
@@ -20,6 +29,12 @@ namespace Nozomi.Infra.Api.Limiter.HostedServices
 
             while (!stoppingToken.IsCancellationRequested)
             {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    // Redis connect!
+                    var connectionMultiplexer = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+                    var database = connectionMultiplexer.GetDatabase((int) RedisDatabases.UnrecordedApiKeyEvents);
+                }
             }
             
             _logger.LogCritical($"{_hostedServiceName} has broken out of its loop.");
