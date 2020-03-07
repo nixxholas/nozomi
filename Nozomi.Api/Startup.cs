d1bc51446581feda04decd8e7927aac2f518a520
Nicholas Chen <nixholas@icloud.com>
2020-03-07 17:06:37 +0800

    Nozomi.Api: Conversion to an IWebHost along wiht Swagger Docs
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Api/Startup.cs
+++ Nozomi.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Nozomi.Preprocessing;
 
 namespace Nozomi.Api
 {
@@ -26,6 +28,14 @@ namespace Nozomi.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc(GlobalApiVariables.CURRENT_API_VERSION, new OpenApiInfo {
+                    Title = "Nozomi API", 
+                    Version = GlobalApiVariables.CURRENT_API_REVISION.ToString()
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +53,14 @@ namespace Nozomi.Api
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{GlobalApiVariables.CURRENT_API_VERSION}/swagger.json", 
+                    $"Nozomi API rev. {GlobalApiVariables.CURRENT_API_REVISION}");
+            });
         }
     }
 }
