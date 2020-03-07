853fcb40d554755502f2511f6c4b9e632ad53aac
Qi Xiang <qixyqix@outlook.com>
2020-03-07 11:44:59 +0800

    Nozomi.Infra.Auth: Services: Remove ChangeSubscription. New implementation ChangePlan in Nozomi.Infra.Payment

--- Nozomi.Infra.Auth/Services/Stripe/IStripeService.cs
+++ Nozomi.Infra.Auth/Services/Stripe/IStripeService.cs
@@ -6,8 +6,5 @@ namespace Nozomi.Infra.Auth.Services.Stripe
     public interface IStripeService
     {
         Task Unsubscribe(Base.Auth.Models.User user);
-
-        void ChangeSubscription(string planId, Base.Auth.Models.User user);
-        
     }
 }
