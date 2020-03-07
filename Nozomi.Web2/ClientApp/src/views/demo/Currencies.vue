cfa3cfa8727754dcef86bd163fbd136c23ae11b7
Bing Huang <bing2609@gmail.com>
2020-03-07 16:23:24 +0800

    Nozomi.Web2: ClientApp: src: views: demo: Added demo UI which combines Currencies, Sources and Aggregated components

---
+++ Nozomi.Web2/ClientApp/src/views/demo/Currencies.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1 class="title">Currencies</h1>
+        <p class="subtitle">We support any types of currencies</p>
+
+        <b-field grouped group-multiline v-if="oidcIsAuthenticated">
+            <div class="control">
+                <CurrencyModel @created="newCurrencyCreated"/>
+            </div>
+        </b-field>
+
+        <CurrencyTable :currency-last-updated="currencyLastUpdated" />
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import CurrencyModel from '@/components/modals/currency-modal';
+    import CurrencyTable from '@/components/tables/currencies-table';
+
+    export default {
+        components: {
+            CurrencyModel,
+            CurrencyTable
+        },
+        data() {
+            return {
+                currencyLastUpdated: null
+            };
+        },
+        computed: {
+            ...mapGetters('oidcStore', [
+                'oidcIsAuthenticated'
+            ])
+        },
+        methods: {
+            newCurrencyCreated(value) {
+                if (value) {
+                    this.currencyLastUpdated = Date.now();
+                }
+            }
+        }
+    }
+</script>
