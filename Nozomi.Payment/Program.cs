bc7c96512222f890342c4af1746770f84a525e51
Nicholas Chen <nixholas@icloud.com>
2020-03-10 05:56:11 +0800

    Nozomi.Payment: Kestrel TLS injection
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Payment/Program.cs
+++ Nozomi.Payment/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Nozomi.Payment
 {
@@ -14,6 +18,51 @@ namespace Nozomi.Payment
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                    
+                    // options.Listen(IPAddress.Any, 8080);         // http:*:80
+                    
+                    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    var isDevelopment = environment == Environments.Development;
+                    var validateSSL = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SSLCERT_PATH"));
+                    
+                    // HTTPS Configuration
+                    if (!System.Diagnostics.Debugger.IsAttached && !isDevelopment && validateSSL)
+                    {
+                        var hasHttpsPortConfigured = int.TryParse(Environment.GetEnvironmentVariable("HTTPS_PORT")
+                            , out var httpsPort);
+                        if (!hasHttpsPortConfigured)
+                        {
+                            httpsPort = 5001; // Default port
+
+                            Console.WriteLine("HTTPS port not configured! Self configuring to 5001.");
+                        }
+                        
+                        var certPath = Environment.GetEnvironmentVariable("SSLCERT_PATH");
+                        if (string.IsNullOrEmpty(certPath)) {
+                            certPath = "nozomi.pfx"; // Deefault
+
+                            Console.WriteLine("SSLCERT_PATH not configured! Self configuring to nozomi.pfx");
+                        }
+                        
+                        var certPassword = Environment.GetEnvironmentVariable("SSLCERT_PASSWORD");
+                        if (string.IsNullOrEmpty(certPassword))
+                        {
+                            certPassword = "290597"; // Deefault
+
+                            Console.WriteLine("SSLCERT_PASSWORD not configured! Self configuring to the defaults.");
+                        }
+                        
+                        options.Listen(IPAddress.Any, httpsPort, listenOptions =>
+                        {
+                            var cert = new X509Certificate2(certPath, certPassword);
+
+                            listenOptions.UseHttps(cert);
+                        });
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
