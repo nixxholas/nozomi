efd88d2637218d4769a57105d607ca28bdf5eb83
Nicholas Chen <nixholas@icloud.com>
2020-03-11 06:54:05 +0800

    Noozmi.Infra.Payment: Services: Bootstripe: Omit redundant injections, make sure stripe services are injected in the respective methods to reduce memory overheads
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Payment/Services/Bootstripe/BootstripeService.cs
+++ Nozomi.Infra.Payment/Services/Bootstripe/BootstripeService.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic.CompilerServices;
 using Nozomi.Base.Auth.Models;
+using Nozomi.Base.BCL.Configurations;
 using Nozomi.Infra.Auth.Events.UserEvent;
 using Nozomi.Infra.Auth.Services.QuotaClaims;
 using Nozomi.Infra.Auth.Services.User;
@@ -18,24 +20,18 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
     public class BootstripeService : BaseService<BootstripeService>, IBootstripeService
     {
         private readonly IUserService _userService;
-        private readonly IQuotaClaimsService _quotaClaimsService;
+        // private readonly IQuotaClaimsService _quotaClaimsService;
         private readonly IUserEvent _userEvent;
-        
-        private readonly CustomerService _customerService;
-        private readonly PaymentMethodService _paymentMethodService;
-        private readonly PlanService _planService;
-        private readonly SubscriptionService _subscriptionService;
-        
-        public BootstripeService(ILogger<BootstripeService> logger, IUserService userService, IUserEvent userEvent, IQuotaClaimsService quotaClaimsService) : base(logger)
+
+        public BootstripeService(ILogger<BootstripeService> logger, IUserService userService, IUserEvent userEvent,
+            IOptions<StripeOptions> stripeOptions// , IQuotaClaimsService quotaClaimsService
+            ) : base(logger)
         {
+            StripeConfiguration.ApiKey = stripeOptions.Value.SecretKey;
+
             _userService = userService;
-            _quotaClaimsService = quotaClaimsService;
+            // _quotaClaimsService = quotaClaimsService;
             _userEvent = userEvent;
-            
-            _customerService = new CustomerService();
-            _paymentMethodService = new PaymentMethodService();
-            _planService = new PlanService();
-            _subscriptionService = new SubscriptionService();
         }
 
         public async Task RegisterCustomer(User user)
@@ -55,7 +51,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
                 Email = user.Email
             };
 
-            Customer stripeCustomer = await _customerService.CreateAsync(createOptions);
+            var customerService = new CustomerService();
+            Customer stripeCustomer = await customerService.CreateAsync(createOptions);
 
             if (stripeCustomer.StripeResponse.StatusCode != HttpStatusCode.OK)
             {
@@ -89,7 +86,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
                 Customer = stripeCustomerId,
             };
 
-            var result = _paymentMethodService.Attach(paymentMethodId, options);
+            var paymentMethodService = new PaymentMethodService();
+            var result = paymentMethodService.Attach(paymentMethodId, options);
 
             if (result != null && result.StripeResponse != null && result.StripeResponse.StatusCode.Equals(HttpStatusCode.OK))
             {
@@ -132,15 +130,16 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
                 Customer = stripeCustomerId
             };
 
-            var paymentMethods = _paymentMethodService.List(paymentMethodOptions);
+            var paymentMethodService = new PaymentMethodService();
+            var paymentMethods = paymentMethodService.List(paymentMethodOptions);
 
             if (paymentMethods.Count() <= 1)
             {
                 _logger.LogInformation($"{_serviceName} {methodName}: {user.Id} only has one payment method, detachment prevented.");
                 throw new StripeException($"{_serviceName} {methodName}: {user.Id} only has one payment method, detachment prevented.");
             }
-
-            var paymentMethod = _paymentMethodService.Detach(paymentMethodId);
+            
+            var paymentMethod = paymentMethodService.Detach(paymentMethodId);
 
             if (paymentMethod == null || paymentMethod.Customer != null)
             {
@@ -152,7 +151,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
             _logger.LogInformation($"{_serviceName} RemovePaymentMethod: {user.Id} successfully " +
                                $"removed a payment method that was tokenized as {paymentMethodId}");
 
-            var customer = await _customerService.GetAsync(stripeCustomerId);
+            var customerService = new CustomerService();
+            var customer = await customerService.GetAsync(stripeCustomerId);
 
             if(customer == null)
                 throw new StripeException($"{_serviceName} {methodName}: An error occured while trying to retrieve stripe customer {stripeCustomerId}");
@@ -182,7 +182,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
                 }
             };
 
-            var updatedCustomer = await _customerService.UpdateAsync(stripeCustomerId, customerUpdateOptions);
+            var customerService = new CustomerService();
+            var updatedCustomer = await customerService.UpdateAsync(stripeCustomerId, customerUpdateOptions);
             if (updatedCustomer.InvoiceSettings.DefaultPaymentMethodId != paymentMethodId)
                 throw new StripeException($"{_serviceName} {methodName}: An error occured while trying to update user's default payment method.");
 
@@ -196,7 +197,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
         }
 
         private async void CheckPaymentMethodOwnership(string userId, string paymentMethodId, string methodName) {
-            var paymentMethod = await _paymentMethodService.GetAsync(paymentMethodId);
+            var paymentMethodService = new PaymentMethodService();
+            var paymentMethod = await paymentMethodService.GetAsync(paymentMethodId);
 
             var stripeCustomerId = _userEvent.GetStripeCustomerId(userId);
 
@@ -209,7 +211,8 @@ namespace Nozomi.Infra.Payment.Services.Bootstripe
 
         private async Task<int> GetPlanQuota(string planid, string methodName)
         {
-            var stripePlan = await _planService.GetAsync(planid);
+            var planService = new PlanService();
+            var stripePlan = await planService.GetAsync(planid);
 
             if (stripePlan == null)
                 throw new StripeException($"{_serviceName} {methodName}: Unable to find plan based on ID");
