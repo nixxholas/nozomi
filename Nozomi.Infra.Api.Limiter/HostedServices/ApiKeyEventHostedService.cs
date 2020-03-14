71e3b083e72c7d67ea0d0f8d5ae212a7d1e1e86c
nixholas <nixholas@icloud.com>
2020-03-15 01:41:16 +0800

    Nozomi.Infra.Api.Limiter: HostedServices: ApiKeyEvent: ApiKey to UserId navigation functionality
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Infra.Api.Limiter/HostedServices/ApiKeyEventHostedService.cs
+++ Nozomi.Infra.Api.Limiter/HostedServices/ApiKeyEventHostedService.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nozomi.Base.Auth.Global;
+using Nozomi.Infra.Api.Limiter.Events.Interfaces;
 using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Repo.Auth.Data;
@@ -51,33 +52,50 @@ namespace Nozomi.Infra.Api.Limiter.HostedServices
                         if (oldestWeight.HasValue && oldestWeight.IsInteger && long.TryParse(oldestWeight, 
                             out var weight) && weight > 0)
                         {
-                            var authDbContext = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
-                            
-                            // Obtain the user's quota
-                            var userQuota = authDbContext.UserClaims
-                                .AsTracking() // Ensure we track to modify directly
-                                .SingleOrDefault(uc => 
-                                    uc.ClaimType.Equals(NozomiJwtClaimTypes.UserQuota));
+                            // First navigate to the user first
+                            var nozomiRedisEvent = scope.ServiceProvider.GetRequiredService<INozomiRedisEvent>();
+                            var userKey = nozomiRedisEvent.GetValue(key, 
+                                RedisDatabases.ApiKeyUser);
 
-                            if (userQuota != null && long.TryParse(userQuota.ClaimValue, out var quotaCount))
+                            if (!userKey.IsNullOrEmpty)
                             {
-                                // Quota is around
-                                if (quotaCount > 0)
+                                var authDbContext = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+
+                                // Obtain the user's quota
+                                var userQuota = authDbContext.UserClaims
+                                    .AsTracking() // Ensure we track to modify directly
+                                    .SingleOrDefault(uc => 
+                                        uc.ClaimType.Equals(NozomiJwtClaimTypes.UserQuota));
+
+                                if (userQuota != null && long.TryParse(userQuota.ClaimValue, out var quotaCount))
                                 {
-                                    userQuota.ClaimValue = (quotaCount + weight).ToString(); // Update it
-                                    authDbContext.UserClaims.Update(userQuota);
-                                    await authDbContext.SaveChangesAsync(stoppingToken);
+                                    // Quota is around
+                                    if (quotaCount > 0)
+                                    {
+                                        userQuota.ClaimValue = (quotaCount + weight).ToString(); // Update it
+                                        authDbContext.UserClaims.Update(userQuota);
+                                        await authDbContext.SaveChangesAsync(stoppingToken);
                                     
-                                    _logger.LogInformation($"{_hostedServiceName} ExecuteAsync: User " +
-                                                           $"{userQuota.UserId} with quota count updated to " +
-                                                           $"{userQuota.ClaimValue}");
-                                }
-                                else // Quota is bad
-                                {
-                                    _logger.LogWarning($"{_hostedServiceName} ExecuteAsync: Erroneous quota " +
-                                                       $"count detected for user {userQuota.UserId}");
+                                        _logger.LogInformation($"{_hostedServiceName} ExecuteAsync: User " +
+                                                               $"{userQuota.UserId} with quota count updated to " +
+                                                               $"{userQuota.ClaimValue}");
+                                    }
+                                    else // Quota is bad
+                                    {
+                                        _logger.LogWarning($"{_hostedServiceName} ExecuteAsync: Erroneous quota " +
+                                                           $"count detected for user {userQuota.UserId}");
+                                    }
                                 }
                             }
+                            else
+                            {
+                                // Pop it back in
+                                database.ListRightPush(key, oldestWeight);
+                                
+                                _logger.LogWarning($"{_hostedServiceName}: ExecuteAsync: Invalid API Key.." +
+                                                   $" Key: {key}");
+                            }
+
                         }
                         
                         
