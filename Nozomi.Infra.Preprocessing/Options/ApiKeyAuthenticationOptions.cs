857447043f10add775dc105fc306593377f2eabf
Nicholas Chen <nixholas@icloud.com>
2020-03-15 19:11:07 +0800

    Nozomi.Infra.Preprocessing: Options: ApiKeyAuthenticationOptions: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

---
+++ Nozomi.Infra.Preprocessing/Options/ApiKeyAuthenticationOptions.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace Nozomi.Preprocessing.Options
+{
+    public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        public const string DefaultScheme = "API Key";
+        public string Scheme => DefaultScheme;
+        public string AuthenticationType = DefaultScheme;
+    }
+}
