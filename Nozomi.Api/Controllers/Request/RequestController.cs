1cceadb796b8119dfbd1b0be60a1f64d08710fc9
nixholas <nixholas@icloud.com>
2020-03-15 11:50:34 +0800

    Nozomi.Api: Controllers: Request: All API: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Api/Controllers/Request/RequestController.cs
+++ Nozomi.Api/Controllers/Request/RequestController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Nozomi.Data.ViewModels.Request;
 using Nozomi.Infra.Api.Limiter.Attributes;
 using Nozomi.Infra.Api.Limiter.Events.Interfaces;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Preprocessing.ActionResults;
 using Nozomi.Service.Events.Interfaces;
@@ -20,6 +21,28 @@ namespace Nozomi.Api.Controllers.Request
         {
             _nozomiRedisEvent = nozomiRedisEvent;
             _requestEvent = requestEvent;
+        }
+
+        [TokenBucket(Name = "Request/Get", Weight = 5)]
+        [HttpGet]
+        [Produces("application/json")]
+        public IActionResult All(int index = 0)
+        {
+            if (index < 0)
+            {
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var apiKey))
+                {
+                    var userId = _nozomiRedisEvent.GetValue(apiKey, RedisDatabases.ApiKeyUser);
+                    
+                    return Ok(_requestEvent.All(index, userId));
+                }
+
+                _logger.LogWarning($"{_controllerName} All: User managed to bypass the token bucket " +
+                                   "attribute without an API key!");
+                return new InternalServerErrorObjectResult("Not sure how you got here, but no.");
+            }
+
+            return BadRequest("Invalid index.");
         }
 
         [TokenBucket(Name = "Request/Get", Weight = 1)]
