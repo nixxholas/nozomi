237d8f3bead04984f81b631b7ec054f3f6395761
Nicholas Chen <nixholas@Nicholass-MacBook-Pro.local>
2020-03-15 18:54:49 +0800

    Nozomi.Api: Controllers: Connect: Validate API: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@Nicholass-MacBook-Pro.local>

--- Nozomi.Api/Controllers/Connect/ConnectController.cs
+++ Nozomi.Api/Controllers/Connect/ConnectController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Infra.Api.Limiter.Events.Interfaces;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
 using Nozomi.Preprocessing.Attributes;
 
@@ -7,15 +9,28 @@ namespace Nozomi.Api.Controllers.Connect
 {
     public class ConnectController : BaseApiController<ConnectController>, IConnectController
     {
-        public ConnectController(ILogger<ConnectController> logger) : base(logger)
+        private readonly INozomiRedisEvent _nozomiRedisEvent;
+        
+        public ConnectController(ILogger<ConnectController> logger, INozomiRedisEvent nozomiRedisEvent) : base(logger)
         {
+            _nozomiRedisEvent = nozomiRedisEvent;
         }
 
         [Throttle(Name = "Connect/Validate", Milliseconds = 2500)]
         [HttpHead]
         public IActionResult Validate()
         {
-            throw new System.NotImplementedException();
+            if (HttpContext.Request.Headers.TryGetValue("Authorization", out var apiKey))
+            {
+                var userId = _nozomiRedisEvent.GetValue(apiKey, RedisDatabases.ApiKeyUser);
+
+                if (userId.HasValue)
+                    return Ok();
+                else
+                    return BadRequest("Invalid API Key.");
+            }
+
+            return BadRequest("Please inject the API key in the authorization header.");
         }
     }
 }
