21a0f96cb1ce9a85db90135e78f04961dac7a725
Nicholas Chen <nixholas@icloud.com>
2020-03-18 06:15:26 +0800

    Nozomi.Base.BCL: Helpers: Native: Numerals: NumberHelper: Random number generator methods
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.BCL/Helpers/Native/Numerals/NumberHelper.cs
+++ Nozomi.Base.BCL/Helpers/Native/Numerals/NumberHelper.cs
@@ -1,8 +1,32 @@
+using System;
+
 namespace Nozomi.Base.BCL.Helpers.Native.Numerals
 {
     public static class NumberHelper
     {
         public static bool IsNumericDecimal(string val) => 
             !string.IsNullOrWhiteSpace(val) && decimal.TryParse(val, out _);
+        
+        /// <summary>
+        /// Returns an Int32 with a random value across the entire range of
+        /// possible values.
+        /// </summary>
+        public static int GenerateInt32(this Random rng)
+        {
+            int firstBits = rng.Next(0, 1 << 4) << 28;
+            int lastBits = rng.Next(0, 1 << 28);
+            return firstBits | lastBits;
+        }
+
+        public static decimal GenerateDecimal(this Random rng)
+        {
+            byte scale = (byte) rng.Next(29);
+            bool sign = rng.Next(2) == 1;
+            return new decimal(rng.GenerateInt32(), 
+                rng.GenerateInt32(),
+                rng.GenerateInt32(),
+                sign,
+                scale);
+        }
     }
 }
