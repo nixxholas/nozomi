20a7f435e4366d75bbccd248c29996fd65552356
Qi Xiang <qixyqix@outlook.com>
2020-03-19 00:20:55 +0800

    Nozomi.Base.Domain: ViewModels: Compute: Added CreateComputeViewModel

--- Nozomi.Base.Domain/ViewModels/Compute/CreateComputeViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/Compute/CreateComputeViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Nozomi.Data.ViewModels.Compute
+﻿using System;
+using FluentValidation;
+
+namespace Nozomi.Data.ViewModels.Compute
 {
-    public class CreateComputeViewModel
+    public class CreateComputeViewModel : BaseComputeViewModel
     {
         
+    }
+
+    public class CreateComputeViewModelValidator : AbstractValidator<CreateComputeViewModel>
+    {
+        public CreateComputeViewModelValidator()
+        {
+            RuleFor(e => e.Key).NotNull();
+            RuleFor(e => e.Formula).NotNull().NotEmpty();
+            RuleFor(e => e.Delay).NotNull().NotEmpty();
+        }
     }
 }
