93cf8075cae02fc793ecedb1602a939aa2699ea8
Qi Xiang <qixyqix@outlook.com>
2020-03-19 00:09:35 +0800

    Nozomi.Base.Domain: ViewModels: Compute Added BaseComputeViewModel

---
+++ Nozomi.Base.Domain/ViewModels/Compute/BaseComputeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Nozomi.Data.ViewModels.Compute
+{
+    public class BaseComputeViewModel
+    {
+        public string Key { get; set; }
+        
+        public string Formula { get; set; }
+       
+        public int Delay { get; set; }
+    }
+}
