c0f37f2c4e1487dccceee7ac011987831d495253
Nicholas Chen <nixholas@icloud.com>
2020-03-21 01:02:53 +0800

    Nozomi.Api: Controllers: WebsocketCommandProperty: All API: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Api/Controllers/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
+++ Nozomi.Api/Controllers/WebsocketCommandProperty/WebsocketCommandPropertyController.cs
@@ -1,8 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nozomi.Api.Controllers.WebsocketCommandProperty.Examples;
+using Nozomi.Data.ViewModels.WebsocketCommandProperty;
+using Nozomi.Infra.Api.Limiter.Attributes;
 using Nozomi.Infra.Api.Limiter.Events.Interfaces;
+using Nozomi.Preprocessing;
 using Nozomi.Preprocessing.Abstracts;
+using Nozomi.Preprocessing.ActionResults;
+using Nozomi.Preprocessing.Options;
 using Nozomi.Service.Events.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace Nozomi.Api.Controllers.WebsocketCommandProperty
 {
@@ -20,9 +32,33 @@ namespace Nozomi.Api.Controllers.WebsocketCommandProperty
             _websocketCommandPropertyEvent = websocketCommandPropertyEvent;
         }
 
+        [Authorize]
+        [TokenBucket(Name = "WebsocketCommandProperty/All", Weight = 5)]
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(ICollection<WebsocketCommandPropertyViewModel>))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Type = typeof(string))]
+        [SwaggerResponseExample((int)HttpStatusCode.OK, typeof(AllOkExample))]
+        [SwaggerResponseExample((int)HttpStatusCode.BadRequest, typeof(AllBadRequestExample))]
+        [SwaggerResponseExample((int)HttpStatusCode.InternalServerError, typeof(AllInternalServerExample))]
         public IActionResult All(int index = 0)
         {
-            throw new System.NotImplementedException();
+            if (index >= 0) return BadRequest(AllBadRequestExample.Result);
+            
+            if (HttpContext.Request.Headers.TryGetValue(ApiKeyAuthenticationOptions.HeaderKey, 
+                out var apiKey))
+            {
+                var userId = _nozomiRedisEvent.GetValue(apiKey, RedisDatabases.ApiKeyUser);
+                
+                if (!Guid.TryParse(userId, out var parsedGuid)) return new InternalServerErrorObjectResult(
+                    AllInternalServerExample.InvalidUserResult);
+                    
+                return Ok(_websocketCommandPropertyEvent.ViewAll(index, null, userId));
+            }
+
+            _logger.LogWarning($"{_controllerName} All: User managed to bypass the token bucket " +
+                               "attribute without an API key!");
+            return new InternalServerErrorObjectResult(AllInternalServerExample.ImpossibleInvalidUserResult);
         }
 
         public IActionResult AllByCommand(string commandGuid, int index = 0)
