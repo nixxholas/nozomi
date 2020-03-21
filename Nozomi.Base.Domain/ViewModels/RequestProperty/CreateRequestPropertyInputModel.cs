9fb9b1bf5b9ae18df6466cf0aee4208c00794e87
Nicholas Chen <nixholas@icloud.com>
2020-03-21 14:43:29 +0800

    Nozomi.Base.Domain: ViewModels: RequestProperty: Docs
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/RequestProperty/CreateRequestPropertyInputModel.cs
+++ Nozomi.Base.Domain/ViewModels/RequestProperty/CreateRequestPropertyInputModel.cs
@@ -14,12 +14,24 @@ namespace Nozomi.Data.ViewModels.RequestProperty
             Value = value;
         }
         
+        /// <summary>
+        /// The type of the request property.
+        /// </summary>
         public RequestPropertyType Type { get; set; }
         
+        /// <summary>
+        /// The key of the request property, if any.
+        /// </summary>
         public string Key { get; set; }
         
+        /// <summary>
+        /// The value of the request property.
+        /// </summary>
         public string Value { get; set; }
         
+        /// <summary>
+        /// The request this property is binded to.
+        /// </summary>
         public string RequestGuid { get; set; }
         
         public bool IsValid()
