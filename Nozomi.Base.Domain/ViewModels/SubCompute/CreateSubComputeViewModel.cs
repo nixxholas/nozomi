adb0f5d78a2ca65c7cd18494452c9fb7efb78556
Qi Xiang <qixyqix@outlook.com>
2020-03-21 15:43:13 +0800

    Nozomi.Base.Domain: ViewModels: SubCompute: Added CreateSubComputeViewModel implementation

--- Nozomi.Base.Domain/ViewModels/SubCompute/CreateSubComputeViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/SubCompute/CreateSubComputeViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace Nozomi.Data.ViewModels.SubCompute
+﻿using System;
+using FluentValidation;
+
+namespace Nozomi.Data.ViewModels.SubCompute
 {
     public class CreateSubComputeViewModel
     {
-        
+        public Guid ParentComputeGuid { get; set; }
+        public Guid ChildComputeGuid { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new CreateSubComputeViewModelValidator();
+            return validator.Validate(this).IsValid;
+        }
+
+        protected class CreateSubComputeViewModelValidator : AbstractValidator<CreateSubComputeViewModel>
+        {
+            public CreateSubComputeViewModelValidator()
+            {
+                RuleFor(e => e.ParentComputeGuid).NotNull().NotEmpty();
+            }
+        }
     }
 }
