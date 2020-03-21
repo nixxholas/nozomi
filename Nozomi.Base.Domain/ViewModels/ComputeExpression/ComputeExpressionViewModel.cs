b45fcd2f910cea9ff7979359ec606a2e93138b1a
Qi Xiang <qixyqix@outlook.com>
2020-03-21 15:00:54 +0800

    Nozomi.Base.Domain: ViewModels: ComputeExpression: Added ComputeExpressionViewModel

---
+++ Nozomi.Base.Domain/ViewModels/ComputeExpression/ComputeExpressionViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Nozomi.Data.Models.Web;
+
+namespace Nozomi.Data.ViewModels.ComputeExpression
+{
+    public class ComputeExpressionViewModel : CreateComputeExpressionViewModel
+    {
+        public Guid Guid { get; set; }
+        
+        public bool IsEnabled { get; set; }
+        
+        public Models.Web.Compute Compute { get; set; }
+
+        public ComputeExpressionViewModel()
+        {
+        }
+
+        public ComputeExpressionViewModel(Guid guid, ComputeExpressionType type, string expression, string value,
+            Guid computeGuid, bool isEnabled)
+        {
+            Guid = guid;
+            Type = type;
+            Expression = expression;
+            Value = value;
+            ComputeGuid = computeGuid;
+            IsEnabled = isEnabled;
+        }
+        
+        public ComputeExpressionViewModel(Guid guid, ComputeExpressionType type, string expression, string value,
+            Guid computeGuid, bool isEnabled, Models.Web.Compute compute)
+        {
+            Guid = guid;
+            Type = type;
+            Expression = expression;
+            Value = value;
+            ComputeGuid = computeGuid;
+            IsEnabled = isEnabled;
+
+            Compute = compute;
+        }
+    }
+}
