cb96a38afe6eb1ecd98d1d77ad3090c0b6fdc335
Qi Xiang <qixyqix@outlook.com>
2020-03-21 15:52:14 +0800

    Nozomi.Base.Domain: ViewModels: SubCompute: Added SubComputeViewModel implementation

---
+++ Nozomi.Base.Domain/ViewModels/SubCompute/SubComputeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Routing;
+
+namespace Nozomi.Data.ViewModels.SubCompute
+{
+    public class SubComputeViewModel : CreateSubComputeViewModel
+    {
+        public SubComputeViewModel()
+        {
+        }
+
+        public SubComputeViewModel(Guid guid, Guid parentComputeGuid, Guid childComputeGuid, bool isEnabled)
+        {
+            Guid = guid;
+            ParentComputeGuid = parentComputeGuid;
+            ChildComputeGuid = childComputeGuid;
+        }
+        
+        public SubComputeViewModel(Guid guid, Guid parentComputeGuid, Guid childComputeGuid, Models.Web.Compute parentCompute, Models.Web.Compute childCompute, bool isEnabled)
+        {
+            Guid = guid;
+            ParentComputeGuid = parentComputeGuid;
+            ChildComputeGuid = childComputeGuid;
+
+            ParentCompute = parentCompute;
+            ChildCompute = childCompute;
+        }
+
+        public Guid Guid { get; set; }
+
+        public bool IsEnabled { get; set; }
+        
+        public Models.Web.Compute ParentCompute { get; set; }
+        
+        public Models.Web.Compute ChildCompute { get; set; }
+    }
+}
