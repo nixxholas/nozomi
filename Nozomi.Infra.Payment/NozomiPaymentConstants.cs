663c96fee446a1e9cd780510a03262e152c8b0c5
Nicholas Chen <nixholas@icloud.com>
2020-03-21 17:34:53 +0800

    Nozomi.Infra.Payment: NozomiPaymentConstants: StripeRequestQuotaMetadataKey: Initial introduction
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Infra.Payment/NozomiPaymentConstants.cs
+++ Nozomi.Infra.Payment/NozomiPaymentConstants.cs
@@ -3,5 +3,7 @@ namespace Nozomi.Infra.Payment
     public static class NozomiPaymentConstants
     {
         public const string StripeQuotaMetadataKey = "API Calls";
+
+        public const string StripeRequestQuotaMetadataKey = "Requests";
     }
 }
