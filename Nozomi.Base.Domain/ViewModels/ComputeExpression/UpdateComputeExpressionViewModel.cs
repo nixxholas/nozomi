9fe9024f697072a850093048f0b0c7fe39c97e68
Qi Xiang <qixyqix@outlook.com>
2020-03-21 15:35:18 +0800

    Nozomi.Base.Domain: ViewModels: ComputeExpression: Added UpdateComputeExpression

---
+++ Nozomi.Base.Domain/ViewModels/ComputeExpression/UpdateComputeExpressionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentValidation;
+using Nozomi.Data.Models.Web;
+
+namespace Nozomi.Data.ViewModels.ComputeExpression
+{
+    public class UpdateComputeExpressionViewModel : ComputeExpressionViewModel
+    {
+        public new ComputeExpressionType Type { get; set; }
+        
+        public new string Expression { get; set; }
+        
+        public new string Value { get; set; }
+        
+        public new Guid ComputeGuid { get; set; }
+        
+        public new bool? IsEnabled { get; set; }
+
+        protected class UpdateComputeExpressionViewModelValidator : AbstractValidator<UpdateComputeExpressionViewModel>
+        {
+            public UpdateComputeExpressionViewModelValidator()
+            {
+                RuleFor(e => e.Guid).NotNull().NotEmpty();
+
+                RuleFor(e => e.Type).NotNull();
+                RuleFor(e => e.Expression).NotNull().NotEmpty();
+                RuleFor(e => e.Value).NotNull().NotEmpty();
+                RuleFor(e => e.ComputeGuid).NotNull().NotEmpty();
+            }
+        }
+    }
+}
