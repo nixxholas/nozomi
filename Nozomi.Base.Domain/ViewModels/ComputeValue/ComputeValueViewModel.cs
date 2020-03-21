03c2c06fd0e546b842cab85fd95a1b7cec1fbe9a
Qi Xiang <qixyqix@outlook.com>
2020-03-21 14:02:16 +0800

    Nozomi.Base.Domain: ViewModel: ComputeValue: Added ComputeValueViewModel

---
+++ Nozomi.Base.Domain/ViewModels/ComputeValue/ComputeValueViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Nozomi.Data.ViewModels.ComputeValue
+{
+    public class ComputeValueViewModel : CreateComputeValueViewModel
+    {
+        public ComputeValueViewModel()
+        {
+        }
+
+        public ComputeValueViewModel(Guid guid, string value, Guid computeGuid, bool isEnabled)
+        {
+            Guid = guid;
+            Value = value;
+            ComputeGuid = computeGuid;
+            IsEnabled = isEnabled;
+        }
+
+        public ComputeValueViewModel(Guid guid, string value, Guid computeGuid, bool isEnabled, Models.Web.Compute compute)
+        {
+            Guid = guid;
+            Value = value;
+            ComputeGuid = computeGuid;
+            IsEnabled = isEnabled;
+            
+            Compute = compute;
+        }
+
+        public Guid Guid { get; set; }
+        
+        public bool IsEnabled { get; set; }
+        
+        public Models.Web.Compute Compute { get; set; }
+    }
+}
