94a283deb003f19a9c0962da6578b5d9c00166c2
Nicholas Chen <nixholas@icloud.com>
2020-03-21 14:49:11 +0800

    Nozomi.Base.Domain: ViewModels: ComponentHistoricItem: Docs
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/ComponentHistoricItem/ComponentHistoricItemViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/ComponentHistoricItem/ComponentHistoricItemViewModel.cs
@@ -4,8 +4,14 @@ namespace Nozomi.Data.ViewModels.ComponentHistoricItem
 {
     public class ComponentHistoricItemViewModel
     {
+        /// <summary>
+        /// The timestamp of this historical value.
+        /// </summary>
         public DateTime Timestamp { get; set; }
         
+        /// <summary>
+        /// The value of this historical item.
+        /// </summary>
         public string Value { get; set; }
     }
 }
