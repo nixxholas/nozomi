cc8f85805f4b5da916563b35337fe2bb5389cd3b
Qi Xiang <qixyqix@outlook.com>
2020-03-21 13:48:13 +0800

    Nozomi.Base.Domain: ViewModels: ComputeValue: Added CreateComputeValueViewModel

--- Nozomi.Base.Domain/ViewModels/ComputeValue/CreateComputeValueViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/ComputeValue/CreateComputeValueViewModel.cs
@@ -1,7 +1,27 @@
-﻿namespace Nozomi.Data.ViewModels.ComputeValue
+﻿using System;
+using FluentValidation;
+
+namespace Nozomi.Data.ViewModels.ComputeValue
 {
     public class CreateComputeValueViewModel
     {
+        public string Value { get; set; }
         
+        public Guid ComputeGuid { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new CreateComputeValueViewModelValidator();
+            return validator.Validate(this).IsValid;
+        }
+
+        protected class CreateComputeValueViewModelValidator : AbstractValidator<CreateComputeValueViewModel>
+        {
+            public CreateComputeValueViewModelValidator()
+            {
+                RuleFor(e => e.Value).NotNull().NotEmpty();
+                RuleFor(e => e.ComputeGuid).NotNull().NotEmpty();
+            }
+        }
     }
 }
