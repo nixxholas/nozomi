4226040bbba5e7e82364baa01a9f07a724e707bd
Qi Xiang <qixyqix@outlook.com>
2020-03-21 13:20:01 +0800

    Nozomi.Base.Domain: ViewModels: Compute: Added UpdateComputeViewModel implementation

--- Nozomi.Base.Domain/ViewModels/Compute/UpdateComputeViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/Compute/UpdateComputeViewModel.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using FluentValidation;
 using Nozomi.Data.Models.Web;
 
 namespace Nozomi.Data.ViewModels.Compute
 {
     public class UpdateComputeViewModel : ComputeViewModel
     {
-        
+        public new bool? IsEnabled { get; set; }
+        public new string Key { get; set; }
+        public new string Formula { get; set; }
+        public new int? Delay { get; set; }
+
+        public new bool IsValid()
+        {
+            var validator = new UpdateComputeValidator();
+            return validator.Validate(this).IsValid;
+        }
+
+        protected class UpdateComputeValidator : AbstractValidator<UpdateComputeViewModel>
+        {
+            public UpdateComputeValidator()
+            {
+                RuleFor(e => e.Guid).NotEmpty().NotNull();   
+                RuleFor(e => e.Key).NotNull();
+                RuleFor(e => e.Formula).NotNull().NotEmpty();
+                RuleFor(e => e.Delay).NotNull().NotEmpty();
+            }
+        }
     }
 }
