30206c69562a00c7ab041ab9cbdb2a3cece5d11d
Qi Xiang <qixyqix@outlook.com>
2020-03-21 14:28:28 +0800

    Nozomi.Base.Domain: ViewModels: ComputeValue: Added UpdateComputeValueViewModel implementation

---
+++ Nozomi.Base.Domain/ViewModels/ComputeValue/UpdateComputeValueViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentValidation;
+
+namespace Nozomi.Data.ViewModels.ComputeValue
+{
+    public class UpdateComputeValueViewModel : ComputeValueViewModel
+    {
+        public new bool? IsEnabled { get; set; }
+
+        public new Guid? ComputeGuid { get; set; }
+        
+        public new string Value { get; set; }
+
+        protected class UpdateComputeValueViewModelValidator : AbstractValidator<UpdateComputeValueViewModel>
+        {
+            public UpdateComputeValueViewModelValidator()
+            {
+                RuleFor(e => e.Guid).NotNull().NotEmpty();
+                RuleFor(e => e.ComputeGuid).NotNull().NotEmpty();
+                RuleFor(e => e.Value).NotNull().NotEmpty();
+            }
+        }
+    }
+}
