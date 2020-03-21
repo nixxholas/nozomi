b689cf06877d8f6be04ddd8405db4602e6306156
Qi Xiang <qixyqix@outlook.com>
2020-03-21 14:52:26 +0800

    Nozomi.Base.Domain: ViewModels: ComputeExpression: Added CreateComputeExpressionViewModel

--- Nozomi.Base.Domain/ViewModels/ComputeExpression/CreateComputeExpressionViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/ComputeExpression/CreateComputeExpressionViewModel.cs
@@ -1,7 +1,33 @@
-﻿namespace Nozomi.Data.ViewModels.ComputeExpression
+﻿using System;
+using FluentValidation;
+using Nozomi.Data.Models.Web;
+using Nozomi.Data.ViewModels.ComputeValue;
+
+namespace Nozomi.Data.ViewModels.ComputeExpression
 {
     public class CreateComputeExpressionViewModel
     {
+        public ComputeExpressionType Type { get; set; }
         
+        public string Expression { get; set; }
+        
+        public string Value { get; set; }
+        
+        public Guid ComputeGuid { get; set; }
+
+        public bool IsValid()
+        {
+            var validator = new CreateComputeExpressioNViewModelValidator();
+            return validator.Validate(this).IsValid;
+        }
+
+        protected class CreateComputeExpressioNViewModelValidator : AbstractValidator<CreateComputeExpressionViewModel> {
+            public CreateComputeExpressioNViewModelValidator()
+            {
+                RuleFor(e => e.Type).NotNull();
+                RuleFor(e => e.Value).NotNull().NotEmpty();
+                RuleFor(e => e.ComputeGuid).NotNull().NotEmpty();
+            }
+        }
     }
 }
