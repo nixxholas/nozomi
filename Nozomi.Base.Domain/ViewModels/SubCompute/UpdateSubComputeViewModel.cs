3a604743eab687d7c2b06ed385a040f640e92ead
Qi Xiang <qixyqix@outlook.com>
2020-03-21 15:58:44 +0800

    Nozomi.Base.Domain: ViewModels: SubCompute: Added UpdateSubComputeViewModel implementation

---
+++ Nozomi.Base.Domain/ViewModels/SubCompute/UpdateSubComputeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+
+namespace Nozomi.Data.ViewModels.SubCompute
+{
+    public class UpdateSubComputeViewModel : SubComputeViewModel
+    {
+        public new Guid ParentComputeGuid { get; set; }
+        public new Guid? ChildComputeGuid { get; set; }
+        public new bool? IsEnabled { get; set; }
+
+        protected class UpdateSubComputeViewModelValidator : AbstractValidator<UpdateSubComputeViewModel>
+        {
+            public UpdateSubComputeViewModelValidator()
+            {
+                RuleFor(e => e.Guid).NotNull().NotEmpty();
+
+                RuleFor(e => e.ParentComputeGuid).NotNull().NotEmpty();
+            }
+        }
+    }
+}
