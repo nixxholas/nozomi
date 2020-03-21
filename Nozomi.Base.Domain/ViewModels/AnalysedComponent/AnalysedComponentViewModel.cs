5f77f675f2f608ebc98d3533c644d7197b0d4802
Nicholas Chen <nixholas@icloud.com>
2020-03-21 08:11:59 +0800

    Nozomi.Base.Domain: ViewModels: AnalysedComponent: Comments
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/AnalysedComponent/AnalysedComponentViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/AnalysedComponent/AnalysedComponentViewModel.cs
@@ -7,12 +7,24 @@ namespace Nozomi.Data.ViewModels.AnalysedComponent
 {
     public class AnalysedComponentViewModel : CreateAnalysedComponentViewModel
     {
+        /// <summary>
+        /// The unique identifier of the analysed component.
+        /// </summary>
         public Guid Guid { get; set; }
         
+        /// <summary>
+        /// The current value.
+        /// </summary>
         public string Value { get; set; }
 
+        /// <summary>
+        /// Self explanatory, if its enabled then its a true of course
+        /// </summary>
         public bool? IsEnabled { get; set; }
 
+        /// <summary>
+        /// The collection of historical values for this entity.
+        /// </summary>
         public IEnumerable<AnalysedHistoricItemViewModel> History { get; set; }
     }
 }
