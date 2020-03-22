d6a6ce2dc8e544da1e964e62bb75fb3b1cb0e9c3
nixholas <nixholas@icloud.com>
2020-03-22 20:10:30 +0800

    Nozomi.Repo.Auth: Migrations: r3 - ApiKeys CreatedAtFix migrations
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.Repo.Auth/Migrations/20200322120958_r3_ApiKeys_CreatedAtFix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nozomi.Repo.Auth.Migrations
+{
+    public partial class r3_ApiKeys_CreatedAtFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ApiKeys",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ApiKeys",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "now()");
+        }
+    }
+}
