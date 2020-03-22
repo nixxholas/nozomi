d7908530575e9c21012dd9a8cd4a3ddb03677cd7
nixholas <nixholas@icloud.com>
2020-03-22 16:54:34 +0800

    Nozomi.Repo.Auth: Data: Mappings: ApiKeyMap: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.Repo.Auth/Data/Mappings/ApiKeyMap.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nozomi.Base.Auth.Models;
+using Nozomi.Base.BCL.Helpers.Mapping;
+
+namespace Nozomi.Repo.Auth.Data.Mappings
+{
+    public class ApiKeyMap : BaseMap<ApiKey>
+    {
+        public ApiKeyMap(EntityTypeBuilder<ApiKey> entityTypeBuilder) : base(entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(e => e.Guid);
+            
+            entityTypeBuilder.HasAlternateKey(e => e.Value);
+
+            entityTypeBuilder.Property(e => e.CreatedAt).ValueGeneratedOnAdd()
+                .HasDefaultValueSql("getdate()");;
+
+            entityTypeBuilder.HasOne(e => e.User)
+                .WithMany(u => u.ApiKeys)
+                .HasForeignKey(e => e.UserId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
