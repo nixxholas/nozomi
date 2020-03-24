537ee99ffef3669b06510c303c5ed13929cf76a3
Nicholas Chen <nixholas@icloud.com>
2020-03-24 11:17:15 +0800

    Nozomi.Base.Domain: ViewModels: Request: CreateRequestViewModel: No longer require categorisation entities to be a requirement
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/Request/CreateRequestViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/Request/CreateRequestViewModel.cs
@@ -75,21 +75,21 @@ namespace Nozomi.Data.ViewModels.Request
                 RuleFor(r => r.Delay).GreaterThan(-1);
                 RuleFor(r => r.FailureDelay).GreaterThan(-1);
                 RuleFor(r => r.ParentType).IsInEnum();
-                // Safetynet for Currencies
-                RuleFor(r => r.CurrencySlug).NotNull().NotEmpty()
-                    // Ignore the check if a currency pair or currency type is selected.
-                    .Unless(r => 
-                        !string.IsNullOrEmpty(r.CurrencyPairGuid) || !string.IsNullOrEmpty(r.CurrencyTypeGuid));
-                // Safetynet for Currency Pairs
-                RuleFor(r => r.CurrencyPairGuid).NotEmpty().NotNull()
-                    // Ignore the check if a currency or currency type is selected.
-                    .Unless(r => 
-                        !string.IsNullOrEmpty(r.CurrencyTypeGuid) || !string.IsNullOrEmpty(r.CurrencySlug));
-                // Safetynet for Currency types
-                RuleFor(r => r.CurrencyTypeGuid).NotEmpty().NotNull()
-                    .Unless(r =>
-                        // Ignore the check if a currency or currency pair is selected
-                        !string.IsNullOrEmpty(r.CurrencySlug) || !string.IsNullOrEmpty(r.CurrencyPairGuid));
+                // // Safetynet for Currencies
+                // RuleFor(r => r.CurrencySlug).NotNull().NotEmpty()
+                //     // Ignore the check if a currency pair or currency type is selected.
+                //     .Unless(r => 
+                //         !string.IsNullOrEmpty(r.CurrencyPairGuid) || !string.IsNullOrEmpty(r.CurrencyTypeGuid));
+                // // Safetynet for Currency Pairs
+                // RuleFor(r => r.CurrencyPairGuid).NotEmpty().NotNull()
+                //     // Ignore the check if a currency or currency type is selected.
+                //     .Unless(r => 
+                //         !string.IsNullOrEmpty(r.CurrencyTypeGuid) || !string.IsNullOrEmpty(r.CurrencySlug));
+                // // Safetynet for Currency types
+                // RuleFor(r => r.CurrencyTypeGuid).NotEmpty().NotNull()
+                //     .Unless(r =>
+                //         // Ignore the check if a currency or currency pair is selected
+                //         !string.IsNullOrEmpty(r.CurrencySlug) || !string.IsNullOrEmpty(r.CurrencyPairGuid));
             }
         }
     }
