7693d211f43b1fd47718fc4899e586d46e14be23
Nicholas Chen <nixholas@icloud.com>
2020-03-25 19:14:09 +0800

    Nozomi.Base.Domain: ViewModels: Dispatch: DispatchInputModel: Initial implementation
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

---
+++ Nozomi.Base.Domain/ViewModels/Dispatch/DispatchInputModel.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using FluentValidation;
+using Nozomi.Data.Models.Web;
+using Nozomi.Data.ViewModels.RequestProperty;
+using Nozomi.Data.ViewModels.WebsocketCommand;
+
+namespace Nozomi.Data.ViewModels.Dispatch
+{
+    /// <summary>
+    /// Parameters required/used to obtain a payload from the target source.
+    /// </summary>
+    public class DispatchInputModel
+    {
+        /// <summary>
+        /// The protocol of the request.
+        /// </summary>
+        public RequestType Type { get; set; }
+        
+        /// <summary>
+        /// The data structure type of the request's incoming payload
+        /// </summary>
+        public ResponseType ResponseType { get; set; }
+        
+        /// <summary>
+        /// The URL of the data source. 
+        /// </summary>
+        public string Endpoint { get; set; }
+
+        /// <summary>
+        /// The properties of this request. (i.e. HTTP Headers)
+        /// </summary>
+        public ICollection<CreateRequestPropertyInputModel> Properties { get; set; }
+        
+        /// <summary>
+        /// Optional. The commands required to be invoked if this is a websocket dispatch.
+        /// </summary>
+        public ICollection<CreateWebsocketCommandInputModel> WebsocketCommands { get; set; }
+
+        public class DispatchInputValidator : AbstractValidator<DispatchInputModel>
+        {
+            public DispatchInputValidator()
+            {
+                RuleFor(e => e.Type).IsInEnum();
+                RuleFor(e => e.ResponseType).IsInEnum();
+                RuleFor(e => e.Endpoint).NotNull().NotEmpty();
+                // Must be empty unless its a websocket dispatch
+                RuleFor(e => e.WebsocketCommands).Empty()
+                    .Unless(e => !e.Type.Equals(RequestType.WebSocket));
+            }
+        }
+    }
+}
