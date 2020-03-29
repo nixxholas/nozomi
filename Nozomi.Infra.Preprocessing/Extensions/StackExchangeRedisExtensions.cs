c0f561f873545b80c2eae4a9f3bd2e89806eacc0
nixholas <nixholas@icloud.com>
2020-03-29 20:13:07 +0800

    Nozomi.Infra.Preprocessing: Extensions: StackExchangeRedisExtensions: ConfigureRedisMultiplexers: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Infra.Preprocessing/Extensions/StackExchangeRedisExtensions.cs
+++ Nozomi.Infra.Preprocessing/Extensions/StackExchangeRedisExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.DependencyInjection;
 using Nozomi.Preprocessing.Options;
+using Nozomi.Preprocessing.Singleton;
 using StackExchange.Redis;
 
 namespace Nozomi.Preprocessing.Extensions
@@ -25,6 +26,27 @@ namespace Nozomi.Preprocessing.Extensions
                 throw new RedisConnectionException(ConnectionFailureType.Loading, "Empty Redis connection " +
                                                                                   "string.");
 
+        }
+
+        public static void ConfigureRedisMultiplexers(this IServiceCollection services,
+            string apiKeyUserCacheConnectionString, string apiKeyEventCacheConnectionString)
+        {
+            if (!string.IsNullOrEmpty(apiKeyUserCacheConnectionString) 
+                && !string.IsNullOrEmpty(apiKeyEventCacheConnectionString))
+            {
+                // Sample multiplexer connection string - "redis0:6380,redis1:6380,allowAdmin=true"
+                // https://stackexchange.github.io/StackExchange.Redis/Configuration.html
+                services.Configure<NozomiRedisCacheOptions>(options =>
+                {
+                    options.ApiKeyEventConnection = apiKeyEventCacheConnectionString;
+                    options.ApiKeyUserConnection = apiKeyUserCacheConnectionString;
+                });
+
+                services.AddScoped<ConnectionMultiplexerManager>();
+            }
+            else
+                throw new RedisConnectionException(ConnectionFailureType.Loading, "Empty Redis connection " +
+                                                                                  "string.");
         }
     }
 }
