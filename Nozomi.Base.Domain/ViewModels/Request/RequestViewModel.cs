77328ffc886f75f3e2e2833fef9ff591038cca1a
nixholas <nixholas@icloud.com>
2020-04-05 22:54:42 +0800

    Nozomi.Base.Domain: ViewModels: Request: RequestViewModel: Don't inherit the Create input model
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/ViewModels/Request/RequestViewModel.cs
+++ Nozomi.Base.Domain/ViewModels/Request/RequestViewModel.cs
@@ -7,7 +7,7 @@ using Nozomi.Data.ViewModels.WebsocketCommand;
 
 namespace Nozomi.Data.ViewModels.Request
 {
-    public class RequestViewModel : CreateRequestInputModel
+    public class RequestViewModel
     {
         public RequestViewModel() {}
         
@@ -52,6 +52,52 @@ namespace Nozomi.Data.ViewModels.Request
         public Guid Guid { get; set; }
         
         /// <summary>
+        /// The protocol type of this request.
+        /// </summary>
+        public RequestType RequestType { get; set; }
+
+        /// <summary>
+        /// The response type (or the payload type) of this request.
+        /// </summary>
+        public ResponseType ResponseType { get; set; }
+
+        /// <summary>
+        /// The URL to the endpoint
+        /// </summary>
+        public string DataPath { get; set; }
+
+        /// <summary>
+        /// The delay between each request, in milliseconds.
+        /// </summary>
+        public int Delay { get; set; }
+
+        /// <summary>
+        /// The delay after a failed request attempt, in milliseconds.
+        /// </summary>
+        public long FailureDelay { get; set; }
+
+        /// <summary>
+        /// This will deduce what type of request this is for
+        /// i.e. CurrencyType, CurrencyPair or Currency.
+        /// </summary>
+        public CreateRequestInputModel.RequestParentType ParentType { get; set; }
+
+        /// <summary>
+        /// The unique slug identifier of the currency linked to this request.
+        /// </summary>
+        public string CurrencySlug { get; set; }
+
+        /// <summary>
+        /// The unique GUID identifier of the currency pair linked to this request.
+        /// </summary>
+        public string CurrencyPairGuid { get; set; }
+
+        /// <summary>
+        /// The unique GUID identifier of the Currency Type linked to this request.
+        /// </summary>
+        public string CurrencyTypeGuid { get; set; }
+        
+        /// <summary>
         /// Enabled?
         /// </summary>
         public bool IsEnabled { get; set; }
@@ -59,13 +105,13 @@ namespace Nozomi.Data.ViewModels.Request
         /// <summary>
         /// The components that are linked to this request.
         /// </summary>
-        public new ICollection<ComponentViewModel> Components { get; set; }
+        public ICollection<ComponentViewModel> Components { get; set; }
         
         /// <summary>
         /// The request properties that are linked to this request.
         /// </summary>
-        public new ICollection<RequestPropertyViewModel> Properties { get; set; }
+        public ICollection<RequestPropertyViewModel> Properties { get; set; }
         
-        public new ICollection<WebsocketCommandViewModel> WebsocketCommands { get; set; }
+        public ICollection<WebsocketCommandViewModel> WebsocketCommands { get; set; }
     }
 }
