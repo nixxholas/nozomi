d354470e9be5493de6fa9439bb0b55bd602449e6
nixholas <nixholas@icloud.com>
2020-04-09 16:24:47 +0800

    Nozomi.Repo: Data: Mappings: CategorisationModels: ItemPairMap: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Base.Domain/Models/Category/ItemPair.cs
+++ Nozomi.Base.Domain/Models/Category/ItemPair.cs
@@ -13,29 +13,19 @@ namespace Nozomi.Data.Models.Category
     {
         public ItemPair() {}
 
-        public ItemPair(string mainTicker, string counterTicker, 
-            string apiUrl, string defaultComponent, long sourceId, bool isEnabled = false)
+        public ItemPair(string mainTicker, string counterTicker, Guid sourceGuid, bool isEnabled = false)
         {
             Id = Guid.NewGuid();
             MainTicker = mainTicker;
             CounterTicker = counterTicker;
-            ApiUrl = apiUrl;
-            DefaultComponent = defaultComponent;
-            SourceId = sourceId;
+            SourceGuid = sourceGuid;
             IsEnabled = isEnabled;
         }
         
         [Key]
         public Guid Id { get; set; }
 
-        public string ApiUrl { get; set; }
-        
-        /// <summary>
-        /// Which component to rely on by default?
-        /// </summary>
-        public string DefaultComponent { get; set; }
-
-        public long SourceId { get; set; }
+        public Guid SourceGuid { get; set; }
         public Source Source { get; set; }
 
         // =========== RELATIONS ============ //
