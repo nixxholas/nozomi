0ae562b8f7f203c2088bb5e304f58451938b1a51
nixholas <nixholas@icloud.com>
2020-04-10 17:29:23 +0800

    Nozomi.Web2: ClientApp: src: services: DispatchService: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.Web2/ClientApp/src/services/DispatchService.ts
+++ Nozomi.Web2/ClientApp/src/services/DispatchService.ts
@@ -3,7 +3,7 @@ import store from "@/store";
 
 const baseUrl = '/api/Dispatch/';
 export default {
-    fetch(vm: Object) {
+    fetch(vm: any) {
         return new Promise((resolve, reject) => {
             axios.post(baseUrl  + 'Fetch', vm, {
                 headers: {
